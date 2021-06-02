9a2e4172766c66d6645bf124cec2b621206a9d24
Misko Hevery <misko@hevery.com>
2021-06-01 17:39:22 -0700

    chore: fix copyright header

--- integration/todo/ui/Footer.ts
+++ integration/todo/ui/Footer.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright BuilderIO All Rights Reserved.
+ * Copyright Builder.io, Inc. All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE

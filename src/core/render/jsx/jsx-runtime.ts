ca17b9230855181bfc3b6695351d391780420821
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-07-14 10:16:07 -0500

    refactor: use latest jsx transform, export both h() and jsx()
    - TypeScript and Babel now provides a new jsx transform, used in React 17
    - Refactor the renderer to use the `children` property on the props argument
    - Create the `@builder.io/qwik/jsx-runtime` submodule
    - The legacy jsxFactory is now just a wrapper to the latest jsx transform
    - Rename `jsxFactory()` to `h()`
    - https://www.typescriptlang.org/tsconfig#jsxImportSource
    - https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html

---
+++ src/core/render/jsx/jsx-runtime.ts
@@ -0,0 +1,56 @@
+import { EMPTY_ARRAY } from '../../util/flyweight';
+import type { ComponentChild, FunctionComponent, JSXInternal, JSXNode } from './types';
+import { qDev } from '../../util/qdev';
+
+/**
+ * @public
+ */
+export function jsx(
+  type: string | FunctionComponent,
+  props: JSXInternal.SVGAttributes &
+    JSXInternal.HTMLAttributes &
+    Record<string, any> & { children?: ComponentChild[] | ComponentChild },
+  key?: string
+) {
+  return new JSXNodeImpl(type, props, key);
+}
+
+export class JSXNodeImpl<T> implements JSXNode<T> {
+  children: any;
+
+  constructor(public type: T, public props: any, public key: any) {
+    if (props && props.children) {
+      if (Array.isArray(props.children)) {
+        this.children = props.children;
+      } else {
+        this.children = [props.children];
+      }
+    } else {
+      this.children = EMPTY_ARRAY;
+    }
+  }
+}
+
+export const isJSXNode = (n: any): n is JSXNode<unknown> => {
+  if (qDev) {
+    if (n instanceof JSXNodeImpl) {
+      return true;
+    }
+    if (n && typeof n === 'object' && n.constructor.name === JSXNodeImpl.name) {
+      throw new Error(`Duplicate implementations of "JSXNodeImpl" found`);
+    }
+    return false;
+  } else {
+    return n instanceof JSXNodeImpl;
+  }
+};
+
+/**
+ * @public
+ */
+export const Fragment = {} as any;
+
+export { JSXInternal as JSX };
+export { JSXInternal };
+
+export { jsx as jsxs, jsx as jsxDEV };

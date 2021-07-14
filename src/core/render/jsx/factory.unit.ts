da2a006314841bd6eb41baaf958553927cd45b3d
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-07-14 10:16:20 -0500

    test: add jsx-runtime and factory tests

---
+++ src/core/render/jsx/factory.unit.ts
@@ -0,0 +1,121 @@
+import { Host } from './host';
+import { h } from './factory';
+import { isJSXNode, Fragment } from './jsx-runtime';
+import type { FunctionComponent } from './types';
+
+describe('classic jsx factory h()', () => {
+  describe('children', () => {
+    it('map multiple nodes, flatten', () => {
+      // <parent>
+      //   a
+      //   {[1, 2].map((n) => (
+      //     <child>{n}</child>
+      //   ))}
+      //   b
+      // </parent>;
+      const v = h(
+        'parent',
+        null,
+        'a',
+        [1, 2].map((n) => h('child', null, n)),
+        'b'
+      );
+      expect(v.children).toHaveLength(4);
+      expect(v.children[0]).toEqual('a');
+      expect(v.children[1].type).toEqual('child');
+      expect(v.children[1].children).toHaveLength(1);
+      expect(v.children[1].children[0]).toEqual(1);
+      expect(v.children[2].type).toEqual('child');
+      expect(v.children[2].children).toHaveLength(1);
+      expect(v.children[2].children[0]).toEqual(2);
+      expect(v.children[3]).toEqual('b');
+    });
+
+    it('one child node', () => {
+      // <parent><child></child></parent>
+      const v = h('parent', null, h('child', null));
+      expect(v.children).toHaveLength(1);
+      expect(v.children[0].type).toEqual('child');
+      expect(v.children[0].props).toEqual({ children: [] });
+      expect(v.children[0].children).toEqual([]);
+    });
+
+    it('text w/ expression', () => {
+      // <div>1 {2} 3</div>
+      const v = h('div', null, '1 ', 2, ' 3');
+      expect(v.children).toEqual(['1 ', 2, ' 3']);
+    });
+
+    it('text child', () => {
+      // <div>text</div>
+      const v = h('div', null, 'text');
+      expect(v.children).toEqual(['text']);
+    });
+
+    it('no children', () => {
+      // <div/>
+      const v = h('div', null);
+      expect(v.children).toEqual([]);
+    });
+  });
+
+  describe('props', () => {
+    it('key', () => {
+      // <div key="val"/>
+      const v = h('div', { key: 'val' });
+      expect(v.props).toEqual({ children: [] });
+      expect(v.key).toEqual('val');
+    });
+
+    it('name/value props', () => {
+      // <div id="val"/>
+      const v = h('div', { id: 'val' });
+      expect(v.props).toEqual({ id: 'val', children: [] });
+    });
+
+    it('boolean props', () => {
+      // <input checked/>
+      const v = h('input', { checked: true });
+      expect(v.props).toEqual({ checked: true, children: [] });
+    });
+
+    it('no props', () => {
+      // <div/>
+      const v = h('div', null);
+      expect(v.props).toEqual({ children: [] });
+      expect(v.key).toBeUndefined();
+    });
+  });
+
+  describe('type', () => {
+    it('tag', () => {
+      const v = h('div', null);
+      expect(v.type).toBe('div');
+    });
+
+    it('Function Component', () => {
+      const Cmp: FunctionComponent<any> = () => h('fn-cmp', null);
+      const v = h(Cmp, {});
+      expect(v.type).toBe(Cmp);
+    });
+
+    it('Host', () => {
+      const v = h(Host, null);
+      expect(v.type).toBe(Host);
+    });
+
+    it('Fragment', () => {
+      // <><div/></>
+      const v = h(Fragment, null, h('div', null));
+      expect(v.type).toBe(Fragment);
+    });
+  });
+
+  describe('isJSXNode', () => {
+    it('valid JSXNode', () => {
+      // <div/>
+      const v = h('div', null);
+      expect(isJSXNode(v)).toBe(true);
+    });
+  });
+});
---
+++ src/core/render/jsx/jsx-runtime.unit.ts
@@ -0,0 +1,118 @@
+import { Host } from './host';
+import { jsx, isJSXNode, Fragment } from './jsx-runtime';
+import type { FunctionComponent } from './types';
+
+describe('jsx-runtime', () => {
+  describe('children', () => {
+    it('map multiple nodes', () => {
+      // <parent>
+      //   {[1,2].map(n => (<child>{n}</child>))}
+      // </parent>
+      const v = jsx('parent', {
+        children: [1, 2].map((n) => jsx('child', { children: n })),
+      });
+      expect(v.children).toHaveLength(2);
+      expect(v.children[0].type).toEqual('child');
+      expect(v.children[0].children).toEqual([1]);
+      expect(v.children[1].children).toEqual([2]);
+    });
+
+    it('one child node', () => {
+      // <parent><child></child></parent>
+      const v = jsx('parent', { children: jsx('child', {}) });
+      expect(v.children).toHaveLength(1);
+      expect(v.children[0].type).toEqual('child');
+      expect(v.children[0].props).toEqual({});
+      expect(v.children[0].children).toEqual([]);
+    });
+
+    it('text w/ expression', () => {
+      // <div>1 {2} 3</div>
+      const v = jsx('div', { children: ['1 ', 2, ' 3'] });
+      expect(v.children).toEqual(['1 ', 2, ' 3']);
+    });
+
+    it('text child', () => {
+      // <div>text</div>
+      const v = jsx('div', { children: 'text' });
+      expect(v.children).toEqual(['text']);
+    });
+
+    it('no children', () => {
+      // <div/>
+      const v = jsx('div', {});
+      expect(v.children).toEqual([]);
+    });
+  });
+
+  describe('props', () => {
+    it('key', () => {
+      // <div key="val"/>
+      const v = jsx('div', {}, 'val');
+      expect(v.props).toEqual({});
+      expect(v.key).toEqual('val');
+    });
+
+    it('name/value props', () => {
+      // <div id="val"/>
+      const v = jsx('div', { id: 'val' });
+      expect(v.props).toEqual({ id: 'val' });
+    });
+
+    it('boolean props', () => {
+      // <input checked/>
+      const v = jsx('input', { checked: true });
+      expect(v.props).toEqual({ checked: true });
+    });
+
+    it('no props', () => {
+      // <div/>
+      const v = jsx('div', {});
+      expect(v.props).toEqual({});
+      expect(v.key).toBeUndefined();
+    });
+  });
+
+  describe('type', () => {
+    it('tag', () => {
+      const v = jsx('div', {});
+      expect(v.type).toBe('div');
+    });
+
+    it('Function Component', () => {
+      const Cmp: FunctionComponent<any> = () => jsx('fn-cmp', {});
+      const v = jsx(Cmp, {});
+      expect(v.type).toBe(Cmp);
+    });
+
+    it('Host', () => {
+      const v = jsx(Host, {});
+      expect(v.type).toBe(Host);
+    });
+
+    it('Fragment', () => {
+      const v = jsx(Fragment, {});
+      expect(v.type).toBe(Fragment);
+    });
+  });
+
+  describe('isJSXNode', () => {
+    it('valid JSXNode', () => {
+      const v = jsx('div', {});
+      expect(isJSXNode(v)).toBe(true);
+    });
+    it('invalid string', () => {
+      expect(isJSXNode('text')).toBe(false);
+    });
+    it('invalid class', () => {
+      expect(isJSXNode(class {})).toBe(false);
+    });
+    it('invalid array', () => {
+      expect(isJSXNode([])).toBe(false);
+    });
+    it('invalid null/undefined', () => {
+      expect(isJSXNode(null)).toBe(false);
+      expect(isJSXNode(undefined)).toBe(false);
+    });
+  });
+});

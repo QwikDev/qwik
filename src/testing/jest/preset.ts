428db48e4ea22515e57cf73a0e4b9530777964b0
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-07-17 14:29:15 -0500

    chore: update jest config

--- src/testing/jest/preset.ts
+++ src/testing/jest/preset.ts
@@ -3,24 +3,26 @@ import { join } from 'path';
 const testingDir = __dirname;
 const packageRootDir = join(testingDir, '..');
 
+const ext = '.' + (globalThis as any)._MODULE_EXT_;
+
 const moduleExtensions = ['ts', 'tsx', 'js', 'mjs', 'jsx'];
 const moduleExtensionRegexp = '(' + moduleExtensions.join('|') + ')';
 
 const jestPreset = {
   moduleFileExtensions: [...moduleExtensions, 'json', 'd.ts'],
   moduleNameMapper: {
-    '^@builder.io/qwik/jsx-runtime$': join(packageRootDir, 'jsx-runtime._MODULE_EXT_'),
-    '^@builder.io/qwik/optimizer$': join(packageRootDir, 'optimizer._MODULE_EXT_'),
-    '^@builder.io/qwik/server$': join(testingDir, 'index._MODULE_EXT_'),
-    '^@builder.io/qwik/testing$': join(testingDir, 'index._MODULE_EXT_'),
-    '^@builder.io/qwik$': join(packageRootDir, 'core._MODULE_EXT_'),
+    '^@builder.io/qwik/jsx-runtime$': join(packageRootDir, 'jsx-runtime' + ext),
+    '^@builder.io/qwik/optimizer$': join(packageRootDir, 'optimizer' + ext),
+    '^@builder.io/qwik/server$': join(packageRootDir, 'server', 'index' + ext),
+    '^@builder.io/qwik/testing$': join(testingDir, 'index' + ext),
+    '^@builder.io/qwik$': join(packageRootDir, 'core' + ext),
   },
   modulePathIgnorePatterns: ['<rootDir>/build', '<rootDir>/dist'],
-  setupFilesAfterEnv: [join(testingDir, 'jest-setuptestframework._MODULE_EXT_')],
+  setupFilesAfterEnv: [join(testingDir, 'jest-setuptestframework' + ext)],
   testPathIgnorePatterns: ['/.cache', '/.github', '/.vscode', '/build', '/dist', '/node_modules'],
   testRegex: '(/__tests__/.*|\\.?(test|spec|unit))\\.' + moduleExtensionRegexp + '$',
   transform: {
-    '^.+\\.(ts|tsx|jsx)$': join(__dirname, 'jest-preprocessor._MODULE_EXT_'),
+    '^.+\\.(ts|tsx|jsx)$': join(__dirname, 'jest-preprocessor' + ext),
   },
   watchPathIgnorePatterns: ['^.+\\.d\\.ts$'],
 };

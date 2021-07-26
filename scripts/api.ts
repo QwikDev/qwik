1b131a398c877057e278e8d4d968858ff4e4a59f
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-07-25 21:00:55 -0500

    chore: bazel build updates

--- scripts/api.ts
+++ scripts/api.ts
@@ -1,57 +1,63 @@
+import type { BuildConfig } from './util';
 import { Extractor, ExtractorConfig } from '@microsoft/api-extractor';
-import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
-import { loadConfig } from './util';
+import { readFileSync, writeFileSync } from 'fs';
 
 /**
  * Create each submodule's bundled dts file, and ensure
  * the public API has not changed for a production build.
  */
-function apiExtractor() {
-  const config = loadConfig(process.argv.slice(2));
-
-  function createTypesApi(submodule: string, corePath?: string) {
-    const extractorConfig = ExtractorConfig.loadFileAndPrepare(
-      join(config.srcDir, submodule, 'api-extractor.json')
-    );
-    const result = Extractor.invoke(extractorConfig, {
-      localBuild: !!config.dev,
-      showVerboseMessages: true,
-    });
-    if (!result.succeeded) {
-      process.exitCode = 1;
-    }
-    const dtsPath = result.extractorConfig.untrimmedFilePath;
-    fixDtsContent(dtsPath, dtsPath, corePath);
-  }
-
+export function apiExtractor(config: BuildConfig) {
   // Run the api extractor for each of the submodules
-  createTypesApi('core');
-  createTypesApi('optimizer');
-  createTypesApi('server', '../core');
-  createTypesApi('testing', '../core');
+  createTypesApi(config, 'core', 'core.d.ts', './core');
+  createTypesApi(config, 'optimizer', 'optimizer.d.ts', './core');
+  createTypesApi(config, 'server', 'server/index.d.ts', '../core');
+  createTypesApi(config, 'testing', 'testing/index.d.ts', '../core');
 
   // the jsx-runtime.d.ts file was already generated with tsc, use this one
   const jsxRuntimeSrcPath = join(config.tscDir, 'src', 'jsx_runtime.d.ts');
   const jsxRuntimeDestPath = join(config.pkgDir, 'jsx-runtime.d.ts');
   fixDtsContent(jsxRuntimeSrcPath, jsxRuntimeDestPath, './core');
 
-  console.log('🦖', 'submodule APIs generated');
+  console.log('🥶', 'submodule APIs generated');
+}
+
+function createTypesApi(
+  config: BuildConfig,
+  submodule: string,
+  outFileName: string,
+  corePath: string
+) {
+  const extractorConfig = ExtractorConfig.loadFileAndPrepare(
+    join(config.srcDir, submodule, 'api-extractor.json')
+  );
+  const result = Extractor.invoke(extractorConfig, {
+    localBuild: !!config.dev,
+    showVerboseMessages: true,
+  });
+  if (!result.succeeded) {
+    console.error(
+      `\n❌ Use "npm run api.update" to automatically update the .md files if the api changes were expected\n`
+    );
+    process.exit(1);
+  }
+  const srcPath = result.extractorConfig.untrimmedFilePath;
+  const destPath = join(config.pkgDir, outFileName);
+  fixDtsContent(srcPath, destPath, corePath);
 }
 
 /**
  * Fix up the generated dts content, and ensure it's using a relative
  * path to find the core.d.ts file, rather than node resolving it.
  */
-function fixDtsContent(srcPath: string, destPath: string, corePath?: string) {
+function fixDtsContent(srcPath: string, destPath: string, corePath: string) {
   let dts = readFileSync(srcPath, 'utf-8');
-  if (corePath) {
-    dts = dts.replace(/@builder\.io\/qwik/g, corePath);
-  }
+
+  // ensure we're just using a relative path
+  dts = dts.replace(/@builder\.io\/qwik/g, corePath);
+
   // for some reason api-extractor is adding this in  ¯\_(ツ)_/¯
   dts = dts.replace('{};', '');
 
   writeFileSync(destPath, dts);
 }
-
-apiExtractor();

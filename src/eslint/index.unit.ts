329cd0b6b76ad6f1998adb5f7a2acb9432f712a8
Oto Dočkal <oto.dockal@b2a.cz>
2021-10-17 12:26:00 +0200

    refactor(eslint): use real qComponent and qHook marker functions in tests

--- src/eslint/index.unit.ts
+++ src/eslint/index.unit.ts
@@ -9,10 +9,15 @@
 /* eslint-disable */
 import { RuleTester } from 'eslint';
 import { rules } from './index';
+import { qComponent } from '../core/component/q-component.public';
+import { qHook } from '../core/component/qrl-hook.public';
 
 const ruleTester = new RuleTester({
-  parserOptions: { ecmaVersion: 2021 },
   parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
 });
 
 /**
@@ -32,16 +37,16 @@ const ruleTester = new RuleTester({
  * VALID qComponent - arrow function
  */
 function validArrowFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(({ item, canEdit }, state, args) => {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook((props) => ({ count: props.value || 0 })),
+    onRender: qHook(({ value, step }, state, args) => {
       const innerConst = 123;
-      qHook(({ item, canEdit }, state, args) => {
+      qHook<typeof Counter>(({ value, step }, state, args) => {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -56,28 +61,28 @@ function validArrowFunctionExpression() {
  * VALID qComponent - arrow function - duplicated declaration
  */
 function validArrowFunctionExpressionDuplicated() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(({ item, canEdit }, state, args) => {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook((props) => ({ count: props.value || 0 })),
+    onRender: qHook(({ value, step }, state, args) => {
       const innerConst = 123;
-      qHook(({ item, canEdit }, state, args) => {
+      qHook<typeof Counter>(({ value, step }, state, args) => {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
           local; // closed over qHook inner function
         };
       });
-      qHook(({ item, canEdit }, state, args) => {
+      qHook<typeof Counter>(({ value, step }, state, args) => {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -92,16 +97,16 @@ function validArrowFunctionExpressionDuplicated() {
  * VALID qComponent - async arrow function
  */
 function validAsyncArrowFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(async ({ item, canEdit }, state, args) => {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(async (props) => ({ count: props.value || 0 })),
+    onRender: qHook(async ({ value, step }, state, args) => {
       const innerConst = 123;
-      qHook(async ({ item, canEdit }, state, args) => {
+      qHook<typeof Counter>(async ({ value, step }, state, args) => {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -116,16 +121,18 @@ function validAsyncArrowFunctionExpression() {
  * VALID qComponent - function
  */
 function validFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(function ({ item, canEdit }, state, args) {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(function ({ value, step }, state, args) {
       const innerConst = 123;
-      qHook(function ({ item, canEdit }, state, args) {
+      qHook<typeof Counter>(function ({ value, step }, state, args) {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -140,16 +147,18 @@ function validFunctionExpression() {
  * VALID qComponent - async function
  */
 function validAsyncFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(async function ({ item, canEdit }, state, args) {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(async function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(async function ({ value, step }, state, args) {
       const innerConst = 123;
-      qHook(async function ({ item, canEdit }, state, args) {
+      qHook<typeof Counter>(async function ({ value, step }, state, args) {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -164,16 +173,18 @@ function validAsyncFunctionExpression() {
  * VALID qComponent - arrow function x function
  */
 function validArrowFunctionFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(({ item, canEdit }, state, args) => {
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(({ value, step }, state, args) => {
       const innerConst = 123;
-      qHook(function ({ item, canEdit }, state, args) {
+      qHook<typeof Counter>(function ({ value, step }, state, args) {
         const local = 123;
         local; // local var
         rules; // import
         ruleTester; // top level var
-        item; // qHook param
+        value; // qHook param
         state; // qHook state
         args; // qHook args
         return () => {
@@ -185,23 +196,40 @@ function validArrowFunctionFunctionExpression() {
 }
 
 /**
+ * VALID qHook - qHook defined outside qComponent should not report an error
+ */
+function validStandaloneQHook() {
+  const Counter_click = qHook((props, state) => {
+    state;
+    const local = 123;
+    local; // OK: Reference to local vars is OK.
+    rules; // OK: Reference to import is OK.
+    ruleTester; // OK: Reference to top level is OK.
+  });
+}
+
+/**
  * INVALID qComponent - arrow function
  */
 function invalidArrowFunctionExpression() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(({ item, canEdit }, state, args) => {
+  const Counter_click = qHook((props, state) => {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook((props) => ({ count: props.value || 0 })),
+    onRender: qHook(({ value, step }, state, args) => {
       const innerConst = 123;
       function innerFn() {}
-      const { innerSpread } = args;
-      qHook(() => {
+      const { innerSpread } = args as any;
+      qHook<typeof Counter>(() => {
         const local = 123;
         local; // OK: Reference to local vars is OK.
         rules; // OK: Reference to import is OK.
         ruleTester; // OK: Reference to top level is OK.
 
-        item; // ERROR: can't access parent scope
-        canEdit; // ERROR: can't access parent scope
+        value; // ERROR: can't access parent scope
+        step; // ERROR: can't access parent scope
         state; // ERROR: can't access parent scope
         args; // ERROR: can't access parent scope
         innerConst; // ERROR: reference to `inner` is not allowed as it closes over non-top-level variable
@@ -220,20 +248,24 @@ function invalidArrowFunctionExpression() {
  * INVALID qComponent - arrow function - duplicated declaration
  */
 function invalidArrowFunctionExpressionDuplicated() {
-  qComponent({
-    onMount: qHook(() => ({ text: '' })),
-    onRender: qHook(({ item, canEdit }, state, args) => {
+  const Counter_click = qHook((props, state) => {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook((props) => ({ count: props.value || 0 })),
+    onRender: qHook(({ value, step }, state, args) => {
       const innerConst = 123;
       function innerFn() {}
-      const { innerSpread } = args;
-      qHook(() => {
+      const { innerSpread } = args as any;
+      qHook<typeof Counter>(() => {
         const local = 123;
         local; // OK: Reference to local vars is OK.
         rules; // OK: Reference to import is OK.
         ruleTester; // OK: Reference to top level is OK.
 
-        item; // ERROR: can't access parent scope
-        canEdit; // ERROR: can't access parent scope
+        value; // ERROR: can't access parent scope
+        step; // ERROR: can't access parent scope
         state; // ERROR: can't access parent scope
         args; // ERROR: can't access parent scope
         innerConst; // ERROR: reference to `inner` is not allowed as it closes over non-top-level variable
@@ -244,14 +276,14 @@ function invalidArrowFunctionExpressionDuplicated() {
           innerConst; // ERROR: reference to `inner` is not allowed as it closes over non-top-level variable
         };
       });
-      qHook(() => {
+      qHook<typeof Counter>(() => {
         const local = 123;
         local; // OK: Reference to local vars is OK.
         rules; // OK: Reference to import is OK.
         ruleTester; // OK: Reference to top level is OK.
 
-        item; // ERROR: can't access parent scope
-        canEdit; // ERROR: can't access parent scope
+        value; // ERROR: can't access parent scope
+        step; // ERROR: can't access parent scope
         state; // ERROR: can't access parent scope
         args; // ERROR: can't access parent scope
         innerConst; // ERROR: reference to `inner` is not allowed as it closes over non-top-level variable
@@ -261,6 +293,85 @@ function invalidArrowFunctionExpressionDuplicated() {
           local; // Closing over `qHook` inner functions is OK inside nested closures.
           innerConst; // ERROR: reference to `inner` is not allowed as it closes over non-top-level variable
         };
+      });
+    }),
+  });
+}
+
+/**
+ * INVALID qComponent - async arrow function
+ */
+function invalidAsyncArrowFunctionExpression() {
+  const Counter_click = qHook(async (props, state) => {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(async (props) => ({ count: props.value || 0 })),
+    onRender: qHook(async ({ value, step }, state, args) => {
+      qHook<typeof Counter>(async ({ value, step }) => {
+        state;
+      });
+    }),
+  });
+}
+
+/**
+ * INVALID qComponent - function
+ */
+function invalidFunctionExpression() {
+  const Counter_click = qHook(function (props, state) {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(function ({ value, step }, state, args) {
+      qHook<typeof Counter>(function ({ value, step }) {
+        state;
+      });
+    }),
+  });
+}
+
+/**
+ * INVALID qComponent - async function
+ */
+function invalidAsyncFunctionExpression() {
+  const Counter_click = qHook(async function (props, state) {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(async function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(async function ({ value, step }, state, args) {
+      qHook<typeof Counter>(async function ({ value, step }) {
+        state;
+      });
+    }),
+  });
+}
+
+/**
+ * INVALID qComponent - arrow function x function
+ */
+function invalidArrowFunctionFunctionExpression() {
+  const Counter_click = qHook((props, state) => {
+    state;
+  });
+
+  const Counter = qComponent<{ value?: number; step?: number }, { count: number }>({
+    onMount: qHook(function (props) {
+      return { count: props.value || 0 };
+    }),
+    onRender: qHook(({ value, step }, state, args) => {
+      const innerConst = 123;
+      qHook<typeof Counter>(function ({ value, step }) {
+        state;
       });
     }),
   });
@@ -289,49 +400,52 @@ ruleTester.run('no-closed-over-variables', rules['no-closed-over-variables'], {
     {
       code: validArrowFunctionFunctionExpression.toString(),
     },
+    {
+      code: validStandaloneQHook.toString(),
+    },
   ],
   invalid: [
     {
       code: invalidArrowFunctionExpression.toString(),
       errors: [
         {
-          message: 'item is closed over.',
-          line: 13,
-          column: 17,
-        },
-        {
-          message: 'canEdit is closed over.',
-          line: 14,
-          column: 17,
-        },
-        {
-          message: 'state is closed over.',
-          line: 15,
-          column: 17,
-        },
-        {
-          message: 'args is closed over.',
+          message: 'value is closed over.',
           line: 16,
           column: 17,
         },
         {
-          message: 'innerConst is closed over.',
+          message: 'step is closed over.',
           line: 17,
           column: 17,
         },
         {
-          message: 'innerFn is closed over.',
+          message: 'state is closed over.',
           line: 18,
           column: 17,
         },
         {
-          message: 'innerSpread is closed over.',
+          message: 'args is closed over.',
           line: 19,
           column: 17,
         },
         {
           message: 'innerConst is closed over.',
+          line: 20,
+          column: 17,
+        },
+        {
+          message: 'innerFn is closed over.',
+          line: 21,
+          column: 17,
+        },
+        {
+          message: 'innerSpread is closed over.',
           line: 22,
+          column: 17,
+        },
+        {
+          message: 'innerConst is closed over.',
+          line: 25,
           column: 21,
         },
       ],
@@ -340,97 +454,127 @@ ruleTester.run('no-closed-over-variables', rules['no-closed-over-variables'], {
       code: invalidArrowFunctionExpressionDuplicated.toString(),
       errors: [
         {
-          message: 'item is closed over.',
-          line: 13,
-          column: 17,
-        },
-        {
-          message: 'canEdit is closed over.',
-          line: 14,
-          column: 17,
-        },
-        {
-          message: 'state is closed over.',
-          line: 15,
-          column: 17,
-        },
-        {
-          message: 'args is closed over.',
+          message: 'value is closed over.',
           line: 16,
           column: 17,
         },
         {
-          message: 'innerConst is closed over.',
+          message: 'step is closed over.',
           line: 17,
           column: 17,
         },
         {
-          message: 'innerFn is closed over.',
+          message: 'state is closed over.',
           line: 18,
           column: 17,
         },
         {
-          message: 'innerSpread is closed over.',
+          message: 'args is closed over.',
           line: 19,
           column: 17,
         },
         {
           message: 'innerConst is closed over.',
-          line: 22,
-          column: 21,
-        },
-        // dups
-        {
-          message: 'item is closed over.',
-          line: 30,
-          column: 17,
-        },
-        {
-          message: 'canEdit is closed over.',
-          line: 31,
-          column: 17,
-        },
-        {
-          message: 'state is closed over.',
-          line: 32,
-          column: 17,
-        },
-        {
-          message: 'args is closed over.',
-          line: 33,
-          column: 17,
-        },
-        {
-          message: 'innerConst is closed over.',
-          line: 34,
+          line: 20,
           column: 17,
         },
         {
           message: 'innerFn is closed over.',
-          line: 35,
+          line: 21,
           column: 17,
         },
         {
           message: 'innerSpread is closed over.',
+          line: 22,
+          column: 17,
+        },
+        {
+          message: 'innerConst is closed over.',
+          line: 25,
+          column: 21,
+        },
+        // dups
+        {
+          message: 'value is closed over.',
+          line: 33,
+          column: 17,
+        },
+        {
+          message: 'step is closed over.',
+          line: 34,
+          column: 17,
+        },
+        {
+          message: 'state is closed over.',
+          line: 35,
+          column: 17,
+        },
+        {
+          message: 'args is closed over.',
           line: 36,
           column: 17,
         },
         {
           message: 'innerConst is closed over.',
+          line: 37,
+          column: 17,
+        },
+        {
+          message: 'innerFn is closed over.',
+          line: 38,
+          column: 17,
+        },
+        {
+          message: 'innerSpread is closed over.',
           line: 39,
+          column: 17,
+        },
+        {
+          message: 'innerConst is closed over.',
+          line: 42,
           column: 21,
+        },
+      ],
+    },
+    {
+      code: invalidAsyncArrowFunctionExpression.toString(),
+      errors: [
+        {
+          message: 'state is closed over.',
+          line: 9,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: invalidFunctionExpression.toString(),
+      errors: [
+        {
+          message: 'state is closed over.',
+          line: 11,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: invalidAsyncFunctionExpression.toString(),
+      errors: [
+        {
+          message: 'state is closed over.',
+          line: 11,
+          column: 17,
+        },
+      ],
+    },
+    {
+      code: invalidArrowFunctionFunctionExpression.toString(),
+      errors: [
+        {
+          message: 'state is closed over.',
+          line: 12,
+          column: 17,
         },
       ],
     },
   ],
 });
-
-/**
- * qHook, qComponent marker functions
- *
- * - TODO: once PR#50 https://github.com/BuilderIO/qwik/pull/50 is merged, use real import
- */
-function qHook(cb: (props?: any, state?: any, args?: any) => any) {
-  cb();
-}
-function qComponent(obj: any) {}

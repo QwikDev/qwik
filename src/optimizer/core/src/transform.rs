58070a0c831a467643e6e0197aac616c359bb3b2
Manu MA <manu.mtza@gmail.com>
2022-02-10 12:43:54 +0000

    fix: platform specific paths (#186)
    fixes #188

--- src/optimizer/core/src/transform.rs
+++ src/optimizer/core/src/transform.rs
@@ -7,6 +7,7 @@ use crate::collector::{
 use crate::entry_strategy::EntryPolicy;
 use crate::parse::PathData;
 use crate::words::*;
+use path_slash::PathExt;
 
 use anyhow::{bail, Error};
 use lazy_static::lazy_static;
@@ -310,11 +311,9 @@ impl<'a> QwikTransform<'a> {
                 filename.push_str(&self.options.extension);
             }
             let import_path = if self.hook_depth > 0 {
-                JsWord::from(filename.to_string())
+                fix_path("a", "a", &filename).unwrap()
             } else {
-                fix_path("a", &self.options.path_data.path, &filename)
-                    // TODO: check with manu
-                    .unwrap()
+                fix_path("a", &self.options.path_data.path, &filename).unwrap()
             };
 
             for id in &local_idents {
@@ -364,7 +363,7 @@ impl<'a> QwikTransform<'a> {
                 expr: Box::new(folded),
                 local_idents,
                 scoped_idents,
-                origin: self.options.path_data.path.to_string_lossy().into(),
+                origin: self.options.path_data.path.to_slash_lossy().into(),
             });
             o
         } else {

bc0e55f142ef9817f166bfa1bf7578c31cac459d
Manu MA <manu.mtza@gmail.com>
2022-02-12 10:46:51 +0000

    fix: rollup plugin

--- starters/apps/base/src/typings.d.ts
+++ starters/apps/base/src/typings.d.ts
@@ -1,29 +1,8 @@
-/* Allows Typescript to import .css files */
-declare module '*.css' {
-  const content: string;
-  export default content;
-}
+/**
+ * Add custom global types in this file
+ */
 
-/* Allows Typescript to import .gif files */
-declare module '*.gif' {
-  const content: string;
-  export default content;
-}
-
-/* Allows Typescript to import .jpg files */
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-/* Allows Typescript to import .png files */
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
-
-/* Allows Typescript to import .svg files */
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
+// declare module '*.css' {
+//   const content: string;
+//   export default content;
+// }

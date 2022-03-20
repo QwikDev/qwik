26a0831a2b7b77ba90006642f63b652e68858c89
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-20 08:25:19 -0500

    docs: mobile menu

--- docs/src/components/head/head.tsx
+++ docs/src/components/head/head.tsx
@@ -5,6 +5,7 @@ export const Head = () => (
     <meta charSet="utf-8" />
 
     <title>Qwik</title>
+    <meta name="viewport" content="width=device-width" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />

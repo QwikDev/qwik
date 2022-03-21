4a0d5e88d3524e5ef88e66b252a99e9252f13f71
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-21 18:49:57 -0500

    docs: fix github edit link (#312)

--- docs/src/components/on-this-page/on-this-page.tsx
+++ docs/src/components/on-this-page/on-this-page.tsx
@@ -14,7 +14,7 @@ export const OnThisPage = component$(
 
       const editUrl = new URL(
         page.source.path,
-        'https://github.com/BuilderIO/qwik/blob/main/docs/pages/'
+        'https://github.com/BuilderIO/qwik/edit/main/docs/pages/'
       );
 
       const onScroll = $(() => {

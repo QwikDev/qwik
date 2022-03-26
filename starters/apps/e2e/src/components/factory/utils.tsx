6a3fce41c6ae9ce11194e42c97db3265c3c9de5e
Manu MA <manu.mtza@gmail.com>
2022-03-26 20:58:06 +0000

    fix: high order components

---
+++ starters/apps/e2e/src/components/factory/utils.tsx
@@ -0,0 +1,16 @@
+import { $, component$, implicit$FirstArg, QRL, useHostElement } from '@builder.io/qwik';
+
+export function factoryQrl<P>(componentQRL: QRL<(props: P) => any>) {
+  return component$((props: P) => {
+    return $(async () => {
+      const hostElement = useHostElement();
+      const Component = await componentQRL.resolve(hostElement);
+      return (
+        <div>
+          <Component {...props} />
+        </div>
+      );
+    });
+  });
+}
+export const factory$ = implicit$FirstArg(factoryQrl);

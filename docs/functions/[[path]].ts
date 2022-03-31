81f4081a881a1cc0b9b32d6ba52adb0244823cad
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-31 15:07:20 -0500

    docs: update headers

--- docs/functions/[[path]].ts
+++ docs/functions/[[path]].ts
@@ -7,26 +7,33 @@ const CACHE_CONTROL = 60;
 
 export const onRequestGet: PagesFunction = async (req) => {
   // Handle static assets
-  if (/\.\w+$/.test(req.request.url)) {
-    return req.next(req.request);
-  }
-
-  // Early return from cache
-  const cache = await caches.open('custom:qwik');
-  if (CACHE_CONTROL > 0) {
-    const cachedResponse = await cache.match(req.request);
-    if (cachedResponse) {
-      return cachedResponse;
+  try {
+    if (/\.\w+$/.test(req.request.url)) {
+      return req.next(req.request);
     }
-  }
 
-  // Generate SSR response
-  const response = (await qwikSSR(req)) as Response;
+    // Early return from cache
+    const cache = await caches.open('custom:qwik');
+    if (CACHE_CONTROL > 0) {
+      const cachedResponse = await cache.match(req.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+    }
 
-  // Cache results
-  if (CACHE_CONTROL > 0) {
-    response.headers.set('Cache-Control', `max-age=${CACHE_CONTROL}`);
-    req.waitUntil(cache.put(req.request, response.clone()));
+    // Generate SSR response
+    const res = (await qwikSSR(req)) as Response;
+
+    // Cache results
+    res.headers.set(
+      'Cache-Control',
+      `max-age=${CACHE_CONTROL}, s-maxage=10, stale-while-revalidate=604800, stale-if-error=604800`
+    );
+    req.waitUntil(cache.put(req.request, res.clone()));
+
+    // Return SSR'd response
+    return res;
+  } catch (e) {
+    return new Response(String(e), { status: 5000 });
   }
-  return response;
 };
--- docs/src/entry.cloudflare.tsx
+++ docs/src/entry.cloudflare.tsx
@@ -5,17 +5,15 @@ import { render } from './entry.server';
 
 import symbols from '../server/q-symbols.json';
 
-export const qwikSSR: PagesFunction = async (req) => {
+export const qwikSSR: PagesFunction = async (ev) => {
   const ssrResult = await render({
-    url: new URL(req.request.url),
+    url: new URL(ev.request.url),
     symbols,
   });
 
   const response = new Response(ssrResult.html, {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
-      'Cache-Control':
-        'max-age=5, s-maxage=10, stale-while-revalidate=604800, stale-if-error=604800',
     },
   });
   return response;

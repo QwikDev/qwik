907d20fbe2e6b0dd53be71dcf9f21ff6020cb224
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-31 15:18:53 -0500

    docs: update headers (#341)

--- docs/functions/[[path]].ts
+++ docs/functions/[[path]].ts
@@ -7,26 +7,32 @@ const CACHE_CONTROL = 60;
 
 export const onRequestGet: PagesFunction = async (req) => {
   // Handle static assets
-  if (/\.\w+$/.test(req.request.url)) {
-    return req.next(req.request);
-  }
+  try {
+    if (/\.\w+$/.test(req.request.url)) {
+      return req.next(req.request);
+    }
 
-  // Early return from cache
-  const cache = await caches.open('custom:qwik');
-  if (CACHE_CONTROL > 0) {
+    // Early return from cache
+    const cache = await caches.open('custom:qwik');
     const cachedResponse = await cache.match(req.request);
     if (cachedResponse) {
       return cachedResponse;
     }
-  }
 
-  // Generate SSR response
-  const response = (await qwikSSR(req)) as Response;
+    // Generate Qwik SSR response
+    const res = (await qwikSSR(req)) as Response;
 
-  // Cache results
-  if (CACHE_CONTROL > 0) {
-    response.headers.set('Cache-Control', `max-age=${CACHE_CONTROL}`);
-    req.waitUntil(cache.put(req.request, response.clone()));
+    // Cache results
+    res.headers.set(
+      'Cache-Control',
+      `max-age=${CACHE_CONTROL}, s-maxage=10, stale-while-revalidate=604800, stale-if-error=604800`
+    );
+    req.waitUntil(cache.put(req.request, res.clone()));
+
+    // Return Qwik SSR response
+    return res;
+  } catch (e) {
+    // 500 Error
+    return new Response(String(e), { status: 500 });
   }
-  return response;
 };

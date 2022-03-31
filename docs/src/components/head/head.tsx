095d94591da4867a0832a23fc440bf5b73f0cab2
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-31 18:51:06 -0500

    docs: add gtm

--- docs/src/components/head/head.tsx
+++ docs/src/components/head/head.tsx
@@ -20,7 +20,18 @@ export const Head = () => (
     <meta name="twitter:description" content="Web Framework focusing on Time-to-Interactive." />
     <meta name="twitter:card" content="summary" />
 
-    <script innerHTML={partytownSnippet()} />
-    <script type="text/partytown">console.debug("🎉");</script>
+    <script
+      innerHTML={partytownSnippet({
+        forward: ['dataLayer.push'],
+      })}
+    />
+    <script
+      type="text/partytown"
+      innerHTML={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NR2STLN');`}
+    />
   </>
 );
--- docs/src/entry.server.tsx
+++ docs/src/entry.server.tsx
@@ -9,6 +9,14 @@ export function render(opts: RenderToStringOptions) {
         <Head />
       </head>
       <body q:base="/">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NR2STLN"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />
+        </noscript>
         <Main />
         <QwikLoader />
       </body>

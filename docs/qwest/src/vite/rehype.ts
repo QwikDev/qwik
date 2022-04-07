586d3184b088e07153374c329b1d08aee7991c85
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-07 11:55:52 -0500

    docs: allow hrefs to sourc md files (#364)

--- docs/qwest/src/vite/rehype.ts
+++ docs/qwest/src/vite/rehype.ts
@@ -7,9 +7,10 @@ import { headingRank } from 'hast-util-heading-rank';
 import { toString } from 'hast-util-to-string';
 import { visit } from 'unist-util-visit';
 import type { PageBreadcrumb, PageHeading, PageSource } from '../runtime';
-import { relative } from 'path';
+import { dirname, relative, resolve } from 'path';
 import type { PluginContext } from './types';
 import { getPagePathname } from './utils';
+import { existsSync } from 'fs';
 
 const slugs = new Slugger();
 
@@ -20,15 +21,48 @@ export function rehypePage(ctx: PluginContext): Transformer {
     const pathname = getPagePathname(ctx, vfile.path);
     const indexPathname = getPageIndexPathname(ctx, pathname);
 
-    getHeadings(mdast);
-    getAttributes(ctx, mdast, pathname);
-    getBreadcrumbs(ctx, mdast, pathname, indexPathname);
-    getPageIndex(ctx, mdast, indexPathname);
-    getSource(ctx, mdast, sourcePath);
+    updateContentLinks(ctx, mdast, sourcePath);
+    exportContentHeadings(mdast);
+    exportPageAttributes(ctx, mdast, pathname);
+    exportBreadcrumbs(ctx, mdast, pathname, indexPathname);
+    exportPageIndex(ctx, mdast, indexPathname);
+    exportPageSource(ctx, mdast, sourcePath);
   };
 }
 
-function getHeadings(mdast: Root) {
+function updateContentLinks(ctx: PluginContext, mdast: Root, sourcePath: string) {
+  visit(mdast, 'element', (node: any) => {
+    const tagName = node && node.type === 'element' && node.tagName.toLowerCase();
+    if (tagName !== 'a') {
+      return;
+    }
+
+    const href = ((node.properties && node.properties.href) || '').trim();
+    if (!isLocalHref(href)) {
+      return;
+    }
+
+    const lowerHref = href.toLowerCase();
+    if (lowerHref.endsWith('.mdx') || lowerHref.endsWith('.md')) {
+      const mdxPath = resolve(dirname(sourcePath), href);
+      const mdxExists = existsSync(mdxPath);
+      if (!mdxExists) {
+        console.warn(
+          `\nThe link "${href}", found within "${sourcePath}", does not have a matching source file.\n`
+        );
+        return;
+      }
+
+      if (lowerHref.endsWith('.mdx')) {
+        node.properties.href = node.properties.href.substring(0, href.length - 4);
+      } else if (lowerHref.endsWith('.md')) {
+        node.properties.href = node.properties.href.substring(0, href.length - 3);
+      }
+    }
+  });
+}
+
+function exportContentHeadings(mdast: Root) {
   slugs.reset();
   const headings: PageHeading[] = [];
 
@@ -50,13 +84,13 @@ function getHeadings(mdast: Root) {
   createExport(mdast, 'headings', headings);
 }
 
-function getAttributes(ctx: PluginContext, mdast: Root, pathname: string) {
+function exportPageAttributes(ctx: PluginContext, mdast: Root, pathname: string) {
   const page = ctx.pages.find((p) => p.pathname === pathname);
   const attributes = page?.attrs || {};
   createExport(mdast, 'attributes', attributes);
 }
 
-function getBreadcrumbs(
+function exportBreadcrumbs(
   ctx: PluginContext,
   mdast: Root,
   pathname: string,
@@ -103,13 +137,13 @@ function getBreadcrumbs(
   createExport(mdast, 'breadcrumbs', []);
 }
 
-function getPageIndex(ctx: PluginContext, mdast: Root, indexPathname: string | undefined) {
+function exportPageIndex(ctx: PluginContext, mdast: Root, indexPathname: string | undefined) {
   createExport(mdast, 'index', {
     path: indexPathname,
   });
 }
 
-function getSource(ctx: PluginContext, mdast: Root, sourcePath: string) {
+function exportPageSource(ctx: PluginContext, mdast: Root, sourcePath: string) {
   const source: PageSource = {
     path: relative(ctx.opts.pagesDir, sourcePath),
   };
@@ -165,6 +199,17 @@ function createExport(mdast: Root, identifierName: string, val: any) {
     },
   };
   mdast.children.unshift(mdxjsEsm);
+}
+
+function isLocalHref(href: string) {
+  href = href.toLowerCase();
+  return !(
+    href === '' ||
+    href.startsWith('#') ||
+    href.startsWith('https://') ||
+    href.startsWith('http://') ||
+    href.startsWith('about:')
+  );
 }
 
 const own = {}.hasOwnProperty;

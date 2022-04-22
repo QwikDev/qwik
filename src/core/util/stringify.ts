32bab6cdf8e51cc049d806467f9cb09ed9b72ea8
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-22 14:16:23 -0500

    chore: remove duplicate licenses

--- src/core/util/stringify.ts
+++ src/core/util/stringify.ts
@@ -1,11 +1,3 @@
-/**
- * @license
- * Copyright Builder.io, Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
- */
-
 export function stringify(value: any): string | null {
   return value == null ? null : String(value);
 }

db4244975a2a64aa9b00779d7166a6a96a29693d
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-21 22:36:08 -0500

    🌯

--- server/app.js
+++
@@ -1,5 +0,0 @@
-import { c as componentQrl, q as qrl } from './core-3bbc8927.js';
-
-const App=/*#__PURE__*/ componentQrl(qrl(()=>import('./entry_hooks-90a71ffe.js'),"App_render"));
-
-export { App };
--- server/root.js
+++
@@ -1,6 +0,0 @@
-import { j as jsx } from './core-3bbc8927.js';
-import { App } from './app.js';
-
-const Root=()=>/*#__PURE__*/ jsx("html",{children:[/*#__PURE__*/ jsx("head",{children:[/*#__PURE__*/ jsx("meta",{charSet:"utf-8"}),/*#__PURE__*/ jsx("title",{children:"Qwik Blank App"})]}),/*#__PURE__*/ jsx("body",{children:/*#__PURE__*/ jsx(App,{})})]});
-
-export { Root };

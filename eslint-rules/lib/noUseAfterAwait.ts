a743e3cebaeb207fa194ca60bee8c0439408291b
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-22 14:30:34 -0500

    chore: remove duplicate licenses (#413)

--- eslint-rules/lib/noUseAfterAwait.ts
+++ eslint-rules/lib/noUseAfterAwait.ts
@@ -1,12 +1,4 @@
 /* eslint-disable no-console */
-/**
- * @license
- * Copyright Builder.io, Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
- */
-
 import type { Rule } from 'eslint';
 
 export const noUseAfterAwait: Rule.RuleModule = {

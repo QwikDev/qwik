933184bcb9906d53fcc5db12faa8aa28519ee84a
Manu MA <manu.mtza@gmail.com>
2022-04-27 18:32:46 +0100

    🏙

--- packages/qwik/src/napi/test.cjs
+++ packages/qwik/src/napi/test.cjs
@@ -1,7 +1,7 @@
 //  node src/napi/test.cjs
 
 async function run() {
-  const { Optimizer } = require('../../dist-dev/@builder.io-qwik/optimizer/index.cjs');
+  const { Optimizer } = require('../../dist/optimizer/index.cjs');
   const optimizer = new Optimizer();
 
   const result = await optimizer.transformFs({

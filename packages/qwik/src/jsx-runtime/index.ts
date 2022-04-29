93700071b86b5136d955ec7f2fd786d98fcdbdf7
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-29 11:52:12 -0500

    refactor: bundle jsx-runtime d.ts

---
+++ packages/qwik/src/jsx-runtime/index.ts
@@ -0,0 +1,2 @@
+export { jsx, jsxs, jsxDEV, Fragment } from '@builder.io/qwik';
+export type { QwikJSX as JSX } from '@builder.io/qwik';
--- packages/qwik/src/optimizer/src/scripts.ts
+++ packages/qwik/src/optimizer/src/scripts.ts
@@ -1,1 +1,2 @@
 export const QWIK_LOADER_DEFAULT_MINIFIED: string = (global as any).QWIK_LOADER_DEFAULT_MINIFIED;
+export const QWIK_LOADER_DEFAULT_DEBUG: string = (global as any).QWIK_LOADER_DEFAULT_DEBUG;

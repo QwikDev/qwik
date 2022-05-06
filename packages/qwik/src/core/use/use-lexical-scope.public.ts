f509eb3d9edc4aeff03d1e544e70c4729b8f12e8
Misko Hevery <misko@hevery.com>
2022-05-06 15:41:34 -0700

    docs: Move API docs content from hackmd to local files.

--- packages/qwik/src/core/use/use-lexical-scope.public.ts
+++ packages/qwik/src/core/use/use-lexical-scope.public.ts
@@ -7,9 +7,9 @@ import { getContainer, getInvokeContext } from './use-core';
 import { useURL } from './use-url.public';
 import type { QRLInternal } from '../import/qrl-class';
 
-// <docs markdown="https://hackmd.io/lQ8v7fyhR-WD3b-2aRUpyw#useLexicalScope">
+// <docs markdown="./use-store.public.md#useLexicalScope">
 // !!DO NOT EDIT THIS COMMENT DIRECTLY!!!
-// (edit https://hackmd.io/@qwik-docs/BkxpSz80Y/%2FlQ8v7fyhR-WD3b-2aRUpyw%3Fboth#useLexicalScope instead)
+// (edit ./use-store.public.md#useLexicalScope instead)
 /**
  * Used by the Qwik Optimizer to restore the lexical scoped variables.
  *

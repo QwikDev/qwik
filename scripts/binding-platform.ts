5a78fcdbd6ffc127a0cff845f4ab74c98879d823
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-05-10 09:31:51 -0500

    🚖

--- scripts/binding-platform.ts
+++ scripts/binding-platform.ts
@@ -1,6 +1,10 @@
 import { BuildConfig, ensureDir } from './util';
 import spawn from 'cross-spawn';
 import { join } from 'path';
+import nodeFetch from 'node-fetch';
+import semver from 'semver';
+import { existsSync } from 'fs';
+import { copyFile, writeFile } from 'fs/promises';
 
 export async function buildPlatformBinding(config: BuildConfig) {
   await new Promise((resolve, reject) => {
@@ -36,4 +40,49 @@ export async function buildPlatformBinding(config: BuildConfig) {
   });
 
   console.log('🐯 native binding');
+}
+
+export async function copyPlatformBindingWasm(config: BuildConfig) {
+  ensureDir(config.distPkgDir);
+  ensureDir(config.distBindingsDir);
+
+  const releaseDataUrl = `https://data.jsdelivr.com/v1/package/npm/@builder.io/qwik`;
+  const releaseRsp = await nodeFetch(releaseDataUrl);
+  const releases = await releaseRsp.json();
+  let buildVersion = releases.tags.latest;
+  Object.values(releases.tags).forEach((version: any) => {
+    if (semver.gt(version, buildVersion)) {
+      buildVersion = version;
+    }
+  });
+
+  const tmpDir = join(config.tmpDir, `cached-bindings`, buildVersion);
+  ensureDir(tmpDir);
+
+  const bindingFilenames = [
+    'qwik.darwin-arm64.node',
+    'qwik.darwin-x64.node',
+    'qwik.wasm.cjs',
+    'qwik.wasm.mjs',
+    'qwik.win32-x64-msvc.node',
+    'qwik_wasm_bg.wasm',
+  ];
+
+  await Promise.all(
+    bindingFilenames.map(async (bindingFilename) => {
+      const cachedPath = join(tmpDir, bindingFilename);
+      const distPath = join(config.distBindingsDir, bindingFilename);
+
+      if (!existsSync(cachedPath)) {
+        const cdnUrl = `https://cdn.jsdelivr.net/npm/@builder.io/qwik@${buildVersion}/bindings/${bindingFilename}`;
+        const rsp = await nodeFetch(cdnUrl);
+        const code = await rsp.text();
+        await writeFile(cachedPath, code);
+      }
+
+      await copyFile(cachedPath, distPath);
+    })
+  );
+
+  console.log(`🦉 native binding / wasm (copied from npm v${buildVersion})`);
 }

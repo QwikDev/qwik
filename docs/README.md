93b8b1604ac8b5cb590b6c4bb87b6df3c38435c2
Misko Hevery <misko@hevery.com>
2022-05-16 14:08:32 -0700

    docs: Add clock example

---
+++ docs/README.md
@@ -0,0 +1,11 @@
+# `docs`
+
+Steps to get Docs running locally.
+
+```
+# From root directory
+yarn build
+cd packages/docs
+yarn build.qwikcity
+yarn devs.ssr
+```
---
+++ packages/docs/pages/examples/clock/app.tsx
@@ -0,0 +1,57 @@
+import { component$, useStore, useStyles$, useClientEffect$ } from '@builder.io/qwik';
+import CLOCK_STYLE from './clock';
+
+export const items = new Array(40).fill(null).map((_, index) => 'item ' + index);
+
+export const App = component$(() => {
+  return (
+    <div>
+      <div>
+        This is an example of Lazy executing code on component when component becomes visible.
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <Clock />
+    </div>
+  );
+});
+
+export const Clock = component$(() => {
+  useStyles$(CLOCK_STYLE);
+
+  const store = useStore({
+    hour: 10,
+    minute: 20,
+    second: 30,
+  });
+
+  useClientEffect$(() => {
+    const id = setInterval(() => {
+      const now = new Date();
+      store.second = (now.getSeconds() * 60) / 360;
+      store.minute = (now.getMinutes() * 60) / 360;
+      store.hour = (now.getHours() * 60) / 360;
+    }, 1000);
+    return () => clearInterval(id);
+  });
+
+  return (
+    <>
+      <style>{CLOCK_STYLE}</style>
+      <div class="clock">
+        <div class="twelve"></div>
+        <div class="three"></div>
+        <div class="six"></div>
+        <div class="nine"></div>
+        <div class="hour" style={{ transform: `rotate(${store.hour}deg)` }}></div>
+        <div class="minute" style={{ transform: `rotate(${store.minute}deg)` }}></div>
+        <div class="second" style={{ transform: `rotate(${store.second}deg)` }}></div>
+      </div>
+    </>
+  );
+});
+
+// Clock inspired by: https://paulund.co.uk/create-a-clock-in-css

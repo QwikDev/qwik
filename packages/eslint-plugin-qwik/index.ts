cf800ae8f8bc84604513a6828622b0a2eafed216
Manu MA <manu.mtza@gmail.com>
2022-06-12 19:33:52 +0100

    🎾

--- packages/eslint-plugin-qwik/index.ts
+++ packages/eslint-plugin-qwik/index.ts
@@ -1,13 +1,22 @@
+import { validLexicalScope } from './lib/validLexicalScope';
 import { noUseAfterAwait } from './lib/noUseAfterAwait';
 
 export const rules = {
   'no-use-after-await': noUseAfterAwait,
+  'valid-lexical-scope': validLexicalScope,
 };
 
 export const configs = {
   recommended: {
     plugins: ['qwik'],
     rules: {
+      'qwik/no-use-after-await': 'error',
+    },
+  },
+  strict: {
+    plugins: ['qwik'],
+    rules: {
+      'qwik/valid-lexical-scope': 'error',
       'qwik/no-use-after-await': 'error',
     },
   },

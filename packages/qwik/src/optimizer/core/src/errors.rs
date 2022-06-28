b9a24dc2b4cfb8115fd6e354e72dfc86f5a002b1
Manu MA <manu.mtza@gmail.com>
2022-06-28 11:16:36 +0100

    🚖

--- packages/qwik/src/optimizer/core/src/errors.rs
+++ packages/qwik/src/optimizer/core/src/errors.rs
@@ -1,14 +1,14 @@
 use swc_common::errors::DiagnosticId;
 
 pub enum Error {
-  RootLevelReference = 1,
-  FunctionReference,
-  CanNotCapture,
-  DynamicImportInsideQhook,
-  MissingQrlImplementation,
+    RootLevelReference = 1,
+    FunctionReference,
+    CanNotCapture,
+    DynamicImportInsideQhook,
+    MissingQrlImplementation,
 }
 
 pub fn get_diagnostic_id(err: Error) -> DiagnosticId {
-  let id = err as u32;
-  DiagnosticId::Error(format!("C{:02}", id))
+    let id = err as u32;
+    DiagnosticId::Error(format!("C{:02}", id))
 }

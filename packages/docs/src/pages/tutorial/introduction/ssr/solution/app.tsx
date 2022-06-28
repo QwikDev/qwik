f324c1f02c8e797dcc35fb4fc750d0cb79d8e0a5
Antoine Pairet <antoine.pairet@rosa.be>
2022-06-28 11:51:49 +0200

    docs: use workaround for isServer

--- packages/docs/src/pages/tutorial/introduction/ssr/solution/app.tsx
+++ packages/docs/src/pages/tutorial/introduction/ssr/solution/app.tsx
@@ -1,6 +1,16 @@
 /* eslint-disable no-console */
 import { component$, useServerMount$, useStore, useWatch$ } from '@builder.io/qwik';
-import { isServer } from '@builder.io/qwik/build';
+
+// import { isServer } from '@builder.io/qwik/build';
+export const isServer = (() => {
+  // This is a workaround for a bug in REPL which fails to package up of @builder.io/qwik/build.
+  // TODO remove this when the bug is fixed.
+  try {
+    return typeof window == 'undefined';
+  } catch (e) {
+    return false;
+  }
+})();
 
 export const App = component$(() => {
   const github = useStore({

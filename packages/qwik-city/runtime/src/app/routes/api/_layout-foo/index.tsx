e02d50341ca39c8228bfbc8ab928bfa878cfbc66
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-06-29 08:03:11 -0500

    api styles

--- packages/qwik-city/runtime/src/app/routes/api/_layout-foo/index.tsx
+++ packages/qwik-city/runtime/src/app/routes/api/_layout-foo/index.tsx
@@ -1,17 +1,24 @@
-import { component$, Host, Slot } from '@builder.io/qwik';
+import { component$, Host, Slot, useScopedStyles$ } from '@builder.io/qwik';
 import type { HeadComponent } from '~qwik-city-runtime';
 import Footer from '../../../components/footer/footer';
 import Header from '../../../components/header/header';
+import styles from './api.css';
 
 export default component$(() => {
+  useScopedStyles$(styles);
+
   return (
     <Host>
       <Header fullWidth={true} />
       <main class="api">
         <aside class="api-menu">
+          <h2>API</h2>
           <ul>
             <li>
-              <a href="/api">API</a>
+              <a href="/api/builder.io/oss.json">Org/User</a>
+            </li>
+            <li>
+              <a href="/api/data.json">Data</a>
             </li>
           </ul>
         </aside>

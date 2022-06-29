bcbe59caf056d1ec9cfeacdf0b8d41c1ddc8a1a6
Manu MA <manu.mtza@gmail.com>
2022-06-29 11:41:08 +0100

    🏈

--- packages/qwik-city/runtime/vite.config.ts
+++ packages/qwik-city/runtime/vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from 'packages/qwik-city/buildtime/vite';
+import { qwikCity } from '@builder.io/qwik-city/vite';
 
 export default defineConfig(() => {
   return {
     ssr: {
       target: 'webworker',
     },
-    plugins: [qwikCity(), qwikVite()],
+    build: {
+      lib: {
+        entry: './src/index.ts',
+        formats: ['es', 'cjs'],
+        fileName: (format) => `index.${format}.qwik.js`,
+      },
+    },
+    plugins: [qwikCity({
+      routesDir: './src/app/routes'
+    }), qwikVite()],
     optimizeDeps: {
       exclude: [
         '@builder.io/qwik',

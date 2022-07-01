a38ef269e4ff42f414d9a15d505ea96475e9e0fd
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-07-01 12:10:21 -0500

    ❄️

--- packages/qwik-city/runtime/src/app/routes/__auth/_layout.tsx
+++ packages/qwik-city/runtime/src/app/routes/__auth/_layout.tsx
@@ -1,13 +1,16 @@
-import { component$, Host, Slot } from '@builder.io/qwik';
+import { component$, Host, Slot, useScopedStyles$ } from '@builder.io/qwik';
+import styles from './_layout.css';
 
 export default component$(() => {
+  useScopedStyles$(styles);
+
   return (
-    <Host>
-      <section>
+    <Host class="auth">
+      <section class="auth-content">
         <Slot />
       </section>
-      <aside>
-        <p>Account Help</p>
+      <aside class="auth-menu">
+        <h3>Account Help</h3>
         <ul>
           <li>Forgot password</li>
         </ul>

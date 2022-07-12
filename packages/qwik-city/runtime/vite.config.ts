ebd3f51f47595d2db186a6c6adb8e1168acdfc39
Manu MA <manu.mtza@gmail.com>
2022-07-12 19:17:05 +0200

    🐽

--- packages/qwik-city/runtime/vite.config.ts
+++ packages/qwik-city/runtime/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 
+
 export default defineConfig(() => {
   return {
     build: {
+      target: 'es2020',
       outDir: '../lib',
       lib: {
         entry: './src/index.ts',
--- starters/apps/qwik-city/src/root.tsx
+++ starters/apps/qwik-city/src/root.tsx
@@ -1,11 +1,12 @@
 import { Content, Html } from '@builder.io/qwik-city';
+import cityPlan from '@qwik-city-plan';
 import { Head } from './components/head/head';
 
 import './global.css';
 
 export default () => {
   return (
-    <Html lang="en">
+    <Html lang="en" cityPlan={cityPlan}>
       <Head />
       <body>
         <Content />

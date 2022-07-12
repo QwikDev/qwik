6bea773495769fba3af22a366c92e39d70739101
Manu MA <manu.mtza@gmail.com>
2022-07-12 19:48:08 +0200

    🐛

--- packages/qwik-city/runtime/vite.config.ts
+++ packages/qwik-city/runtime/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 
-
 export default defineConfig(() => {
   return {
     build: {

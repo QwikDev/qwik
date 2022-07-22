d6dc45db17b6341e46aac83f13f9cd9d130194c0
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-07-21 19:28:39 -0500

    chore: update qwik-city starter to 0.0.25

--- starters/apps/qwik-city/src/routes/api/[org]/[user].json/index.ts
+++ starters/apps/qwik-city/src/routes/api/[org]/[user].json/index.ts
@@ -3,27 +3,19 @@ import os from 'os';
 
 export const onGet: EndpointHandler = ({ request, params }) => {
   return {
-    status: 200,
-    body: {
-      timestamp: Date.now(),
-      method: request.method,
-      url: request.url,
-      params,
-      os: os.platform(),
-      arch: os.arch(),
-      node: process.versions.node,
-    },
+    timestamp: Date.now(),
+    method: request.method,
+    url: request.url,
+    params,
+    os: os.platform(),
+    arch: os.arch(),
+    node: process.versions.node,
   };
 };
 
-export const onPost: EndpointHandler = async ({ request }) => {
-  return {
-    status: 200,
-    body: `Platform: ${os.platform()}, Node: ${process.versions.node}, HTTP Method: ${
-      request.method
-    }`,
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  };
+export const onPost: EndpointHandler = async ({ request, response }) => {
+  response.headers.set('Content-Type', 'text/plain');
+  return `Platform: ${os.platform()}, Node: ${process.versions.node}, HTTP Method: ${
+    request.method
+  }`;
 };

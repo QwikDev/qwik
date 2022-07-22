8ee0ab4439f887427816dc04f4fc9f8cfa1e7d84
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-07-21 19:43:44 -0500

    chore: update qwik-city starter to 0.0.25 (#828)

--- starters/apps/qwik-city/src/routes/products/[id].tsx
+++ starters/apps/qwik-city/src/routes/products/[id].tsx
@@ -79,7 +79,7 @@ export const head: DocumentHead<ProductData | null> = ({ data }) => {
   };
 };
 
-export const onGet: EndpointHandler<EndpointData> = async ({ params }) => {
+export const onGet: EndpointHandler<EndpointData> = async ({ params, response }) => {
   // Serverside Endpoint
   // During SSR, this method is called directly on the server and returns the data object
   // On the client, this same data can be requested with fetch() at the same URL, but also
@@ -90,12 +90,8 @@ export const onGet: EndpointHandler<EndpointData> = async ({ params }) => {
 
   if (params.id === 'shirt') {
     // Redirect, which will skip any rendering and the server will immediately redirect
-    return {
-      status: 307,
-      headers: {
-        location: '/products/tshirt',
-      },
-    };
+    response.redirect('/products/tshirt');
+    return;
   }
 
   const productPrice = PRODUCT_DB[params.id];
@@ -103,26 +99,20 @@ export const onGet: EndpointHandler<EndpointData> = async ({ params }) => {
   if (!productPrice) {
     // Product data not found
     // but the data is still given to the renderer to decide what to do
-    return {
-      status: 404,
-      body: null,
-    };
+    response.status(404);
+    return null;
   }
 
+  // Found the product data
+  // This same data is passed to the head() function
+  // and in the component$() it can be access with useEndpoint()
+  response.headers.set('Cache-Control', 'no-cache, no-store, no-fun');
   return {
-    // Found the product data
-    // This same data is passed to the head() function
-    // and in the component$() it can be access with useEndpoint()
-    body: {
-      productId: params.id,
-      price: productPrice,
-      description: `Node ${process.versions.node} ${os.platform()} ${os.arch()} ${
-        os.cpus()[0].model
-      }`,
-    },
-    headers: {
-      'Cache-Control': 'no-cache, no-store, no-fun',
-    },
+    productId: params.id,
+    price: productPrice,
+    description: `Node ${process.versions.node} ${os.platform()} ${os.arch()} ${
+      os.cpus()[0].model
+    }`,
   };
 };
 

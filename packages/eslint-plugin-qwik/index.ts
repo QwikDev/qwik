843ee4fd5921349062f00ca3db0a8fef6823447c
GlennBecker <nnelgxorz@gmail.com>
2022-08-01 16:49:29 +0000

    Types for plugin configuration

--- packages/eslint-plugin-qwik/index.ts
+++ packages/eslint-plugin-qwik/index.ts
@@ -3,6 +3,15 @@ import { noUseAfterAwait } from './src/noUseAfterAwait';
 import { noUseFunctionInsideBranch } from './src/noUseFunctionInsideBranch';
 import { noUseFunctionOutsideComponent } from './src/useFunctionOutsideValidContext';
 
+type Rules = {
+  [K in keyof typeof rules as `qwik/${K}`]: string;
+};
+
+type Config = {
+  plugins: string[];
+  rules: Partial<Rules>;
+};
+
 export const rules = {
   'no-use-after-await': noUseAfterAwait,
   'valid-lexical-scope': validLexicalScope,
@@ -10,24 +19,23 @@ export const rules = {
   'no-use-function-outside-component': noUseFunctionOutsideComponent,
 } as const;
 
-type Rules = {
-  [ K in keyof typeof rules as `qwik/${K}`]: string
-}
-
-const QWIK_RULES: Rules = {
-  'qwik/no-use-after-await': 'error',
-  'qwik/valid-lexical-scope': 'error',
-  'qwik/no-use-function-inside-branch': 'error',
-  'qwik/no-use-function-outside-component': 'error',
-} 
-
-export const configs = {
+export const configs: Record<string, Config> = {
   recommended: {
     plugins: ['qwik'],
-    rules: QWIK_RULES,
+    rules: {
+      'qwik/no-use-after-await': 'error',
+      'qwik/valid-lexical-scope': 'error',
+      'qwik/no-use-function-inside-branch': 'error',
+      'qwik/no-use-function-outside-component': 'error',
+    },
   },
   strict: {
     plugins: ['qwik'],
-    rules: QWIK_RULES,
+    rules: {
+      'qwik/no-use-after-await': 'error',
+      'qwik/valid-lexical-scope': 'error',
+      'qwik/no-use-function-inside-branch': 'error',
+      'qwik/no-use-function-outside-component': 'error',
+    },
   },
 };

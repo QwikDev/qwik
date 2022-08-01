8f9ccc533e679fe138db21c7d6585eae6f8b9924
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-07-31 20:57:09 -0500

    🏍

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -1,28 +1,29 @@
-import { component$, useUserContext } from '@builder.io/qwik';
-import type { EndpointHandler } from '@builder.io/qwik-city';
+import { component$, Host, Resource, useUserContext } from '@builder.io/qwik';
+import { EndpointHandler, useEndpoint } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  // const resource = useEndpoint<typeof onRequest>();
+  const resource = useEndpoint<typeof onRequest>();
 
   const userCtx = useUserContext('qwikcity');
 
-  return <div>{JSON.stringify(userCtx)}</div>;
-
-  // return (
-  //   <Resource
-  //     resource={resource}
-  //     onResolved={(builderContent) => {
-  //       return <main class="builder" dangerouslySetInnerHTML={builderContent.html} />;
-  //     }}
-  //     onRejected={(r) => {
-  //       return (
-  //         <div>
-  //           Unable to load content <span hidden>{r}</span>
-  //         </div>
-  //       );
-  //     }}
-  //   />
-  // );
+  return (
+    <Host>
+      <div>{JSON.stringify(userCtx)}</div>
+      <Resource
+        resource={resource}
+        onResolved={(builderContent) => {
+          return <main class="builder" dangerouslySetInnerHTML={builderContent.html} />;
+        }}
+        onRejected={(r) => {
+          return (
+            <div>
+              Unable to load content <span hidden>{r}</span>
+            </div>
+          );
+        }}
+      />
+    </Host>
+  );
 });
 
 export const onRequest: EndpointHandler<BuilderContent> = async ({ url }) => {
@@ -30,17 +31,12 @@ export const onRequest: EndpointHandler<BuilderContent> = async ({ url }) => {
   qwikUrl.searchParams.set('apiKey', 'fe30f73e01ef40558cd69a9493eba2a2');
   qwikUrl.searchParams.set('userAttributes.urlPath', url.pathname);
 
-  const content: BuilderContent = {
-    html: qwikUrl.href,
-  };
-  return content;
-
-  // const response = await fetch(qwikUrl.href);
-  // if (response.ok) {
-  //   const content: BuilderContent = await response.json();
-  //   return content;
-  // }
-  // throw new Error('Unable to load content');
+  const response = await fetch(qwikUrl.href);
+  if (response.ok) {
+    const content: BuilderContent = await response.json();
+    return content;
+  }
+  throw new Error('Unable to load Builder content');
 };
 
 interface BuilderContent {

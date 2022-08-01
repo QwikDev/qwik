6f3f0ef5a8eb472a9dab0e934a5327dd1005cfb2
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-01 17:48:19 -0500

    fix: qwik-city frontmatter (#886)

--- packages/qwik-city/buildtime/markdown/frontmatter.ts
+++ packages/qwik-city/buildtime/markdown/frontmatter.ts
@@ -1,20 +1,57 @@
 import type { Transformer } from 'unified';
-import type { BuildContext } from '../types';
+import type { BuildContext, FrontmatterAttrs } from '../types';
 import { normalizePath } from '../utils/fs';
 import { visit } from 'unist-util-visit';
+import type { ResolvedDocumentHead } from '../../runtime/src';
 
 export function parseFrontmatter(ctx: BuildContext): Transformer {
   return (mdast, vfile) => {
-    const attrs: string[] = [];
+    const attrs: FrontmatterAttrs = {};
 
     visit(mdast, 'yaml', (node: any) => {
-      if (typeof node.value === 'string' && node.value.length > 0) {
-        attrs.push(node.value);
-      }
+      parseFrontmatterAttrs(attrs, node.value);
     });
 
-    if (attrs.length > 0) {
+    if (Object.keys(attrs).length > 0) {
       ctx.frontmatter.set(normalizePath(vfile.path), attrs);
     }
   };
+}
+
+export function parseFrontmatterAttrs(attrs: FrontmatterAttrs, yaml: string) {
+  if (typeof yaml === 'string') {
+    yaml = yaml.trim();
+    if (yaml !== '') {
+      const lines = yaml.split(/\r?\n|\r|\n/g);
+      for (const line of lines) {
+        const parts = line.split(':');
+        if (parts.length > 1) {
+          const attrName = parts[0].trim();
+          const attrValue = parts.slice(1).join(':').trim();
+          attrs[attrName] = attrValue;
+        }
+      }
+    }
+  }
+  return null;
+}
+
+export function frontmatterAttrsToDocumentHead(attrs: FrontmatterAttrs | undefined) {
+  if (attrs && Object.keys(attrs).length > 0) {
+    const head: Required<ResolvedDocumentHead> = { title: '', meta: [], styles: [], links: [] };
+
+    for (const attrName in attrs) {
+      const attrValue = attrs[attrName];
+      if (attrName === 'title') {
+        head.title = attrValue;
+      } else if (attrName === 'description') {
+        head.meta.push({
+          name: attrName,
+          content: attrValue,
+        });
+      }
+    }
+    return head;
+  }
+  return null;
 }

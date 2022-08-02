cb06340a411325a658126625b78c22751bec1cfc
Manu MA <manu.mtza@gmail.com>
2022-08-02 16:06:17 +0200

    🌶

--- starters/apps/e2e/src/entry.ssr.tsx
+++ starters/apps/e2e/src/entry.ssr.tsx
@@ -60,9 +60,6 @@ export default function (opts: RenderToStreamOptions) {
       {
         debug: true,
         fragmentTagName: 'container',
-        streaming: {
-          in
-        },
         qwikLoader: {
           include: url.searchParams.get('loader') === 'false' ? 'never' : 'auto',
           events: ['click'],

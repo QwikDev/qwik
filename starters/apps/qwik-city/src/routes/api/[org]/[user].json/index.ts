9bc7e36d358be0242dd77a62d8a0c189e2565cb5
Glenn Becker <nnelgxorz@gmail.com>
2022-08-04 22:36:18 -0600

    fix: Remove node apis from Qwik City starter (#938)

--- starters/apps/qwik-city/src/routes/api/[org]/[user].json/index.ts
+++ starters/apps/qwik-city/src/routes/api/[org]/[user].json/index.ts
@@ -1,5 +1,4 @@
 import type { EndpointHandler } from '@builder.io/qwik-city';
-import os from 'os';
 
 export const onGet: EndpointHandler = ({ request, params }) => {
   return {
@@ -7,15 +6,10 @@ export const onGet: EndpointHandler = ({ request, params }) => {
     method: request.method,
     url: request.url,
     params,
-    os: os.platform(),
-    arch: os.arch(),
-    node: process.versions.node,
   };
 };
 
 export const onPost: EndpointHandler = async ({ request, response }) => {
   response.headers.set('Content-Type', 'text/plain');
-  return `Platform: ${os.platform()}, Node: ${process.versions.node}, HTTP Method: ${
-    request.method
-  }`;
+  return `HTTP Method: ${request.method}`;
 };
--- starters/apps/qwik-city/src/routes/api/data.json.ts
+++ starters/apps/qwik-city/src/routes/api/data.json.ts
@@ -1,13 +1,9 @@
 import type { EndpointHandler } from '@builder.io/qwik-city';
-import os from 'os';
 
 export const onGet: EndpointHandler = ({ request }) => {
   return {
     timestamp: Date.now(),
     method: request.method,
     url: request.url,
-    os: os.platform(),
-    arch: os.arch(),
-    node: process.versions.node,
   };
 };

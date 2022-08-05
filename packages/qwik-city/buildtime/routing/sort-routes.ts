b40c3c6722e2b22052391fe04136d68c810fe567
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-05 00:58:47 -0500

    fix: route sorting (#940)

--- packages/qwik-city/buildtime/routing/sort-routes.ts
+++ packages/qwik-city/buildtime/routing/sort-routes.ts
@@ -1,19 +1,68 @@
 import type { BuildRoute } from '../types';
 
 export function sortRoutes(a: BuildRoute, b: BuildRoute) {
-  const aNames = a.paramNames.length;
-  const bNames = b.paramNames.length;
+  const aSegments = a.pathname.split('/');
+  const bSegments = b.pathname.split('/');
 
-  if (aNames === 0 && bNames > 0) return -1;
-  if (aNames > 0 && bNames === 0) return 1;
-  if (aNames > bNames) return -1;
-  if (aNames < bNames) return 1;
+  const aSegmentsLen = aSegments.length;
+  const bSegmentsLen = bSegments.length;
 
-  const aSegments = a.pathname.split('/').length;
-  const bSegments = b.pathname.split('/').length;
+  if (aSegmentsLen > bSegmentsLen) return -1;
+  if (aSegmentsLen < bSegmentsLen) return 1;
 
-  if (aSegments > bSegments) return -1;
-  if (aSegments < bSegments) return 1;
+  if (aSegmentsLen === bSegmentsLen) {
+    let aCatchalls = 0;
+    let bCatchalls = 0;
+    let aParams = 0;
+    let bParams = 0;
+    for (let i = 1; i < aSegmentsLen; i++) {
+      const aSegment = aSegments[i];
+      const bSegment = bSegments[i];
+
+      const aHasCatchall = aSegment.includes('[...');
+      const bHasCatchall = bSegment.includes('[...');
+      if (!aHasCatchall && bHasCatchall) {
+        return -1;
+      }
+      if (aHasCatchall && !bHasCatchall) {
+        return 1;
+      }
+
+      const aHasParam = aSegment.includes('[');
+      const bHasParam = bSegment.includes('[');
+      if (!aHasParam && bHasParam) {
+        return -1;
+      }
+      if (aHasParam && !bHasParam) {
+        return 1;
+      }
+
+      if (aHasCatchall) {
+        aCatchalls++;
+      }
+      if (bHasCatchall) {
+        bCatchalls++;
+      }
+      if (aHasParam) {
+        aParams++;
+      }
+      if (bHasParam) {
+        bParams++;
+      }
+    }
+    if (aCatchalls < bCatchalls) {
+      return -1;
+    }
+    if (aCatchalls > bCatchalls) {
+      return 1;
+    }
+    if (aParams < bParams) {
+      return -1;
+    }
+    if (aParams > bParams) {
+      return 1;
+    }
+  }
 
   if (a.type === 'endpoint' && b.type === 'page') return -1;
   if (a.type === 'page' && b.type === 'endpoint') return 1;

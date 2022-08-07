5c72169907048487f1a39e1ecd797a9639b600e6
yang bo <yangbohnzz@gmail.com>
2022-08-07 10:04:15 +0800

    fix: remove logon.png test

--- starters/apps/blank/src/components/logo/logo.tsx
+++ starters/apps/blank/src/components/logo/logo.tsx
@@ -1,11 +1,15 @@
 import { component$, Host } from '@builder.io/qwik';
-import logoPng from './logo.png';
 
 export const Logo = component$(() => {
   return (
     <Host style={{ 'text-align': 'center' }}>
       <a href="https://qwik.builder.io/">
-        <img alt="Qwik Logo" width={400} height={147} src={logoPng} />
+        <img
+          alt="Qwik Logo"
+          width={400}
+          height={147}
+          src="https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F667ab6c2283d4c4d878fb9083aacc10f"
+        />
       </a>
     </Host>
   );

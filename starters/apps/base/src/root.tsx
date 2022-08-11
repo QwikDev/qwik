5136a92b348d24b36916c2f2b3c49618be031048
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-11 14:26:58 -0500

    chore: update starter meta viewport (#1051)

--- starters/apps/base/src/root.tsx
+++ starters/apps/base/src/root.tsx
@@ -6,6 +6,7 @@ export default () => {
       <head>
         <meta charSet="utf-8" />
         <title>Qwik Blank App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body></body>
     </html>
--- starters/apps/blank/src/root.tsx
+++ starters/apps/blank/src/root.tsx
@@ -8,6 +8,7 @@ export default () => {
       <head>
         <meta charSet="utf-8" />
         <title>Qwik Blank App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         <App />

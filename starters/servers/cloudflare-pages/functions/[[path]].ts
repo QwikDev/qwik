21fc8cbbffb0a60ebba28e077814d356ac4a9458
kaihnguyen <46040137+kaihnguyen@users.noreply.github.com>
2022-08-11 23:35:36 -0700

    fix: export matched module (#1054)
    entry.cloudflare.js exported onRequestGet not onRequest.

--- starters/servers/cloudflare-pages/functions/[[path]].ts
+++ starters/servers/cloudflare-pages/functions/[[path]].ts
@@ -2,4 +2,4 @@
 
 // Cloudflare Pages Functions
 // https://developers.cloudflare.com/pages/platform/functions/
-export { onRequest } from '../server/entry.cloudflare';
+export { onRequestGet } from '../server/entry.cloudflare';

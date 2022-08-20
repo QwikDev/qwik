19aabbf83102f4fcdf5b32e8c681c956d096a227
Félix Sanz <me@felixsanz.com>
2022-08-20 16:19:03 +0200

    CSS Scoping tests reworked

---
+++ packages/qwik/src/core/util/markers.js
@@ -0,0 +1,43 @@
+"use strict";
+exports.__esModule = true;
+exports.ELEMENT_ID_Q_PROPS_PREFIX = exports.ELEMENT_ID_PREFIX = exports.ELEMENT_ID_SELECTOR = exports.ELEMENT_ID = exports.QSlotInertName = exports.RenderEvent = exports.QContainerSelector = exports.QContainerAttr = exports.QCtxAttr = exports.QScopedStyle = exports.QStyle = exports.QSlotName = exports.QSlotRef = exports.QSlot = exports.EventAny = exports.EventPrefix = exports.ComponentStylesPrefixContent = exports.ComponentStylesPrefixHost = exports.OnRenderProp = void 0;
+/**
+ * State factory of the component.
+ */
+exports.OnRenderProp = 'q:renderFn';
+/**
+ * Component style host prefix
+ */
+exports.ComponentStylesPrefixHost = '💎';
+/**
+ * Component style content prefix
+ */
+exports.ComponentStylesPrefixContent = '⭐️';
+/**
+ * Prefix used to identify on listeners.
+ */
+exports.EventPrefix = 'on:';
+/**
+ * Attribute used to mark that an event listener is attached.
+ */
+exports.EventAny = 'on:.';
+/**
+ * `<some-element q:slot="...">`
+ */
+exports.QSlot = 'q:slot';
+exports.QSlotRef = 'q:sref';
+exports.QSlotName = 'q:sname';
+exports.QStyle = 'q:style';
+exports.QScopedStyle = 'q:sstyle';
+exports.QCtxAttr = 'q:ctx';
+exports.QContainerAttr = 'q:container';
+exports.QContainerSelector = '[q\\:container]';
+exports.RenderEvent = 'qRender';
+/**
+ * `<q:slot name="...">`
+ */
+exports.QSlotInertName = '\u0000';
+exports.ELEMENT_ID = 'q:id';
+exports.ELEMENT_ID_SELECTOR = '[q\\:id]';
+exports.ELEMENT_ID_PREFIX = '#';
+exports.ELEMENT_ID_Q_PROPS_PREFIX = '&';

bb0b4f15a4fe5fda5e889b8ad1e351d6448c90e9
Michael McClintock <michael.mcclintock@hey.com>
2022-08-21 19:14:49 +1000

    fix: cloudflare pages starter (#1083)
    * entry.cloudflare.tsx and cloudflare pages function should export
    onRequest so qwik city can handle updates.
    
    * update docs to show using compiled middleware from /functions directory

--- starters/servers/cloudflare-pages/functions/[[path]].ts
+++ starters/servers/cloudflare-pages/functions/[[path]].ts
@@ -2,4 +2,4 @@
 
 // Cloudflare Pages Functions
 // https://developers.cloudflare.com/pages/platform/functions/
-export { onRequestGet } from '../server/entry.cloudflare';
+export { onRequest } from '../server/entry.cloudflare';

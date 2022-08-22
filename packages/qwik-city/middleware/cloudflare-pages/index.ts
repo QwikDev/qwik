8a3aff33fea34d6ca5dfa7852657042017a05e00
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-22 09:00:35 -0500

    fix: check next middleware for handled response (#1111)

--- packages/qwik-city/middleware/cloudflare-pages/index.ts
+++ packages/qwik-city/middleware/cloudflare-pages/index.ts
@@ -64,17 +64,25 @@ export function qwikCity(render: Render, opts?: QwikCityCloudflarePagesOptions) 
         },
       };
 
-      const handledResponse = await requestHandler<Response>(requestCtx, render, opts);
-      if (handledResponse) {
-        return handledResponse;
-      }
-
+      // check if the next middleware is able to handle this request
+      // useful if the request is for a static asset but app uses a catchall route
       const nextResponse = await next();
+
       if (nextResponse.status === 404) {
+        // next middleware unable to handle request
+        // send request to qwik city request handler
+        const handledResponse = await requestHandler<Response>(requestCtx, render, opts);
+        if (handledResponse) {
+          return handledResponse;
+        }
+
+        // qwik city did not have a route for this request
+        // respond with qwik city's 404 handler
         const notFoundResponse = await notFoundHandler<Response>(requestCtx);
         return notFoundResponse;
       }
 
+      // use the next middleware's response
       return nextResponse;
     } catch (e: any) {
       return new Response(String(e || 'Error'), {

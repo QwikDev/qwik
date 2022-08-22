14071b595134f5dd28410de253d0b23833e47507
Manu MA <manu.mtza@gmail.com>
2022-08-22 11:03:45 +0100

    refactor: remove useDocument() (#1107)

--- packages/docs/src/routes/tutorial/hooks/use-document/solution/app.tsx
+++
@@ -1,6 +0,0 @@
-import { component$, useDocument } from '@builder.io/qwik';
-
-export const App = component$(() => {
-  const doc = useDocument();
-  return <my-app>Host element tag-name: {doc.location.toString()}</my-app>;
-});

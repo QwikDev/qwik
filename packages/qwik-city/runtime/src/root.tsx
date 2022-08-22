f0b6e51a5f11b2504aeb6dfa65d26c94f7d2f9b7
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-22 14:27:53 -0500

    feat: provide "url" envData (#1116)

--- packages/qwik-city/runtime/src/root.tsx
+++ packages/qwik-city/runtime/src/root.tsx
@@ -1,13 +1,14 @@
-import { QwikCity } from '~qwik-city-runtime';
+import { QwikCity, RouterOutlet } from '~qwik-city-runtime';
 import { Head } from './app/components/head/head';
-import { Body } from './app/components/body/body';
 import './global.css';
 
 export default function Root() {
   return (
     <QwikCity>
       <Head />
-      <Body />
+      <body>
+        <RouterOutlet />
+      </body>
     </QwikCity>
   );
 }

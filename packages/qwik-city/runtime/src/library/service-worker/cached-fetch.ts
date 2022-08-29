c42bace3a6f4573b2084e8beda5718e993b9dc2e
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-08-29 09:23:50 -0500

    fix: service-worker caching

--- packages/qwik-city/runtime/src/library/service-worker/cached-fetch.ts
+++ packages/qwik-city/runtime/src/library/service-worker/cached-fetch.ts
@@ -1,66 +1,85 @@
-import { activeRequests, existingPrefetches } from './constants';
-import type { Fetch } from './types';
+import { existingPrefetches } from './constants';
+import type { AwaitingRequests, Fetch } from './types';
 import { useCache } from './utils';
 
-export const cachedFetch = (cache: Cache, fetch: Fetch, request: Request) => {
-  const url = request.url;
+export const cachedFetch = (
+  cache: Cache,
+  fetch: Fetch,
+  awaitingRequests: AwaitingRequests,
+  request: Request
+) =>
+  new Promise<Response>((promiseResolve, promiseReject) => {
+    const url = request.url;
+    const awaitingRequestResolves = awaitingRequests.get(url);
 
-  // possible multiple requests come in for the same url
-  let activeRequest = activeRequests.get(url);
-
-  if (!activeRequest) {
-    // if there's already an active request (async cache lookup and async network fetch)
-    // then use the active request before starting a new one
-
-    let resolve: ((rsp: Response) => void) | null;
-    activeRequest = new Promise<Response>((activeResolve) => (resolve = activeResolve));
-
-    // set this url has an active request to prevent double network requests
-    activeRequests.set(url, activeRequest);
-
-    cache
-      .match(url)
-      .then((cachedResponse) => {
-        if (useCache(request, cachedResponse)) {
-          // cached response found and user did not specifically send
-          // a request header to NOT use the cache (wasn't a hard refresh)
-          resolve!(cachedResponse!.clone());
-        } else {
-          // no cached response found or user didn't want to use the cache
-          // do a full network request
-          return fetch(request).then((networkResponse) => {
-            return cache.put(url, networkResponse.clone()).then(() => {
-              resolve!(networkResponse.clone());
-              existingPrefetches.add(url);
-            });
-          });
-        }
-      })
-      .catch(() => {
-        // network error, probably offline
-        return cache.match(url).then((cachedResponse) => {
-          if (cachedResponse) {
-            // luckily we have a cached version, let's use it instead of an offline message
-            resolve!(cachedResponse.clone());
-          } else {
-            // darn, we've got no connectivity and no cached response
-            // respond with a 503 offline message
-            resolve!(
-              new Response('Offline', {
-                status: 503,
-                headers: {
-                  'Content-Type': 'text/plain',
-                },
-              })
-            );
+    if (awaitingRequestResolves) {
+      // there's already an active request happening
+      // don't start a new request
+      awaitingRequestResolves.push({ resolve: promiseResolve, reject: promiseReject });
+    } else {
+      // there isn't already an active request for this url
+      // start a new request
+      const resolve = (response: Response) => {
+        // the response has been resolved
+        const resolves = awaitingRequests.get(url);
+        if (resolves) {
+          // loop through each of the active request
+          awaitingRequests.delete(url);
+          for (const { resolve } of resolves) {
+            // clone a new response for each of the active requests
+            resolve(response.clone());
           }
-        });
-      })
-      .finally(() => {
-        // promise resolved (or errored), remove from active request
-        activeRequests.delete(url);
-      });
-  }
+        } else {
+          // somehow the array of awaiting requests doesn't exist
+          promiseResolve(response.clone());
+        }
+      };
 
-  return activeRequest;
-};
+      const reject = (msg: any) => {
+        const resolves = awaitingRequests.get(url);
+        if (resolves) {
+          awaitingRequests.delete(url);
+          for (const { reject } of resolves) {
+            reject(msg);
+          }
+        } else {
+          promiseReject(msg);
+        }
+      };
+
+      // create a new array of the request waiting to be resolved
+      awaitingRequests.set(url, [{ resolve: promiseResolve, reject: promiseReject }]);
+      existingPrefetches.add(url);
+
+      cache
+        .match(url)
+        .then((cachedResponse) => {
+          if (useCache(request, cachedResponse)) {
+            // cached response found and user did not specifically send
+            // a request header to NOT use the cache (wasn't a hard refresh)
+            resolve(cachedResponse!);
+          } else {
+            // no cached response found or user didn't want to use the cache
+            // do a full network request
+            return fetch(request).then((networkResponse) => {
+              return cache.put(url, networkResponse.clone()).then(() => {
+                resolve(networkResponse);
+              });
+            });
+          }
+        })
+        .catch((err) => {
+          // network error, probably offline
+          return cache.match(url).then((cachedResponse) => {
+            if (cachedResponse) {
+              // luckily we have a cached version, let's use it instead of an offline message
+              resolve(cachedResponse);
+            } else {
+              // darn, we've got no connectivity and no cached response
+              // respond with a 503 offline message
+              reject(err);
+            }
+          });
+        });
+    }
+  });
---
+++ packages/qwik-city/runtime/src/library/service-worker/utils.unit.ts
@@ -0,0 +1,89 @@
+import { test } from 'uvu';
+import { equal } from 'uvu/assert';
+import { Request as NodeRequest, Response as NodeResponse } from 'node-fetch';
+import type { ServiceWorkerBundles } from './types';
+import { getCacheToDelete, isBuildRequest, useCache } from './utils';
+
+test('getCacheToDelete, delete bundles no longer possible', () => {
+  const buildBundles: ServiceWorkerBundles = {
+    'q-abc.js': [],
+    'q-def.js': [],
+  };
+  const cachedUrls = [
+    'https://qwik.builder.io/build/q-abc.js',
+    'https://qwik.builder.io/build/q-xyz.js',
+  ];
+  const c = getCacheToDelete(buildBundles, cachedUrls);
+  equal(c, ['https://qwik.builder.io/build/q-xyz.js']);
+});
+
+test('getCacheToDelete, none to delete', () => {
+  const buildBundles: ServiceWorkerBundles = {
+    'q-abc.js': [],
+    'q-def.js': [],
+  };
+  const cachedUrls = ['https://qwik.builder.io/build/q-abc.js'];
+  const c = getCacheToDelete(buildBundles, cachedUrls);
+  equal(c, []);
+});
+
+test('isBuildRequest, in buildBundles', () => {
+  const buildBundles: ServiceWorkerBundles = {
+    'q-abc.js': [],
+    'q-def.js': [],
+  };
+  const pathname = '/build/q-abc.js';
+  const c = isBuildRequest(buildBundles, pathname);
+  equal(c, true);
+});
+
+test('isBuildRequest, not in buildBundles', () => {
+  const buildBundles: ServiceWorkerBundles = {
+    'q-abc.js': [],
+    'q-def.js': [],
+  };
+  const pathname = '/build/q-xyz.js';
+  const c = isBuildRequest(buildBundles, pathname);
+  equal(c, false);
+});
+
+test('do not useCache, no response', () => {
+  const request = mockRequest();
+  const response = undefined;
+  const c = useCache(request, response);
+  equal(c, false);
+});
+
+test('do not useCache, response has no-cache', () => {
+  const request = mockRequest();
+  const response = mockResponse();
+  response.headers.set('cache-control', 'no-cache');
+  const c = useCache(request, response);
+  equal(c, false);
+});
+
+test('do not useCache, request has no-cache', () => {
+  const request = mockRequest();
+  request.headers.set('cache-control', 'no-cache');
+  const response = mockResponse();
+  const c = useCache(request, response);
+  equal(c, false);
+});
+
+test('useCache', () => {
+  const request = mockRequest();
+  const response = mockResponse();
+  const c = useCache(request, response);
+  equal(c, true);
+});
+
+test.run();
+
+export function mockRequest(url?: string): Request {
+  url = url || 'https://qwik.builder.io/';
+  return new NodeRequest(url) as any;
+}
+
+export function mockResponse(body?: any): Response {
+  return new NodeResponse(body) as any;
+}

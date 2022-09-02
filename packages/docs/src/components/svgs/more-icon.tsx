dfc2fcde9d137304524e969151cf81ef49447d84
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-01 21:21:01 -0500

    🚌

--- packages/docs/src/components/svgs/more-icon.tsx
+++ packages/docs/src/components/svgs/more-icon.tsx
@@ -5,7 +5,7 @@ interface MoreIconProps {
 
 export const MoreIcon = ({ width, height }: MoreIconProps) => (
   <svg width={width} height={height} fill="currentColor" viewBox="0 0 512 512">
-    <title>More</title>
+    {/* <title>More</title> */}
     <circle cx="256" cy="256" r="48" />
     <circle cx="256" cy="416" r="48" />
     <circle cx="256" cy="96" r="48" />

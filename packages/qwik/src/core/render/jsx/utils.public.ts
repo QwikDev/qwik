8fc77d36dfedd64be3fbbb8fdd2613bdc61183e1
Manu MA <manu.mtza@gmail.com>
2022-09-06 11:14:58 +0200

    feat: <SSRStream>

--- packages/qwik/src/core/render/jsx/utils.public.ts
+++ packages/qwik/src/core/render/jsx/utils.public.ts
@@ -1,5 +1,8 @@
-import { jsx } from './jsx-runtime';
+import type { ValueOrPromise } from '../../util/types';
+import { jsx } from '../jsx/jsx-runtime';
+import type { StreamWriter } from '../ssr/render-ssr';
 import type { FunctionComponent, JSXNode } from './types/jsx-node';
+import type { JSXChildren } from './types/jsx-qwik-attributes';
 
 export const QOnce = 'qonce';
 
@@ -45,3 +48,18 @@ export const SSRStreamBlock: FunctionComponent<{ children?: any }> = (props) => 
     jsx(SSRComment, { data: 'qkssr-po' }),
   ] as any;
 };
+
+/**
+ * @alpha
+ */
+export interface StreamProps {
+  children: (stream: StreamWriter) => ValueOrPromise<void> | AsyncGenerator<JSXChildren, void, any>;
+}
+
+/**
+ * @alpha
+ */
+export const SSRStream: FunctionComponent<StreamProps> = (props, key) =>
+  jsx(RenderOnce, { children: jsx(InternalSSRStream, props) }, key);
+
+export const InternalSSRStream: FunctionComponent<StreamProps> = () => null;

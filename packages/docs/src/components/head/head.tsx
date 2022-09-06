39a8a6b9517209c100ac8f529d9d2008610f94d5
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-06 09:19:45 -0500

    docs: qwik-city update, remove app manifest (#1183)

--- packages/docs/src/components/head/head.tsx
+++ packages/docs/src/components/head/head.tsx
@@ -1,6 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
-import { Manifest } from './manifest';
 import { Social } from './social';
 import { Vendor } from './vendor';
 
@@ -15,8 +14,13 @@ export const Head = component$(() => {
       <title>{head.title ? `${head.title} - Qwik` : `Qwik`}</title>
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width" />
+      <meta name="apple-mobile-web-app-title" content="Qwik" />
+      <meta name="application-name" content="Qwik" />
+      <meta name="apple-mobile-web-app-title" content="Qwik" />
+      <meta name="theme-color" content="#0093ee" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
 
-      <Manifest />
       {import.meta.env.PROD && (
         <>
           <Social loc={loc} head={head} />

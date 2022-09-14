a06f0b6cefefd49363d5da97c96a177ed26255c9
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-14 14:44:19 -0500

    feat: update qwik blank starter (#1242)

---
+++ starters/apps/blank/src/routes/index.tsx
@@ -0,0 +1,81 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>
+        Welcome to Qwik <span class="lightning">⚡️</span>
+      </h1>
+
+      <ul>
+        <li>
+          Check out the <code>src/routes</code> directory to get started.
+        </li>
+        <li>
+          Add integrations with <code>npm run qwik add</code>.
+        </li>
+        <li>
+          More info about development in <code>README.md</code>
+        </li>
+      </ul>
+
+      <h2>Commands</h2>
+
+      <table class="commands">
+        <tr>
+          <td>
+            <code>npm run dev</code>
+          </td>
+          <td>Start the dev server and watch for changes.</td>
+        </tr>
+        <tr>
+          <td>
+            <code>npm run preview</code>
+          </td>
+          <td>Production build and start preview server.</td>
+        </tr>
+        <tr>
+          <td>
+            <code>npm run build</code>
+          </td>
+          <td>Production build.</td>
+        </tr>
+        <tr>
+          <td>
+            <code>npm run qwik add</code>
+          </td>
+          <td>Add integration like edge servers and SSG.</td>
+        </tr>
+      </table>
+
+      <h2>Community</h2>
+
+      <ul>
+        <li>
+          <span>Questions or just want to say hi? </span>
+          <a href="https://qwik.builder.io/chat" target="_blank">
+            Chat on discord!
+          </a>
+        </li>
+        <li>
+          <span>Follow </span>
+          <a href="https://twitter.com/QwikDev" target="_blank">
+            @QwikDev
+          </a>
+          <span> on Twitter</span>
+        </li>
+        <li>
+          <span>Open issues and contribute on </span>
+          <a href="https://github.com/BuilderIO/qwik" target="_blank">
+            Github
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+};

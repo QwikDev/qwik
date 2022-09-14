019205648615d4bfa66e01b973c7f24c63c14c1c
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-14 14:44:19 -0500

    feat: update qwik blank starter (#1242)

--- starters/apps/blank/src/root.tsx
+++ starters/apps/blank/src/root.tsx
@@ -1,18 +1,26 @@
-import { App } from './components/app/app';
+import { component$ } from '@builder.io/qwik';
+import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
 
-export default function () {
+export default component$(() => {
+  /*
+   * The root of a QwikCity site always start with the <QwikCity> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Dont remove the `<head>` and `<body>` elements.
+   */
   return (
-    <html>
+    <QwikCity>
       <head>
         <meta charSet="utf-8" />
-        <title>Qwik Blank App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <RouterHead />
       </head>
-      <body>
-        <App />
+      <body lang="en">
+        <RouterOutlet />
+        <ServiceWorkerRegister />
       </body>
-    </html>
+    </QwikCity>
   );
-}
+});

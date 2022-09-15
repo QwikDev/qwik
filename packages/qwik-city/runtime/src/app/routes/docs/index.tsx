c7ec4bbc5bf9135d226d42182c9571cd8742c976
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-15 11:27:12 -0500

    fix: dev server redirect to trailing slash (#1257)

--- packages/qwik-city/runtime/src/app/routes/docs/index.tsx
+++ packages/qwik-city/runtime/src/app/routes/docs/index.tsx
@@ -5,6 +5,10 @@ export default component$(() => {
   return (
     <div>
       <h1>Welcome to the Docs!</h1>
+
+      <p>
+        <a href="/docs/">Docs link with trailing slash (should redirect without slash)</a>
+      </p>
     </div>
   );
 });

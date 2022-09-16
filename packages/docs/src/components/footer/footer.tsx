6e1b290352fabe5cd78697f9fdd8bf35366f6187
Steve Sewell <steve@builder.io>
2022-09-16 10:03:48 -0700

    chore: update docs site footer (#1270)
    Use Qwik accounts, backlink to Builder on Builder logo, add a max width for large screens, add Discord link

--- packages/docs/src/components/footer/footer.tsx
+++ packages/docs/src/components/footer/footer.tsx
@@ -3,17 +3,29 @@ import styles from './footer.css?inline';
 import builder from './builder.svg';
 import { TwitterLogo } from '../svgs/twitter-logo';
 import { GithubLogo } from '../svgs/github-logo';
+import { DiscordLogo } from '../svgs/discord-logo';
+
 export const Footer = component$(() => {
   useStyles$(styles);
 
   return (
-    <footer>
-      <div class="made-with-love">Made with love by</div>
+    <footer class="container mx-auto">
+      <div class="made-with-love">Made with ❤️ by</div>
       <div class="footer-top">
-        <img class="builder-logo" src={builder} alt="Builder.io logo" />
+        <a href="https://www.builder.io" target="_blank">
+          <img class="builder-logo" src={builder} alt="Builder.io logo" />
+        </a>
         <ul class="footer-social">
           <li>
-            <a href="https://github.com/BuilderIO" target="_blank" title="Github">
+            <a href="https://qwik.builder.io/chat" target="_blank" title="Discrod">
+              <span class="md:hidden">Discord</span>
+              <span class="hidden md:block">
+                <DiscordLogo width={22} height={22} />
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/BuilderIO/qwik" target="_blank" title="Github">
               <span class="md:hidden">Github</span>
               <span class="hidden md:block">
                 <GithubLogo width={22} height={22} />
@@ -21,8 +33,8 @@ export const Footer = component$(() => {
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/builderio" target="_blank" title="Twitter">
-              <span class="md:hidden">@builderio</span>
+            <a href="https://twitter.com/qwikdev" target="_blank" title="Twitter">
+              <span class="md:hidden">@qwikdev</span>
               <span class="hidden md:block">
                 <TwitterLogo width={22} height={22} />
               </span>

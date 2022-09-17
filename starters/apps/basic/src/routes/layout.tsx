80b9bc541cfeb3a3f863e1a0471b68ece836b764
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-17 16:51:14 -0500

    chore: starter updates (#1286)

--- starters/apps/basic/src/routes/layout.tsx
+++ starters/apps/basic/src/routes/layout.tsx
@@ -3,11 +3,18 @@ import Header from '../components/header/header';
 
 export default component$(() => {
   return (
-    <main>
-      <Header />
-      <section>
-        <Slot />
-      </section>
-    </main>
+    <>
+      <main>
+        <Header />
+        <section>
+          <Slot />
+        </section>
+      </main>
+      <footer>
+        <a href="https://www.builder.io/" target="_blank">
+          Made with ♡ by Builder.io
+        </a>
+      </footer>
+    </>
   );
 });

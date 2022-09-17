daf64ad7663719f3f370909a38e5ee4ac9ed1940
Manu MA <manu.mtza@gmail.com>
2022-09-18 00:03:34 +0200

    chore: improve starters

--- starters/apps/base/src/routes/service-worker.ts
+++ starters/apps/base/src/routes/service-worker.ts
@@ -1,5 +1,12 @@
-declare const self: ServiceWorkerGlobalScope;
-
+/*
+ * WHAT IS THIS FILE?
+ *
+ * The service-worker.ts file is used to have state of the art prefetching.
+ * https://qwik.builder.io/docs/advanced/prefetching/#prefetching
+ *
+ * Qwik uses service worker to speed up your site and reduce latency, ie, not used in the tradicional way of offiline.
+ * You can also use this file to add more functionality that runs in the service worker.
+ */
 import { setupServiceWorker } from '@builder.io/qwik-city/service-worker';
 
 setupServiceWorker();
@@ -7,3 +14,5 @@ setupServiceWorker();
 addEventListener('install', () => self.skipWaiting());
 
 addEventListener('activate', () => self.clients.claim());
+
+declare const self: ServiceWorkerGlobalScope;

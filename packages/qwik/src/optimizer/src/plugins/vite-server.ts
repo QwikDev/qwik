0361613363b7266201000ce8f24d1a728e1e5407
Manu MA <manu.mtza@gmail.com>
2022-09-18 19:16:34 +0200

    chore: add ssr dev logs (#1300)

--- packages/qwik/src/optimizer/src/plugins/vite-server.ts
+++ packages/qwik/src/optimizer/src/plugins/vite-server.ts
@@ -258,10 +258,19 @@ document.addEventListener('qerror', ev => {
 });
 </script>`;
 
+const PERF_WARNING = `
+<script>
+if (!window.__qwikViteLog) {
+  window.__qwikViteLog = true;
+  console.debug("%c⭐️ Qwik Dev SSR Mode","background: #0c75d2; color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;","App is running in SSR development mode!\\n - Additional JS is loaded by Vite for debugging and live reloading\\n - Rendering performance might not be optimal\\n - Delayed interactivity because prefetching is disabled\\n - Vite dev bundles do not represent production output\\n\\nProduction build can be tested running 'npm run preview'");
+}
+</script>`;
+
 const END_SSR_SCRIPT = `
 <script type="module" src="/@vite/client"></script>
 ${DEV_ERROR_HANDLING}
 ${ERROR_HOST}
+${PERF_WARNING}
 `;
 
 function getViteDevIndexHtml(entryUrl: string, envData: Record<string, any>) {

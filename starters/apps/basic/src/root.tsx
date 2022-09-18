3ab774dc6a4abfc89c94b085fb71c689a21d234e
Manu MA <manu.mtza@gmail.com>
2022-09-18 06:49:02 +0200

    fix: beta starters improvements (#1289)

--- starters/apps/basic/src/root.tsx
+++ starters/apps/basic/src/root.tsx
@@ -5,7 +5,7 @@ import { RouterHead } from './components/router-head/router-head';
 import './global.css';
 
 export default component$(() => {
-  /*
+  /**
    * The root of a QwikCity site always start with the <QwikCity> component,
    * immediately followed by the document's <head> and <body>.
    *

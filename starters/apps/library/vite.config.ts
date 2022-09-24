0fea8ca703c77f2ca23f38f89fcd34218b0110ac
Naor Peled <thehecticbyte@gmail.com>
2022-09-24 17:33:49 +0300

    added sourcemaps to library vite config

--- starters/apps/library/vite.config.ts
+++ starters/apps/library/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const isDevelopmentMode = mode === 'development';
+
   return {
     build: {
       target: 'es2020',
+      sourcemap: isDevelopmentMode,
       lib: {
         entry: './src/index.ts',
         formats: ['es', 'cjs'],

6293fdf91d42e7b8fac1654f51a57b0f3206b548
Manu MA <manu.mtza@gmail.com>
2022-09-24 14:45:19 +0200

    🚑

--- packages/qwik-city/runtime/src/app/routes/index.tsx
+++ packages/qwik-city/runtime/src/app/routes/index.tsx
@@ -1,23 +1,10 @@
-import { component$, useStore } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '../../library/types';
 
 export default component$(() => {
-  const state = useStore({
-    count: 0,
-  });
-  if (state.count > 0) {
-    throw new Error('hola');
-  }
   return (
     <div>
-      <h1
-        onClick$={() => {
-          console.warn('hola');
-          state.count++;
-        }}
-      >
-        Welcome to Qwik City {state.count}
-      </h1>
+      <h1 onClick$={() => console.warn('hola')}>Welcome to Qwik City</h1>
 
       <p>The meta-framework for Qwik.</p>
     </div>

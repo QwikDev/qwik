7fd3cf6fd94f4737bb61cb1d4998cc28d92073af
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-28 11:39:13 -0600

    docs: add links to media page (#1498)

--- packages/qwik/src/cli/utils/log.ts
+++ packages/qwik/src/cli/utils/log.ts
@@ -5,5 +5,9 @@ export function logSuccessFooter() {
   console.log(`💬 ${color.cyan('Questions? Start the conversation at:')}`);
   console.log(`   https://qwik.builder.io/chat`);
   console.log(`   https://twitter.com/QwikDev`);
+  console.log(`   https://twitter.com/QwikDev`);
+  console.log(``);
+  console.log(`📺 ${color.cyan('Presentations, Podcasts and Videos:')}`);
+  console.log(`   https://qwik.builder.io/media/`);
   console.log(``);
 }

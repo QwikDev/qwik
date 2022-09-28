8e51c178bfc5018c193226a62ef91e94c62b4cdc
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-09-28 13:26:43 -0600

    docs: update logo and media blogs (#1502)

--- packages/docs/src/routes/media/index.tsx
+++ packages/docs/src/routes/media/index.tsx
@@ -16,13 +16,13 @@ export default component$(() => {
         <ul class="thumbnails">
           <li>
             <a href="https://youtu.be/x2eF3YLiNhY" target="_blank">
-              <img src="http://i3.ytimg.com/vi/x2eF3YLiNhY/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/x2eF3YLiNhY/hqdefault.jpg" aria-hidden="true" />
               <p>Qwik… the world's first O(1) JavaScript framework?</p>
             </a>
           </li>
           <li>
             <a href="https://youtu.be/z14c3u9q8rI" target="_blank">
-              <img src="http://i3.ytimg.com/vi/z14c3u9q8rI/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/z14c3u9q8rI/hqdefault.jpg" aria-hidden="true" />
               <p>Qwik JS and the future of frameworks</p>
             </a>
           </li>
@@ -37,31 +37,31 @@ export default component$(() => {
         <ul class="thumbnails">
           <li>
             <a href="https://www.youtube.com/watch?v=_PDpoJUacuc" target="_blank">
-              <img src="http://i3.ytimg.com/vi/_PDpoJUacuc/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/_PDpoJUacuc/hqdefault.jpg" aria-hidden="true" />
               <p>Build Resumable Apps with Qwik</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=iJZaT-AvJ-o" target="_blank">
-              <img src="http://i3.ytimg.com/vi/iJZaT-AvJ-o/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/iJZaT-AvJ-o/hqdefault.jpg" aria-hidden="true" />
               <p>Introducing Qwik w/ Misko Hevery &amp; Shai Reznik</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=LbMRs7l4czI" target="_blank">
-              <img src="http://i3.ytimg.com/vi/LbMRs7l4czI/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/LbMRs7l4czI/hqdefault.jpg" aria-hidden="true" />
               <p>Resumable Apps in Qwik</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=0tCuUQe_ZA0" target="_blank">
-              <img src="http://i3.ytimg.com/vi/0tCuUQe_ZA0/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/0tCuUQe_ZA0/hqdefault.jpg" aria-hidden="true" />
               <p>Qwik: A no-hydration instant-on personalized web applications</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=7MgNMIPISY4" target="_blank">
-              <img src="http://i3.ytimg.com/vi/7MgNMIPISY4/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/7MgNMIPISY4/hqdefault.jpg" aria-hidden="true" />
               <p>QWIK - Set of great demos by Misko Hevery</p>
             </a>
           </li>
@@ -76,20 +76,66 @@ export default component$(() => {
         <ul class="thumbnails">
           <li>
             <a href="https://www.youtube.com/watch?v=0dC11DMR3fU&amp;t=154s" target="_blank">
-              <img src="http://i3.ytimg.com/vi/0dC11DMR3fU/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/0dC11DMR3fU/hqdefault.jpg" aria-hidden="true" />
               <p>WWC22 - Qwik + Partytown: How to remove 99% of JavaScript from main thread</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=GHbNaDSWUX8" target="_blank">
-              <img src="http://i3.ytimg.com/vi/GHbNaDSWUX8/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/GHbNaDSWUX8/hqdefault.jpg" aria-hidden="true" />
               <p>Qwik Workshop Part 1 - Live Coding</p>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/watch?v=Jf_E1_19aB4&t=629s" target="_blank">
-              <img src="http://i3.ytimg.com/vi/Jf_E1_19aB4/hqdefault.jpg" />
+              <img src="http://i3.ytimg.com/vi/Jf_E1_19aB4/hqdefault.jpg" aria-hidden="true" />
               <p>Qwik framework overview</p>
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2 id="blogs">
+          <a href="#blogs">Blogs</a>
+        </h2>
+
+        <ul class="bullets">
+          <li>
+            <a href="https://www.builder.io/blog/hydration-is-pure-overhead" target="_blank">
+              Hydration is Pure Overhead
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dev.to/mhevery/a-first-look-at-qwik-the-html-first-framework-af"
+              target="_blank"
+            >
+              HTML-first, JavaScript last: the secret to web speed!
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dev.to/builderio/a-first-look-at-qwik-the-html-first-framework-af"
+              target="_blank"
+            >
+              A first look at Qwik - the HTML first framework
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dev.to/mhevery/death-by-closure-and-how-qwik-solves-it-44jj"
+              target="_blank"
+            >
+              Death by Closure (and how Qwik solves it)
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://dev.to/mhevery/qwik-the-answer-to-optimal-fine-grained-lazy-loading-2hdp"
+              target="_blank"
+            >
+              Qwik: the answer to optimal fine-grained lazy loading
             </a>
           </li>
         </ul>

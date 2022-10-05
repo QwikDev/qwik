071571fa695618bcc64085b2cb4540537bf6e36c
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-10-05 12:14:04 -0500

    fix: edge next middleware (#1591)

--- packages/qwik-city/middleware/netlify-edge/index.ts
+++ packages/qwik-city/middleware/netlify-edge/index.ts
@@ -45,29 +45,25 @@ export function qwikCity(render: Render, opts?: QwikCityNetlifyOptions) {
         },
       };
 
+      // check if the next middleware is able to handle this request
+      // useful if the request is for a static asset but app uses a catchall route
+      const nextResponse = await context.next();
+      if (nextResponse.ok) {
+        // next response is able to handle this request
+        return nextResponse;
+      }
+
+      // next middleware unable to handle request
+      // send request to qwik city request handler
       const handledResponse = await requestHandler<Response>(requestCtx, render, context, opts);
       if (handledResponse) {
         return handledResponse;
       }
 
-      const nextResponse = await context.next();
-
-      if (nextResponse.status === 404) {
-        // next middleware unable to handle request
-        // send request to qwik city request handler
-        const handledResponse = await requestHandler<Response>(requestCtx, render, context, opts);
-        if (handledResponse) {
-          return handledResponse;
-        }
-
-        // qwik city did not have a route for this request
-        // respond with qwik city's 404 handler
-        const notFoundResponse = await notFoundHandler<Response>(requestCtx);
-        return notFoundResponse;
-      }
-
-      // use the next middleware's response
-      return nextResponse;
+      // qwik city did not have a route for this request
+      // respond with qwik city's 404 handler
+      const notFoundResponse = await notFoundHandler<Response>(requestCtx);
+      return notFoundResponse;
     } catch (e: any) {
       return new Response(String(e || 'Error'), {
         status: 500,

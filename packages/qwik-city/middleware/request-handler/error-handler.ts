74b70295bd09d576b261eb7cc8a69341f4e6749f
Mousa Aboubaker <93449860+MasterMousa@users.noreply.github.com>
2022-10-07 23:03:54 +0200

    Fix: Let developers make their own error pags design

--- packages/qwik-city/middleware/request-handler/error-handler.ts
+++ packages/qwik-city/middleware/request-handler/error-handler.ts
@@ -69,37 +69,8 @@ export function getErrorHtml(status: number, e: any) {
 }
 
 function minimalHtmlResponse(status: number, message?: string, stack?: string) {
-  const width = typeof message === 'string' ? '600px' : '300px';
-  const color = status >= 500 ? COLOR_500 : COLOR_400;
-  if (status < 500) {
-    stack = '';
-  }
 
-  return `<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="Status" content="${status}"/>
-  <title>${status} ${message}</title>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <style>
-    body { color: ${color}; background-color: #fafafa; padding: 30px; font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Roboto, sans-serif; }
-    p { max-width: ${width}; margin: 60px auto 30px auto; background: white; border-radius: 4px; box-shadow: 0px 0px 50px -20px ${color}; overflow: hidden; }
-    strong { display: inline-block; padding: 15px; background: ${color}; color: white; }
-    span { display: inline-block; padding: 15px; }
-    pre { max-width: 580px; margin: 0 auto; }
-  </style>
-</head>
-<body>
-  <p>
-    <strong>${status}</strong>
-    <span>${message}</span>
-  </p>
-  ${stack ? `<pre><code>${stack}</code></pre>` : ``}
-</body>
-</html>
-`;
+  return `<script>
+    window.location.href = '/errors/err-${status}-page';
+  </script>`;
 }
-
-const COLOR_400 = '#006ce9';
-const COLOR_500 = '#713fc2';

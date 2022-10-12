3af9c9acd117f32168b5eb82c7502a868424a136
Manu MA <manu.mtza@gmail.com>
2022-10-12 23:39:58 +0100

    🐒

--- starters/apps/e2e/src/components/render/render.tsx
+++ starters/apps/e2e/src/components/render/render.tsx
@@ -22,6 +22,7 @@ export const Render = component$(() => {
         Increment
       </button>
       <Child counter={state.counter}></Child>
+      <Issue1475 />
     </>
   );
 });
@@ -76,33 +77,39 @@ export const Child = component$((props: { counter: { count: number } }) => {
   );
 });
 
-
-export const Issue1475 = component$((props: { counter: { count: number } }) => {
+export const Issue1475 = component$(() => {
   const render = useSignal(false);
   return (
     <>
-      <button onClick$={() => render.value = true}>Render</button>
-      <div>
-        {render.value && (
-        <>
-          <h1>Before</h1>
-          Some text
-          <LazyIssue1475/>
-          <h2>After</h2>
-        </>
+      <button id="issue-1475-button" onClick$={() => (render.value = true)}>
+        Render
+      </button>
+      <div id="issue-1475-result">
+        {render.value ? (
+          <>
+            <h1>1. Before</h1>
+            2. Some text
+            <LazyIssue1475 />
+            {'\n'}
+            <h2>3 After</h2>
+            <p>Stuff</p>
+          </>
+        ) : (
+          <>
+            <h1>Welcome</h1>
+            <ul></ul>
+            <h2>Here</h2>
+          </>
         )}
       </div>
     </>
   );
 });
 
-
 export const LazyIssue1475 = component$(() => {
-  useWatch$(async() => {
-    await delay(100);
+  useWatch$(async () => {
+    await delay(50);
   });
 
-  return (
-    <div>Middle</div>
-  );
+  return <div>Middle</div>;
 });

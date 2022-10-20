2e06b8b448ec7c29074c2590b8c9afe649a70d30
Manu MA <manu.mtza@gmail.com>
2022-10-20 18:45:27 +0200

    fix: final react starter (#1822)

--- starters/features/qwik-react/src/routes/react/index.tsx
+++ starters/features/qwik-react/src/routes/react/index.tsx
@@ -5,27 +5,40 @@ import { MUIAlert, MUIButton, MUISlider, TableApp } from '~/integrations/react/m
 export default component$(() => {
   const show = useSignal(false);
   const count = useSignal(0);
+  const variant = useSignal<'contained' | 'outlined' | 'text'>('contained');
 
   return (
     <>
+      <h1>Qwik/React mother of all demos</h1>
+      <select
+        value={variant.value}
+        onChange$={(ev) => {
+          variant.value = (ev.target as any).value;
+        }}
+      >
+        <option>text</option>
+        <option>outlined</option>
+        <option selected>contained</option>
+      </select>
+
       <MUISlider
         value={count.value}
-        onChange$={(_, e) => {
-          count.value = e as number;
+        onChange$={(_, value) => {
+          count.value = value as number;
         }}
       />
 
-      <MUIButton variant="contained" host:onClick$={() => alert('click')}>
+      <MUIButton variant={variant.value} host:onClick$={() => alert('click')}>
         Slider is {count.value}
       </MUIButton>
 
-      <MUIAlert severity="error">
+      <MUIAlert severity="warning">
         This is a warning from Qwik
         <QwikCounter></QwikCounter>
       </MUIAlert>
 
       <button onClick$={() => (show.value = true)}>Show table</button>
-      {show.value && <TableApp client:visible></TableApp>}
+      {show.value && <TableApp client:visible>Slider is {count.value}</TableApp>}
     </>
   );
 });

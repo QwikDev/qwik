662c707cc9753da22d329183cacf91c8372ab40d
Leifer <leifer.jesus.mendez.zambrano@external.gloval.es>
2022-10-22 14:38:00 +0200

    queue trigger tag element

--- packages/qwik/src/testing/library.ts
+++ packages/qwik/src/testing/library.ts
@@ -1,10 +1,30 @@
 import { trigger } from './element-fixture';
 import { setTestPlatform } from './platform';
+
+/**
+ * convenience: events...
+ * userEvent.click(#element)
+ */
+const userEvent = {
+  click: function (element: Element) {
+    const classListValue = element.classList.value
+      .split(' ')
+      .map((c) => `.${c}`)
+      .join('');
+    return trigger(element, `${element.tagName}${classListValue}`, 'click');
+  },
+  dbClick: function (element: Element) {
+    const queueEvent = [];
+  },
+};
+
 /**
  *
  * @alpha
  */
 export const CreateMock = {
+  userEvent,
+  // userEvent.click(screen.getByRole('button'))
   setTestPlatform,
   triggerEvent: function (element: Element, selector: string) {
     return trigger(element, selector, 'click');

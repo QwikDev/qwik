7bc720999ee69d05ccf5723ac2a7b66ec23553f1
Miško Hevery <misko@hevery.com>
2022-10-25 16:33:50 +0200

    fixup! fix: remove `mutable()` references

--- starters/apps/e2e/src/components/lexical-scope/lexicalScope.tsx
+++ starters/apps/e2e/src/components/lexical-scope/lexicalScope.tsx
@@ -1,4 +1,4 @@
-import { component$, $, useStore, mutable, noSerialize, useSignal, Signal } from '@builder.io/qwik';
+import { component$, $, useStore, noSerialize, useSignal, Signal } from '@builder.io/qwik';
 
 export const LexicalScope = component$(() => {
   const signal = useSignal(0);

04ff3faf6a7116b8eb32ef6203e367c3916cfa73
Manu MA <manu.mtza@gmail.com>
2022-10-26 23:56:14 +0100

    🎠

--- starters/e2e/e2e.resource.spec.ts
+++ starters/e2e/e2e.resource.spec.ts
@@ -73,14 +73,14 @@ test.describe('resource serialization', () => {
 
     await expect(button1).toHaveText('PASS: Success 0');
     await expect(button2).toHaveText('ERROR: Error: failed 0');
-    await expect(button3).toHaveText('ERROR: timeout 0');
+    await expect(button3).toHaveText('ERROR: Error: timeout 0');
 
     // Click button 1
     await button1.click();
 
     await expect(button1).toHaveText('PASS: Success 1');
     await expect(button2).toHaveText('ERROR: Error: failed 0');
-    await expect(button3).toHaveText('ERROR: timeout 0');
+    await expect(button3).toHaveText('ERROR: Error: timeout 0');
 
     // Click button 2
     await button2.click();

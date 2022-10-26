30613a9187bc2897cde54c34492cc5e85e6f3e46
root <nnelgxorz@gmail.com>
2022-10-26 17:56:03 +0000

    yarn fmt

--- packages/qwik-city/middleware/node/cookie.ts
+++ packages/qwik-city/middleware/node/cookie.ts
@@ -1,126 +1,125 @@
-export type TimeUnit =
-    | "seconds"
-    | "minutes"
-    | "hours"
-    | "days"
-    | "weeks"
+export type TimeUnit = 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks';
 
 export interface CookieOptions {
-    domain?: string;
-    expires?: number | Date | string;
-    httpOnly?: boolean;
-    maxAge?: number | [number, TimeUnit];
-    path?: string;
-    sameSite?: boolean | 'lax' | 'none' | 'strict';
-    secure?: boolean;
+  domain?: string;
+  expires?: number | Date | string;
+  httpOnly?: boolean;
+  maxAge?: number | [number, TimeUnit];
+  path?: string;
+  sameSite?: boolean | 'lax' | 'none' | 'strict';
+  secure?: boolean;
 }
 
 export interface CookieValue {
-    value: string,
-    json: <T = unknown>() => T
-    number: () => number
+  value: string;
+  json: <T = unknown>() => T;
+  number: () => number;
 }
 
 const UNIT: Record<TimeUnit, number> = {
-    seconds: 1,
-    minutes: 1 * 60,
-    hours: 1 * 60 * 60,
-    days: 1 * 60 * 60 * 24,
-    weeks: 1 * 60 * 60 * 24 * 7,
-}
+  seconds: 1,
+  minutes: 1 * 60,
+  hours: 1 * 60 * 60,
+  days: 1 * 60 * 60 * 24,
+  weeks: 1 * 60 * 60 * 24 * 7,
+};
 
-const handleOptions = (options: CookieOptions): string[]  => {
-    const opts: string[] = []
+const handleOptions = (options: CookieOptions): string[] => {
+  const opts: string[] = [];
 
-    if (options.domain) {
-        opts.push(`Domain=${options.domain}`);
-    }
+  if (options.domain) {
+    opts.push(`Domain=${options.domain}`);
+  }
 
-    if (options.expires) {
-        const resolvedValue = typeof options.expires === 'number' || typeof options.expires == 'string'
-            ? options.expires
-            : options.expires.toISOString()
-        ;
-        opts.push(`Expires=${resolvedValue}`);
-    }
+  if (options.expires) {
+    const resolvedValue =
+      typeof options.expires === 'number' || typeof options.expires == 'string'
+        ? options.expires
+        : options.expires.toISOString();
+    opts.push(`Expires=${resolvedValue}`);
+  }
 
-    if (options.httpOnly) {
-        opts.push('HttpOnly');
-    }
+  if (options.httpOnly) {
+    opts.push('HttpOnly');
+  }
 
-    if (options.maxAge) {
-        const resolvedValue = typeof options.maxAge === 'number' 
-            ? options.maxAge 
-            : options.maxAge[0] * UNIT[options.maxAge[1]]
-            ;
-        opts.push(`MaxAge=${resolvedValue}`);
-    }
+  if (options.maxAge) {
+    const resolvedValue =
+      typeof options.maxAge === 'number'
+        ? options.maxAge
+        : options.maxAge[0] * UNIT[options.maxAge[1]];
+    opts.push(`MaxAge=${resolvedValue}`);
+  }
 
-    if (options.path) {
-        opts.push(`Path=${options.path}`);
-    }
+  if (options.path) {
+    opts.push(`Path=${options.path}`);
+  }
 
-    if (options.sameSite) {
-        opts.push(`SameSite`);
-    }
+  if (options.sameSite) {
+    opts.push(`SameSite`);
+  }
 
-    if (options.secure) {
-        opts.push('Secure');
-    }
+  if (options.secure) {
+    opts.push('Secure');
+  }
 
-    return opts
-}
+  return opts;
+};
 
 const createCookie = (name: string, value: string, options: CookieOptions = {}) => {
-    return[`${name}=${value}`, ...handleOptions(options)].join('; ');
-}
+  return [`${name}=${value}`, ...handleOptions(options)].join('; ');
+};
 
 export class Cookie {
-    private readonly _cookie: Record<string, string>;
-    private readonly _headers: Record<string, string>;
+  private readonly _cookie: Record<string, string>;
+  private readonly _headers: Record<string, string>;
 
-    constructor(cookieString: string) {
-        let parsedCookie: Record<string, string> = cookieString
-            .split(";")
-            .reduce((prev, cookie_value) => {
-                let split = cookie_value.split("=");
-                return {...prev, [split[0]]: split[1]}
-            }, {})
-        this._cookie = parsedCookie;
-        this._headers = {};
-    }
+  constructor(cookieString: string) {
+    let parsedCookie: Record<string, string> = cookieString
+      .split(';')
+      .reduce((prev, cookie_value) => {
+        let split = cookie_value.split('=');
+        return { ...prev, [split[0]]: split[1] };
+      }, {});
+    this._cookie = parsedCookie;
+    this._headers = {};
+  }
 
-    public get(name: string): CookieValue | null {
-        let cookie = this._cookie[name];
-        if (!cookie) { return null };
-        let value = cookie.split('=')[1];
-        return {
-            value,
-            json() {
-                return JSON.parse(value)
-            },
-            number()  { return Number(value) }
-        }
+  public get(name: string): CookieValue | null {
+    let cookie = this._cookie[name];
+    if (!cookie) {
+      return null;
     }
+    let value = cookie.split('=')[1];
+    return {
+      value,
+      json() {
+        return JSON.parse(value);
+      },
+      number() {
+        return Number(value);
+      },
+    };
+  }
 
-    public set(name: string, value: string, options: CookieOptions = {}) {
-        this._headers[name] = createCookie(name, value, options)
-    }
+  public set(name: string, value: string, options: CookieOptions = {}) {
+    this._headers[name] = createCookie(name, value, options);
+  }
 
-    public has(name: string) {
-        return !!this._cookie[name]
-    }
+  public has(name: string) {
+    return !!this._cookie[name];
+  }
 
-    public delete(name: string) {
-        let value = this._cookie[name];
-        if (value) {
-            this.set(name, value, { expires: new Date(0) })
-        }
+  public delete(name: string) {
+    let value = this._cookie[name];
+    if (value) {
+      this.set(name, value, { expires: new Date(0) });
     }
+  }
 
-    public *headers(): IterableIterator<string> {
-        for (const header of Object.values(this._headers)) {
-            yield header
-        }
+  public *headers(): IterableIterator<string> {
+    for (const header of Object.values(this._headers)) {
+      yield header;
     }
+  }
 }

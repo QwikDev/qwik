c12e73fb64d6413855d900f3a9cc84f60f65f712
Manu MA <manu.mtza@gmail.com>
2022-10-27 01:56:24 +0100

    🐄

--- starters/adaptors/static/src/entry.static.tsx
+++
@@ -1,8 +0,0 @@
-import { generate } from '@builder.io/qwik-city/static';
-
-generate({
-  outDir: 'dist',
-  origin: 'https://my-site.local',
-  renderModulePath: './entry.ssr',
-  qwikCityPlanModulePath: '@qwik-city-plan',
-});

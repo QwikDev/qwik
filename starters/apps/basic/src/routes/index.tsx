8c16422ca1edd090ff4410579910e5053a003c7f
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2022-11-02 10:41:13 +0100

    fix(SEO audit): 100% for mobile and desktop device (#1953)
    Co-authored-by: Van Vuong Ngo <112967559+mcuw@users.noreply.github.com>

--- starters/apps/basic/src/routes/index.tsx
+++ starters/apps/basic/src/routes/index.tsx
@@ -136,4 +136,10 @@ export default component$(() => {
 
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
 };

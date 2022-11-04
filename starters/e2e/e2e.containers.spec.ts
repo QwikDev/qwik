b84ab981437d383995b5c57c2495fc744003ca8d
Manu MA <manu.mtza@gmail.com>
2022-11-04 19:26:17 +0100

    🐓

--- starters/e2e/e2e.containers.spec.ts
+++ starters/e2e/e2e.containers.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('no resume', () => {
+test.describe('container', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/e2e/container');
   });

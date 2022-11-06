3dae2bbd1c20f0e91e824b47685dafcdd31bff1d
Alastair Smith <25154859+Alastair-smith2@users.noreply.github.com>
2022-11-06 20:41:32 +0000

    fix(tailwind.config.js): support tailwind usage in .mdx files (#2003)
    fix #2002
    
    Co-authored-by: Alastair Smith <Alastair-smith2@users.noreply.github.com>

--- starters/features/tailwind/tailwind.config.js
+++ starters/features/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },

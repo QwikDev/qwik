6ffadbe8c9565dced50925e4a1928888f86258aa
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-07 14:24:51 -0600

    fix: build.server error and sw build (#2033)

--- packages/qwik-city/buildtime/vite/plugin.ts
+++ packages/qwik-city/buildtime/vite/plugin.ts
@@ -235,14 +235,18 @@ export function qwikCity(userOpts?: QwikCityVitePluginOptions): any {
           for (const swEntry of ctx.serviceWorkers) {
             try {
               const swClientDistPath = join(clientOutDir, swEntry.chunkFileName);
-
               const swCode = await fs.promises.readFile(swClientDistPath, 'utf-8');
-              const swCodeUpdate = prependManifestToServiceWorker(ctx, manifest, swCode);
-              if (swCodeUpdate) {
-                await fs.promises.writeFile(swClientDistPath, swCodeUpdate);
+              try {
+                const swCodeUpdate = prependManifestToServiceWorker(ctx, manifest, swCode);
+                if (swCodeUpdate) {
+                  await fs.promises.mkdir(clientOutDir, { recursive: true });
+                  await fs.promises.writeFile(swClientDistPath, swCodeUpdate);
+                }
+              } catch (e2) {
+                console.error(e2);
               }
             } catch (e) {
-              console.error(e);
+              // safe to ignore if a service-worker.js not found
             }
           }
         }

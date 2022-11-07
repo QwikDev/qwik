c5aa61a2754b0d623ada9df549324f2c544ad135
Leifer Mendez <leifer33@gmail.com>
2022-11-07 19:18:24 +0100

    minimal setting

--- starters/features/vitest/src/components/example/example.spec.tsx
+++ starters/features/vitest/src/components/example/example.spec.tsx
@@ -20,13 +20,13 @@ test(`[ExampleTest Component]: Click counter +1`, async () => {
   const { screen, render, userEvent } = await createDOM();
   await render(<ExampleTest flag={true} />);
 
-  expect(screen.outerHTML).toContain('Contador:0');
+  expect(screen.outerHTML).toContain('Count:0');
 
   const spanBefore = screen.querySelector('span') as HTMLDivElement;
   await userEvent('.btn-counter', 'click');
-  expect(spanBefore.innerHTML).toEqual('Contador:1');
+  expect(spanBefore.innerHTML).toEqual('Count:1');
 
   const spanAfter = screen.querySelector('span') as HTMLDivElement;
   await userEvent('button', 'click');
-  expect(spanAfter.innerHTML).toEqual('Contador:2');
+  expect(spanAfter.innerHTML).toEqual('Count:2');
 });

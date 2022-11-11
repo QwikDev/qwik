db03ac4406c2860373465900dfe254c948fd9e73
Wout Mertens <Wout.Mertens@gmail.com>
2022-11-11 13:39:49 +0100

    feat: useSignal() support an initializer fn (#2077)

--- packages/qwik/src/core/use/use-signal.ts
+++ packages/qwik/src/core/use/use-signal.ts
@@ -1,4 +1,5 @@
 import { createSignal, Signal } from '../state/signal';
+import { isFunction } from '../util/types';
 import { useSequentialScope } from './use-sequential-scope';
 
 /**
@@ -6,7 +7,7 @@ import { useSequentialScope } from './use-sequential-scope';
  */
 export interface UseSignal {
   <T>(): Signal<T | undefined>;
-  <T>(value: T): Signal<T>;
+  <T>(value: T | (() => T)): Signal<T>;
 }
 
 /**
@@ -19,7 +20,8 @@ export const useSignal: UseSignal = <STATE>(initialState?: STATE): Signal<STATE>
   }
 
   const containerState = ctx.$renderCtx$.$static$.$containerState$;
-  const signal = createSignal(initialState, containerState, undefined) as Signal<STATE>;
+  const value = isFunction(initialState) ? (initialState as Function)() : initialState;
+  const signal = createSignal(value, containerState, undefined) as Signal<STATE>;
   set(signal);
   return signal;
 };

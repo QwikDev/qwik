25988bdf80ccc6988be772d0882288c56392dd97
Willkuerlich <8741947+willkuerlich@users.noreply.github.com>
2022-11-13 17:08:14 +0100

    docs: update example to use useSignal (#2112)

--- packages/docs/src/routes/tutorial/events/synchronous/solution/app.tsx
+++ packages/docs/src/routes/tutorial/events/synchronous/solution/app.tsx
@@ -1,14 +1,14 @@
-import { component$, useClientEffect$, useRef } from '@builder.io/qwik';
+import { component$, useClientEffect$, useSignal } from '@builder.io/qwik';
 
 export default component$(() => {
-  const aHref = useRef();
+  const aHref = useSignal<Element>();
   useClientEffect$(() => {
     const handler = (event: Event) => {
       event.preventDefault();
       window.open('http://qwik.builder.io');
     };
-    aHref.current!.addEventListener('click', handler);
-    return () => aHref.current!.removeEventListener('click', handler);
+    aHref.value!.addEventListener('click', handler);
+    return () => aHref.value!.removeEventListener('click', handler);
   });
 
   return (

871b78d3c6f3139f0abefe1cd61464f4bd09d4b9
Manu MA <manu.mtza@gmail.com>
2022-11-16 15:04:31 +0000

    🐺

--- starters/apps/documentation-site/src/root.tsx
+++ starters/apps/documentation-site/src/root.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
@@ -12,7 +12,7 @@ export default component$(() => {
    * Dont remove the `<head>` and `<body>` elements.
    */
   return (
-    <QwikCity>
+    <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <RouterHead />

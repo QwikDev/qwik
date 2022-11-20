4e445a8f9d7a6bf92557b450e01a940ee7785e51
Joe Moulton <67028427+jmmon@users.noreply.github.com>
2022-11-19 23:44:27 -0800

    docs(tutorial): useResource now has a task (#1151)

--- packages/docs/src/routes/tutorial/reactivity/resource/problem/app.tsx
+++ packages/docs/src/routes/tutorial/reactivity/resource/problem/app.tsx
@@ -6,6 +6,8 @@ export default component$(() => {
     org: 'BuilderIO',
   });
 
+  // Use useResource$() to set up how the data is fetched from the server.
+  // See the example for Fetching Data in the text on the left.
   const reposResource = useResource$<string[]>(({ track, cleanup }) => {
     // We need a way to re-run fetching data whenever the `github.org` changes.
     // Use `track` to trigger re-running of the this data fetching function.
@@ -32,11 +34,9 @@ export default component$(() => {
         />
       </span>
       <div>
-        <Resource
-          value={reposResource}
-          onPending={() => <>Loading...</>}
-          onRejected={(error) => <>Error: {error.message}</>}
-          onResolved={(repos) => (
+        {/* Use <Resource> to display the data from the useResource$() function. */}
+        {/* To help, here's a callback function to display the data on resolved. */}
+        {/* (repos) => (
             <ul>
               {repos.map((repo) => (
                 <li>
@@ -44,8 +44,7 @@ export default component$(() => {
                 </li>
               ))}
             </ul>
-          )}
-        />
+          ) */}
       </div>
     </div>
   );

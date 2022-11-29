b132133d76ae2d2b054ae09f1dcd59f3accecf89
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-29 16:17:26 -0600

    fix: useContent menu lookup

--- packages/qwik-city/runtime/src/routing.unit.ts
+++ packages/qwik-city/runtime/src/routing.unit.ts
@@ -1,7 +1,8 @@
 import { parseRoutePathname } from '../../buildtime/routing/parse-pathname';
-import { suite } from 'uvu';
+import { suite, test } from 'uvu';
 import { equal } from 'uvu/assert';
-import { getRouteParams } from './routing';
+import { getRouteParams, getMenuLoader } from './routing';
+import type { MenuData } from './types';
 
 const routingTest = suite('routing');
 
@@ -53,4 +54,75 @@ const testMatch = (
   }
 };
 
+test(`getMenuLoader, crawl up root, trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/a/b/c/');
+  equal(await loader!(), { default: { text: 'Root' } });
+});
+
+test(`getMenuLoader, crawl up root, no trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/a/b/c');
+  equal(await loader!(), { default: { text: 'Root' } });
+});
+
+test(`getMenuLoader, crawl up one, trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/foo/no-menu/');
+  equal(await loader!(), { default: { text: 'Foo' } });
+});
+
+test(`getMenuLoader, crawl up one, no trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/foo/no-menu');
+  equal(await loader!(), { default: { text: 'Foo' } });
+});
+
+test(`getMenuLoader, exact path, trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/foo/bar/');
+  equal(await loader!(), { default: { text: 'Bar' } });
+});
+
+test(`getMenuLoader, exact path, no trailing slash`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/foo/bar');
+  equal(await loader!(), { default: { text: 'Bar' } });
+});
+
+test(`getMenuLoader, root`, async () => {
+  const menus: MenuData[] = [
+    ['/foo/bar/', async () => ({ default: { text: 'Bar' } })],
+    ['/foo/', async () => ({ default: { text: 'Foo' } })],
+    ['/', async () => ({ default: { text: 'Root' } })],
+  ];
+  const loader = getMenuLoader(menus, '/');
+  equal(await loader!(), { default: { text: 'Root' } });
+});
+
 routingTest.run();
+test.run();

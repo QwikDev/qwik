5f85063af6897948d38fc773798d6a6c6fe79d94
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-29 16:21:34 -0600

    fix: useContent menu lookup (#2315)

--- packages/qwik-city/utils/fs.unit.ts
+++ packages/qwik-city/utils/fs.unit.ts
@@ -7,6 +7,7 @@ import {
   createFileId,
   getExtension,
   getPathnameFromDirPath,
+  getMenuPathname,
   isGroupedLayoutName,
   isMarkdownExt,
   isMenuFileName,
@@ -290,6 +291,74 @@ test('parseRouteIndexName', () => {
     const r = parseRouteIndexName(c.extlessName);
     equal(r.layoutName, c.expect.layoutName, `${c.extlessName} layoutName`);
     equal(r.layoutStop, c.expect.layoutStop, `${c.extlessName} layoutStop`);
+  });
+});
+
+[
+  {
+    filePath: join(routesDir, 'dir', 'menu.md'),
+    basePathname: '/basepath/',
+    trailingSlash: true,
+    expect: '/basepath/dir/',
+  },
+  {
+    filePath: join(routesDir, 'dir', 'menu.md'),
+    basePathname: '/basepath/',
+    trailingSlash: false,
+    expect: '/basepath/dir/',
+  },
+  {
+    filePath: join(routesDir, 'menu.md'),
+    basePathname: '/basepath/',
+    trailingSlash: true,
+    expect: '/basepath/',
+  },
+  {
+    filePath: join(routesDir, 'menu.md'),
+    basePathname: '/basepath/',
+    trailingSlash: false,
+    expect: '/basepath/',
+  },
+  {
+    filePath: join(routesDir, 'dir', 'menu.md'),
+    basePathname: '/',
+    trailingSlash: true,
+    expect: '/dir/',
+  },
+  {
+    filePath: join(routesDir, 'dir', 'menu.md'),
+    basePathname: '/',
+    trailingSlash: false,
+    expect: '/dir/',
+  },
+  {
+    filePath: join(routesDir, 'menu.md'),
+    basePathname: '/',
+    trailingSlash: true,
+    expect: '/',
+  },
+  {
+    filePath: join(routesDir, 'menu.md'),
+    basePathname: '/',
+    trailingSlash: false,
+    expect: '/',
+  },
+].forEach((t) => {
+  test(``, () => {
+    const opts: NormalizedPluginOptions = {
+      routesDir: routesDir,
+      basePathname: t.basePathname,
+      trailingSlash: t.trailingSlash,
+      mdxPlugins: {
+        remarkGfm: true,
+        rehypeSyntaxHighlight: true,
+        rehypeAutolinkHeadings: true,
+      },
+      mdx: {},
+      baseUrl: t.basePathname,
+    };
+    const pathname = getMenuPathname(opts, t.filePath);
+    equal(pathname, t.expect);
   });
 });
 

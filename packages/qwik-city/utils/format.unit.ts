d010aa7dc5fca72b5b640f61d147d92f09030f03
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-28 19:08:49 -0600

    feat: non-serializable response data dev error (#2305)

--- packages/qwik-city/utils/format.unit.ts
+++ packages/qwik-city/utils/format.unit.ts
@@ -1,33 +1,106 @@
 import { test } from 'uvu';
-import { equal } from 'uvu/assert';
-import { msToString } from './format';
+import { equal, throws } from 'uvu/assert';
+import { validateSerializable, msToString } from './format';
 
-test('msToString', () => {
-  const tests = [
-    {
-      ms: 0.05,
-      expect: '0.05 ms',
-    },
-    {
-      ms: 10.5,
-      expect: '10.5 ms',
-    },
-    {
-      ms: 100,
-      expect: '100.0 ms',
-    },
-    {
-      ms: 2000,
-      expect: '2.0 s',
-    },
-    {
-      ms: 120000,
-      expect: '2.0 m',
-    },
-  ];
-
-  tests.forEach((t) => {
+[
+  {
+    ms: 0.05,
+    expect: '0.05 ms',
+  },
+  {
+    ms: 10.5,
+    expect: '10.5 ms',
+  },
+  {
+    ms: 100,
+    expect: '100.0 ms',
+  },
+  {
+    ms: 2000,
+    expect: '2.0 s',
+  },
+  {
+    ms: 120000,
+    expect: '2.0 m',
+  },
+].forEach((t) => {
+  test(`msToString(${t.ms})`, () => {
     equal(msToString(t.ms), t.expect);
+  });
+});
+
+[
+  {
+    val: true,
+    throws: false,
+  },
+  {
+    val: false,
+    throws: false,
+  },
+  {
+    val: null,
+    throws: false,
+  },
+  {
+    val: undefined,
+    throws: false,
+  },
+  {
+    val: 'string',
+    throws: false,
+  },
+  {
+    val: 88,
+    throws: false,
+  },
+  {
+    val: {},
+    throws: false,
+  },
+  {
+    val: Object.create(null),
+    throws: false,
+  },
+  {
+    val: {
+      num: 88,
+      str: 'string',
+      bool: true,
+      nl: null,
+      undef: undefined,
+    },
+    throws: false,
+  },
+  {
+    val: new Map(),
+    throws: true,
+  },
+  {
+    val: {
+      obj: {
+        set: new Set(),
+      },
+    },
+    throws: true,
+  },
+].forEach((t) => {
+  test(`isSerializable(${JSON.stringify(t.val)})`, () => {
+    if (t.throws) {
+      throws(() => {
+        validateSerializable(t.val);
+      });
+    } else {
+      validateSerializable(t.val);
+    }
+  });
+});
+
+test(`not isSerializable if circular`, () => {
+  const obj: any = {};
+  obj.obj = obj;
+  throws(() => {
+    validateSerializable(obj);
   });
 });
 

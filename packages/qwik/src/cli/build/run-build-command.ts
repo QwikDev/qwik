b4c68cd6a98fcca348a76f2bb5790e69cb076da8
langbamit <langbamit@gmail.com>
2022-12-04 09:58:48 +0700

    fix(cli): fix build.types trigger condition

--- packages/qwik/src/cli/build/run-build-command.ts
+++ packages/qwik/src/cli/build/run-build-command.ts
@@ -61,7 +61,7 @@ export async function runBuildCommand(app: AppCommand) {
 
   let typecheck: Promise<Step> | null = null;
 
-  if (buildTypes && buildTypes.startsWith('tsc')) {
+  if (buildTypes && buildTypes.startsWith(`${pkgManager} run tsc`)) {
     let copyScript = buildTypes;
     if (!copyScript.includes('--pretty')) {
       // ensures colors flow throw when we console log the stdout

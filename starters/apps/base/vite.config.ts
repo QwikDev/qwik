239bcfb222dbd479a27c6ab15c3bfcd2bb6de14f
Roman Zanettin <roman.zanettin@gmail.com>
2022-12-04 22:44:03 +0100

    reverted imp order

--- starters/apps/base/vite.config.ts
+++ starters/apps/base/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import { qwikCity } from '@builder.io/qwik-city/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {

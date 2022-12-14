5cdd5d76872ed320cb5fc79b2b43ed230d494ac1
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2022-12-14 11:19:08 +0100

    feat: add skeleton starter app - makes starting with own style easy without cleanups

---
+++ starters/apps/skeleton/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>
+        Welcome to Qwik
+      </h1>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};

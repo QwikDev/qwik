c1af63ff08219dde328b8c27956a6a5649e14d28
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2022-12-17 01:27:44 +0100

    feat(add-skeleton-starter): fix build

--- starters/apps/skeleton/src/components/routes/layout.tsx
+++ starters/apps/skeleton/src/components/routes/layout.tsx
@@ -1,5 +1,5 @@
 import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
+import Header from '../header/header';
 
 export default component$(() => {
   return (

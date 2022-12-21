aff702892d14bec5fc16cb60d3a475a2bee8523a
Miško Hevery <misko@hevery.com>
2022-12-20 16:17:36 -0800

    docs: add builder.io OSS header bar (#2440)

--- packages/docs/src/components/builder-content/index.tsx
+++ packages/docs/src/components/builder-content/index.tsx
@@ -2,58 +2,75 @@ import { component$, Resource, useResource$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 import { getBuilderSearchParams, getContent, RenderContent } from '@builder.io/sdk-qwik';
 
-export default component$<{ html?: any; apiKey: string; model: string; tag: 'main' | 'div' }>(
-  (props) => {
-    const location = useLocation();
-    const query = location.query;
-    const render =
-      typeof query.get === 'function' ? query.get('render') : (query as { render?: string }).render;
-    const isSDK = render === 'sdk';
-    const builderContentRsrc = useResource$<any>(() => {
-      if (isSDK) {
-        return getContent({
-          model: props.model!,
-          apiKey: props.apiKey!,
-          options: getBuilderSearchParams(location.query),
-          userAttributes: {
-            urlPath: location.pathname,
-          },
-        });
-      } else if (props.html) {
-        return { html: props.html };
-      } else {
-        return getBuilderContent(props.apiKey, props.model, location.pathname);
-      }
-    });
+export default component$<{
+  html?: any;
+  apiKey: string;
+  model: string;
+  tag: 'main' | 'div';
+}>((props) => {
+  const location = useLocation();
+  const query = location.query;
+  const render =
+    typeof query.get === 'function' ? query.get('render') : (query as { render?: string }).render;
+  const isSDK = render === 'sdk';
+  const builderContentRsrc = useResource$<any>(() => {
+    if (isSDK) {
+      return getContent({
+        model: props.model!,
+        apiKey: props.apiKey!,
+        options: getBuilderSearchParams(location.query),
+        userAttributes: {
+          urlPath: location.pathname,
+        },
+      });
+    } else if (props.html) {
+      return { html: props.html };
+    } else {
+      return getBuilderContent({
+        apiKey: props.apiKey,
+        model: props.model,
+        urlPath: location.pathname,
+        cacheBust: true,
+      });
+    }
+  });
 
-    return (
-      <Resource
-        value={builderContentRsrc}
-        onPending={() => <div>Loading...</div>}
-        onResolved={(content) =>
-          content.html ? (
-            <props.tag class="builder" dangerouslySetInnerHTML={content.html} />
-          ) : (
-            <RenderContent model={props.model} content={content} apiKey={props.apiKey} />
-          )
-        }
-      />
-    );
-  }
-);
+  return (
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) =>
+        content.html ? (
+          <props.tag class="builder" dangerouslySetInnerHTML={content.html} />
+        ) : (
+          <RenderContent model={props.model} content={content} apiKey={props.apiKey} />
+        )
+      }
+    />
+  );
+});
 
 export interface BuilderContent {
   html: string;
 }
 
-export async function getBuilderContent(
-  apiKey: string,
-  model: string,
-  urlPath: string
-): Promise<BuilderContent> {
+export async function getBuilderContent({
+  apiKey,
+  model,
+  urlPath,
+  cacheBust = false,
+}: {
+  apiKey: string;
+  model: string;
+  urlPath: string;
+  cacheBust?: boolean;
+}): Promise<BuilderContent> {
   const qwikUrl = new URL('https://cdn.builder.io/api/v1/qwik/' + model);
   qwikUrl.searchParams.set('apiKey', apiKey);
   qwikUrl.searchParams.set('userAttributes.urlPath', urlPath);
+  if (cacheBust) {
+    qwikUrl.searchParams.set('cachebust', 'true');
+  }
 
   const response = await fetch(qwikUrl.href);
   if (response.ok) {
--- packages/docs/src/routes/layout.tsx
+++ packages/docs/src/routes/layout.tsx
@@ -2,13 +2,10 @@ import { component$, Slot } from '@builder.io/qwik';
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { Header } from '../components/header/header';
 import { Footer } from '../components/footer/footer';
-import BuilderContentComp from '../components/builder-content';
-import { BUILDER_MODEL, BUILDER_PUBLIC_API_KEY } from '../constants';
 
 export default component$(() => {
   return (
     <>
-      <BuilderContentComp apiKey={BUILDER_PUBLIC_API_KEY} model={BUILDER_MODEL} tag="div" />
       <Header />
       <main>
         <Slot />

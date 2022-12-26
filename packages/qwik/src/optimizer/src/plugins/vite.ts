QwikDev/qwik

commit 307afcd3271678c638b1f3923900c6b96ee4728e

fix: scope vite plugin qwik manifest so that apps can run in parallel (#2398)

packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -175,7 +175,13 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
         // Client build will write to this path, and SSR will read from it. For this reason,
         // the Client build should always start and finish before the SSR build.
         const nodeOs: typeof import('os') = await sys.dynamicImport('node:os');
-        tmpClientManifestPath = path.join(nodeOs.tmpdir(), `vite-plugin-qwik-q-manifest.json`);
+        // Additional we add a prefix to scope the file to the current application so that different
+        // applications can be run in parallel without generating conflicts
+        const scopePrefix = pluginOpts.scope ? `${pluginOpts.scope}-` : '';
+        tmpClientManifestPath = path.join(
+          nodeOs.tmpdir(),
+          `${scopePrefix}vite-plugin-qwik-q-manifest.json`
+        );
 
         if (target === 'ssr' && !pluginOpts.manifestInput) {
           // This is a SSR build so we should load the client build's manifest

commit 5d50d71bd7b64bdaaa05c59d6d387abe0e3e2053

fix: manifest file fails to create when package is namespaced (e.g., @org/package)
The issue was introduced in https://github.com/BuilderIO/qwik/pull/2398.

packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -179,7 +179,7 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
         const nodeOs: typeof import('os') = await sys.dynamicImport('node:os');
         // Additional we add a prefix to scope the file to the current application so that different
         // applications can be run in parallel without generating conflicts
-        const scopePrefix = pluginOpts.scope ? `${pluginOpts.scope}-` : '';
+        const scopePrefix = pluginOpts.scope ? `${pluginOpts.scope.replaceAll('/', '--')}-` : '';
         tmpClientManifestPath = path.join(
           nodeOs.tmpdir(),
           `${scopePrefix}vite-plugin-qwik-q-manifest.json`
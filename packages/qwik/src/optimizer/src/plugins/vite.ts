QwikDev/qwik

commit 307afcd3271678c638b1f3923900c6b96ee4728e

fix: scope vite plugin qwik manifest so that apps can run in parallel (#2398)

packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -175,7 +175,13 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
         // Client build will write to this path, and SSR will read from it. For this reason,
         // the Client build should always start and finish before the SSR build.
         const nodeOs: typeof import('os') = await sys.dynamicImport('node:os');
-        tmpClientManifestPath = path.join(nodeOs.tmpdir(), `vite-plugin-qwik-q-manifest.json`);
+        // Additional we add a prefix to scope the file to the current application so that different
+        // applications can be run in parallel without generating conflicts
+        const scopePrefix = pluginOpts.scope ? `${pluginOpts.scope}-` : '';
+        tmpClientManifestPath = path.join(
+          nodeOs.tmpdir(),
+          `${scopePrefix}vite-plugin-qwik-q-manifest.json`
+        );
 
         if (target === 'ssr' && !pluginOpts.manifestInput) {
           // This is a SSR build so we should load the client build's manifest

commit 10381e28c084130a6b1b685824c88bddb13908d0

fix: plugin scoping for namespaced packages (#2521)
The issue was introduced in https://github.com/BuilderIO/qwik/pull/2398.

packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -177,12 +177,14 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
         // Client build will write to this path, and SSR will read from it. For this reason,
         // the Client build should always start and finish before the SSR build.
         const nodeOs: typeof import('os') = await sys.dynamicImport('node:os');
-        // Additional we add a prefix to scope the file to the current application so that different
-        // applications can be run in parallel without generating conflicts
-        const scopePrefix = pluginOpts.scope ? `${pluginOpts.scope}-` : '';
+
+        // Additionally, we add a suffix to scope the file to the current application so that
+        // different applications can be run in parallel without generating conflicts.
+        const scopeSuffix = pluginOpts.scope ? `-${pluginOpts.scope.replace(/\//g, '--')}` : '';
+
         tmpClientManifestPath = path.join(
           nodeOs.tmpdir(),
-          `${scopePrefix}vite-plugin-qwik-q-manifest.json`
+          `vite-plugin-qwik-q-manifest${scopeSuffix}.json`
         );
 
         if (target === 'ssr' && !pluginOpts.manifestInput) {
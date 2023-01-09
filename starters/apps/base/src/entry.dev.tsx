832df9ce52e69ccb8fe45a2322b30800e4dc2a12
Harsh Mangalam <57381638+harshmangalam@users.noreply.github.com>
2023-01-09 22:21:41 +0530

    docs: fixed consistent type imports warning (#2578)

--- starters/apps/base/src/entry.dev.tsx
+++ starters/apps/base/src/entry.dev.tsx
@@ -9,7 +9,7 @@
  * - More code is transferred to the browser than in SSR mode.
  * - Optimizer/Serialization/Deserialization code is not exercised!
  */
-import { render, RenderOptions } from '@builder.io/qwik';
+import { render, type RenderOptions } from '@builder.io/qwik';
 import Root from './root';
 
 export default function (opts: RenderOptions) {

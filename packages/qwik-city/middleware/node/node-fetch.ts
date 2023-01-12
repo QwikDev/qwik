64d0c2a350658c50f2d9c37850986cbb72e55f04
Manu MA <manu.mtza@gmail.com>
2023-01-12 15:17:27 +0100

    😜

--- packages/qwik-city/middleware/node/node-fetch.ts
+++ packages/qwik-city/middleware/node/node-fetch.ts
@@ -15,23 +15,21 @@ export function patchGlobalThis() {
     typeof process !== 'undefined' &&
     process.versions.node
   ) {
-    if (!globalThis.fetch) {
-      globalThis.fetch = fetch as any;
-      globalThis.Headers = Headers as any;
-      globalThis.Request = Request as any;
-      globalThis.Response = Response as any;
-      globalThis.FormData = FormData as any;
-    }
-    if (typeof globalThis.TextEncoderStream === 'undefined') {
-      globalThis.TextEncoderStream = TextEncoderStream;
-      globalThis.TextDecoderStream = TextDecoderStream;
-    }
-    if (typeof globalThis.WritableStream === 'undefined') {
-      globalThis.WritableStream = WritableStream as any;
-      globalThis.ReadableStream = ReadableStream as any;
-    }
-    if (typeof globalThis.crypto === 'undefined') {
-      globalThis.crypto = crypto.webcrypto;
-    }
+    globalThis.fetch = fetch as any;
+    globalThis.Headers = Headers as any;
+    globalThis.Request = Request as any;
+    globalThis.Response = Response as any;
+    globalThis.FormData = FormData as any;
+  }
+  if (typeof globalThis.TextEncoderStream === 'undefined') {
+    globalThis.TextEncoderStream = TextEncoderStream;
+    globalThis.TextDecoderStream = TextDecoderStream;
+  }
+  if (typeof globalThis.WritableStream === 'undefined') {
+    globalThis.WritableStream = WritableStream as any;
+    globalThis.ReadableStream = ReadableStream as any;
+  }
+  if (typeof globalThis.crypto === 'undefined') {
+    globalThis.crypto = crypto.webcrypto;
   }
 }

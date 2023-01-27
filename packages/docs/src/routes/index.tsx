81e9b02121ce6452ac467f34a603f0691c6b3058
Manu MA <manu.mtza@gmail.com>
2023-01-27 22:28:18 +0100

    docs: about loader until finalized (#2740)

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -1,28 +1,10 @@
 import { component$ } from '@builder.io/qwik';
 import { DocumentHead, loader$ } from '@builder.io/qwik-city';
-import BuilderContentComp, { getBuilderContent } from '../components/builder-content';
+import BuilderContentComp from '../components/builder-content';
 import { QWIK_MODEL, QWIK_PUBLIC_API_KEY } from '../constants';
 
 export default component$(() => {
-  const builder = getBuilder.use();
-
-  return (
-    <BuilderContentComp
-      html={builder.value.html}
-      apiKey={QWIK_PUBLIC_API_KEY}
-      model={QWIK_MODEL}
-      tag="main"
-    />
-  );
-});
-
-export const getBuilder = loader$(({ pathname }) => {
-  return getBuilderContent({
-    apiKey: QWIK_PUBLIC_API_KEY,
-    model: QWIK_MODEL,
-    urlPath: pathname,
-    cacheBust: true,
-  });
+  return <BuilderContentComp apiKey={QWIK_PUBLIC_API_KEY} model={QWIK_MODEL} tag="main" />;
 });
 
 export const head: DocumentHead = {

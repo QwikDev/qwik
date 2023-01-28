a13b54f027fe9379ae1d3d238104f12fd2ce2874
Manu MA <manu.mtza@gmail.com>
2023-01-28 16:35:41 +0100

    🐂

--- packages/qwik/src/optimizer/src/plugins/vite-utils.ts
+++ packages/qwik/src/optimizer/src/plugins/vite-utils.ts
@@ -1,25 +1,23 @@
 import type { OptimizerSystem } from '../types';
 
-export async function formatError(sys: OptimizerSystem, e: any) {
-  if (e instanceof Error) {
-    const err = e as any;
-    let loc = err.loc;
-    if (!err.frame && !err.plugin) {
-      if (!loc) {
-        loc = findLocation(err);
-      }
-      if (loc) {
-        err.loc = loc;
-        if (loc.file) {
-          const fs: typeof import('fs') = await sys.dynamicImport('node:fs');
-          const { normalizePath }: typeof import('vite') = await sys.dynamicImport('vite');
-          err.id = normalizePath(err.loc.file);
-          try {
-            const code = fs.readFileSync(err.loc.file, 'utf-8');
-            err.frame = generateCodeFrame(code, err.loc);
-          } catch {
-            // nothing
-          }
+export async function formatError(sys: OptimizerSystem, e: Error) {
+  const err = e as any;
+  let loc = err.loc;
+  if (!err.frame && !err.plugin) {
+    if (!loc) {
+      loc = findLocation(err);
+    }
+    if (loc) {
+      err.loc = loc;
+      if (loc.file) {
+        const fs: typeof import('fs') = await sys.dynamicImport('node:fs');
+        const { normalizePath }: typeof import('vite') = await sys.dynamicImport('vite');
+        err.id = normalizePath(err.loc.file);
+        try {
+          const code = fs.readFileSync(err.loc.file, 'utf-8');
+          err.frame = generateCodeFrame(code, err.loc);
+        } catch {
+          // nothing
         }
       }
     }

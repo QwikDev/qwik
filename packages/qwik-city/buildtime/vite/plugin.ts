4f923685dea6e18873ecec5de9716a7630b297ec
Manu MA <manu.mtza@gmail.com>
2023-02-14 16:34:10 +0100

    chore: prepare 0.18.0 release (#2916)

--- packages/qwik-city/buildtime/vite/plugin.ts
+++ packages/qwik-city/buildtime/vite/plugin.ts
@@ -69,10 +69,11 @@ export function qwikCity(userOpts?: QwikCityVitePluginOptions): any {
         appType: 'custom',
         base: userOpts?.basePathname,
         optimizeDeps: {
+          include: ['zod'],
           exclude: [QWIK_CITY, QWIK_CITY_PLAN_ID, QWIK_CITY_ENTRIES_ID, QWIK_CITY_SW_REGISTER],
         },
         ssr: {
-          noExternal: [QWIK_CITY, QWIK_CITY_PLAN_ID, QWIK_CITY_ENTRIES_ID, QWIK_CITY_SW_REGISTER],
+          noExternal: [QWIK_CITY_PLAN_ID, QWIK_CITY_ENTRIES_ID, QWIK_CITY_SW_REGISTER],
         },
       };
       return updatedViteConfig;

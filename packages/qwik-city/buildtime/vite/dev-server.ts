aa12118bbb33c35d7c1b3434f8aad4e7268ccd8c
Genki Takiuchi <genki@s21g.com>
2023-02-24 02:23:59 +0900

    Removed unneccesary code

--- packages/qwik-city/buildtime/vite/dev-server.ts
+++ packages/qwik-city/buildtime/vite/dev-server.ts
@@ -115,10 +115,6 @@ export function ssrDevMiddleware(ctx: BuildContext, server: ViteDevServer) {
             requestEv.headers.forEach((value, key) => {
               res.setHeader(key, value);
             });
-            const cookieHeaders = requestEv.cookie.headers();
-            if (cookieHeaders.length > 0) {
-              res.setHeader('Set-Cookie', cookieHeaders);
-            }
 
             (res as QwikViteDevResponse)._qwikEnvData = {
               ...(res as QwikViteDevResponse)._qwikEnvData,

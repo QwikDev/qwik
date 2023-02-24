f60ff589502c090c0ce7edab00973e135c8fe286
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-23 19:35:06 -0600

    fix: render base with basePathname (#3128)

--- packages/qwik-city/middleware/request-handler/resolve-request-handlers.ts
+++ packages/qwik-city/middleware/request-handler/resolve-request-handlers.ts
@@ -20,7 +20,7 @@ import { QACTION_KEY, QFN_KEY } from '../../runtime/src/constants';
 import { isFormContentType, isQDataJson, QDATA_JSON } from './user-response';
 import { HttpStatus } from './http-status-codes';
 import type { Render, RenderToStringResult } from '@builder.io/qwik/server';
-import type { QRL, RenderOptions, _deserializeData, _serializeData } from '@builder.io/qwik';
+import type { QRL, _deserializeData, _serializeData } from '@builder.io/qwik';
 import { getQwikCityServerData } from './response-page';
 import { RedirectMessage } from './redirect-handler';
 import { isDev } from '@builder.io/qwik/build';
@@ -316,14 +316,7 @@ export function getPathname(url: URL, trailingSlash: boolean | undefined) {
 
 export const encoder = /*@__PURE__*/ new TextEncoder();
 
-export function securityMiddleware({
-  method,
-  url,
-  request,
-  error,
-  next,
-  getWritableStream,
-}: RequestEvent) {
+export function securityMiddleware({ method, url, request, error }: RequestEvent) {
   const forbidden =
     method === 'POST' &&
     request.headers.get('origin') !== url.origin &&
@@ -332,7 +325,7 @@ export function securityMiddleware({
     throw error(403, `Cross-site ${request.method} form submissions are forbidden`);
   }
 }
-export function renderQwikMiddleware(render: Render, opts?: RenderOptions) {
+export function renderQwikMiddleware(render: Render) {
   return async (requestEv: RequestEvent) => {
     if (requestEv.headersSent) {
       return;
@@ -358,6 +351,7 @@ export function renderQwikMiddleware(render: Render, opts?: RenderOptions) {
     try {
       const isStatic = getRequestMode(requestEv) === 'static';
       const result = await render({
+        base: requestEv.basePathname + 'build/',
         stream: stream,
         serverData: getQwikCityServerData(requestEv),
         containerAttributes: {

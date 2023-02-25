0505543c9c5b4432992b1d8a0c115362dfad55a3
manuelsanchez2 <manusansan22@gmail.com>
2023-02-25 08:50:03 +0100

    🏑

--- starters/apps/qwikcity-test/src/routes/(common)/index.tsx
+++ starters/apps/qwikcity-test/src/routes/(common)/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { DocumentHead } from '@builder.io/qwik-city';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (

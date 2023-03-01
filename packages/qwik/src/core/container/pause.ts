c58ea5297bb48e2021938e72a6e9fcb852986f56
Manu MA <manu.mtza@gmail.com>
2023-03-01 12:29:34 +0100

    perf: treeshake dep free tasks (#3213)

--- packages/qwik/src/core/container/pause.ts
+++ packages/qwik/src/core/container/pause.ts
@@ -700,8 +700,11 @@ export const collectElementData = (
     }
   }
   if (elCtx.$watches$) {
+    const map = collector.$containerState$.$subsManager$.$groupToManagers$;
     for (const obj of elCtx.$watches$) {
-      collectValue(obj, collector, dynamicCtx);
+      if (map.has(obj)) {
+        collectValue(obj, collector, dynamicCtx);
+      }
     }
   }
 

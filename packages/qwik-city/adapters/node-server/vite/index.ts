9da2efe94e169ae201c388aa39038370b4ee4bd8
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-03-06 21:47:30 +0100

    feat: node-server

---
+++ packages/qwik-city/adapters/node-server/vite/index.ts
@@ -0,0 +1,38 @@
+import type { StaticGenerateRenderOptions } from '@builder.io/qwik-city/static';
+import { ServerAdapterOptions, viteAdapter } from '../../shared/vite';
+
+/**
+ * @alpha
+ */
+export function expressAdapter(opts: NodeServerAdapterOptions = {}): any {
+  return viteAdapter({
+    name: opts.name || 'node-server',
+    origin: process?.env?.URL || 'https://yoursitename.qwik.builder.io',
+    ssg: opts.ssg,
+    cleanStaticGenerated: true,
+
+    config() {
+      return {
+        ssr: {
+          target: 'node',
+        },
+        build: {
+          ssr: true,
+        },
+        publicDir: false,
+      };
+    },
+  });
+}
+
+/**
+ * @alpha
+ */
+export interface NodeServerAdapterOptions extends ServerAdapterOptions {
+  name?: string;
+}
+
+/**
+ * @alpha
+ */
+export { StaticGenerateRenderOptions };

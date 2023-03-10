89bdde937402950f6bc075fbe415a88d4d53507b
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-03-10 13:49:15 -0600

    🐄

--- packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
+++ packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
@@ -15,131 +15,56 @@ export default component$(() => {
         <h2>
           <Link href="/deployments/">Deployments</Link>
         </h2>
-
-        <ul class="grid">
-          {data.deployments.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <span>
-                  <img src={d.logo} alt={d.name + ' Logo'} />
-                </span>
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/integrations/">Integrations</Link>
         </h2>
-        <ul class="grid">
-          {data.integrations.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.integrations.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/showcase/">Videos</Link>
         </h2>
-        <ul class="grid">
-          {data.integrations.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/media/#videos">Podcasts</Link>
         </h2>
-        <ul class="grid">
-          {data.integrations.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/media/#presentations">Presentations</Link>
         </h2>
-        <ul class="grid">
-          {data.integrations.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/community/groups/">Communities</Link>
         </h2>
-        <ul class="grid">
-          {data.communities.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/showcase/">Showcase</Link>
         </h2>
-        <ul class="grid">
-          {data.integrations.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
 
       <section>
         <h2>
           <Link href="/community/groups/">Online</Link>
         </h2>
-        <ul class="grid">
-          {data.online.map((d) => (
-            <li key={d.name}>
-              <Link href={d.url}>
-                <img src={d.logo} alt={d.name + ' Logo'} />
-                <span>{d.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <ul class="grid">{data.deployments.map(GridItem)}</ul>
       </section>
     </article>
   );
@@ -148,3 +73,22 @@ export default component$(() => {
 export const head: DocumentHead = {
   title: 'Qwik Ecosystem',
 };
+
+export function GridItem(props: GridItemProps) {
+  return (
+    <li key={props.name}>
+      <Link href={props.url}>
+        <span class="img">
+          <img src={props.logo} alt={props.name + ' Logo'} />
+        </span>
+        <span>{props.name}</span>
+      </Link>
+    </li>
+  );
+}
+
+interface GridItemProps {
+  name: string;
+  url: string;
+  logo: string;
+}

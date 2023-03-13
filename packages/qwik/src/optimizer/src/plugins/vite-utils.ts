7a2c62cb4c409b48f58f7c5b8adea58b919916af
Manu MA <manu.mtza@gmail.com>
2023-03-13 20:51:32 +0100

    fix: findLocation (#3354)
    fixes #3317

--- packages/qwik/src/optimizer/src/plugins/vite-utils.ts
+++ packages/qwik/src/optimizer/src/plugins/vite-utils.ts
@@ -43,7 +43,7 @@ export const findLocation = (e: Error): Loc | undefined => {
       const line = lines[i].replace('file:///', '/');
       if (/^\s+at/.test(line)) {
         const start = line.indexOf('/');
-        const end = line.indexOf(')', start);
+        const end = line.lastIndexOf(')', start);
         if (start > 0) {
           const path = line.slice(start, end);
           const parts = path.split(':');

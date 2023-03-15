19189cbac3ae464c604374568368a785a5c7e1bd
cunzaizhuyi <877824709@qq.com>
2023-03-15 23:09:28 +0800

    fix(docs): merge duplicate import statement (#3383)

--- packages/docs/src/entry.dev.tsx
+++ packages/docs/src/entry.dev.tsx
@@ -6,7 +6,7 @@ import Root from './root';
  * - Do not use this mode in production!
  * - No SSR
  * - No portion of the application is pre-rendered on the server.
- * - All of the application is running eagerly in the browser.
+ * - All the application is running eagerly in the browser.
  * - More code is transferred to the browser than in SSR mode.
  * - Optimizer/Serialization/Deserialization code is not exercised!
  */

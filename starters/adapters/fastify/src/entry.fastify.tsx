d732d8795fd7e6ca76a0bb8d22bbe5955224440a
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-03-16 23:04:39 +0100

    fix: function types

--- starters/adapters/fastify/src/entry.fastify.tsx
+++ starters/adapters/fastify/src/entry.fastify.tsx
@@ -23,21 +23,25 @@ const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
 const buildDir = join(distDir, 'build');
 
 // Allow for dynamic port
-const PORT = process.env.PORT ?? 3000;
+const PORT = parseInt(process.env.PORT ?? '3000');
 
-// Create the fastify server
-// https://www.fastify.io/docs/latest/Guides/Getting-Started/
-const fastify = Fastify({
-  logger: true,
-});
+const start = async () => {
+  // Create the fastify server
+  // https://www.fastify.io/docs/latest/Guides/Getting-Started/
+  const fastify = Fastify({
+    logger: true,
+  });
 
-// Enable compression
-// https://github.com/fastify/fastify-compress
-// IMPORTANT NOTE: THIS MUST BE REGISTERED BEFORE THE fastify-qwik PLUGIN
-// await fastify.register(import('@fastify/compress'))
+  // Enable compression
+  // https://github.com/fastify/fastify-compress
+  // IMPORTANT NOTE: THIS MUST BE REGISTERED BEFORE THE fastify-qwik PLUGIN
+  // await fastify.register(import('@fastify/compress'))
 
-// Handle Qwik City using a plugin
-await fastify.register(import('./plugins/fastify-qwik'), { distDir, buildDir, render });
+  // Handle Qwik City using a plugin
+  await fastify.register(import('./plugins/fastify-qwik'), { distDir, buildDir, render });
 
-// Start the fastify server
-await fastify.listen({ port: PORT });
+  // Start the fastify server
+  await fastify.listen({ port: PORT });
+};
+
+start();

1fc7afad7d92f872008fbe6ef3db7451fba90643
Roman Zanettin <roman.zanettin@gmail.com>
2023-03-17 21:26:24 +0100

    wip

--- packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
+++ packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
@@ -16,62 +16,63 @@ export default component$(() => {
 
   return (
     <article class="ecosystem">
-      <h1>Qwik+</h1>
+      <h1 class="text-6xl mb-3 text-center font-thin">Qwik+</h1>
+      <h2 class="text-md text-slate-600 text-center mt-0 mb-10">There is even more ...</h2>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/deployments/">Deployments</Link>
         </h2>
-        <ul class="grid">{data.deployments.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-4">{data.deployments.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/integrations/">Integrations</Link>
         </h2>
-        <ul class="grid">{data.integrations.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-4">{data.integrations.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/media/#videos">Videos</Link>
         </h2>
-        <ul class="grid full-item">{videos.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-3">{videos.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/media/#podcasts">Podcasts</Link>
         </h2>
-        <ul class="grid full-item">{podcasts.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-3">{podcasts.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/showcase/">Showcase</Link>
         </h2>
-        <ul class="grid">{showcaseSites.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-3">{showcaseSites.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/media/#presentations">Presentations</Link>
         </h2>
-        <ul class="grid full-item">{presentations.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-3">{presentations.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/community/groups/">Communities</Link>
         </h2>
-        <ul class="grid">{data.communities.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-4">{data.communities.map(GridItem)}</ul>
       </section>
 
-      <section>
-        <h2>
+      <section class="grid grid-cols-[200px,1fr]">
+        <h2 class="font-thin text-2xl">
           <Link href="/community/groups/">Social</Link>
         </h2>
-        <ul class="grid transparent-item">{data.social.map(GridItem)}</ul>
+        <ul class="grid gap-1 grid-cols-4">{data.social.map(GridItem)}</ul>
       </section>
     </article>
   );
@@ -83,12 +84,15 @@ export const head: DocumentHead = {
 
 export const GridItem = component$((props: GridItemProps) => {
   return (
-    <li key={props.title} class="grid-card">
+    <li
+      key={props.title}
+      class="rounded-md bg-slate-800 text-white text-center overflow-hidden min-h-52"
+    >
       <Link href={props.href}>
-        <span class="grid-img">
-          <img src={props.imgSrc} alt={props.title} />
-        </span>
-        <span>{props.title}</span>
+        <div class="grid h-36 items-center p-10">
+          <img src={props.imgSrc} alt={props.title} class="m-auto max-h-20" />
+        </div>
+        <span class="block px-2 py-5 bg-slate-900">{props.title}</span>
       </Link>
     </li>
   );

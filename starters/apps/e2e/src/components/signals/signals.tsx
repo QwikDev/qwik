8c7a4c6b06dd2bed36f4da1d694c081d5ba904d3
Manu MA <manu.mtza@gmail.com>
2023-03-18 17:06:33 +0100

    fix: signal rendering of innerHTML
    fixes #3415

--- starters/apps/e2e/src/components/signals/signals.tsx
+++ starters/apps/e2e/src/components/signals/signals.tsx
@@ -121,6 +121,7 @@ export const SignalsChildren = component$(() => {
       <Issue3212 />
       <FineGrainedTextSub />
       <FineGrainedUnsubs />
+      <Issue3415 />
     </div>
   );
 });
@@ -772,5 +773,23 @@ export const FineGrainedUnsubs = component$(() => {
       )}
       <div>{count.value?.nu ?? 'EMPTY'}</div>
     </div>
+  );
+});
+
+export const Issue3415 = component$(() => {
+  const signal = useSignal('<b>foo</b>');
+
+  return (
+    <>
+      <button
+        id="issue-3415-button"
+        onClick$={() => {
+          signal.value = '<i>bar</i>';
+        }}
+      >
+        Toggle
+      </button>
+      <div id="issue-3415-result" dangerouslySetInnerHTML={signal.value} />
+    </>
   );
 });

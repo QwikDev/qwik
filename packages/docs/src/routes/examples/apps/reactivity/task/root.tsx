ce5b9e3f3e9d4717f58b44f4d3f6bcc22193f3b9
Manu MA <manu.mtza@gmail.com>
2023-03-21 20:45:24 +0100

    fix: docs tutorials (#3467)

--- packages/docs/src/routes/examples/apps/reactivity/task/root.tsx
+++ packages/docs/src/routes/examples/apps/reactivity/task/root.tsx
@@ -2,13 +2,13 @@ import App from './app';
 
 export const Root = () => {
   return (
-    <html>
+    <>
       <head>
         <title>Hello Qwik</title>
       </head>
       <body>
         <App />
       </body>
-    </html>
+    </>
   );
 };

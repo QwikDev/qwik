cd3a0fa9f198d9d06def095ae0ac62d16f0f6d11
Roman Zanettin <roman.zanettin@gmail.com>
2023-03-21 23:46:03 +0100

    overview wip

---
+++ packages/docs/src/routes/api/index.tsx
@@ -0,0 +1,40 @@
+import { component$ } from '@builder.io/qwik';
+import qwikApiData from './qwik/api-data.json';
+
+export default component$(() => {
+  return (
+    <>
+      <h1>API Reference</h1>
+
+      <h2>Qwik</h2>
+      <a href="qwik">
+        <h3>Qwik</h3>
+      </a>
+      <ApiMemberList data={qwikApiData} />
+    </>
+  );
+});
+
+// TODO: move into standalone cmp and adjust typings!
+export const ApiMemberList = component$(({ data }: any) => (
+  <ul class="grid md:grid-cols-2 lg:grid-cols-3">
+    {data.members.map((member) => {
+      if (!member.name) {
+        return;
+      }
+
+      // pascal to snake case
+      const kindClass = member.kind
+        .split(/\.?(?=[A-Z])/)
+        .join('-')
+        .toLowerCase();
+
+      // TODO: link must be adjusted!
+      return (
+        <li key="member" class={`list-none text-sm ${kindClass}`}>
+          <a href={`qwik#${member.name}`}>{member.name}</a>
+        </li>
+      );
+    })}
+  </ul>
+));

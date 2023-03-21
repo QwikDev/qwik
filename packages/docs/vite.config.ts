bb47e792c0f1df87c13fa2ea881f5007b3afd7dd
Manu MA <manu.mtza@gmail.com>
2023-03-21 16:31:58 +0100

    🎀

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -9,7 +9,14 @@ export default defineConfig(() => {
   const routesDir = resolve('src', 'routes');
   return {
     ssr: {
-      noExternal: true,
+      noExternal: [
+        '@algolia/autocomplete-core/dist/esm/resolve',
+        '@algolia/autocomplete-core',
+        '@algolia/autocomplete-shared',
+        'algoliasearch/lite',
+        'algoliasearch',
+        '@algolia/autocomplete-core/dist/esm/reshape',
+      ],
     },
     plugins: [
       qwikCity({
@@ -47,36 +54,36 @@ export default defineConfig(() => {
 });
 
 const page = {
-  'xEi06O8vOjU': 'page',
+  xEi06O8vOjU: 'page',
   '9t1uPE4yoLA': 'page',
-}
+};
 
 const algoliaSearch = {
-  'I5CyQjO9FjQ': 'algoliasearch',
-  'NsnidK2eXPg': 'algoliasearch',
-  'kDw0latGeM0': 'algoliasearch',
+  I5CyQjO9FjQ: 'algoliasearch',
+  NsnidK2eXPg: 'algoliasearch',
+  kDw0latGeM0: 'algoliasearch',
   '9dP8xDD36tk': 'algoliasearch',
   '7YcOLMha9lM': 'algoliasearch',
-  'Ly5oFWTkofs': 'algoliasearch',
-  'fTU5LQ1VhcU': 'algoliasearch',
-  'X3ZkFa9P7Dc': 'algoliasearch',
-  'cGb8pS0shrs': 'algoliasearch',
+  Ly5oFWTkofs: 'algoliasearch',
+  fTU5LQ1VhcU: 'algoliasearch',
+  X3ZkFa9P7Dc: 'algoliasearch',
+  cGb8pS0shrs: 'algoliasearch',
   '0TG0b0n4wNg': 'algoliasearch',
-  'qQlSSnFvEvs': 'algoliasearch',
+  qQlSSnFvEvs: 'algoliasearch',
   '01FQcGhldRU': 'algoliasearch',
-  'qolFAthnlPo': 'algoliasearch',
-  'J3Nim3Y9sio': 'algoliasearch',
+  qolFAthnlPo: 'algoliasearch',
+  J3Nim3Y9sio: 'algoliasearch',
 };
 
 const repl = {
-  'XoQB11UZ1S0': 'repl',
-  'AqHBIVNKf34': 'repl',
-  'IRhp4u7HN3o': 'repl',
-  'Qf2nEuUdHpM': 'repl',
-  'oEksvFPgMEM': 'repl',
-  'eePwnt3YTI8': 'repl',
-  'iw211Du0bw8': 'repl',
-  'lWGaPPYlcvs': 'repl',
-  'uCl5Lf0Typ8': 'repl',
-  'IW29huCoDkc': 'repl',
+  XoQB11UZ1S0: 'repl',
+  AqHBIVNKf34: 'repl',
+  IRhp4u7HN3o: 'repl',
+  Qf2nEuUdHpM: 'repl',
+  oEksvFPgMEM: 'repl',
+  eePwnt3YTI8: 'repl',
+  iw211Du0bw8: 'repl',
+  lWGaPPYlcvs: 'repl',
+  uCl5Lf0Typ8: 'repl',
+  IW29huCoDkc: 'repl',
 };

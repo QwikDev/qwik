80d350f7e9a04c341c1d9a2eeb310cef57151c00
geekreal <42457717+LockingReal@users.noreply.github.com>
2023-03-21 20:25:51 +0800

    docs: improved emoji shuffle(#3344)

--- packages/docs/src/routes/docs/(qwik)/community.ts
+++ packages/docs/src/routes/docs/(qwik)/community.ts
@@ -120,7 +120,7 @@ export function getCommunity() {
 
 function shuffleArray(array: string[]) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * i);
     [array[i], array[j]] = [array[j], array[i]];
   }
 }

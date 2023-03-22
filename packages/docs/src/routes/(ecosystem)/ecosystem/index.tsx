87490bb2861b8682ed0a10e1c392a19433b876f2
roman zanettin <zanettin@users.noreply.github.com>
2023-03-22 21:17:30 +0100

    docs: update ecosystem page for mobile (#3473)

--- packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
+++ packages/docs/src/routes/(ecosystem)/ecosystem/index.tsx
@@ -19,88 +19,88 @@ export default component$(() => {
       <h1 class="text-6xl mb-3 text-center font-thin">Qwik+</h1>
       <h2 class="text-md text-slate-600 text-center mt-0 mb-10">There is even more ...</h2>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/deployments/">Deployments</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-4">
+        <ul class="grid gap-1 grid-cols-2 md:grid-cols-4">
           {data.deployments.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/integrations/">Integrations</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-4">
+        <ul class="grid gap-1 grid-cols-2 md:grid-cols-4">
           {data.integrations.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/media/#videos">Videos</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-3">
+        <ul class="grid gap-1 grid-col-2 md:grid-cols-3">
           {videos.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/media/#podcasts">Podcasts</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-3">
+        <ul class="grid gap-1 grid-col-2 md:grid-cols-3">
           {podcasts.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/showcase/">Showcase</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-3">
+        <ul class="grid gap-1 grid-col-2 md:grid-cols-3">
           {showcaseSites.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/media/#presentations">Presentations</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-3">
+        <ul class="grid gap-1 grid-col-2 md:grid-cols-3">
           {presentations.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/community/groups/">Communities</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-4">
+        <ul class="grid gap-1 grid-cols-2 md:grid-cols-4">
           {data.communities.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
         </ul>
       </section>
 
-      <section class="grid grid-cols-[200px,1fr]">
+      <section class="lg:grid grid-cols-[200px,1fr]">
         <h2 class="font-thin text-2xl">
           <Link href="/community/groups/">Social</Link>
         </h2>
-        <ul class="grid gap-1 grid-cols-4">
+        <ul class="grid gap-1 grid-cols-2 md:grid-cols-4">
           {data.social.map((item, i) => (
             <GridItem title={item.title} href={item.href} imgSrc={item.imgSrc} key={i} />
           ))}
@@ -116,12 +116,14 @@ export const head: DocumentHead = {
 
 export const GridItem = component$((props: GridItemProps) => {
   return (
-    <li class="rounded-md bg-slate-800 text-white text-center overflow-hidden min-h-52">
+    <li class="rounded-md bg-slate-800 text-white text-center overflow-hidden relative h-52">
       <Link href={props.href}>
-        <div class="grid h-36 items-center p-10">
+        <div class="grid h-36 items-center p-6 md:p-10">
           <img src={props.imgSrc} alt={props.title} class="m-auto max-h-20" />
         </div>
-        <span class="block px-2 py-5 bg-slate-900">{props.title}</span>
+        <span class="block px-2 py-5 bg-slate-900 absolute bottom-0 left-0 right-0">
+          {props.title}
+        </span>
       </Link>
     </li>
   );

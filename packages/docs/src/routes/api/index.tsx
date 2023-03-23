00f33dd789d7c85c0b000ed29dacdda57fa1b143
Roman Zanettin <roman.zanettin@gmail.com>
2023-03-23 22:55:14 +0100

    🛝

--- packages/docs/src/routes/api/index.tsx
+++ packages/docs/src/routes/api/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useStore, useTask$ } from '@builder.io/qwik';
 import qwikApiData from './qwik/api.json';
 import qwikCityApiData from './qwik-city/api.json';
 import { toSnakeCase } from '../../utils/utils';
@@ -10,10 +10,12 @@ const getUniqueKinds = () => {
   }
 
   qwikApiData.members.forEach((member) => KINDS.add(toSnakeCase(member.kind)));
-}
+};
 
 export default component$(() => {
   getUniqueKinds();
+  const filters = useStore({});
+
   return (
     <>
       <h1>API Reference</h1>
@@ -21,7 +23,17 @@ export default component$(() => {
       <h2>Filter</h2>
       <div class="grid grid-cols-4 gap-2">
         {Array.from(KINDS).map((kind) => (
-          <button class="block bg-slate-200 text-sm rounded-md text-left" data-kind-label={kind.substring(0,1).toUpperCase()}>{kind}</button>
+          <button
+            key={`filter-${kind}`}
+            onClick$={() => {
+              filters[kind] = !filters[kind];
+              console.log(filters);
+            }}
+            class={`block bg-slate-200 text-sm rounded-md text-left ${filters[kind] ? 'bg-blue-200' : ''}`}
+            data-kind-label={kind.substring(0, 1).toUpperCase()}
+          >
+            {kind}
+          </button>
         ))}
       </div>
 
@@ -29,19 +41,19 @@ export default component$(() => {
       <a href="qwik">
         <h3>Qwik</h3>
       </a>
-      <ApiMemberList data={qwikApiData} />
+      <ApiMemberList id="qwik" data={qwikApiData} />
 
       <h2>Qwik City</h2>
       <a href="qwik-city">
         <h3>Qwik-City</h3>
       </a>
-      <ApiMemberList data={qwikCityApiData} />
+      <ApiMemberList id="qwik-city" data={qwikCityApiData} />
     </>
   );
 });
 
 // TODO: move into standalone cmp and adjust typings!
-export const ApiMemberList = component$(({ data }: any) => (
+export const ApiMemberList = component$(({ id, data}: any) => (
   <ul class="grid md:grid-cols-2 lg:grid-cols-3">
     {data.members.map((member) => {
       if (!member.name) {
@@ -54,7 +66,7 @@ export const ApiMemberList = component$(({ data }: any) => (
       // TODO: link must be adjusted!
       return (
         <li
-          key={`member-${member.id}`}
+          key={`${id}-member-${member.id}`}
           data-kind={kind}
           data-kind-label={kind.substring(0, 1).toUpperCase()}
           class="api-item list-none text-sm"

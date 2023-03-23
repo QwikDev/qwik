ec622ed1c46472acaeaeb9c0a0c9224c75aa5420
Roman Zanettin <roman.zanettin@gmail.com>
2023-03-23 22:04:01 +0100

    🍹

--- packages/docs/src/routes/api/index.tsx
+++ packages/docs/src/routes/api/index.tsx
@@ -1,17 +1,41 @@
 import { component$ } from '@builder.io/qwik';
 import qwikApiData from './qwik/api.json';
+import qwikCityApiData from './qwik-city/api.json';
 import { toSnakeCase } from '../../utils/utils';
 
+const KINDS = new Set();
+const getUniqueKinds = () => {
+  if (KINDS.size) {
+    return;
+  }
+
+  qwikApiData.members.forEach((member) => KINDS.add(toSnakeCase(member.kind)));
+}
+
 export default component$(() => {
+  getUniqueKinds();
   return (
     <>
       <h1>API Reference</h1>
+
+      <h2>Filter</h2>
+      <div class="grid grid-cols-4 gap-2">
+        {Array.from(KINDS).map((kind) => (
+          <button class="block bg-slate-200 text-sm rounded-md text-left" data-kind-label={kind.substring(0,1).toUpperCase()}>{kind}</button>
+        ))}
+      </div>
 
       <h2>Qwik</h2>
       <a href="qwik">
         <h3>Qwik</h3>
       </a>
       <ApiMemberList data={qwikApiData} />
+
+      <h2>Qwik City</h2>
+      <a href="qwik-city">
+        <h3>Qwik-City</h3>
+      </a>
+      <ApiMemberList data={qwikCityApiData} />
     </>
   );
 });
@@ -30,12 +54,12 @@ export const ApiMemberList = component$(({ data }: any) => (
       // TODO: link must be adjusted!
       return (
         <li
-          key="member"
+          key={`member-${member.id}`}
           data-kind={kind}
           data-kind-label={kind.substring(0, 1).toUpperCase()}
           class="api-item list-none text-sm"
         >
-          <a href={`qwik#${member.name}`}>{member.name}</a>
+          <a href={`qwik#${member.id}`}>{member.name}</a>
         </li>
       );
     })}

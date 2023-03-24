28d4984ea73f8ce1f8ffe27c5ce62367947dc453
Manu MA <manu.mtza@gmail.com>
2023-03-24 10:25:59 +0100

    fix: deprecate express adapter (#3500)

--- packages/qwik-city/adapters/express/vite/index.ts
+++ packages/qwik-city/adapters/express/vite/index.ts
@@ -3,6 +3,7 @@ import { type ServerAdapterOptions, viteAdapter } from '../../shared/vite';
 
 /**
  * @public
+ * @deprecated - Use `nodeServerAdapter` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export function expressAdapter(opts: ExpressAdapterOptions = {}): any {
   const env = process?.env;
@@ -29,22 +30,24 @@ export function expressAdapter(opts: ExpressAdapterOptions = {}): any {
 
 /**
  * @public
- * @deprecated Use `expressAdapter` exported from `@builder.io/qwik-city/adapters/express/vite` instead.
+ * @deprecated - Use `nodeServerAdapter` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export const expressAdaptor = expressAdapter;
 
 /**
  * @public
+ * @deprecated - Use `NodeServerAdapterOptions` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export interface ExpressAdapterOptions extends ServerAdapterOptions {}
 
 /**
  * @public
- * @deprecated Use `ExpressAdapterOptions` instead.
+ * @deprecated - Use `NodeServerAdapterOptions` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export type ExpressAdaptorOptions = ExpressAdapterOptions;
 
 /**
  * @public
+ * @deprecated - Use `StaticGenerateRenderOptions` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export type { StaticGenerateRenderOptions };

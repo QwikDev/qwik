eb10b4e12c751c768a516b7bd3694e0a290c1125
roman zanettin <zanettin@users.noreply.github.com>
2023-04-01 00:17:10 +0200

    docs: add edit button to example browser (#3614)

--- packages/docs/src/components/code-example/index.tsx
+++ packages/docs/src/components/code-example/index.tsx
@@ -1,6 +1,7 @@
 import { component$, useContext, useStylesScoped$ } from '@builder.io/qwik';
 import { CodeBlock } from '../code-block/code-block';
 import { useLocation } from '@builder.io/qwik-city';
+import { EditIcon } from '../svgs/edit-icon';
 import { GlobalStore } from '../../context';
 import CSS from './index.css?inline';
 
@@ -13,12 +14,13 @@ export default component$<{
   const location = useLocation();
   const state = useContext(GlobalStore);
   const browserURL = new URL(examplePath({ path: src.path }), location.url).toString();
+  const editUrl = `https://github.com/BuilderIO/qwik/edit/main/packages/docs${src.path}`;
   return (
     <div>
       <CodeBlock code={src.code} path={src.path} language="tsx" />
       {sandbox !== false && (
         <div class="browser shadow-xl">
-          <div class="bar bg-slate-200 rounded-tl-md rounded-tr-md flex flex-row justify-left px-5 py-2 gap-6">
+          <div class="bar bg-slate-200 rounded-tl-md rounded-tr-md flex flex-row justify-left px-5 py-2 gap-5">
             <ul>
               <li>
                 <span class="bg-red-600 rounded-full w-3 h-3 inline-block"></span>
@@ -35,6 +37,13 @@ export default component$<{
                 {browserURL}
               </a>
             </div>
+            <ul>
+              <li class="edit">
+                <a href={editUrl} rel="noopener" target="_blank" title="edit this snippet">
+                  <EditIcon width={20} height={20} />
+                </a>
+              </li>
+            </ul>
           </div>
           <div>
             <iframe

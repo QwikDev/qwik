4bb01e74ce79f0e63f64dc1b3c2fc1b3caecdee0
Dustin Goodman <dustin.s.goodman@gmail.com>
2023-04-02 12:36:48 -0400

    docs: fix tutorial examples to follow best practices (#3623)

--- packages/docs/src/routes/demo/getting-started/03-posting-data/index.tsx
+++ packages/docs/src/routes/demo/getting-started/03-posting-data/index.tsx
@@ -2,7 +2,7 @@
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$, Form, routeAction$, server$ } from '@builder.io/qwik-city';
 
-const useDadJoke = routeLoader$(async () => {
+export const useDadJoke = routeLoader$(async () => {
   const response = await fetch('https://icanhazdadjoke.com/', {
     headers: { Accept: 'application/json' },
   });
@@ -13,7 +13,7 @@ const useDadJoke = routeLoader$(async () => {
   };
 });
 
-const useJokeVoteAction = routeAction$((props) => {
+export const useJokeVoteAction = routeAction$((props) => {
   console.log('VOTE', props);
 });
 

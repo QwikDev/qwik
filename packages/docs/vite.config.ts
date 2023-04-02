8be37e7a0a4da0598d56f522f3db1f17519847ee
Manu MA <manu.mtza@gmail.com>
2023-04-02 20:52:18 +0200

    🐝

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -5,6 +5,7 @@ import { qwikCity } from '@builder.io/qwik-city/vite';
 import { partytownVite } from '@builder.io/partytown/utils';
 import { examplesData, playgroundData, tutorialData } from './vite.repl-apps';
 import { sourceResolver } from './vite.source-resolver';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export default defineConfig(() => {
   const routesDir = resolve('src', 'routes');
@@ -22,11 +23,36 @@ export default defineConfig(() => {
     },
     plugins: [
       qwikCity({
-        // mdx: {
-        //   remarkPlugins: [
-        //     remarkCodeSnippets()
-        //   ],
-        // }
+        mdxPlugins: {
+          rehypeSyntaxHighlight: false,
+          remarkGfm: true,
+          rehypeAutolinkHeadings: true,
+        },
+        mdx: {
+          rehypePlugins: [
+            [
+              rehypePrettyCode,
+              {
+                theme: 'dark-plus',
+                onVisitLine(node) {
+                  // Prevent lines from collapsing in `display: grid` mode, and
+                  // allow empty lines to be copy/pasted
+                  if (node.children.length === 0) {
+                    node.children = [{ type: 'text', value: ' ' }];
+                  }
+                },
+                onVisitHighlightedLine(node) {
+                  // Each line node by default has `class="line"`.
+                  node.properties.className.push('line--highlighted');
+                },
+                onVisitHighlightedWord(node) {
+                  // Each word node has no className by default.
+                  node.properties.className = ['word'];
+                },
+              },
+            ],
+          ],
+        },
       }),
       qwikVite({
         entryStrategy: {

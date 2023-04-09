ea6614bcfec4957834f52c9d167cade9c28c3242
the-r3aper7 <saisrikardumpeti@gmail.com>
2023-04-09 12:02:21 +0530

    ✨

--- starters/features/auth/src/routes/plugin@auth.ts
+++ starters/features/auth/src/routes/plugin@auth.ts
@@ -2,15 +2,13 @@ import { serverAuth$ } from '@builder.io/qwik-auth';
 import GitHub from '@auth/core/providers/github';
 import type { Provider } from '@auth/core/providers';
 
-export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(
-  () => ({
-    secret: import.meta.env.VITE_AUTH_SECRET,
-    trustHost: true,
-    providers: [
-      GitHub({
-        clientId: import.meta.env.VITE_GITHUB_ID,
-        clientSecret: import.meta.env.VITE_GITHUB_SECRET,
-      }),
-    ] as Provider[],
-  })
-);
+export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(() => ({
+  secret: import.meta.env.VITE_AUTH_SECRET,
+  trustHost: true,
+  providers: [
+    GitHub({
+      clientId: import.meta.env.VITE_GITHUB_ID,
+      clientSecret: import.meta.env.VITE_GITHUB_SECRET,
+    }),
+  ] as Provider[],
+}));

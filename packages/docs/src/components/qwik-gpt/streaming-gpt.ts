6284b48653e2884b3d3681b68b188eaf09ca6feb
Manu MA <manu.mtza@gmail.com>
2023-04-09 23:53:12 +0200

    🚀

--- packages/docs/src/components/qwik-gpt/streaming-gpt.ts
+++ packages/docs/src/components/qwik-gpt/streaming-gpt.ts
@@ -21,73 +21,45 @@ export async function* chatCompletion(apiKey: string, request: CreateChatComplet
 }
 
 async function* toIterable(data: ReadableStream<Uint8Array>) {
-  const { writable, readable } = getSSETransformer();
-  data.pipeTo(writable);
 
-  const a = readable.getReader();
+  const reader = data.getReader();
+  const encoder = new TextDecoder();
+  let currentLine = '';
   while (true) {
-    const { done, value } = await a.read();
+    const { done, value } = await reader.read();
     if (done) {
       return;
     }
-    console.log('LINE', value);
-    const message = value.trim();
-    if (message === '[DONE]') {
-      return;
+    const lines = encoder.decode(value).split('\n\n');
+    for (let i = 0; i < lines.length - 1; i++) {
+      const line = currentLine + lines[i];
+      if (line.length === 0) {
+        return;
+      } else {
+        const message = parseEvent(line).trim();
+        console.log('message', message);
+        if (message === '[DONE]') {
+          return;
+        }
+        const json = JSON.parse(message);
+        const token = json.choices?.[0]?.delta?.content;
+        if (token) {
+          yield token;
+        }
+        currentLine = '';
+      }
     }
-    const json = JSON.parse(message);
-    const token = json.choices?.[0]?.delta?.content;
-    if (token) {
-      yield token;
-    }
+    currentLine += lines[lines.length - 1];
   }
 }
 
-interface SSEvent {
-  data: string;
-  [key: string]: string;
-}
-
-const parseEvent = (message: string): SSEvent => {
+const parseEvent = (message: string): string => {
   const lines = message.split('\n');
-  const event: SSEvent = {
-    data: '',
-  };
   let data = '';
   for (const line of lines) {
     if (line.startsWith('data: ')) {
       data += line.slice(6) + '\n';
-    } else {
-      const [key, value] = line.split(':');
-      if (typeof key === 'string' && typeof value === 'string') {
-        event[key] = value.trim();
-      }
     }
   }
-  event.data = data;
-  return event;
-};
-
-
-const getSSETransformer = () => {
-  // Convert the stream into a stream of lines
-  let currentLine = '';
-  const encoder = new TextDecoder();
-  const transformer = new TransformStream<Uint8Array, string>({
-    transform(chunk, controller) {
-      const lines = encoder.decode(chunk).split('\n\n');
-      for (let i = 0; i < lines.length - 1; i++) {
-        const line = currentLine + lines[i];
-        if (line.length === 0) {
-          controller.terminate();
-          break;
-        } else {
-          controller.enqueue(parseEvent(line).data);
-          currentLine = '';
-        }
-      }
-      currentLine += lines[lines.length - 1];
-    },
-  });
-  return transformer;
+  return data;
 };

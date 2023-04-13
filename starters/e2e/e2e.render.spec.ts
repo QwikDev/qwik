45da5516750db541030a686672c60c944d79a09f
Manu MA <manu.mtza@gmail.com>
2023-04-14 01:59:51 +0200

    fix: optimizer inlined expressions (#3764)

--- starters/e2e/e2e.render.spec.ts
+++ starters/e2e/e2e.render.spec.ts
@@ -364,6 +364,16 @@ test.describe('render', () => {
         'slots',
       ]);
     });
+
+    test('issue3702', async ({ page }) => {
+      const button = page.locator('#issue-3702-button');
+      const result = page.locator('#issue-3702-result');
+      await expect(result).toHaveAttribute('data-title', 'Bye 0');
+      await button.click();
+      await expect(result).toHaveAttribute('data-title', 'Bye 1');
+      await button.click();
+      await expect(result).toHaveAttribute('data-title', 'Bye 2');
+    });
   }
 
   tests();

4de9824238432306271e504fb3837e1f8fd177c2
Sai Srikar Dumpeti <80447788+the-r3aper7@users.noreply.github.com>
2023-04-15 00:36:38 +0530

    Update plugin@auth.ts

--- starters/features/auth/src/routes/plugin@auth.ts
+++ starters/features/auth/src/routes/plugin@auth.ts
@@ -2,13 +2,13 @@ import { serverAuth$ } from '@builder.io/qwik-auth';
 import GitHub from '@auth/core/providers/github';
 import type { Provider } from '@auth/core/providers';
 
-export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(() => ({
-  secret: import.meta.env.VITE_AUTH_SECRET,
+export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(({ env }) => ({
+  secret: env.get('VITE_AUTH_SECRET'),
   trustHost: true,
   providers: [
     GitHub({
-      clientId: import.meta.env.VITE_GITHUB_ID,
-      clientSecret: import.meta.env.VITE_GITHUB_SECRET,
+      clientId: env.get('VITE_GITHUB_ID')!,
+      clientSecret: env.get('VITE_GITHUB_SECRET')!,
     }),
   ] as Provider[],
 }));

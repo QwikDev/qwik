fd5d56f64b2d4dd2258d9bfd39fd65f1f15c2fd8
Seth Horsley <isethi@me.com>
2023-04-14 16:35:36 +0200

    fix: home dir creation when running pnpm create qwik@latest (#3763)

--- packages/create-qwik/create-app.ts
+++ packages/create-qwik/create-app.ts
@@ -11,6 +11,7 @@ import {
 import { loadIntegrations } from '../qwik/src/cli/utils/integrations';
 import { logSuccessFooter } from '../qwik/src/cli/utils/log';
 import { updateApp } from '../qwik/src/cli/add/update-app';
+import os from 'node:os';
 
 export async function runCreateCli(starterId: string, outDir: string) {
   if (writeToCwd()) {
@@ -176,7 +177,12 @@ function isValidOption(value: any) {
 }
 
 export function getOutDir(outDir: string) {
-  return resolve(process.cwd(), outDir);
+  // check if the outDir start with home ~
+  if (outDir.startsWith('~/')) {
+    return resolve(os.homedir(), outDir);
+  } else {
+    return resolve(process.cwd(), outDir);
+  }
 }
 
 export function writeToCwd() {

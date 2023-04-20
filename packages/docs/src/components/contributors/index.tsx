b8ca7067539485c09e218e9e2eee70c8f53d1a25
roman zanettin <zanettin@users.noreply.github.com>
2023-04-21 00:18:01 +0200

    docs: show contributors on docs pages (#3865)

---
+++ packages/docs/src/components/contributors/index.tsx
@@ -0,0 +1,29 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './contributors.css?inline';
+import { useDocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  useStylesScoped$(styles);
+  const { frontmatter } = useDocumentHead();
+  const contributors = frontmatter.contributors || [];
+
+  if (!contributors.length) {
+    return null;
+  }
+
+  return (
+    <div class="wrapper card">
+      <h3>Contributors</h3>
+      <p>Thanks to all the contributors who have helped make this documentation better!</p>
+      <ul class="list">
+        {contributors.map((contributor: string) => (
+          <li key={`contributor-${contributor}`} class="contributor">
+            <a href={`https://github.com/${contributor}`} target="_blank" rel="noreferrer">
+              <img src={`https://github.com/${contributor}.png`} alt={contributor} class="avatar" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});

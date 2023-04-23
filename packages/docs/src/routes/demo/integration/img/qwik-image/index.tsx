c40eb37e0d68a7192d628c1f1269e0253a80991c
gioboa <giorgiob.boa@gmail.com>
2023-04-23 18:26:24 +0200

    run fmt command

--- packages/docs/src/routes/demo/integration/img/qwik-image/index.tsx
+++ packages/docs/src/routes/demo/integration/img/qwik-image/index.tsx
@@ -2,31 +2,31 @@ import { $, component$ } from '@builder.io/qwik';
 import { Image, ImageTransformerProps, useImageProvider } from 'qwik-image';
 
 export default component$(() => {
-	const imageTransformer$ = $(
-		({ src, width, height }: ImageTransformerProps): string => {
+  const imageTransformer$ = $(
+    ({ src, width, height }: ImageTransformerProps): string => {
       // Here you can set your favourite image loader CDN
-			return `https://cdn.builder.io/api/v1/${src}?height=${height}&width=${width}}&format=webp&fit=fill`;
-		}
-	);
+      return `https://cdn.builder.io/api/v1/${src}?height=${height}&width=${width}}&format=webp&fit=fill`;
+    }
+  );
 
   // Global Provider (required)
-	useImageProvider({
+  useImageProvider({
     // You can set this prop to overwrite default values [3840, 1920, 1280, 960, 640]
-    resolutions: [640], 
-		imageTransformer$,
-	});
+    resolutions: [640],
+    imageTransformer$,
+  });
 
-	return (
-		<Image
-			layout='constrained'
-			objectFit='fill'
-			width={400}
-			height={500}
-			alt='Tropical paradise'
-			placeholder='#e6e6e6'
-			src={
-				'image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fe5113e1c02db40e5bac75146fa46386f'
-			}
-		/>
-	);
+  return (
+    <Image
+      layout="constrained"
+      objectFit="fill"
+      width={400}
+      height={500}
+      alt="Tropical paradise"
+      placeholder="#e6e6e6"
+      src={
+        'image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fe5113e1c02db40e5bac75146fa46386f'
+      }
+    />
+  );
 });

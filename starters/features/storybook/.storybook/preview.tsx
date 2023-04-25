a538c0516821cd896f8a25129d73824a37909519
Manu MA <manu.mtza@gmail.com>
2023-04-25 14:52:48 +0200

    feat: storybook integration (#3917)

---
+++ starters/features/storybook/.storybook/preview.tsx
@@ -0,0 +1,17 @@
+import { Parameters } from 'storybook-framework-qwik';
+
+export const parameters: Parameters = {
+  a11y: {
+    config: {},
+    options: {
+      checks: { 'color-contrast': { options: { noScroll: true } } },
+      restoreScroll: true,
+    },
+  },
+  options: {
+    showRoots: true,
+  },
+  docs: {
+    iframeHeight: '200px',
+  },
+};

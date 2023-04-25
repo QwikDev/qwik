97573cbf4de950a72f2135f9370305baecbd5052
roman zanettin <zanettin@users.noreply.github.com>
2023-04-25 13:06:17 +0200

    starter: redesign starter (#3827)

--- starters/apps/basic/src/components/starter/header/header.tsx
+++ starters/apps/basic/src/components/starter/header/header.tsx
@@ -5,28 +5,30 @@ import styles from './header.module.css';
 export default component$(() => {
   return (
     <header class={styles.header}>
-      <div class={styles.logo}>
-        <a href="/" title="qwik">
-          <QwikLogo />
-        </a>
+      <div class={['container', styles.wrapper]}>
+        <div class={styles.logo}>
+          <a href="/" title="qwik">
+            <QwikLogo height={50} width={143} />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
+              Docs
+            </a>
+          </li>
+          <li>
+            <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
+              Examples
+            </a>
+          </li>
+          <li>
+            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
+              Tutorials
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-            Docs
-          </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-            Examples
-          </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-            Tutorials
-          </a>
-        </li>
-      </ul>
     </header>
   );
 });
--- starters/apps/basic/src/components/starter/next-steps/next-steps.tsx
+++ starters/apps/basic/src/components/starter/next-steps/next-steps.tsx
@@ -3,7 +3,7 @@ import styles from './next-steps.module.css';
 
 export const GETTING_STARTED_STEPS = [
   {
-    message: '<b>Ready</b> to make some changes?<br />Press and hold the <b>ALT</b> key',
+    message: "Press and hold the <b>ALT</b> key to active 'Click-to-Source' mode",
   },
   {
     message: 'Select the title of this page while keeping the <b>ALT</b> key pressed',
@@ -11,12 +11,12 @@ export const GETTING_STARTED_STEPS = [
   },
   {
     message:
-      '<b>Update</b> now the <code>routeLoader$</code> defined in the <code>src/routes/layout.tsx</code> file',
+      '<b>Update</b> now the <code>routeLoader$</code> defined in the <code>src/routes/(starter)/layout.tsx</code> file',
     hint: 'Instead of returning the current date, you could return any possible string.<br />The output is displayed in the footer.',
   },
   {
     message: 'Create a <b>new Route</b> called <code>/me</code>',
-    hint: 'Create a new directory called <code>me</code> in <code>src/routes</code>. Within this directory create a <code>index.tsx</code> file or copy the <code>src/routes/index.tsx</code> file. Your new route is now accessible <a href="/me" target="_blank">here</a> ✨',
+    hint: 'Create a new directory called <code>me</code> in <code>src/routes</code>. Within this directory create a <code>index.tsx</code> file or copy the <code>src/routes/(starter)/index.tsx</code> file. Your new route is now accessible <a href="/me" target="_blank">here</a> ✨',
   },
   {
     message: 'Time to have a look at <b>Forms</b>',
@@ -41,23 +41,31 @@ export default component$(() => {
   );
 
   return (
-    <>
+    <div class="container container-purple container-center">
+      <h2>
+        Time for a quick
+        <br />
+        <span class="highlight">qwik intro</span>?
+      </h2>
       <div class={styles.gettingstarted}>
-        <div dangerouslySetInnerHTML={GETTING_STARTED_STEPS[gettingStartedStep.value].message} />
+        <div
+          class={styles.intro}
+          dangerouslySetInnerHTML={GETTING_STARTED_STEPS[gettingStartedStep.value].message}
+        />
         <span
           class={styles.hint}
           dangerouslySetInnerHTML={GETTING_STARTED_STEPS[gettingStartedStep.value].hint}
         />
       </div>
       {gettingStartedStep.value + 1 < GETTING_STARTED_STEPS.length ? (
-        <button class="gray small" onClick$={() => gettingStartedStep.value++}>
+        <button class="button-dark" onClick$={() => gettingStartedStep.value++}>
           Continue with Step {gettingStartedStep.value + 2} of {GETTING_STARTED_STEPS.length}
         </button>
       ) : (
-        <button class="gray small" onClick$={() => (gettingStartedStep.value = 0)}>
+        <button class="button-dark" onClick$={() => (gettingStartedStep.value = 0)}>
           Re-Start
         </button>
       )}
-    </>
+    </div>
   );
 });

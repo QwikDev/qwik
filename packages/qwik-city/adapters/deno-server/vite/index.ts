cb90d2b69cbde3759a98e168014a92b20d16a29f
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-04-27 16:48:44 -0500

    feat: deno adapter/middleware integration (#3936)

---
+++ packages/qwik-city/adapters/deno-server/vite/index.ts
@@ -0,0 +1,50 @@
+import type { StaticGenerateRenderOptions } from '@builder.io/qwik-city/static';
+import { viteAdapter, type ServerAdapterOptions } from '../../shared/vite';
+
+/**
+ * @alpha
+ */
+export function denoServerAdapter(opts: DenoServerAdapterOptions = {}): any {
+  const env = process?.env;
+  return viteAdapter({
+    name: opts.name || 'deno-server',
+    origin: env?.ORIGIN ?? env?.URL ?? 'https://yoursitename.qwik.builder.io',
+    ssg: opts.ssg,
+    cleanStaticGenerated: true,
+
+    config() {
+      return {
+        resolve: {
+          conditions: ['webworker', 'worker'],
+        },
+        ssr: {
+          target: 'webworker',
+          noExternal: true,
+        },
+        build: {
+          ssr: true,
+          target: 'esnext',
+          rollupOptions: {
+            output: {
+              format: 'es',
+              hoistTransitiveImports: false,
+            },
+          },
+        },
+        publicDir: false,
+      };
+    },
+  });
+}
+
+/**
+ * @alpha
+ */
+export interface DenoServerAdapterOptions extends ServerAdapterOptions {
+  name?: string;
+}
+
+/**
+ * @alpha
+ */
+export type { StaticGenerateRenderOptions };

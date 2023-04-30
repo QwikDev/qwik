a6d17ee890bdc4bdda81b28c10dc10509b4059ba
Manu MA <manu.mtza@gmail.com>
2023-04-30 10:46:42 +0200

    docs: fix docs (#3982)

--- packages/docs/src/entry.cloudflare-pages.tsx
+++ packages/docs/src/entry.cloudflare-pages.tsx
@@ -2,6 +2,6 @@ import { createQwikCity } from '@builder.io/qwik-city/middleware/cloudflare-page
 import qwikCityPlan from '@qwik-city-plan';
 import render from './entry.ssr';
 
-const onRequest = createQwikCity({ render, qwikCityPlan });
+const fetch = createQwikCity({ render, qwikCityPlan });
 
-export { onRequest };
+export { fetch };

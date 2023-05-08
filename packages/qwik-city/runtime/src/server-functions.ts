1885c4b914f53dcb669d82efdf7ff6dccb6855f9
Yoav Ganbar <yoav@builder.io>
2023-05-08 11:13:27 +0300

    fix error message

--- packages/qwik-city/runtime/src/server-functions.ts
+++ packages/qwik-city/runtime/src/server-functions.ts
@@ -189,7 +189,7 @@ export const routeLoaderQrl = ((
   function loader() {
     return useContext(RouteStateContext, (state) => {
       if (!(id in state)) {
-        throw new Error(`Loader (${id}) was used in a path where the 'loader$' was not declared.
+        throw new Error(`routeLoader (${id}) was used in a path where the 'routeLoader$' was not declared.
     This is likely because the used loader was not exported in a layout.tsx or index.tsx file of the existing route.
     For more information check: https://qwik.builder.io/qwikcity/route-loader/`);
       }

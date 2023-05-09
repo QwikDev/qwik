1cb1d01e0b72ce396067d547623855a31826d618
Misael Aguayo <aguayo.misael@gmail.com>
2023-05-09 11:51:04 -0500

    docs: fix typo on ssr sample applications (#4129)

--- starters/apps/base/src/entry.ssr.tsx
+++ starters/apps/base/src/entry.ssr.tsx
@@ -1,7 +1,7 @@
 /**
  * WHAT IS THIS FILE?
  *
- * SSR entry point, in all cases the application is render outside the browser, this
+ * SSR entry point, in all cases the application is rendered outside the browser, this
  * entry point will be the common one.
  *
  * - Server (express, cloudflare...)

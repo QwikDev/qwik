e219019dea5126820f90078d9d8dfad3dde4d365
Manu MA <manu.mtza@gmail.com>
2023-05-09 12:23:32 +0100

    fix: status code can be any number (#4123)

--- packages/qwik-city/middleware/request-handler/types.ts
+++ packages/qwik-city/middleware/request-handler/types.ts
@@ -67,7 +67,8 @@ export type StatusCodes =
   | SuccessCode
   | ClientErrorCode
   | ServerErrorCode
-  | RedirectCode;
+  | RedirectCode
+  | number;
 
 export type ErrorCodes = ClientErrorCode | ServerErrorCode;
 
@@ -95,7 +96,7 @@ type SuccessCode =
   | 206 // Partial Content
   | 207 // Multi-Status
   | 208 // Already Reported
-  | 226; // IM Used
+  | 226; // IM Used;
 
 /**
  * HTTP Redirect Status Codes

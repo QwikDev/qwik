cadb1d173604a58303d384914c76898b7002c2e9
gioboa <giorgiob.boa@gmail.com>
2023-05-15 08:37:34 +0200

    add loading state

--- packages/docs/src/routes/(store)/store/index.tsx
+++ packages/docs/src/routes/(store)/store/index.tsx
@@ -1,10 +1,9 @@
-import { component$, useContext, useSignal, useStyles$ } from '@builder.io/qwik';
+import { component$, useContext, useStyles$ } from '@builder.io/qwik';
 import styles from '../store.css?inline';
-import { STORE_CONTEXT, fetchFromShopify } from '../utils';
+import { STORE_CONTEXT } from '../utils';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import type { ProductType } from '../types';
-import { addLineItemMutation } from '../mutation';
 import { StoreIcon } from './store';
+import { Product } from './product';
 
 export default component$(() => {
   useStyles$(styles);
@@ -12,7 +11,7 @@ export default component$(() => {
 
   return (
     <div class="store">
-      <StoreIcon class="mx-auto my-10"/>
+      <StoreIcon class="mx-auto my-10" />
       <article>
         <div class="purple-gradient" role="presentation" />
         <div class="blue-gradient" role="presentation" />
@@ -22,102 +21,6 @@ export default component$(() => {
           ))}
         </div>
       </article>
-    </div>
-  );
-});
-
-type Props = {
-  product: ProductType;
-};
-
-export const Product = component$<Props>(({ product }) => {
-  const appStore = useContext(STORE_CONTEXT);
-  const showProductSignal = useSignal(true);
-  const selectedVariantId = useSignal(product.variants.find((v) => v.available)?.id || '');
-  return (
-    <div class="product">
-      <h5 class="title">{product.title}</h5>
-      <div class={`info ${showProductSignal.value ? 'overflow-hidden' : 'overflow-auto'}`}>
-        {showProductSignal.value ? (
-          <img
-            class="object-contain"
-            width="300"
-            height="300"
-            src={product.image.src}
-            alt="product image"
-          />
-        ) : (
-          <div dangerouslySetInnerHTML={product.body_html} />
-        )}
-      </div>
-      <div class="py-2 px-5 flex items-center">
-        <span
-          class="font-medium hover:underline text-black"
-          onClick$={() => {
-            showProductSignal.value = !showProductSignal.value;
-          }}
-        >
-          {showProductSignal.value ? 'Description' : 'Image'}
-        </span>
-      </div>
-      <div class="pb-6 px-5">
-        <div class="flex flex-end items-center justify-between pb-4">
-          {product.variants.length > 1 ? (
-            <select
-              class="select"
-              onChange$={(event) => {
-                selectedVariantId.value = event.target.value;
-              }}
-            >
-              {product.variants.map((variant) => (
-                <option
-                  key={variant.id}
-                  selected={selectedVariantId.value === variant.id}
-                  disabled={!variant.available}
-                  value={variant.id}
-                >
-                  {variant.title}
-                </option>
-              ))}
-            </select>
-          ) : (
-            <div />
-          )}
-          <span class="text-3xl font-bold text-slate-900 py-2">
-            ${product.variants[0].price.amount}
-          </span>
-        </div>
-        <div class="flex">
-          <button
-            class="button_primary"
-            onClick$={async () => {
-              const response = await fetchFromShopify(
-                addLineItemMutation(appStore.cart.id, selectedVariantId.value)
-              );
-              const {
-                data: { checkoutLineItemsAdd },
-              } = await response.json();
-              appStore.cart = checkoutLineItemsAdd.checkout;
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="mr-2 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            Add to cart
-          </button>
-        </div>
-      </div>
     </div>
   );
 });

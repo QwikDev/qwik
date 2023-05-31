b89795f70c2f27d449e69c2396c5b1c7e751dc03
hbendev <hajnalbendeguz@gmail.com>
2023-05-31 05:58:00 +0200

    docs: Add documentation and example for QwikIntrinsicElements (#4357)
    * docs: ✏️ Add documentation and example for QwikIntrinsicElements
    
    ✅ Closes: #4148
    
    * Generate API and format code
    
    * Update example code
    
    To be an instantly working one

--- packages/qwik/src/core/render/jsx/types/jsx-qwik-elements.ts
+++ packages/qwik/src/core/render/jsx/types/jsx-qwik-elements.ts
@@ -18,6 +18,24 @@ export interface QwikIntrinsicAttributes {
 }
 
 /**
+ * The interface holds available attributes of both native DOM elements and custom Qwik elements.
+ * An example showing how to define a customizable wrapper component:
+ *
+ * ```tsx
+ * import { component$, Slot, type QwikIntrinsicElements } from "@builder.io/qwik";
+ *
+ * type WrapperProps = {
+ *   attributes?: QwikIntrinsicElements["div"];
+ * };
+ *
+ * export default component$<WrapperProps>(({ attributes }) => {
+ *   return (
+ *     <div {...attributes} class="p-2">
+ *       <Slot />
+ *     </div>
+ *   );
+ * });
+ * ```
  * @public
  */
 export interface QwikIntrinsicElements extends IntrinsicHTMLElements {

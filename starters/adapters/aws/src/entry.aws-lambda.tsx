043fafcb4dc4f7264a4874730cf76c5aebba6bbd
Leifer <leifer33@gmail.com>
2023-06-01 18:09:29 +0300

    feat(adapter): :fire: aws adapter

--- starters/adapters/aws/src/entry.aws-lambda.tsx
+++ starters/adapters/aws/src/entry.aws-lambda.tsx
@@ -1,6 +1,6 @@
 import { createQwikCity } from '@builder.io/qwik-city/middleware/node';
 import qwikCityPlan from '@qwik-city-plan';
-
+import { manifest } from '@qwik-client-manifest';
 import { fileURLToPath } from 'node:url';
 import { join } from 'node:path';
 

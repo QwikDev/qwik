ad8cc6ec8734c5bd16e392cca70a1ae2d7d2e6b4
Leifer <leifer33@gmail.com>
2023-06-07 21:48:18 +0300

    feat: :zap: implemente `entry_` in vite plugin

--- starters/adapters/aws-lambda/adapters/aws-lambda/vite.config.ts
+++ starters/adapters/aws-lambda/adapters/aws-lambda/vite.config.ts
@@ -1,25 +1,15 @@
 import { nodeServerAdapter } from '@builder.io/qwik-city/adapters/node-server/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
-import { writeFileSync } from 'node:fs';
-import { join } from 'node:path';
-
-export const entryPointWithDot = () => ({
-  name: 'rename-entry-point',
-  writeBundle() {
-    const output = join(process.cwd(), 'server', 'aws.js');
-    writeFileSync(output, 'export * from "./entry.aws-lambda.mjs";');
-  },
-});
 
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
       rollupOptions: {
-        input: ['src/entry.aws-lambda.tsx', 'src/entry.ssr.tsx', '@qwik-city-plan'],
+        input: ['src/entry_aws-lambda.tsx', 'src/entry.ssr.tsx', '@qwik-city-plan'],
       },
     },
-    plugins: [nodeServerAdapter({ name: 'express' }), entryPointWithDot()],
+    plugins: [nodeServerAdapter({ name: 'express' })],
   };
 });

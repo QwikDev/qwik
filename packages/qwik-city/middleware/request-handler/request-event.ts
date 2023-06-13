49d226bd8ea0c4df2eca1996674d11f805e67207
Miško Hevery <misko@hevery.com>
2023-06-13 22:36:02 +0300

    fix: implicitly end middleware chain on response (#4441)
    When the endpoint sends HTTP response it should implicitly end the
    middleware chain.
    
    Previous behavior: Sending HTTP response would not stop the chain.
    
    Justification: There is an inconsistency in behavior between the
    middleware and `routeLoader$`. You can think of the `routeLoader$`
    as and endpoint. If developer has their own endpoint and they return
    HTTP response, this action prevents the `routeLoader$` from running.
    This implicitly ends the middleware chain and prevents the `routeLoader$`
    fro running. This inconsistency makes it difficulty to explain the behavior.
    
    New Behavior: Everything is a middleware, including the `routeLoader$`
    and the default component. At any point in the chain returning an
    HTTP response effectively creates and endpoint and prevents any
    additional middleware from running (including the `routeLouder` or the
    default component.)
    
    There is no reason to run a middleware after an endpoint. If the
    developer wants to run middleware code after endpoint (for example
    logging) they should do that in a middleware earlier in the chain
    and wrap the remainder of the chain using the `next()` API.

--- packages/qwik-city/middleware/request-handler/request-event.ts
+++ packages/qwik-city/middleware/request-handler/request-event.ts
@@ -96,6 +96,11 @@ export function createRequestEvent(
         }
       }
     }
+    return exit();
+  };
+
+  const exit = () => {
+    routeModuleIndex = ABORT_INDEX;
     return new AbortMessage();
   };
 
@@ -133,10 +138,7 @@ export function createRequestEvent(
 
     next,
 
-    exit: () => {
-      routeModuleIndex = ABORT_INDEX;
-      return new AbortMessage();
-    },
+    exit,
 
     cacheControl: (cacheControl) => {
       check();
@@ -285,7 +287,7 @@ export function getRequestMode(requestEv: RequestEventCommon) {
   return (requestEv as RequestEventInternal)[RequestEvMode];
 }
 
-const ABORT_INDEX = 999999999;
+const ABORT_INDEX = Number.MAX_SAFE_INTEGER;
 
 const parseRequest = async (
   request: Request,

8350d56b940ece7ca90481fdc386bc56750b2ffa
Manu MA <manu.mtza@gmail.com>
2023-06-15 00:44:02 +0200

    feat: add image to starter (#4497)

--- starters/apps/basic/src/components/starter/hero/hero.tsx
+++ starters/apps/basic/src/components/starter/hero/hero.tsx
@@ -1,9 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 import styles from './hero.module.css';
+import ImgThunder from '~/media/thunder.png?jsx';
 
 export default component$(() => {
   return (
     <div class={['container', styles.hero]}>
+      <ImgThunder class={styles['hero-image']} />
       <h1>
         So <span class="highlight">fantastic</span>
         <br />

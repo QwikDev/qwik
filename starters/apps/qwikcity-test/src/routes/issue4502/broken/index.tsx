4dd8aa56de91f2601d68ea455b15eb6a3fe6a87a
Manu MA <manu.mtza@gmail.com>
2023-06-17 18:00:06 +0200

    🎭

--- starters/apps/qwikcity-test/src/routes/issue4502/broken/index.tsx
+++ starters/apps/qwikcity-test/src/routes/issue4502/broken/index.tsx
@@ -2,6 +2,5 @@ import type { RequestHandler } from '@builder.io/qwik-city';
 
 export const onRequest: RequestHandler<void> = async (onRequestArgs) => {
   const { redirect, url } = onRequestArgs;
-  console.log('redirect', url.pathname);
   throw redirect(302, `${url.pathname}route/`);
 };

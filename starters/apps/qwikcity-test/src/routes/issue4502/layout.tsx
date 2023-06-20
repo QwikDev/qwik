31af3686328f545804200910ed83e90f027c1e1f
Manu MA <manu.mtza@gmail.com>
2023-06-20 17:24:01 +0200

    😎

--- starters/apps/qwikcity-test/src/routes/issue4502/layout.tsx
+++ starters/apps/qwikcity-test/src/routes/issue4502/layout.tsx
@@ -2,7 +2,6 @@ import { Slot, component$, useSignal } from '@builder.io/qwik';
 
 export default component$(() => {
   const count = useSignal(0);
-  console.log('RENDER');
   return (
     <div>
       <button id="count" onClick$={() => count.value++}>

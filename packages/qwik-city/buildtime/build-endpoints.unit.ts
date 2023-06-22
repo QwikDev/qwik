8dc83853d21c839d453fb4ada31a6758b1e8dcce
Bendegúz Hajnal <hajnalbendeguz@gmail.com>
2023-06-22 19:18:13 +0200

    fix(qwik-city buildtime): make generated file ids unique
    by appending type to the id generated from the path - except for menus and layouts, because they
    already contain their type based on the file names
    
    fix #4504

--- packages/qwik-city/buildtime/build-endpoints.unit.ts
+++ packages/qwik-city/buildtime/build-endpoints.unit.ts
@@ -5,14 +5,14 @@ const test = testAppSuite('Build Endpoints');
 
 test('endpoint', ({ assertRoute }) => {
   const r = assertRoute('/api/data.json');
-  equal(r.id, 'CommonApiData');
+  equal(r.id, 'CommonApiDataRoute');
   equal(r.pattern, /^\/api\/data\.json\/?$/);
   equal(r.paramNames.length, 0);
 });
 
 test('endpoint w/ params', ({ assertRoute }) => {
   const r = assertRoute('/api/[org]/[user].json');
-  equal(r.id, 'CommonApiOrgUser');
+  equal(r.id, 'CommonApiOrgUserRoute');
   equal(r.pattern, /^\/api\/([^/]+?)\/([^/]+?)\.json\/?$/);
   equal(r.paramNames.length, 2);
   equal(r.paramNames[0], 'org');

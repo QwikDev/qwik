b1434d47b9c3b6d150891cc516fcc6490dc76ec2
Jordan White <code.u6ac@jordanwhite.org>
2023-06-22 10:36:40 +0000

    refined outline

--- packages/qwik-city/runtime/src/init-popstate.js
+++ packages/qwik-city/runtime/src/init-popstate.js
@@ -1,5 +1,5 @@
-// TODO!!! Finalize this file, manual minification, etc.
-export default function () {
+// TODO!!! Finalize this file, manual minification, etc.?
+export default () => {
   console.log('BEGIN: init-popstate.js');
 
   const loc = location;
@@ -8,25 +8,30 @@ export default function () {
   const history = '_qCityHistory';
 
   // TODO Shim for expanded SPA recovery pop.
-  // TODO Test conditions for checking SPA status...
+
+  // TODO Conditions for checking SPA status:
   // - history.scrollRestoration = 'manual'?
-  // - history.state._qCityScroll?
-  // - localStorage?
-  // - Hash checking?
-  //!- MINIMAL footprint. (low overhead on MPA-only, every byte counts)
+  // --- This will ALWAYS be set if a page was arrived at via SPA.
+  // --- Robust, stored in browser history state, will always be attached to history entry.
+  // --- If this is not set, your page is MPA and you cannot pop anywhere. (at least not from us)
+  // --- Tested this on Chromium, FF, WebKitGTK.
 
   // TODO Potential load strategies to consider for SPA init:
   // - "Install" the script in localStorage upon SPA upgrade. Load it. (XSS? minimal w/ checks)
   // - Build SPA init to a file under /build/, conditional add to DOM. (likely)
   // --- This one we might be able to save by removing the popstate fallback below.
   // --- State-based? (no need to worry if user clears browser)
-  //!--- Investigate edgecases around spa->anchor->pop, etc.
+  // --- Add this file to the browser's cache ahead of time when uprading to SPA.
+  // --- SPA reasonable expectation this file is already cached for history revists. (blocking import)
   // - Cookie? (might be brittle, overhead)
   // - etc...
-  //!- Will need to add configuration notes on CSP docs.
+  // ! Will need to add configuration notes on CSP docs.
+  // - Hash checking?
+  // ! MINIMAL footprint. (low overhead on MPA-only, every byte counts)
 
   if (!window[fallback]) {
     window[fallback] = () => {
+      console.log('--- popped ---');
       if (!window[history] && currentPath !== loc.pathname + loc.search) {
         loc.reload();
       }

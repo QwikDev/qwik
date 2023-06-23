54fa64a0b9b06296cde402d37aff9f9d193e23e6
Jordan White <code.u6ac@jordanwhite.org>
2023-06-23 02:09:04 +0000

    refine spa init script

--- packages/qwik-city/runtime/src/init-spa.ts
+++ packages/qwik-city/runtime/src/init-spa.ts
@@ -1,9 +1,8 @@
-// TODO!!! Finalize this file, manual minification, etc.?
+// TODO!!! Finalize this file, method of import, etc.?
 import type { ClientSPAWindow } from './qwik-city-component';
 import type { ScrollHistoryState } from './scroll-restoration';
 import type { ScrollState } from './types';
 
-// TODO!!! Determine extent of need, method of import.
 export default (
   window: ClientSPAWindow,
   location: Location,
@@ -23,32 +22,16 @@ export default (
   const debounceTimeout = '_qCityScrollDebounce';
   const scrollHistory = '_qCityScroll';
 
-  // TODO Replace this with extended script for recovering SPA.
-  // Needs:
-  // * - OnLoad:
-  // * --- Always scroll if history._qCityScroll + scrollRestoration = 'manual'.
-  // *         (might need to be inside SHIM depending how this is loaded)
-  // --- Browser refresh on hashes?
-  // * - Popstate Handler:
-  // * --- Hook into Navigate context if available.
-  // * --- Use a Link or ctx directly if possible (manifest?).
-  // * --- If no Navigate context, use the fallback reload().
-  // * - Scroll Handler:
-  // * --- Setup a scroll handler to save scrollState.
-  // * --- This is required in case we can't recover on this page. (no ctx, leaf edgecase)
-  // * --- Also if we pop to restore Navigate ctx, saving current scroll to state is too late.
-  // * --- Reuse the same `window._` that internal scroll handler uses.
-  // * - Window:
-  // * --- Define these on `window` in a way that Navigate upgrade can destroy them.
-  // - Navigation API:
-  // --- Leave a TODO for future Navigation PR to accommodate.
-  // --- Likely will not need anything except 1 `navigate` handler to enter Navigate.
-
-  // TODO Click hash anchor while on SPA w/o context?
-  // TODO Click handler to patch same-page anchor hashes maybe.
-
-  // TODO Replace this with the SHIM, if shim imports us then this MUST be 'manual'.
-  // TODO Shim will scroll. If also pop handler then load this script as deferred?
+  /**
+   * TODO Move this history.ScrollRestoration check into the shim script.
+   * TODO Move scroll also into the shim script.
+   * ? If shim has backup pop handler, this can be loaded as deferred/async?
+   * ? If deferred/async:
+   *    - Forward document.currentScript from shim.
+   *    - Forward bootstrap ctx from shim.
+   *    - Remove backup pop handler.
+   *    - Save scroll state on load. (user can scroll during delay?)
+   */
   if (history[scrollRestoration] === 'manual') {
     const checkAndScroll = (scrollState: ScrollState | undefined) => {
       if (scrollState) {
@@ -107,7 +90,7 @@ export default (
             scrollHeight: Math.max(elm.scrollHeight, elm.clientHeight),
           };
 
-          const state: ScrollHistoryState = history.state;
+          const state: ScrollHistoryState = history.state || {};
           state[scrollHistory] = scrollState;
           history.replaceState(state, '');
           // Needed for e2e debounceDetector.
@@ -116,6 +99,13 @@ export default (
       };
 
       window[scrollEnabled] = true;
+
+      /**
+       * TODO Patch same-page hash anchors?
+       * TODO Visibility change handler for improved scrollState reliability.
+       * TODO Patch history object?
+       * TODO Navigation API; check for support & simplify.
+       */
 
       setTimeout(() => {
         addEventListener('popstate', window[initPopstate]!);

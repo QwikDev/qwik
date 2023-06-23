ee88de49bf2b2918586ddadd5d1c632a452fce60
mrhoodz <hoodzt7@gmail.com>
2023-06-23 14:30:14 +0200

    header.tsx from main

--- packages/docs/src/components/header/header.tsx
+++ packages/docs/src/components/header/header.tsx
@@ -1,12 +1,5 @@
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
-import {
-  component$,
-  useStyles$,
-  useContext,
-  useVisibleTask$,
-  useSignal,
-  useTask$,
-} from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
+import { component$, useStyles$, useContext, useVisibleTask$ } from '@builder.io/qwik';
 import { DocSearch } from '../docsearch/doc-search';
 import { CloseIcon } from '../svgs/close-icon';
 import { DiscordLogo } from '../svgs/discord-logo';
@@ -15,7 +8,6 @@ import { MoreIcon } from '../svgs/more-icon';
 import { QwikLogo } from '../svgs/qwik-logo';
 import { TwitterLogo } from '../svgs/twitter-logo';
 import styles from './header.css?inline';
-import ogImage from './ogImage.css?inline';
 import { GlobalStore } from '../../context';
 import {
   colorSchemeChangeListener,
@@ -27,85 +19,11 @@ import BuilderContentComp from '../../components/builder-content';
 import { BUILDER_TOP_BAR_MODEL, BUILDER_PUBLIC_API_KEY } from '../../constants';
 
 export const Header = component$(() => {
-  // the title of the current page
-  const pageTitle = useDocumentHead().title;
-  const { url } = useLocation();
-
-  // arrayed url
-
-  // parent route
-  const arrayedUrl = url.href.split('/');
-  const parentRoute = arrayedUrl.slice(3)[0];
-
-  //turn the title into array
-  const arrayedTitle = pageTitle.split(' | ');
-
-  //check if we are on home page or level 0 or 1 route
-  let isBaseRoute = true;
-  isBaseRoute = arrayedTitle.length > 0 ? false : true;
-
-  // set the text for the ogimage
-  const biggerTitle = isBaseRoute ? undefined : arrayedTitle[0]; //.replace('#', '');
-  const smallerTitle = isBaseRoute ? undefined : arrayedTitle[1];
-
-  // check the outputs on the server
-  console.log(arrayedTitle);
-  console.log('Array length ' + arrayedTitle.length);
-  console.log('Bigger text is ' + biggerTitle + ' and smaller text ' + smallerTitle);
-  console.log(arrayedUrl);
-  console.log(parentRoute);
-  console.log('third level route is ' + arrayedUrl[2]);
-
   useStyles$(styles);
-  useStyles$(ogImage);
   const globalStore = useContext(GlobalStore);
   const pathname = useLocation().url.pathname;
-  const routeLevel = useSignal(0);
-
-  const imageUrl = useSignal('');
-  const ogImgTitle = useSignal('');
-  const ogImgSubTitle = useSignal('');
-  // const ogImgFragment = useSignal('');
-
-  //the url
-  const urlString = pathname;
-
-  //turn the url into array
-  const array = urlString.split('/').slice(1, -1).reverse();
-  //check if we are on home page or level 0/1 route
-  let isHomePage = true;
-  isHomePage = array.length > 0 ? false : true;
-
-  useTask$(() => {
-    //change the value of the title and subtitle
-    ogImgTitle.value = biggerTitle!;
-    ogImgSubTitle.value = smallerTitle!;
-
-    //decide whether or not to show subtitle
-    if (ogImgSubTitle.value == undefined || ogImgTitle == undefined) {
-      ogImgTitle.value = biggerTitle!;
-
-      routeLevel.value = 0;
-      imageUrl.value = `/logos/social-card.jpg`;
-    } else {
-      routeLevel.value = 1;
-      // check if on example a.k.a qwik-sandbox because the navigation in qwik-sandbox does not update useDocumenthead()
-      if (parentRoute == 'examples') {
-        imageUrl.value = `https://next-satori.vercel.app/api/og/?level=${
-          routeLevel.value
-        }&title=${'Examples'}&subtitle=${'Qwik Sandbox'}`;
-      } else {
-        imageUrl.value = `https://next-satori.vercel.app/api/og/?level=${routeLevel.value}&title=${ogImgTitle.value}&subtitle=${ogImgSubTitle.value}`;
-      }
-    }
-
-    // console.log(ogImgTitle.value + "aaaa "+ ogImgSubTitle.value + " bbb")
-  });
 
   useVisibleTask$(() => {
-    // console.log(useLocation().url.hash); // dont forget to remove this bruh
-    console.log(pageTitle);
-
     globalStore.theme = getColorPreference();
     return colorSchemeChangeListener((isDark) => {
       globalStore.theme = isDark ? 'dark' : 'light';
@@ -225,40 +143,6 @@ export const Header = component$(() => {
             </li>
           </ul>
         </div>
-        {/* <div
-          style={{
-            position: 'absolute',
-            left: '20px',
-            display: 'block',
-            height: '300px',
-            width: '300px',
-          }}
-        > */}
-        <div
-          style={{
-            position: 'absolute',
-            right: '-80px',
-            display: 'block',
-            height: '300px',
-            width: '600px',
-          }}
-        >
-          {' '}
-          <img
-            id="ogImage"
-            src={imageUrl.value}
-            draggable={true}
-            // style={{
-            //   position: 'absolute',
-            //   width: '300px',
-            //   height: 'auto',
-            //   top: '360px',
-            //   left: '1100px',
-            // }}
-          />
-          <img id="tweetPostShell" src={`/logos/tweetPostshell.png`} />
-        </div>
-        {/* </div> */}
       </header>
     </>
   );

b45915304fbc9dcc7846ebed4122cfa8dfc56acf
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-06-27 11:23:25 -0500

    feat: qwik add builder.io (#4627)

--- starters/features/builder.io/src/components/builder-registry.ts
+++ starters/features/builder.io/src/components/builder-registry.ts
@@ -1,20 +1,24 @@
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
-import { MyFunComponent } from "./fun/fun";
+import Counter from "./counter/counter";
 
-// You will find these components in the "custom components"
-// section of the visual editor
-// You can also turn on "components only mode" to limit
-// editing to only these components
-// https://www.builder.io/c/docs/guides/components-only-mode
+/**
+ * This array is used to integrate custom components within Builder.
+ * https://www.builder.io/c/docs/custom-components-intro
+ *
+ * These components will be found the "Custom Components"
+ * section of Builder's visual editor.
+ * You can also turn on "components only mode" to limit
+ * editing to only these components.
+ * https://www.builder.io/c/docs/guides/components-only-mode
+ */
 export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
   {
-    component: MyFunComponent,
-    name: "MyFunComponent",
+    component: Counter,
+    name: "Counter",
     inputs: [
       {
-        name: "text",
-        type: "string",
-        defaultValue: "Hello world",
+        name: "initialValue",
+        type: "number",
       },
     ],
   },

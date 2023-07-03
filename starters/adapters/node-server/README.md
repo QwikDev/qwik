f5b9b056ed724fc0087101bc8200c2d037a82b9b
Manu MA <manu.mtza@gmail.com>
2023-07-03 09:12:41 +0200

    fix: add vanilla node server (#4699)

---
+++ starters/adapters/node-server/README.md
@@ -0,0 +1,12 @@
+## Node Server
+
+This app has a minimal zero-dependencies server. Using the built-in `http.createServer` API.
+This should be faster and less overhead than Express or other frameworks.
+
+After running a full build, you can preview the build using the command:
+
+```
+npm run serve
+```
+
+Then visit [http://localhost:8080/](http://localhost:8080/)
---
+++ starters/adapters/node-server/src/entry.node-server.tsx
@@ -0,0 +1,36 @@
+/*
+ * WHAT IS THIS FILE?
+ *
+ * It's the entry point for the Express HTTP server when building for production.
+ *
+ * Learn more about Node.js server integrations here:
+ * - https://qwik.builder.io/docs/deployments/node/
+ *
+ */
+import { createQwikCity } from "@builder.io/qwik-city/middleware/node";
+import qwikCityPlan from "@qwik-city-plan";
+import render from "./entry.ssr";
+import { manifest } from "@qwik-client-manifest";
+import { createServer } from "node:http";
+
+// Allow for dynamic port
+const PORT = process.env.PORT ?? 3004;
+
+// Create the Qwik City express middleware
+const { router, notFound, staticFile } = createQwikCity({
+  render,
+  qwikCityPlan,
+  manifest,
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  staticFile(req, res, () => {
+    router(req, res, () => {
+      notFound(req, res, () => {});
+    });
+  });
+});
+
+server.listen(PORT);

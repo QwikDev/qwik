6c2858e6ebe14a9b9e7b5ad32a7e2264351a321e
Manu MA <manu.mtza@gmail.com>
2023-07-05 18:18:20 +0200

    fix: visual cms starter (#4731)

--- starters/apps/site-with-visual-cms/vite.config.ts
+++ starters/apps/site-with-visual-cms/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { builderDevTools } from "@builder.io/dev-tools/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [builderDevTools(), qwikCity(), qwikVite()],
+    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
   };
 });

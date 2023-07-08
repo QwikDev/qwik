7b5416d9130988828b32f88fe1347b80e6f0f520
gioboa <giorgiob.boa@gmail.com>
2023-07-08 22:07:22 +0200

    menu: fix styles.active + link href

--- packages/insights/src/routes/app/[publicApiKey]/layout.tsx
+++ packages/insights/src/routes/app/[publicApiKey]/layout.tsx
@@ -1,36 +1,61 @@
 import { Slot, component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { Link, useLocation } from '@builder.io/qwik-city';
 import Layout from '~/components/layout';
 import styles from './styles.module.css';
 import { EditIcon } from '~/components/icons/edit';
 
 export default component$(() => {
+  const location = useLocation();
+  const path = `/app/${location.params.publicApiKey}/`;
+  const subPath = location.url.pathname.replace(path, '');
   return (
     <Layout class={styles['no-padding-left']}>
       <div class={styles.container}>
         <aside class={styles.aside}>
           <div class={styles.menu}>
-            <Link href="./edit" class={[styles['menu-item'], ]}>
+            <Link href={path} class={[styles['menu-item'], subPath === '' ? styles.active : '']}>
+              <EditIcon />
+              <span class={styles['menu-item-label']}>Dashboard</span>
+            </Link>
+            <Link
+              href={path + 'edit/'}
+              class={[styles['menu-item'], subPath === 'edit/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Edit</span>
             </Link>
-            <Link href="./symbols" class={[styles['menu-item'], styles.active]}>
+            <Link
+              href={path + 'symbols/'}
+              class={[styles['menu-item'], subPath === 'symbols/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Symbols</span>
             </Link>
-            <Link href="./edit" class={styles['menu-item']}>
+            <Link
+              href={path + 'symbols/edge/'}
+              class={[styles['menu-item'], subPath === 'symbols/edge/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Edge</span>
             </Link>
-            <Link href="./edit" class={styles['menu-item']}>
+            <Link
+              href={path + 'symbols/bundles/'}
+              class={[styles['menu-item'], subPath === 'symbols/bundles/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Bundles</span>
             </Link>
-            <Link href="./edit" class={styles['menu-item']}>
+            <Link
+              href={path + 'symbols/slow/'}
+              class={[styles['menu-item'], subPath === 'symbols/slow/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Slow Symbols</span>
             </Link>
-            <Link href="./errors" class={styles['menu-item']}>
+            <Link
+              href={path + 'errors/'}
+              class={[styles['menu-item'], subPath === 'errors/' ? styles.active : '']}
+            >
               <EditIcon />
               <span class={styles['menu-item-label']}>Errors</span>
             </Link>

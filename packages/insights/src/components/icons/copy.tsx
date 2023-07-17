dde1890d062ec1172bcfc01de17caaaa6febfcdd
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2023-07-17 23:10:16 +0200

    feat(insights): fix create app flow (#4804)
    * implement create app route
    
    * add copy publicApiKey button
    
    * fix CopyIcon cmp

--- packages/insights/src/components/icons/copy.tsx
+++ packages/insights/src/components/icons/copy.tsx
@@ -1,27 +1,35 @@
-import { component$, useSignal, type PropFunction } from '@builder.io/qwik';
+import { component$, useSignal, type PropFunction, useStylesScoped$ } from '@builder.io/qwik';
 
 interface CopyIconProps {
-  class: string;
+  class?: string;
   onClick$: PropFunction<() => void>;
 }
 
 export const CopyIcon = component$<CopyIconProps>(({ onClick$, ...props }) => {
+  useStylesScoped$(`
+    .wrapper {
+      display: flex;
+    }
+  `);
   const copiedSig = useSignal(false);
 
   return (
-    <>
+    <span
+      class="wrapper"
+      preventdefault:click
+      onClick$={() => {
+        onClick$();
+        copiedSig.value = true;
+        setTimeout(() => (copiedSig.value = false), 2000);
+      }}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
+        width="1rem"
+        height="1rem"
         viewBox="0 0 24 24"
         aria-hidden="true"
         preventdefault:click
-        onClick$={() => {
-          onClick$();
-          copiedSig.value = true;
-          setTimeout(() => (copiedSig.value = false), 2000);
-        }}
         {...props}
       >
         <path
@@ -29,7 +37,7 @@ export const CopyIcon = component$<CopyIconProps>(({ onClick$, ...props }) => {
           d="M19 21H8V7h11m0-2H8a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2m-3-4H4a2 2 0 0 0-2 2v14h2V3h12V1Z"
         ></path>
       </svg>
-      {copiedSig.value ? 'Copied' : 'Copy'}
-    </>
+      &nbsp;{copiedSig.value ? 'Copied' : 'Copy'}
+    </span>
   );
 });
--- packages/insights/src/routes/app/add/index.tsx
+++ packages/insights/src/routes/app/add/index.tsx
@@ -1,10 +1,62 @@
+import { component$, useSignal } from '@builder.io/qwik';
+import { formAction$, useForm, zodForm$ } from '@modular-forms/qwik';
 import Container from '~/components/container';
+import { DiskIcon } from '~/components/icons/disk';
 import Layout from '~/components/layout';
-import { component$ } from '@builder.io/qwik';
+import { applicationTable, getDB } from '~/db';
+import { appUrl } from '~/routes.config';
+import { ApplicationForm } from '../[publicApiKey]/app.form';
+import styles from './styles.module.css';
 
-export default component$(() => (
-  <Layout mode="bright">
-    <Container position="center" width="small"></Container>
-    <h1>add app</h1>
-  </Layout>
-));
+export const useFormAction = formAction$<ApplicationForm>(
+  async ({ name, description }, { redirect }) => {
+    const db = getDB();
+    const publicApiKey = Math.round(Math.random() * Number.MAX_SAFE_INTEGER).toString(36);
+    await db
+      .insert(applicationTable)
+      .values({
+        name,
+        description,
+        publicApiKey,
+      })
+      .run();
+    redirect(302, appUrl(`/app/[publicApiKey]/`, { publicApiKey }));
+  },
+  zodForm$(ApplicationForm)
+);
+
+export default component$(() => {
+  const [, { Form, Field }] = useForm<ApplicationForm>({
+    loader: useSignal({ name: '', description: '' }),
+    action: useFormAction(),
+    validate: zodForm$(ApplicationForm),
+  });
+  return (
+    <Layout mode="bright">
+      <Container position="center" width="small"></Container>
+      <div class={styles['add-app-wrapper']}>
+        <h1 class="h3">Create Application</h1>
+        <Form>
+          <div>
+            <label>Name</label>
+            <Field name="name">
+              {(field, props) => <input {...props} type="text" value={field.value} />}
+            </Field>
+          </div>
+          <div>
+            <label>Description</label>
+            <Field name="description">
+              {(field, props) => <input {...props} type="text" value={field.value} />}
+            </Field>
+          </div>
+          <div style={{ 'margin-top': 'calc(var(--form-element-margin-bottom) * 2)' }}>
+            <button type="submit" class="button">
+              <DiskIcon />
+              Create
+            </button>
+          </div>
+        </Form>
+      </div>
+    </Layout>
+  );
+});

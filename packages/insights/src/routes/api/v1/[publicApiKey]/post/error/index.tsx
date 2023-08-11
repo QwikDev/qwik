931fb390367abcb80e8cb1f7540cbbdf278711e1
Miško Hevery <misko@hevery.com>
2023-08-11 16:03:13 -0700

    chore(insights): update to latest drizzle (#4929)

--- packages/insights/src/routes/api/v1/[publicApiKey]/post/error/index.tsx
+++ packages/insights/src/routes/api/v1/[publicApiKey]/post/error/index.tsx
@@ -5,7 +5,11 @@ import { InsightsError } from '@builder.io/qwik-labs';
 export const onPost: RequestHandler = async ({ exit, json, request, params }) => {
   // console.log('API: POST: symbol');
   const publicApiKey = params.publicApiKey;
-  const payload = InsightsError.parse(await request.json());
+  const payloadRaw = InsightsError.parse(await request.json());
+  const payload = {
+    ...payloadRaw,
+    timestamp: new Date(payloadRaw.timestamp),
+  };
   exit();
   json(200, { code: 200, message: 'OK' });
   const db = getDB();

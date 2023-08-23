623df35a539b49313eb52bc9676a4274be647c62
gioboa <giorgiob.boa@gmail.com>
2023-08-23 20:16:16 +0200

    create qwikInsights vite plugin

--- packages/qwik-labs/src-vite/insights/index.ts
+++ packages/qwik-labs/src-vite/insights/index.ts
@@ -1,21 +1,52 @@
 import { type QwikVitePluginOptions } from '@builder.io/qwik/optimizer';
+import { existsSync } from 'fs';
+import { readFile, unlink, writeFile } from 'fs/promises';
+import { PluginOption } from 'vite';
 
-export async function insightsEntryStrategy({
-  publicApiKey,
-}: {
+const INSIGHTS_Q_MANIFEST_FILENAME = './insights-q-manifest.json';
+
+const logWarn = (message?: any) => {
+  console.warn('\x1b[33m%s\x1b[0m', `\n\nQWIK WARN: ${message}\n`);
+};
+
+export async function qwikInsights(qwikInsightsOpts: {
   publicApiKey: string;
-}): Promise<QwikVitePluginOptions['entryStrategy']> {
-  const request = await fetch(`https://qwik-insights.builder.io/api/v1/${publicApiKey}/bundles/`);
-  try {
-    const bundles = await request.json();
-    return {
-      type: 'smart',
-      manual: bundles,
-    } as any;
-  } catch (e) {
-    console.error(e);
-    return {
-      type: 'smart',
-    };
-  }
+  baseUrl?: string;
+}): Promise<PluginOption> {
+  const { publicApiKey, baseUrl = 'https://qwik-insights.builder.io' } = qwikInsightsOpts;
+  let isProd = false;
+  const vitePlugin: PluginOption = {
+    name: 'vite-plugin-qwik-insights',
+    enforce: 'pre',
+    async config(viteConfig) {
+      isProd = viteConfig.mode !== 'ssr';
+      if (isProd) {
+        const qManifest: QwikVitePluginOptions['entryStrategy'] = { type: 'smart' };
+        try {
+          const response = await fetch(`${baseUrl}/api/v1/${publicApiKey}/bundles/`);
+          const bundles = await response.json();
+          qManifest.manual = bundles;
+        } catch (e) {
+          logWarn('fail to fetch manifest from Insights DB');
+        }
+        await writeFile(INSIGHTS_Q_MANIFEST_FILENAME, JSON.stringify(qManifest));
+      }
+    },
+    closeBundle: async () => {
+      if (isProd && existsSync(INSIGHTS_Q_MANIFEST_FILENAME)) {
+        const qManifest = await readFile(INSIGHTS_Q_MANIFEST_FILENAME, 'utf-8');
+        await unlink(INSIGHTS_Q_MANIFEST_FILENAME);
+
+        try {
+          await fetch(`${baseUrl}/api/v1/${publicApiKey}/post/manifest`, {
+            method: 'post',
+            body: qManifest,
+          });
+        } catch (e) {
+          logWarn('fail to post manifest to Insights DB');
+        }
+      }
+    },
+  };
+  return vitePlugin;
 }

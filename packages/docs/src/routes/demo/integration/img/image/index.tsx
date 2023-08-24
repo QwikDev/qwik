5c9c4ee6a8c0ddfc99cfb43f9a55cde4ef43a0da
Fabio Biondi <info@fabiobiondi.com>
2023-08-24 16:58:51 +0200

    docs: add responsive image recipe

---
+++ packages/docs/src/routes/demo/integration/img/image/index.tsx
@@ -0,0 +1,15 @@
+import { component$ } from '@builder.io/qwik';
+import Image from '/docs/integrations/image-optimization/emote.png';
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Image Example</h1>
+      <div class="image-wrapper" >
+        <Image />
+      </div>
+    </>
+  );
+});
+
+efe

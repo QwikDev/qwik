4b2da5d5520be921650a42b36f47253eb243ec75
Wout Mertens <Wout.Mertens@gmail.com>
2023-09-01 22:47:47 +0200

    fix(starters): don't bundle dependencies (#2008)
    If they are dependencies or peer dependencies, they should not be in the resulting library bundle.
    
    Co-authored-by: Roman Zanettin <roman.zanettin@gmail.com>

--- starters/apps/library/vite.config.ts
+++ starters/apps/library/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const { dependencies = {}, peerDependencies = {} } = pkg as any;
+const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
+const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 
 export default defineConfig(() => {
   return {
@@ -10,6 +15,14 @@ export default defineConfig(() => {
         entry: "./src/index.ts",
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+      },
+      rollupOptions: {
+        // externalize deps that shouldn't be bundled into the library
+        external: [
+          /^node:.*/,
+          ...excludeAll(dependencies),
+          ...excludeAll(peerDependencies),
+        ],
       },
     },
     plugins: [qwikVite(), tsconfigPaths()],

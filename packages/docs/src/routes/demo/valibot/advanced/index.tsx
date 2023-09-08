59ec4412ed9d64ef9b9de19944688bcb12ba4f16
Brandon Pittman <brandon@blp.is>
2023-09-08 16:11:59 +0900

    Remove optional() call
    Because searchParams will never be `undefined`

--- packages/docs/src/routes/demo/valibot/advanced/index.tsx
+++ packages/docs/src/routes/demo/valibot/advanced/index.tsx
@@ -1,12 +1,10 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { routeAction$, valibot$, Form } from '@builder.io/qwik-city';
 import styles from './styles.css?inline';
-import { string, object, optional, minLength } from 'valibot';
+import { string, object, minLength } from 'valibot';
 
 export const useAddUser = routeAction$(
   async (user) => {
-    // The "user" is still strongly typed, but firstname
-    // is now optional: { firstName?: string | undefined, lastName: string }
     return {
       success: true,
       firstName: user.firstName,
@@ -14,11 +12,10 @@ export const useAddUser = routeAction$(
   },
   // Valibot schema is used to validate that the FormData includes "firstName" and "lastName"
   valibot$((ev) => {
-    // lastName is optional the query parameter "lastname=optional" is present
     const firstName = string([minLength(1)]);
     const lastName =
       ev.url.searchParams.get('lastname') === 'optional'
-        ? optional(string())
+        ? string()
         : string([minLength(1)]);
 
     return object({

48438c2d51adbf51faa7396cefabc169c5f081fc
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-09-12 11:56:26 +0200

    feat: add new options in leaflet map integration

--- starters/features/leaflet-map/src/components/leaflet-map/index.tsx
+++ starters/features/leaflet-map/src/components/leaflet-map/index.tsx
@@ -1,60 +1,62 @@
 import {
-    component$,
-    noSerialize,
-    useSignal,
-    useStyles$,
-    useVisibleTask$,
-  } from '@builder.io/qwik';
-  import { Map} from 'leaflet';
-  
-  export const LeafletMap = component$(({ location }: any) => {
-    useStyles$(`
-      #map {
-          width: 100%;
-          height: 350px;
-        }
-      `);
-  
-    const mapContainer$ = useSignal<Map>();
-  
-    useVisibleTask$(async ({ track }) => {
-      track(location);
+  component$,
+  noSerialize,
+  useSignal,
+  useStyles$,
+  useVisibleTask$,
+} from '@builder.io/qwik';
+import { Map } from 'leaflet';
+import { MapProps } from './../../models/map';
 
-      const { tileLayer, marker } = await import ('leaflet');
+export const LeafletMap = component$<MapProps>(({ location }: MapProps) => {
+  // Modify with your preferences. By default take all screen
+  useStyles$(`
+    #map {
+      width: 100%;
+      height: 100vh;
+    }
+  `);
 
-  
-      const { getBoundaryBox } = await import('../../helpers/boundary-box');
-  
-      if (location && window) {
-        if (mapContainer$.value) {
-          mapContainer$.value.remove();
-        }
-  
-        const { value: locationData } = location;
-  
-        const centerPosition: [number, number] = locationData.point as [
-          number,
-          number
-        ];
-  
-        const map: any = new Map('map').setView(
-          centerPosition,
-          locationData.zoom || 14
-        );
-  
-        tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution:
-            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        }).addTo(map);
-  
-        // Assign select boundary box to use in OSM API if you want
-        location.boundaryBox = getBoundaryBox(map);
-  
-        locationData.marker && marker(centerPosition).addTo(map);
-  
-        mapContainer$.value = noSerialize(map);
+  const mapContainer$ = useSignal<Map>();
+
+  useVisibleTask$(async ({ track }) => {
+    track(location);
+
+    const { tileLayer, marker, bindPopup } = await import('leaflet');
+
+    const { getBoundaryBox } = await import('../../helpers/boundary-box');
+
+    if (location && window) {
+      if (mapContainer$.value) {
+        mapContainer$.value.remove();
       }
-    });
-    return <div id='map'></div>;
+
+      const { value: locationData } = location;
+
+      const centerPosition: [number, number] = locationData.point as [
+        number,
+        number
+      ];
+
+      const map: any = new Map('map').setView(
+        centerPosition,
+        locationData.zoom || 14
+      );
+
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      // Assign select boundary box to use in OSM API if you want
+      location.boundaryBox = getBoundaryBox(map);
+
+      locationData.marker &&
+        marker(centerPosition).bindPopup(`Soraluze (Gipuzkoa) :)`).addTo(map);
+
+      mapContainer$.value = noSerialize(map);
+    }
   });
+  return <div id='map'></div>;
+});

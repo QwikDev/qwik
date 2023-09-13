ac1f33348976a5b85942b7e8836ec1a1ba1b3d0d
Anartz Mugika Ledo <mugan86@gmail.com>
2023-09-13 19:28:44 +0200

    Update starters/features/leaflet-map/src/components/leaflet-map/index.tsx
    Co-authored-by: Giorgio Boa <35845425+gioboa@users.noreply.github.com>

--- starters/features/leaflet-map/src/components/leaflet-map/index.tsx
+++ starters/features/leaflet-map/src/components/leaflet-map/index.tsx
@@ -26,37 +26,35 @@ export const LeafletMap = component$<MapProps>(({ location }: MapProps) => {
 
     const { getBoundaryBox } = await import("../../helpers/boundary-box");
 
-    if (location && window) {
-      if (mapContainer$.value) {
-        mapContainer$.value.remove();
-      }
-
-      const { value: locationData } = location;
-
-      const centerPosition: [number, number] = locationData.point as [
-        number,
-        number,
-      ];
-
-      const map: any = new Map("map").setView(
-        centerPosition,
-        locationData.zoom || 14,
-      );
-
-      tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
-
-      // Assign select boundary box to use in OSM API if you want
-      locationData.boundaryBox = getBoundaryBox(map);
-
-      locationData.marker &&
-        marker(centerPosition).bindPopup(`Soraluze (Gipuzkoa) :)`).addTo(map);
-
-      mapContainer$.value = noSerialize(map);
+    if (mapContainer$.value) {
+      mapContainer$.value.remove();
     }
+
+    const { value: locationData } = location;
+
+    const centerPosition: [number, number] = locationData.point as [
+      number,
+      number,
+    ];
+
+    const map: any = new Map("map").setView(
+      centerPosition,
+      locationData.zoom || 14,
+    );
+
+    tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    // Assign select boundary box to use in OSM API if you want
+    locationData.boundaryBox = getBoundaryBox(map);
+
+    locationData.marker &&
+      marker(centerPosition).bindPopup(`Soraluze (Gipuzkoa) :)`).addTo(map);
+
+    mapContainer$.value = noSerialize(map);
   });
   return <div id="map"></div>;
 });

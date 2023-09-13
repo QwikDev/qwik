2658ca05d9a2b9a2276d9f344180492752281365
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-09-13 07:42:10 +0200

    fix: apply lint and prettier fix

--- starters/features/leaflet-map/src/helpers/boundary-box.tsx
+++ starters/features/leaflet-map/src/helpers/boundary-box.tsx
@@ -1,11 +1,6 @@
 import type { Map } from "leaflet";
-export const getBoundaryBox = (map: Map, log: boolean = false) => {
+export const getBoundaryBox = (map: Map) => {
   const northEast = map.getBounds().getNorthEast();
   const southWest = map.getBounds().getSouthWest();
-
-  log &&
-    console.log(
-      `${southWest.lat},${southWest.lng},${northEast.lat},${northEast.lng}`,
-    );
   return `${southWest.lat},${southWest.lng},${northEast.lat},${northEast.lng}`;
 };
--- starters/features/leaflet-map/src/models/map.ts
+++ starters/features/leaflet-map/src/models/map.ts
@@ -1,7 +1,7 @@
 import { LocationsProps } from "./location";
 import { type Signal } from "@builder.io/qwik";
 export interface MapProps {
-    // default options
-    location: Signal<LocationsProps>;
-    // add other options to customization map
+  // default options
+  location: Signal<LocationsProps>;
+  // add other options to customization map
 }

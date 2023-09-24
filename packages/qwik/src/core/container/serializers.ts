879736b611971f4ea0fd984b027e29431ecdd393
Varixo <78650133+Varixo@users.noreply.github.com>
2023-09-25 01:03:15 +0200

    fix(qwik): fix for serialization of an empty set or map (#5209)
    fix #5207, fix #4786

--- packages/qwik/src/core/container/serializers.ts
+++ packages/qwik/src/core/container/serializers.ts
@@ -473,7 +473,8 @@ const SetSerializer = /*#__PURE__*/ serializer<Set<any>>({
     const data = (set as any)[DATA];
     (set as any)[DATA] = undefined;
     assertString(data, 'SetSerializer should be defined');
-    for (const id of data.split(' ')) {
+    const items = data.length === 0 ? [] : data.split(' ');
+    for (const id of items) {
       set.add(getObject(id));
     }
   },
@@ -504,7 +505,7 @@ const MapSerializer = /*#__PURE__*/ serializer<Map<any, any>>({
     const data = (set as any)[DATA];
     (set as any)[DATA] = undefined;
     assertString(data, 'SetSerializer should be defined');
-    const items = data.split(' ');
+    const items = data.length === 0 ? [] : data.split(' ');
     assertTrue(items.length % 2 === 0, 'MapSerializer should have even number of items');
     for (let i = 0; i < items.length; i += 2) {
       set.set(getObject(items[i]), getObject(items[i + 1]));

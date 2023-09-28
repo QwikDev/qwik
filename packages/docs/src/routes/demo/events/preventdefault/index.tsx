57961ac847d81d7ce45be05390f8a12f9f7c6eb0
Miško Hevery <misko@hevery.com>
2023-09-28 13:30:02 -0400

    docs: fix 404 links (#5236)

--- packages/docs/src/routes/demo/events/preventdefault/index.tsx
+++ packages/docs/src/routes/demo/events/preventdefault/index.tsx
@@ -3,14 +3,14 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <a
-      href="/about"
+      href="/docs"
       preventdefault:click // This will prevent the default behavior of the "click" event.
       onClick$={() => {
         // event.PreventDefault() will not work here, because handler is dispatched asynchronously.
         alert('Do something else to simulate navigation...');
       }}
     >
-      Go to about page
+      Go to docs page
     </a>
   );
 });

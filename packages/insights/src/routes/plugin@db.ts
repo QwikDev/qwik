d8a66eae816cd22b35cdd026aafb57f3954e251b
Miško Hevery <misko@hevery.com>
2023-11-07 12:28:04 +0100

    DEBUG

--- packages/insights/src/routes/plugin@db.ts
+++ packages/insights/src/routes/plugin@db.ts
@@ -3,10 +3,11 @@ import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
 import { type AppDatabase, initializeDbIfNeeded } from '~/db';
 
-export const onRequest: RequestHandler = async ({ env }) => {
-  const url = env.get('PRIVATE_LIBSQL_DB_URL')!;
+export const onRequest: RequestHandler = async ({ env, url }) => {
+  console.log('DEBUG CITY:', url.toString(), url.pathname);
+  const dbUrl = env.get('PRIVATE_LIBSQL_DB_URL')!;
   const authToken = env.get('PRIVATE_LIBSQL_DB_API_TOKEN')!;
-  await initializeDbIfNeeded(initLibSql(url, authToken));
+  await initializeDbIfNeeded(initLibSql(dbUrl, authToken));
 };
 
 const LOG_QUERY: boolean = false;

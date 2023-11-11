e82771cb223078015e944b57b0dee8ad58a7c3a0
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2023-11-11 15:44:31 +0100

    Fix cache max-age vite.config for dev mode

--- starters/apps/base/vite.config.ts
+++ starters/apps/base/vite.config.ts
@@ -6,6 +6,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    dev: {
+      headers: {
+        "Cache-Control": "public, max-age=0",
+      },
+    },
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",

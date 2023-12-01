9b0d76a317ba73ab3c849cdb0485073ff7fe205d
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-12-01 09:28:20 +0200

    fix(qwik): handle cypress dev server path

--- packages/qwik/src/optimizer/src/plugins/vite-server.ts
+++ packages/qwik/src/optimizer/src/plugins/vite-server.ts
@@ -282,11 +282,12 @@ function invalidPreviewMessage(middlewares: Connect.Server, msg: string) {
   });
 }
 
+const CYPRESS_DEV_SERVER_PATH = '/__cypress/src';
 const FS_PREFIX = `/@fs/`;
 const VALID_ID_PREFIX = `/@id/`;
 const VITE_PUBLIC_PATH = `/@vite/`;
 const internalPrefixes = [FS_PREFIX, VALID_ID_PREFIX, VITE_PUBLIC_PATH];
-const InternalPrefixRE = new RegExp(`^(?:${internalPrefixes.join('|')})`);
+const InternalPrefixRE = new RegExp(`^(${CYPRESS_DEV_SERVER_PATH})?(?:${internalPrefixes.join('|')})`);
 
 const shouldSsrRender = (req: IncomingMessage, url: URL) => {
   const pathname = url.pathname;

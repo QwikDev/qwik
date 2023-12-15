db03cd9d80dc43b47cbaf01bfc2c9af862fa465e
Wout Mertens <Wout.Mertens@gmail.com>
2023-12-15 19:36:02 +0100

    chore: add Nix build flake environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, rust-overlay, nixpkgs }:
+    let
+      b = builtins;
+      devShell = system: _pkgs:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              bashInteractive
+              gitMinimal
+
+              nodejs_20
+
+              # Qwik optimizer deps
+              clippy
+              wasm-pack
+              # Provides rustc and cargo
+              ((rust-bin.fromRustupToolchainFile
+                ./rust-toolchain).override {
+                targets = [ "wasm32-unknown-unknown" ];
+              })
+            ];
+          };
+        };
+    in
+    {
+      devShells = b.mapAttrs (devShell) nixpkgs.legacyPackages;
+    };
+}

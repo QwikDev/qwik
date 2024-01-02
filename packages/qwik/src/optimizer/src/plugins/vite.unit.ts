f1127a9d488cd97080cb54657886c7075eb8b050
Miško Hevery <misko@hevery.com>
2024-01-02 12:14:38 -0800

    WIP: fix broken e2e tests

--- packages/qwik/src/optimizer/src/plugins/vite.unit.ts
+++ packages/qwik/src/optimizer/src/plugins/vite.unit.ts
@@ -22,12 +22,7 @@ function mockOptimizerOptions(): OptimizerOptions {
 }
 
 const includeDeps = undefined;
-const noExternal = [
-  '@builder.io/qwik',
-  '@builder.io/qwik/server',
-  '@builder.io/qwik/build',
-  '@builder.io/qwik-city',
-];
+const noExternal = ['@builder.io/qwik', '@builder.io/qwik/server', '@builder.io/qwik/build'];
 
 const excludeDeps = [
   '@vite/client',
@@ -40,7 +35,6 @@ const excludeDeps = [
   '@builder.io/qwik/jsx-dev-runtime',
   '@builder.io/qwik/build',
   '@qwik-client-manifest',
-  '@builder.io/qwik-city',
 ];
 
 test('command: serve, mode: development', async () => {

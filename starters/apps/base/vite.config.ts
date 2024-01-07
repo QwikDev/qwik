aca079ea90197fa861f8b7eabd04788f58a01c35
Riajul Islam <kmriajulislami@gmail.com>
2024-01-07 17:04:36 +0600

    fix(starter): add vite plugin type (#5670)
    * (starter): Better UX when editing vite.config.ts
    
    Added proper type for the return type of the vite config closure. It helps when customizing the config.
    
    * fix(starter): base vite.config.ts use `server` field instead of `dev` (nonexistent)

--- starters/apps/base/vite.config.ts
+++ starters/apps/base/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
+export default defineConfig((): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    dev: {
+    server: {
       headers: {
         "Cache-Control": "public, max-age=0",
       },

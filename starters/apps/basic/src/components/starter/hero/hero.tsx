32d6c983c241b1ae8e3f747e5fba8540dc05dfa4
François VIDIT <tidiview@users.noreply.github.com>
2024-01-09 08:01:46 +0100

    refactor(starter): add alt attribute in order to improve accessibility (#5674)

--- starters/apps/basic/src/components/starter/hero/hero.tsx
+++ starters/apps/basic/src/components/starter/hero/hero.tsx
@@ -5,7 +5,7 @@ import ImgThunder from "../../../media/thunder.png?jsx";
 export default component$(() => {
   return (
     <div class={["container", styles.hero]}>
-      <ImgThunder class={styles["hero-image"]} />
+      <ImgThunder class={styles["hero-image"]} alt="Image thunder" />
       <h1>
         So <span class="highlight">fantastic</span>
         <br />

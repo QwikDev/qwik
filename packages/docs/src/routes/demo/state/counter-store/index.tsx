7e256a974b32d04cdaade873c54592b3e3158f27
Leonardo Montini <lion.48m@gmail.com>
2024-01-15 21:12:08 +0100

    docs: emphasize on useStore and onInput$ (#5719)

--- packages/docs/src/routes/demo/state/counter-store/index.tsx
+++ packages/docs/src/routes/demo/state/counter-store/index.tsx
@@ -1,12 +1,16 @@
 import { component$, useStore } from '@builder.io/qwik';
 
 export default component$(() => {
-  const state = useStore({ count: 0 });
+  const state = useStore({ count: 0, name: 'Qwik' });
 
   return (
     <>
       <button onClick$={() => state.count++}>Increment</button>
       <p>Count: {state.count}</p>
+      <input
+        value={state.name}
+        onInput$={(_, el) => (state.name = el.value)}
+      />
     </>
   );
 });

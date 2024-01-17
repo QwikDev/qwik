4e31892520d972d07e39d7f73c296e5e2b673a98
Wout Mertens <Wout.Mertens@gmail.com>
2024-01-17 17:14:23 +0100

    fix(build): qwik binary path in package.json

--- scripts/package-json.ts
+++ scripts/package-json.ts
@@ -18,7 +18,7 @@ export async function generatePackageJson(config: BuildConfig) {
     main: './core.mjs',
     types: './core.d.ts',
     bin: {
-      qwik: './qwik.cjs',
+      qwik: './qwik-cli.cjs',
     },
     type: 'module',
     peerDependencies: {

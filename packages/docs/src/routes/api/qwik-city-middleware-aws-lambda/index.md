bb269bc7cff426b18d2f82dda2f2dd43be6c9d36
leifermendez <leifer33@gmail.com>
2024-01-18 16:06:22 +0200

    fix: :fire: aws adaptar as qwik citiy middleware

--- packages/docs/src/routes/api/qwik-city-middleware-aws-lambda/index.md
+++ packages/docs/src/routes/api/qwik-city-middleware-aws-lambda/index.md
@@ -7,16 +7,34 @@ title: \@builder.io/qwik-city/middleware/aws-lambda API Reference
 ## createQwikCity
 
 ```typescript
-export declare function createQwikCity(opts: any): serverless.Handler;
+export declare function createQwikCity(opts: AwsOpt): {
+  fixPath: (pathT: string) => string;
+  router: (
+    req: import("http").IncomingMessage | import("http2").Http2ServerRequest,
+    res: import("http").ServerResponse<import("http").IncomingMessage>,
+    next: import("@builder.io/qwik-city/middleware/node").NodeRequestNextFunction,
+  ) => Promise<void>;
+  staticFile: (
+    req: import("http").IncomingMessage | import("http2").Http2ServerRequest,
+    res: import("http").ServerResponse<import("http").IncomingMessage>,
+    next: (e?: any) => void,
+  ) => Promise<void>;
+  notFound: (
+    req: import("http").IncomingMessage | import("http2").Http2ServerRequest,
+    res: import("http").ServerResponse<import("http").IncomingMessage>,
+    next: (e: any) => void,
+  ) => Promise<void>;
+  handle: (req: any, res: any) => void;
+};
 ```
 
-| Parameter | Type | Description |
-| --------- | ---- | ----------- |
-| opts      | any  |             |
+| Parameter | Type   | Description |
+| --------- | ------ | ----------- |
+| opts      | AwsOpt |             |
 
 **Returns:**
 
-serverless.Handler
+{ fixPath: (pathT: string) =&gt; string; router: (req: import("http").IncomingMessage \| import("http2").Http2ServerRequest, res: import("http").ServerResponse&lt;import("http").IncomingMessage&gt;, next: import("@builder.io/qwik-city/middleware/node").NodeRequestNextFunction) =&gt; Promise&lt;void&gt;; staticFile: (req: import("http").IncomingMessage \| import("http2").Http2ServerRequest, res: import("http").ServerResponse&lt;import("http").IncomingMessage&gt;, next: (e?: any) =&gt; void) =&gt; Promise&lt;void&gt;; notFound: (req: import("http").IncomingMessage \| import("http2").Http2ServerRequest, res: import("http").ServerResponse&lt;import("http").IncomingMessage&gt;, next: (e: any) =&gt; void) =&gt; Promise&lt;void&gt;; handle: (req: any, res: any) =&gt; void; }
 
 [Edit this section](https://github.com/BuilderIO/qwik/tree/main/packages/qwik-city/middleware/aws-lambda/index.ts)
 
--- packages/qwik-city/middleware/aws-lambda/index.ts
+++ packages/qwik-city/middleware/aws-lambda/index.ts
@@ -1,15 +1,22 @@
-import 'source-map-support/register';
-import serverless from 'serverless-http';
+
 import { createQwikCity as createQwikCityNode } from '@builder.io/qwik-city/middleware/node';
 import type { ServerRenderOptions } from '@builder.io/qwik-city/middleware/request-handler';
+import type { QwikCityPlan } from 'packages/qwik-city/runtime/src/types';
+import type { QwikManifest, Render } from 'packages/qwik/src/server/types';
+
+interface AwsOpt {
+  render: Render
+  manifest: QwikManifest,
+  qwikCityPlan: QwikCityPlan
+}
 
 /** @public */
-export function createQwikCity(opts: any) {
+export function createQwikCity(opts: AwsOpt){
   try {
     const { router, staticFile, notFound } = createQwikCityNode({
       render: opts.render,
-      manifest: opts.manifest,
       qwikCityPlan: opts.qwikCityPlan,
+      manifest: opts.manifest,
       static: {
         cacheControl: 'public, max-age=31557600',
       },
@@ -21,36 +28,29 @@ export function createQwikCity(opts: any) {
       },
     });
 
-    const qwikApp = serverless(
-      {
-        handle: (req: any, res: any) => {
-          req.url = fixPath(req.url);
-          staticFile(req, res, () => {
-            router(req, res, () => {
-              notFound(req, res, () => {});
-            });
-          });
-        },
-      },
-      {
-        binary: true,
-      }
-    );
-
-    const fixPath = (path: string) => {
+    const fixPath = (pathT: string) => {
       if (opts.qwikCityPlan.trailingSlash) {
-        const url = new URL(path, 'http://aws-qwik.local');
+        const url = new URL(pathT, 'http://aws-qwik.local');
         if (url.pathname.includes('.', url.pathname.lastIndexOf('/'))) {
-          return path;
+          return pathT;
         }
         if (!url.pathname.endsWith('/')) {
           return url.pathname + '/' + url.search;
         }
       }
-      return path;
+      return pathT;
     };
 
-    return qwikApp;
+    const handle = (req: any, res: any) => {
+      req.url = fixPath(req.url);
+      staticFile(req, res, () => {
+        router(req, res, () => {
+          notFound(req, res, () => { });
+        });
+      });
+    }
+
+    return {fixPath, router, staticFile, notFound, handle};
   } catch (err: any) {
     throw new Error(err.message);
   }

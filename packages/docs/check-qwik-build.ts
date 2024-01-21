4a5380c132833b0d8e409aac667a3505fef28235
Wout Mertens <Wout.Mertens@gmail.com>
2024-01-21 22:21:49 +0100

    feat(repl): use bundled qwik and prettier versions

--- packages/docs/check-qwik-build.ts
+++ packages/docs/check-qwik-build.ts
@@ -1,17 +1,45 @@
 // verify that ../qwik/dist/core.d.ts exists or run `pnpm run build.core` in the root directory
 // we need it for development and for the REPL
-import fs from 'fs';
+import fs, { copyFileSync, mkdirSync } from 'fs';
 import path from 'path';
 import { spawnSync } from 'child_process';
+import { qwikFiles } from './src/repl/qwikFiles';
 
-const coreDtsPath = path.join(__dirname, '../qwik/dist/core.d.ts');
-if (!fs.existsSync(coreDtsPath)) {
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const qwikPkgDir = path.join(__dirname, '..', 'qwik', 'dist');
+
+if (!fs.existsSync(path.join(qwikPkgDir, 'core.d.ts'))) {
   console.warn(
-    `Missing ${coreDtsPath}. Running 'pnpm run build.core' in the root directory to generate it.`
+    `\n\n=== Running 'pnpm run build.local' to generate missing imports for the docs ===\n`
   );
-  // now run `pnpm run build.core` in the root directory
-  spawnSync('pnpm', ['run', 'build.core'], {
-    cwd: path.join(__dirname, '../..'),
+  const out = spawnSync('pnpm', ['run', 'build.local'], {
+    cwd: path.join(__dirname, '..', '..'),
     stdio: 'inherit',
   });
+  if (out.status !== 0) {
+    console.error('Failed to build local packages');
+    process.exit(1);
+  }
+}
+
+// Copy the qwik files to public/ for the REPL
+const qwikBundleDir = path.join(__dirname, 'public', 'repl', 'bundled', 'qwik');
+// We cheat, knowing that build/ is the deepest dir
+mkdirSync(path.join(qwikBundleDir, 'build'), { recursive: true });
+for (const f of qwikFiles) {
+  const p = f.split('/');
+  copyFileSync(path.join(qwikPkgDir, ...p), path.join(qwikBundleDir, ...p));
+}
+
+if (!fs.existsSync(path.join(__dirname, 'dist', 'repl', '~repl-server-host.js'))) {
+  console.warn(
+    `\n\n=== Running 'pnpm run build.client' to generate missing REPL service worker dist/repl/~repl-server-host.js ===\n`
+  );
+  const out = spawnSync('pnpm', ['run', 'build.client'], {
+    stdio: 'inherit',
+  });
+  if (out.status !== 0) {
+    console.error('Failed to build REPL service worker');
+    process.exit(1);
+  }
 }

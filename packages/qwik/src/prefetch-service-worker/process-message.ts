eaa6e907a274c4944fb8001ab0073e3a9574befe
Miško Hevery <misko@hevery.com>
2024-01-23 08:39:37 -0800

    fix(qwik): ensure that SW cache is properly opened

--- packages/qwik/src/prefetch-service-worker/process-message.ts
+++ packages/qwik/src/prefetch-service-worker/process-message.ts
@@ -101,12 +101,12 @@ async function processBundleGraph(
   });
   if (cleanup) {
     const bundles = new Set<string>(graph.filter((item) => typeof item === 'string') as string[]);
-    for (const request of await (await swState.$cache$).keys()) {
+    for (const request of await swState.$cache$.keys()) {
       const [cacheBase, filename] = parseBaseFilename(new URL(request.url));
       const promises: Promise<boolean>[] = [];
       if (cacheBase === base && !bundles.has(filename)) {
         swState.$log$('deleting', request.url);
-        promises.push((await swState.$cache$).delete(request));
+        promises.push(swState.$cache$.delete(request));
       }
       await Promise.all(promises);
     }
--- packages/qwik/src/prefetch-service-worker/setup.ts
+++ packages/qwik/src/prefetch-service-worker/setup.ts
@@ -7,9 +7,15 @@ export const setupServiceWorker = (swScope: ServiceWorkerGlobalScope) => {
   swScope.addEventListener('fetch', async (ev) => {
     const request = ev.request;
     if (request.method === 'GET') {
-      const response = directFetch(swState, new URL(request.url));
-      if (response) {
-        ev.respondWith(response);
+      const previousCache = swState.$cache$;
+      try {
+        !previousCache && swState.$openCache$();
+        const response = directFetch(swState, new URL(request.url));
+        if (response) {
+          ev.respondWith(response);
+        }
+      } finally {
+        swState.$cache$ = previousCache;
       }
     }
   });

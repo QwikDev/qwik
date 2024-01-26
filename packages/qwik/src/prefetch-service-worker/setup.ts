857ebd6055cc2b794901ddcebfc0892800034ce6
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2024-01-26 17:53:08 -0600

    fix: SW cache race condition (#5780)
    * fix(sw): fix cache race condition
    
    * chore(lint): lint package
    
    * refactor(sw): remove excess logging from core

--- packages/qwik/src/prefetch-service-worker/setup.ts
+++ packages/qwik/src/prefetch-service-worker/setup.ts
@@ -4,6 +4,17 @@ import { createState, type SWState } from './state';
 
 export const setupServiceWorker = (swScope: ServiceWorkerGlobalScope) => {
   const swState: SWState = createState(swScope.fetch.bind(swScope), new URL(swScope.location.href));
+  let cacheTimeout: any;
+  swState.$getCache$ = () => {
+    if (swState.$cache$) {
+      return swState.$cache$;
+    }
+    clearTimeout(cacheTimeout);
+    setTimeout(() => {
+      swState.$cache$ = null;
+    }, 5000);
+    return swScope.caches.open('QwikBundles');
+  };
   swScope.addEventListener('fetch', (ev) => {
     const request = ev.request;
     if (request.method === 'GET') {
@@ -17,7 +28,9 @@ export const setupServiceWorker = (swScope: ServiceWorkerGlobalScope) => {
     swState.$msgQueue$.push(ev.data);
     drainMsgQueue(swState);
   });
-  swScope.addEventListener('install', () => swScope.skipWaiting());
+  swScope.addEventListener('install', () => {
+    swScope.skipWaiting();
+  });
   swScope.addEventListener('activate', (event) => {
     let cacheTimeout: any;
     swState.$getCache$ = () => {

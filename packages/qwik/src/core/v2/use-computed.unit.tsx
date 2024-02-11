0cbf68a587662eac63b6623dc1efcff020f06588
Miško Hevery <misko@hevery.com>
2024-02-11 08:53:58 -0800

    fixup! refactor(use-computed): fix useComputed$ impl for v2

--- packages/qwik/src/core/v2/use-computed.unit.tsx
+++ packages/qwik/src/core/v2/use-computed.unit.tsx
@@ -4,13 +4,11 @@ import { component$ } from '../component/component.public';
 import { inlinedQrl } from '../qrl/qrl';
 import { useLexicalScope } from '../use/use-lexical-scope.public';
 import { useSignal } from '../use/use-signal';
+import { useComputedQrl } from '../use/use-task';
 import { domRender, ssrRenderToDom } from './rendering.unit-util';
 import './vdom-diff.unit-util';
-import { _jsxQ, _jsxC } from '../render/jsx/jsx-runtime';
-import { _IMMUTABLE, _fnSignal } from '../internal';
-import { useComputed$ } from '../use/use-task';
 
-const debug = false; //true;
+const debug = true; //true;
 Error.stackTraceLimit = 100;
 
 [
@@ -21,7 +19,9 @@ Error.stackTraceLimit = 100;
     it('should update value based on signal', async () => {
       const DoubleCounter = component$((props: { initial: number }) => {
         const count = useSignal(props.initial);
-        const doubleCount = useComputed$(() => count.value * 2);
+        const doubleCount = useComputedQrl(
+          inlinedQrl(() => useLexicalScope()[0].value * 2, 's_doubleCount', [count])
+        );
         return (
           <button onClick$={inlinedQrl(() => useLexicalScope()[0].value++, 's_onClick', [count])}>
             Double count: {doubleCount.value}! {count.value}
@@ -32,13 +32,17 @@ Error.stackTraceLimit = 100;
       const { vNode, container } = await render(<DoubleCounter initial={123} />, { debug });
       expect(vNode).toMatchVDOM(
         <>
-          <button>Double count: {'246'}! {'123'}</button>
+          <button>
+            Double count: {'246'}! {'123'}
+          </button>
         </>
       );
       await trigger(container.element, 'button', 'click');
       expect(vNode).toMatchVDOM(
         <>
-          <button>Double count: {'248'}! {'124'}</button>
+          <button>
+            Double count: {'248'}! {'124'}
+          </button>
         </>
       );
     });
@@ -46,8 +50,12 @@ Error.stackTraceLimit = 100;
     it('should update value based on another computed', async () => {
       const QuadrupleCounter = component$((props: { initial: number }) => {
         const count = useSignal(props.initial);
-        const doubleCount = useComputed$(() => count.value * 2);
-        const quadrupleCount = useComputed$(() => doubleCount.value * 2);
+        const doubleCount = useComputedQrl(
+          inlinedQrl(() => useLexicalScope()[0].value * 2, 's_doubleCount', [count])
+        );
+        const quadrupleCount = useComputedQrl(
+          inlinedQrl(() => useLexicalScope()[0].value * 2, 's_quadrupleCount', [doubleCount])
+        );
         return (
           <button onClick$={inlinedQrl(() => useLexicalScope()[0].value++, 's_onClick', [count])}>
             Double count: {quadrupleCount.value}!

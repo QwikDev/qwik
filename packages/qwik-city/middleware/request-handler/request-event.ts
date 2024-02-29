26b6f08a7da2041b68dd92a3079845c32c705181
PatrickJS <github@gdi2290.com>
2024-02-28 19:22:45 -0800

    Update packages/qwik-city/middleware/request-handler/request-event.ts
    Co-authored-by: Miško Hevery <misko@hevery.com>

--- packages/qwik-city/middleware/request-handler/request-event.ts
+++ packages/qwik-city/middleware/request-handler/request-event.ts
@@ -74,7 +74,7 @@ export function createRequestEvent(
     while (routeModuleIndex < requestHandlers.length) {
       const moduleRequestHandler = requestHandlers[routeModuleIndex];
       const result = (globalThis as any).asyncStore
-        ? (globalThis as any).asyncStore.run(requestEv, moduleRequestHandler, requestEv)
+        ? (globalThis as QGlobal).asyncStore.run(requestEv, moduleRequestHandler, requestEv)
         : moduleRequestHandler(requestEv);
       if (isPromise(result)) {
         await result;

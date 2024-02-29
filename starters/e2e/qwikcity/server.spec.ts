9bcd0072c81b5ab8b131f99d618355ed971779e7
PatrickJS <github@patrickjs.com>
2024-02-28 20:14:26 -0800

    test: Multiple cookie users

--- starters/e2e/qwikcity/server.spec.ts
+++ starters/e2e/qwikcity/server.spec.ts
@@ -72,5 +72,35 @@ test.describe("server$", () => {
       const methodsContainer = page.locator("#methods");
       await expect(methodsContainer).toContainText("GETGET");
     });
+    test("should have multiple user cookie values", async ({ browser }) => {
+      const user1Context = await browser.newContext();
+      const user2Context = await browser.newContext();
+
+      const user1Cookies = [
+        { name: "user", value: "user1", url: "http://localhost:3301" },
+      ];
+      const user2Cookies = [
+        { name: "user", value: "user2", url: "http://localhost:3301" },
+      ];
+      await user1Context.addCookies(user1Cookies);
+      await user2Context.addCookies(user2Cookies);
+
+      const [user1Page, user2Page] = await Promise.all([
+        user1Context.newPage(),
+        user2Context.newPage(),
+      ]);
+      await Promise.all([
+        user1Page.goto("/qwikcity-test/server-func/cookie"),
+        user2Page.goto("/qwikcity-test/server-func/cookie"),
+      ]);
+      const usersContainer1 = user1Page.locator("#users");
+      const usersContainer2 = user2Page.locator("#users");
+      await Promise.all([
+        usersContainer1.waitFor({ state: "attached" }),
+        usersContainer2.waitFor({ state: "attached" }),
+      ]);
+      await expect(usersContainer1).toContainText("user1user1");
+      await expect(usersContainer2).toContainText("user2user2");
+    });
   });
 });

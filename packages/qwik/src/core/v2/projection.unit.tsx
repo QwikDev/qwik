c00b3804288bb20df0d179c5375021a3a16d9f89
Varixo <admin@varixo.pl>
2024-03-02 21:02:04 +0100

    move failing tests to another PR

--- packages/qwik/src/core/v2/projection.unit.tsx
+++ packages/qwik/src/core/v2/projection.unit.tsx
@@ -181,43 +181,6 @@ const debug = false;
         </Component>
       );
     });
-
-    it('should render conditional projection', async () => {
-      const Child = component$(() => {
-        const show = useSignal(false);
-        return (
-          <button
-            onClick$={inlinedQrl(() => (useLexicalScope()[0].value = true), 's_onClick', [show])}
-          >
-            {show.value && <Slot />}
-          </button>
-        );
-      });
-      const Parent = component$(() => {
-        return <Child>parent-content</Child>;
-      });
-      const { vNode, container} = await render(<Parent>render-content</Parent>, { debug });
-      expect(vNode).toMatchVDOM(
-        <Fragment>
-          <Fragment>
-            <button>
-              {''}
-            </button>
-          </Fragment>
-        </Fragment>
-      );
-      trigger(container.element, 'button', 'click');
-      expect(vNode).toMatchVDOM(
-        <Fragment>
-          <Fragment>
-            <button>
-              <Fragment>parent-content</Fragment>
-            </button>
-          </Fragment>
-        </Fragment>
-      );
-    });
-
   });
   it('should project default content', async () => {
     const Child = componentQrl(

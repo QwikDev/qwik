94311e58a4018204f8efe1df64837ba068004d3c
Varixo <78650133+Varixo@users.noreply.github.com>
2024-03-10 19:51:28 +0100

    test(v2): test for issue #4038 (#5989)

--- packages/qwik/src/core/v2/use-context.unit.tsx
+++ packages/qwik/src/core/v2/use-context.unit.tsx
@@ -1,4 +1,8 @@
-import { Fragment as Component } from '@builder.io/qwik/jsx-runtime';
+import {
+  Fragment as Component,
+  Fragment,
+  Fragment as Projection,
+} from '@builder.io/qwik/jsx-runtime';
 import { describe, expect, it } from 'vitest';
 import { trigger } from '../../testing/element-fixture';
 import { component$ } from '../component/component.public';
@@ -8,6 +12,7 @@ import { useLexicalScope } from '../use/use-lexical-scope.public';
 import { useSignal } from '../use/use-signal';
 import { domRender, ssrRenderToDom } from './rendering.unit-util';
 import './vdom-diff.unit-util';
+import { Slot } from '../render/jsx/slot.public';
 
 const debug = false; //true;
 Error.stackTraceLimit = 100;
@@ -62,6 +67,110 @@ Error.stackTraceLimit = 100;
           <Component>
             <span>CONTEXT_VALUE</span>
           </Component>
+        </Component>
+      );
+    });
+  });
+
+  describe('regression', () => {
+    it('#4038', async () => {
+      interface MyStore {
+        value: number;
+      }
+      const myContext = createContextId<MyStore>('mytitle');
+      const useFn = () => {
+        const state = useContext(myContext);
+
+        return (val: number) => {
+          return (state.value + val).toString();
+        };
+      };
+      interface IMyComponent {
+        val: string;
+      }
+      const MyComponent = component$((props: IMyComponent) => {
+        const count = useSignal(0);
+        const c = useFn();
+
+        return (
+          <>
+            <p>{props.val}</p>
+            <p>{c(count.value)}</p>
+            <p>{count.value}</p>
+            <button
+              onClick$={inlinedQrl(
+                () => {
+                  const [count] = useLexicalScope();
+                  count.value++;
+                },
+                's_onClick',
+                [count]
+              )}
+            >
+              Increment
+            </button>
+          </>
+        );
+      });
+
+      const Parent = component$(() => {
+        const c = useFn();
+
+        return (
+          <div>
+            <MyComponent val={c(1)} />
+          </div>
+        );
+      });
+
+      const Layout = component$(() => {
+        useContextProvider(myContext, {
+          value: 0,
+        });
+        return <Slot />;
+      });
+      const { vNode, document } = await render(
+        <Layout>
+          <Parent />
+        </Layout>,
+        { debug }
+      );
+      expect(vNode).toMatchVDOM(
+        <Component>
+          <Projection>
+            <Component>
+              <div>
+                <Component>
+                  <Fragment>
+                    <p>1</p>
+                    <p>0</p>
+                    <p>0</p>
+                    <button>Increment</button>
+                  </Fragment>
+                </Component>
+              </div>
+            </Component>
+          </Projection>
+        </Component>
+      );
+      await trigger(document.body, 'button', 'click');
+      await trigger(document.body, 'button', 'click');
+      expect(vNode).toMatchVDOM(
+        <Component>
+          <Projection>
+            <Component>
+              <div>
+                <Component>
+                  <Fragment>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>2</p>
+                    <button>Increment</button>
+                  </Fragment>
+                </Component>
+              </div>
+            </Component>
+          </Projection>
         </Component>
       );
     });

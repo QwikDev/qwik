6f9eaa8db67343c50e1170f1f05a9ebaf59efbbf
Ole Reglitzki <zerg@git.xoria.dev>
2024-03-13 20:14:16 +0100

    fix(layout): fix layout for non-default html font-size (#5853)

--- packages/docs/src/routes/docs/layout.tsx
+++ packages/docs/src/routes/docs/layout.tsx
@@ -3,7 +3,6 @@ import { useContent, useLocation } from '@builder.io/qwik-city';
 import { ContentNav } from '../../components/content-nav/content-nav';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
-import { OnThisPage } from '../../components/on-this-page/on-this-page';
 import { createBreadcrumbs, SideBar } from '../../components/sidebar/sidebar';
 import { GlobalStore } from '../../context';
 import styles from './docs.css?inline';
@@ -13,8 +12,6 @@ import Contributors from '../../components/contributors';
 export { useMarkdownItems } from '../../components/sidebar/sidebar';
 
 export default component$(() => {
-  const loc = useLocation();
-  const noRightMenu = ['/docs/'].includes(loc.url.pathname);
   useStyles$(styles);
   const { menu } = useContent();
   const globalStore = useContext(GlobalStore);
@@ -50,22 +47,17 @@ export default component$(() => {
           </ol>
         ) : null}
       </nav>
-      <SideBar />
-      <main
-        class={{
-          'no-right-menu': noRightMenu,
-        }}
-      >
-        <div class="docs-container">
+      <div class="flex gap-12 xl:gap-20 items-stretch content-container">
+        <SideBar />
+        <main class="docs-container">
           <article>
             <Slot />
             <Contributors />
           </article>
           <ContentNav />
           <Footer />
-        </div>
-        <OnThisPage />
-      </main>
+        </main>
+      </div>
     </div>
   );
 });

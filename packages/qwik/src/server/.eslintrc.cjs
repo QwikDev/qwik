01f26c2c9e9852e9ee1dd60c75abac6a376663fc
Varixo <78650133+Varixo@users.noreply.github.com>
2024-03-24 04:48:13 +0100

    test(v2): add svg tests (#6042)
    * test(v2): add some svg tests
    
    * fix lint
    
    * fix conditional rendering

--- packages/qwik/src/server/.eslintrc.cjs
+++ packages/qwik/src/server/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: '../../../../.eslintrc.cjs',
   plugins: ['import'],
   rules: {
-    'no-restricted-imports': [
+    '@typescript-eslint/no-restricted-imports': [
       'error',
       {
         patterns: [
@@ -20,4 +20,28 @@ module.exports = {
     'no-duplicate-imports': 'error',
     'import/no-useless-path-segments': 'error',
   },
+  overrides: [
+    {
+      files: ['./qwik-types.ts'],
+      rules: {
+        '@typescript-eslint/no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: ['packages/*'],
+                message: 'Absolute imports are not allowed.',
+                allowTypeImports: true,
+              },
+              {
+                group: ['../'],
+                message: 'Relative imports are not allowed.',
+                allowTypeImports: true,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };

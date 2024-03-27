e613d880c7fa306ba627bcc38b44cf15a2292917
sidmohanty11 <sidmohanty11@gmail.com>
2024-03-28 00:47:44 +0530

    fix(starters): uses latest qwik sdk and fixes the starter

--- starters/features/builder.io/src/routes/[...index]/index.tsx
+++ starters/features/builder.io/src/routes/[...index]/index.tsx
@@ -1,33 +1,27 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
-import {
-  getContent,
-  RenderContent,
-  getBuilderSearchParams,
-} from "@builder.io/sdk-qwik";
+import { fetchOneEntry, Content } from "@builder.io/sdk-qwik";
 import { CUSTOM_COMPONENTS } from "~/components/builder-registry";
 
 export const BUILDER_MODEL = "page";
 
 // Use Qwik City's `useBuilderContent` to get your content from Builder.
 // `routeLoader$()` takes an async function to fetch content
-// from Builder with `getContent()`.
-export const useBuilderContent = routeLoader$(async ({ url, error }) => {
-  const isPreviewing = url.searchParams.has("builder.preview");
+// from Builder with `fetchOneEntry()`.
+export const useBuilderContent = routeLoader$(async (event) => {
+  const isPreviewing = event.url.searchParams.has("builder.preview");
 
-  const builderContent = await getContent({
-    model: BUILDER_MODEL,
+  const builderContent = await fetchOneEntry({
+    model: "page",
     apiKey: import.meta.env.PUBLIC_BUILDER_API_KEY,
-    options: getBuilderSearchParams(url.searchParams),
-    userAttributes: {
-      urlPath: url.pathname,
-    },
+    userAttributes: { urlPath: event.url.pathname },
+    options: event.query,
   });
 
   // If there's no content, throw a 404.
   // You can use your own 404 component here
   if (!builderContent && !isPreviewing) {
-    throw error(404, "Page not found");
+    throw event.error(404, "Page not found");
   }
   // return content fetched from Builder, which is JSON
   return builderContent;
@@ -36,12 +30,12 @@ export const useBuilderContent = routeLoader$(async ({ url, error }) => {
 export default component$(() => {
   const content = useBuilderContent();
 
-  // RenderContent uses `content` to
+  // Content uses `content` to
   // render the content of the given model, here a page,
   // of your space (specified by the API Key)
   return (
-    <RenderContent
-      model={BUILDER_MODEL}
+    <Content
+      model="page"
       content={content.value}
       apiKey={import.meta.env.PUBLIC_BUILDER_API_KEY}
       customComponents={CUSTOM_COMPONENTS}

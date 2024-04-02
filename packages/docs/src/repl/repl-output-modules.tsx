34f10eeca154646a265ec4d7dbb9e06302ee2661
Vaibhav Rai <raivaibhav08@gmail.com>
2024-04-02 13:28:02 +0530

    Fix layout of code block on mobile

--- packages/docs/src/repl/repl-output-modules.tsx
+++ packages/docs/src/repl/repl-output-modules.tsx
@@ -1,7 +1,6 @@
 import { $, useStore, component$ } from '@builder.io/qwik';
 import { CodeBlock } from '../components/code-block/code-block';
 import type { PathInView, ReplModuleOutput } from './types';
-import { CopyCode } from '../components/copy-code/copy-code-block';
 const FILE_MODULE_DIV_ID = 'file-modules-client-buisness';
 
 export const ReplOutputModules = component$(({ outputs, headerText }: ReplOutputModulesProps) => {
@@ -17,20 +16,42 @@ export const ReplOutputModules = component$(({ outputs, headerText }: ReplOutput
         <div class="file-tree-header">{outputs.length > 0 ? headerText : ''}</div>
         <div class="file-tree-items">
           {outputs.map((o, i) => (
-            <a
-              href="#"
-              onClick$={() => {
-                const fileItem = document.querySelector(`[data-file-item="${i}"]`);
-                if (fileItem) {
-                  fileItem.scrollIntoView();
-                }
-              }}
-              class={{ 'in-view': store.selectedPath && store.selectedPath === o.path }}
-              preventdefault:click
-              key={o.path}
-            >
-              {o.path}
-            </a>
+            <div key={o.path}>
+              <a
+                href="#"
+                onClick$={() => {
+                  const fileItem = document.querySelector(`[data-file-item="${i}"]`);
+                  if (fileItem) {
+                    fileItem.scrollIntoView();
+                  }
+                }}
+                class={{
+                  'in-view': store.selectedPath && store.selectedPath === o.path,
+                  "!hidden": true,
+                  'md:!block': true,
+                }}
+                preventdefault:click
+                key={o.path}
+              >
+                {o.path}
+              </a>
+              <div class="block md:hidden">
+                <div class="file-item" data-file-item={i} key={o.path}>
+                  <div class="file-info">
+                    <span>{o.path}</span>
+                    {o.size ? <span class="file-size">({o.size})</span> : null}
+                  </div>
+                  <div class="file-text">
+                    <CodeBlock
+                      pathInView$={pathInView$}
+                      path={o.path}
+                      code={o.code}
+                      observerRootId={FILE_MODULE_DIV_ID}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
--- packages/docs/src/repl/repl-output-symbols.tsx
+++ packages/docs/src/repl/repl-output-symbols.tsx
@@ -2,7 +2,6 @@ import type { TransformModule } from '@builder.io/qwik/optimizer';
 import { CodeBlock } from '../components/code-block/code-block';
 import { $, useStore, component$ } from '@builder.io/qwik';
 import type { PathInView } from './types';
-import { CopyCode } from '../components/copy-code/copy-code-block';
 const FILE_MODULE_DIV_ID = 'file-modules-symbol';
 
 export const ReplOutputSymbols = component$(({ outputs }: ReplOutputSymbolsProps) => {
@@ -19,25 +18,45 @@ export const ReplOutputSymbols = component$(({ outputs }: ReplOutputSymbolsProps
         <div class="file-tree-header">Symbols</div>
         <div class="file-tree-items">
           {outputs.map((o, i) => (
-            <a
-              href="#"
-              onClick$={() => {
-                const fileItem = document.querySelector(`[data-file-item="${i}"]`);
-                if (fileItem) {
-                  store.selectedPath = o.path;
-                  fileItem.scrollIntoView();
-                }
-              }}
-              class={{ 'in-view': store.selectedPath && store.selectedPath === o.path }}
-              preventdefault:click
-              key={o.path}
-            >
-              {o.hook?.canonicalFilename}
-            </a>
+            <div key={o.path}>
+              <a
+                href="#"
+                onClick$={() => {
+                  const fileItem = document.querySelector(`[data-file-item="${i}"]`);
+                  if (fileItem) {
+                    store.selectedPath = o.path;
+                    fileItem.scrollIntoView();
+                  }
+                }}
+                class={{
+                  'in-view': store.selectedPath && store.selectedPath === o.path,
+                  '!hidden': true,
+                  'md:!block': true,
+                }}
+                preventdefault:click
+              >
+                {o.hook?.canonicalFilename}
+              </a>
+              <div class="block md:hidden">
+                <div class="file-item" data-file-item={i} key={o.path}>
+                  <div class="file-info">
+                    <span>{o.hook?.canonicalFilename}</span>
+                  </div>
+                  <div class="file-text">
+                    <CodeBlock
+                      pathInView$={pathInView$}
+                      path={o.path}
+                      code={o.code}
+                      observerRootId={FILE_MODULE_DIV_ID}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
-      <div class="file-modules" id={FILE_MODULE_DIV_ID}>
+      <div class="file-modules hidden md:block" id={FILE_MODULE_DIV_ID}>
         {outputs
           .filter((o) => !!o.hook)
           .map((o, i) => (

51e9924a9fcf7f4369f0d5306774f3f37282ab28
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2024-05-07 10:37:08 -0500

    revert: bundle optimization improvements (#6265)

--- starters/apps/base/vite.config.ts
+++ starters/apps/base/vite.config.ts
@@ -28,20 +28,24 @@ export default defineConfig(({ command, mode }): UserConfig => {
       // For example ['better-sqlite3'] if you use that in server functions.
       exclude: [],
     },
-    // This tells Vite how to bundle the server code.
-    ssr:
-      command === "build" && mode === "production"
-        ? {
-            // All dev dependencies should be bundled in the server build
-            noExternal: Object.keys(devDependencies),
-            // Anything marked as a dependency will not be bundled
-            // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-            // If a dep-of-dep needs to be external, add it here
-            // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-            // external: [...Object.keys(dependencies), 'bcrypt']
-            external: Object.keys(dependencies),
-          }
-        : undefined,
+
+    /**
+     * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev depencies. (otherwise things will break in production)
+     */
+    // ssr:
+    //   command === "build" && mode === "production"
+    //     ? {
+    //         // All dev dependencies should be bundled in the server build
+    //         noExternal: Object.keys(devDependencies),
+    //         // Anything marked as a dependency will not be bundled
+    //         // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
+    //         // If a dep-of-dep needs to be external, add it here
+    //         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
+    //         // external: [...Object.keys(dependencies), 'bcrypt']
+    //         external: Object.keys(dependencies),
+    //       }
+    //     : undefined,
+
     server: {
       headers: {
         // Don't cache the server response in dev mode

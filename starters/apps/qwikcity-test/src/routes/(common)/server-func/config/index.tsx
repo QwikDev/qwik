fe8d22954a77661d6435ada18d72a1a9ae7a6a20
PatrickJS <github@patrickjs.com>
2024-05-09 14:30:45 -0700

    chore(server$): test

---
+++ starters/apps/qwikcity-test/src/routes/(common)/server-func/config/index.tsx
@@ -0,0 +1,37 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { server$ } from "@builder.io/qwik-city";
+import { delay } from "../../actions/login";
+
+const serverFunctionA = server$(
+  async function a() {
+    return this.method;
+  },
+  {
+    method: "get",
+  },
+);
+
+export const MultipleServerFunctionsInvokedInTask = component$(() => {
+  const methodA = useSignal("");
+  const methodB = useSignal("");
+  useTask$(async () => {
+    methodA.value = await serverFunctionA();
+    await delay(1);
+    methodB.value = await serverFunctionA();
+  });
+
+  return (
+    <div id="methods">
+      {methodA.value}
+      {methodB.value}
+    </div>
+  );
+});
+
+export default component$(() => {
+  return (
+    <>
+      <MultipleServerFunctionsInvokedInTask />
+    </>
+  );
+});

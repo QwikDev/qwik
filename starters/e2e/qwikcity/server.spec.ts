7aa4455836e597e8c241d64a64cbecaaa3fbf463
PatrickJS <github@patrickjs.com>
2024-05-09 18:46:53 -0700

    chore: trailing slash

--- starters/e2e/qwikcity/server.spec.ts
+++ starters/e2e/qwikcity/server.spec.ts
@@ -103,7 +103,7 @@ test.describe("server$", () => {
       await expect(usersContainer2).toContainText("user2user2");
     });
     test("should work with config GET and custom header", async ({ page }) => {
-      await page.goto("/qwikcity-test/server-func/server-configs");
+      await page.goto("/qwikcity-test/server-func/server-configs/");
       const serverConfigContainer = page.locator("#server-configs");
       await serverConfigContainer.waitFor({ state: "attached" });
 

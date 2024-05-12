8c005db2348db67366d7fedccf4cb0feb558fbcb
Vaibhav Rai <raivaibhav08@gmail.com>
2024-05-12 09:04:02 +0530

    docs: show selected path if block scroll in view (#6079)
    Co-authored-by: PatrickJS <github@patrickjs.com>

--- packages/docs/src/repl/repl-output-modules.tsx
+++ packages/docs/src/repl/repl-output-modules.tsx
@@ -1,30 +1,61 @@
+import { $, useStore, component$ } from '@builder.io/qwik';
 import { CodeBlock } from '../components/code-block/code-block';
-import type { ReplModuleOutput } from './types';
+import type { PathInView, ReplModuleOutput } from './types';
+const FILE_MODULE_DIV_ID = 'file-modules-client-buisness';
 
-export const ReplOutputModules = ({ outputs, headerText }: ReplOutputModulesProps) => {
+export const ReplOutputModules = component$(({ outputs, headerText }: ReplOutputModulesProps) => {
+  const store = useStore<PathInView>({
+    selectedPath: outputs.length ? outputs[0].path : '',
+  });
+  const pathInView$ = $((path: string) => {
+    store.selectedPath = path;
+  });
   return (
     <div class="output-result output-modules">
       <div class="file-tree">
         <div class="file-tree-header">{outputs.length > 0 ? headerText : ''}</div>
         <div class="file-tree-items">
           {outputs.map((o, i) => (
-            <a
-              href="#"
-              onClick$={() => {
-                const fileItem = document.querySelector(`[data-file-item="${i}"]`);
-                if (fileItem) {
-                  fileItem.scrollIntoView();
-                }
-              }}
-              preventdefault:click
-              key={o.path}
-            >
-              {o.path}
-            </a>
+            <div key={o.path}>
+              <a
+                href="#"
+                onClick$={() => {
+                  const fileItem = document.querySelector(`[data-file-item="${i}"]`);
+                  if (fileItem) {
+                    fileItem.scrollIntoView();
+                  }
+                }}
+                class={{
+                  'in-view': store.selectedPath && store.selectedPath === o.path,
+                  '!hidden': true,
+                  'md:!block': true,
+                }}
+                preventdefault:click
+                key={o.path}
+              >
+                {o.path}
+              </a>
+              <div class="block md:hidden">
+                <div class="file-item" data-file-item={i} key={o.path}>
+                  <div class="file-info">
+                    <span>{o.path}</span>
+                    {o.size ? <span class="file-size">({o.size})</span> : null}
+                  </div>
+                  <div class="file-text">
+                    <CodeBlock
+                      pathInView$={pathInView$}
+                      path={o.path}
+                      code={o.code}
+                      observerRootId={FILE_MODULE_DIV_ID}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
-      <div class="file-modules">
+      <div class="file-modules" id={FILE_MODULE_DIV_ID}>
         {outputs.map((o, i) => (
           <div class="file-item" data-file-item={i} key={o.path}>
             <div class="file-info">
@@ -32,14 +63,19 @@ export const ReplOutputModules = ({ outputs, headerText }: ReplOutputModulesProp
               {o.size ? <span class="file-size">({o.size})</span> : null}
             </div>
             <div class="file-text">
-              <CodeBlock path={o.path} code={o.code} />
+              <CodeBlock
+                pathInView$={pathInView$}
+                path={o.path}
+                code={o.code}
+                observerRootId={FILE_MODULE_DIV_ID}
+              />
             </div>
           </div>
         ))}
       </div>
     </div>
   );
-};
+});
 
 interface ReplOutputModulesProps {
   headerText: string;

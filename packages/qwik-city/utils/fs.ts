6a7291d1153acc91c3ee07f3f8e8bcf537735be9
wuls <linsheng.wu@beantechs.com>
2024-05-13 17:38:44 +0800

    fix: a bug

--- packages/qwik-city/utils/fs.ts
+++ packages/qwik-city/utils/fs.ts
@@ -33,7 +33,7 @@ export function getPathnameFromDirPath(opts: NormalizedPluginOptions, dirPath: s
     // remove grouped layout segments
     .filter((segment) => !isGroupedLayoutName(segment))
     .join('/');
-
+  if (pathname === '') return '/';
   return pathname;
 }
 

329f490bbf0e7b2f85c13ac28ac69695770d662b
PatrickJS <github@patrickjs.com>
2024-05-20 00:00:36 -0700

    fix: test name

--- starters/e2e/qwikcity/adapter.spec.ts
+++ starters/e2e/qwikcity/adapter.spec.ts
@@ -1,7 +1,9 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Qwik City Adapter", () => {
-  test("Qwik City Adapter", async ({ page: api }) => {
+  test("should have simple Not Found response for middleware/node static files", async ({
+    page: api,
+  }) => {
     const nestedUrl = "by/pass/other-routes-for-testing";
     const rsp = (await api.goto(
       `/qwikcity-test/build/${nestedUrl}/a-random-file-after-that.js`,

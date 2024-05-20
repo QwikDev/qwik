46aed289993352467153cfe8c12d5ce4d7654995
PatrickJS <github@patrickjs.com>
2024-05-19 23:35:17 -0700

    chore: maybe fix

--- starters/e2e/qwikcity/adapter.spec.ts
+++ starters/e2e/qwikcity/adapter.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("Qwik City Adapter", () => {
   test("Qwik City Adapter", async ({ page: api }) => {
     const rsp = (await api.goto(
-      "/qwikcity-test/build/a-random-file-after-that.js",
+      "/qwikcity-test/adapter/build/a-random-file-after-that.js",
     ))!;
     expect(rsp.status()).toBe(200);
     expect(rsp.headers()["content-type"]).toBe("text/html; charset=utf-8");

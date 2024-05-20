27a1e8fdffe8001351f723bf60d10274f41d294b
Nick K <nick.kruchinin@gmail.com>
2024-05-20 18:49:06 +0300

    chore: Run pnpm fmt then pnpm lint

--- starters/adapters/bun/adapters/bun/vite.config.ts
+++ starters/adapters/bun/adapters/bun/vite.config.ts
@@ -1,10 +1,10 @@
 import { bunServerAdapter } from "@builder.io/qwik-city/adapters/bun-server/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
-import {_TextEncoderStream_polyfill} from "@builder.io/qwik-city/middleware/request-handler"
+import { _TextEncoderStream_polyfill } from "@builder.io/qwik-city/middleware/request-handler";
 import baseConfig from "../../vite.config";
 
 // This polyfill is required when you use SSG and build your app with Bun, because Bun does not have TextEncoderStream. See: https://github.com/oven-sh/bun/issues/5648
-globalThis.TextEncoderStream ||= _TextEncoderStream_polyfill
+globalThis.TextEncoderStream ||= _TextEncoderStream_polyfill;
 
 export default extendConfig(baseConfig, () => {
   return {

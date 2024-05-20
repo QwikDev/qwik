cb16f4d3ba72218f7a944c300aac56820dcf0368
PatrickJS <github@patrickjs.com>
2024-05-19 23:36:19 -0700

    fix: status

--- starters/e2e/qwikcity/adapter.spec.ts
+++ starters/e2e/qwikcity/adapter.spec.ts
@@ -5,7 +5,7 @@ test.describe("Qwik City Adapter", () => {
     const rsp = (await api.goto(
       "/qwikcity-test/adapter/build/a-random-file-after-that.js",
     ))!;
-    expect(rsp.status()).toBe(200);
+    expect(rsp.status()).toBe(404);
     expect(rsp.headers()["content-type"]).toBe("text/html; charset=utf-8");
 
     const data = await rsp.text();

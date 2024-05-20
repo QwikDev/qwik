2964700a31833012ba562d3b8482aee07d680969
PatrickJS <github@patrickjs.com>
2024-05-19 22:40:36 -0700

    chore: fix no tetst

--- packages/qwik-city/middleware/request-handler/generated/static-paths.ts
+++ packages/qwik-city/middleware/request-handler/generated/static-paths.ts
@@ -12,5 +12,24 @@ export function isStaticPath(method: string, url: URL) {
   if (url.search !== '') {
     return false;
   }
+
+  /**
+   * - Generated values in vite post build
+   *
+   * Const p = url.pathname;
+   *
+   * If (p.startsWith(baseBuildPath)) { return true; }
+   *
+   * If (p.startsWith(assetsPath)) { return true; }
+   *
+   * If (staticPaths.has(p)) { return true; }
+   *
+   * If (p.endsWith('/q-data.json')) { const pWithoutQdata = p.replace(//q-data.json$/, '');
+   *
+   * If (staticPaths.has(pWithoutQdata + '/')) { return true; }
+   *
+   * If (staticPaths.has(pWithoutQdata)) { return true; }
+   */
+
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url.pathname);
 }

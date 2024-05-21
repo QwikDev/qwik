89543210b7295f7573c19104614193b257889b02
Nick K <nick.kruchinin@gmail.com>
2024-05-21 16:54:44 +0300

    fix(bun): Add checks for Bun in platform.ts

--- packages/qwik/src/optimizer/src/platform.ts
+++ packages/qwik/src/optimizer/src/platform.ts
@@ -35,7 +35,7 @@ export async function getSystem() {
   }
 
   if (globalThis.IS_CJS) {
-    if (sysEnv === 'node') {
+    if (sysEnv === 'node' || sysEnv === 'bun') {
       // using this api object as a way to ensure bundlers
       // do not try to inline or rewrite require()
       sys.dynamicImport = (path) => require(path);
@@ -62,7 +62,7 @@ export async function getSystem() {
     }
   }
 
-  if (sysEnv === 'node') {
+  if (sysEnv === 'node' || sysEnv === 'bun') {
     sys.path = await sys.dynamicImport('node:path');
     sys.cwd = () => process.cwd();
     sys.os = process.platform;
@@ -135,7 +135,7 @@ export async function loadPlatformBinding(sys: OptimizerSystem) {
   const sysEnv = getEnv();
 
   // Try native build
-  if (sysEnv === 'node') {
+  if (sysEnv === 'node' || sysEnv === 'bun') {
     // Node.js
     const platform = (QWIK_BINDING_MAP as any)[process.platform];
     if (platform) {
@@ -164,7 +164,7 @@ export async function loadPlatformBinding(sys: OptimizerSystem) {
   if (globalThis.IS_CJS) {
     // CJS WASM
 
-    if (sysEnv === 'node') {
+    if (sysEnv === 'node' || sysEnv === 'bun') {
       // CJS WASM Node.js
       const wasmPath = sys.path.join(__dirname, 'bindings', 'qwik_wasm_bg.wasm');
       const mod = await sys.dynamicImport(`./bindings/qwik.wasm.cjs`);
@@ -225,7 +225,7 @@ export async function loadPlatformBinding(sys: OptimizerSystem) {
   }
 
   if (globalThis.IS_ESM) {
-    if (sysEnv === 'node') {
+    if (sysEnv === 'node' || sysEnv === 'bun') {
       // CJS WASM Node.js
       const url: typeof import('url') = await sys.dynamicImport('node:url');
       const __dirname = sys.path.dirname(url.fileURLToPath(import.meta.url));
@@ -263,6 +263,10 @@ export interface PlatformBinding {
 const getEnv = (): SystemEnvironment => {
   if (typeof Deno !== 'undefined') {
     return 'deno';
+  }
+
+  if (typeof Bun !== 'undefined') {
+    return 'bun';
   }
 
   if (

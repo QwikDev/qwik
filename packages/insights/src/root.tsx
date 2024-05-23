0eb936a6fe7918daffef444b483fcd2b39d8a722
PatrickJS <github@patrickjs.com>
2024-05-22 20:23:39 -0700

    Update root.tsx

--- packages/insights/src/root.tsx
+++ packages/insights/src/root.tsx
@@ -3,57 +3,19 @@ import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { Insights } from '@builder.io/qwik-labs';
 import { RouterHead } from './components/router-head/router-head';
-
-export const uwu = /*javascript*/ `
-;(function () {
-  try {
-    var preferredUwu;
-    try {
-      preferredUwu = localStorage.getItem('uwu');
-    } catch (err) { }
-
-    const isUwuValue = window.location
-      && window.location.search
-      && window.location.search.match(/uwu=(true|false)/);
-
-    if (isUwuValue) {
-      const isUwu = isUwuValue[1] === 'true';
-      if (isUwu) {
-        try {
-          localStorage.setItem('uwu', true);
-        } catch (err) { }
-        document.documentElement.classList.add('uwu');
-        console.log('uwu mode enabled. turn off with ?uwu=false')
-        console.log('logo credit to @sawaratsuki1004 via https://github.com/SAWARATSUKI/ServiceLogos');
-      } else {
-        try {
-          localStorage.removeItem('uwu', false);
-        } catch (err) { }
-      }
-    } else if (preferredUwu) {
-      document.documentElement.classList.add('uwu');
-    }
-  } catch (err) { }
-})();
-`;
 export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
         <meta charset="utf-8" />
-        <script dangerouslySetInnerHTML={uwu} />
+        <link rel="manifest" href="/manifest.json" />
         <RouterHead />
         <Insights
           publicApiKey={import.meta.env.PUBLIC_QWIK_INSIGHTS_KEY}
           postUrl="/api/v1/${publicApiKey}/post/"
         />
       </head>
-      <body
-        class={{
-          'header-open': store.headerMenuOpen,
-          'menu-open': store.sideMenuOpen,
-        }}
-      >
+      <body lang="en">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>

aca9bced7dd7d1c43e53ab9b9c1116462469e993
Alek Vila <39844914+greatgraphicdesign@users.noreply.github.com>
2024-05-26 14:59:47 -0700

    Update use-resource.ts
    Duplicates the text changes that were approved for the core readme in this PR: https://github.com/QwikDev/qwik/pull/6354/files

--- packages/qwik/src/core/use/use-resource.ts
+++ packages/qwik/src/core/use/use-resource.ts
@@ -49,9 +49,11 @@ export interface ResourceOptions {
  *
  * The status can be one of the following:
  *
- * - 'pending' - the data is not yet available.
- * - 'resolved' - the data is available.
- * - 'rejected' - the data is not available due to an error or timeout.
+ * - `pending` - the data is not yet available.
+ * - `resolved` - the data is available.
+ * - `rejected` - the data is not available due to an error or timeout.
+ *
+ * Avoid using a `try/catch` statement in `useResource$`. If you catch the error instead of passing it, the resource status will never be `rejected`.
  *
  * ### Example
  *

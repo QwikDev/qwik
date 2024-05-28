74e5af5169e5ba81a8f034d8e556058ac895af73
maiieul <maieul.chevalier@gmail.com>
2024-05-28 19:42:43 +0200

    fix(jsx-generated): simplify input type

--- packages/qwik/src/core/render/jsx/types/jsx-generated.ts
+++ packages/qwik/src/core/render/jsx/types/jsx-generated.ts
@@ -599,37 +599,9 @@ type SpecialAttrs = {
     value?: string | ReadonlyArray<string> | number | undefined | null | FormDataEntryValue;
     width?: Size | undefined;
     children?: undefined;
-  } & (
-    | {
-        type?:
-          | Exclude<HTMLInputTypeAttribute, 'button' | 'reset' | 'submit' | 'checkbox' | 'radio'>
-          | undefined;
-        'bind:checked'?: undefined;
-      }
-    | {
-        type: 'button' | 'reset' | 'submit';
-        'bind:value'?: undefined;
-        'bind:checked'?: undefined;
-        autoComplete?: undefined;
-      }
-    | {
-        type: 'checkbox' | 'radio';
-        'bind:value'?: undefined;
-        autoComplete?: undefined;
-      }
-  ) &
-    (
-      | {
-          type?: Exclude<HTMLInputTypeAttribute, 'button'> | undefined;
-          popovertarget?: undefined;
-          popovertargetaction?: undefined;
-        }
-      | {
-          type: 'button';
-          popovertarget?: string | undefined;
-          popovertargetaction?: PopoverTargetAction | undefined;
-        }
-    );
+    popovertarget?: string | undefined;
+    popovertargetaction?: PopoverTargetAction | undefined;
+  };
   label: {
     form?: string | undefined;
     for?: string | undefined;

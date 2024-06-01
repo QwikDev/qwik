d52e28bef1b5ecabfa1bc64126cab77c1f21bf66
Shai Reznik <shairez@users.noreply.github.com>
2024-06-02 02:42:25 +0300

    test(locale): used proper selector

--- starters/e2e/qwikcity/locale.e2e.ts
+++ starters/e2e/qwikcity/locale.e2e.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "@playwright/test";
+import { QContainerSelector } from "../../../packages/qwik/src/core/util/markers";
 
 test.describe("Qwik City locale API", () => {
   test("pass locale to Qwik", async ({ page }) => {
     await page.goto("/qwikcity-test/locale");
     const locale = page.locator(".locale");
     await expect(locale).toHaveText("test-locale");
-    const qContainer = page.locator("[q\\:container=paused]");
+    const qContainer = page.locator(QContainerSelector);
     await expect(qContainer).toHaveAttribute("q:locale", "test-locale");
   });
 });

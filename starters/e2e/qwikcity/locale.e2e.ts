c69eb329c7f537ba09a058311ad1a2e7acda598c
Shai Reznik <shairez@users.noreply.github.com>
2024-06-01 03:13:47 +0300

    test: fix locale e2e test

--- starters/e2e/qwikcity/locale.e2e.ts
+++ starters/e2e/qwikcity/locale.e2e.ts
@@ -1,12 +1,11 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Qwik City locale API", () => {
-  // TODO(v2): fix this
-  test.skip("pass locale to Qwik", async ({ page }) => {
+  test("pass locale to Qwik", async ({ page }) => {
     await page.goto("/qwikcity-test/locale");
     const locale = page.locator(".locale");
-    const qContainer = page.locator("[q\\:container]");
     await expect(locale).toHaveText("test-locale");
+    const qContainer = page.locator("[q\\:container=paused]");
     await expect(qContainer).toHaveAttribute("q:locale", "test-locale");
   });
 });

5b4004b44efd692a87e1a353548e0f503d181d3f
jack shelton <me@jackshelton.com>
2024-06-01 10:45:11 -0500

    remove top bar

--- packages/docs/src/components/footer/footer.tsx
+++ packages/docs/src/components/footer/footer.tsx
@@ -1,11 +1,63 @@
 import { component$ } from '@builder.io/qwik';
 import BuilderContentComp from '../../components/builder-content';
 import { BUILDER_FOOTER_MODEL, BUILDER_PUBLIC_API_KEY } from '../../constants';
+import { QwikLogo } from '../svgs/qwik-logo';
 
 export const Footer = component$(() => {
   return (
-    <footer class="container mx-auto">
+    <>
+      <footer class="mx-auto">
+        <div class="flex justify-center flex-wrap max-w-screen-xl px-4">
+          <div class="flex-1">
+            <QwikLogo width={210} height={110} />
+          </div>
+          <div class="flex flex-1 gap-y-4 gap-x-20 text-xs">
+            <div class="flex flex-col gap-4 flex-1">
+              <a class="hover:text-interactive-blue" href="https://qwik.dev/docs/">
+                Docs
+              </a>
+              <a class="hover:text-interactive-blue" href="https://qwik.dev/docs/qwikcity/">
+                Qwik City
+              </a>
+              <a class="hover:text-interactive-blue" href="https://qwik.dev/ecosystem/">
+                Ecosystem
+              </a>
+              <a class="hover:text-interactive-blue" href="https://qwik.dev/playground/">
+                Playground
+              </a>
+            </div>
+            <div class="flex flex-col gap-4 flex-1">
+              <a
+                class="hover:text-interactive-blue"
+                href="https://qwik.dev/ecosystem/#integrations"
+              >
+                Integrations
+              </a>
+              <a class="hover:text-interactive-blue" href="https://qwik.dev/ecosystem/#deployments">
+                Deployments
+              </a>
+              <a class="hover:text-interactive-blue" href="">
+                Media
+              </a>
+              <a class="hover:text-interactive-blue" href="">
+                Showcase
+              </a>
+            </div>
+            <div class="flex flex-col gap-4 flex-1">
+              <a class="hover:text-interactive-blue" href="">
+                Tutorial
+              </a>
+              <a class="hover:text-interactive-blue" href="">
+                Presentations
+              </a>
+              <a class="hover:text-interactive-blue" href="">
+                Community
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
       <BuilderContentComp apiKey={BUILDER_PUBLIC_API_KEY} model={BUILDER_FOOTER_MODEL} tag="div" />
-    </footer>
+    </>
   );
 });
--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,7 +1,13 @@
+const { colors } = require('@mui/material');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './routes/**/*.{md,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'interactive-blue': '#009dfd',
+      },
+    },
   },
   plugins: [],
 };

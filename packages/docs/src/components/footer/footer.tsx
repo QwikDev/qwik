a03b2c78d1e5234ce66661d8cb1b273000974f99
jack shelton <me@jackshelton.com>
2024-06-01 21:01:43 -0500

    feat: new footer

--- packages/docs/src/components/footer/footer.tsx
+++ packages/docs/src/components/footer/footer.tsx
@@ -1,63 +1,98 @@
 import { component$ } from '@builder.io/qwik';
-import BuilderContentComp from '../../components/builder-content';
-import { BUILDER_FOOTER_MODEL, BUILDER_PUBLIC_API_KEY } from '../../constants';
-import { QwikLogo } from '../svgs/qwik-logo';
+import { QwikLogo } from '~/components/svgs/qwik-logo';
+import { DiscordLogo } from '~/components/svgs/discord-logo';
+import { GithubLogo } from '~/components/svgs/github-logo';
+import { TwitterLogo } from '~/components/svgs/twitter-logo';
+
+const baseUrl = 'https://qwik.dev';
+const linkColumns = [
+  [
+    { title: 'Docs', href: `${baseUrl}/docs/` },
+    { title: 'Qwik City', href: `${baseUrl}/docs/qwikcity/` },
+    { title: 'Ecosystem', href: `${baseUrl}/ecosystem/` },
+    { title: 'Playground', href: `${baseUrl}/playground/` },
+  ],
+  [
+    { title: 'Integrations', href: `${baseUrl}/ecosystem/#integrations` },
+    { title: 'Deployments', href: `${baseUrl}/ecosystem/#deployments` },
+    { title: 'Media', href: `${baseUrl}/ecosystem/#videos` },
+    { title: 'Showcase', href: `${baseUrl}/showcase/` },
+  ],
+  [
+    { title: 'Tutorial', href: `${baseUrl}/ecosystem/#courses` },
+    { title: 'Presentations', href: `${baseUrl}/ecosystem/#presentations` },
+    { title: 'Community', href: `${baseUrl}/ecosystem/#community` },
+  ],
+];
 
 export const Footer = component$(() => {
   return (
-    <>
-      <footer class="mx-auto">
-        <div class="flex justify-center flex-wrap max-w-screen-xl px-4">
-          <div class="flex-1">
-            <QwikLogo width={210} height={110} />
+    <footer class="mb-12 mt-10">
+      <div class="flex justify-center my-12">
+        <div class="flex flex-grow justify-center flex-wrap max-w-screen-xl flex-col sm:flex-row gap-y-4">
+          <div class="flex-1 sm:flex-[2] flex sm:justify-center sm:block mb-4 sm:mb-0">
+            <QwikLogo width={170} height={54} />
           </div>
-          <div class="flex flex-1 gap-y-4 gap-x-20 text-xs">
-            <div class="flex flex-col gap-4 flex-1">
-              <a class="hover:text-interactive-blue" href="https://qwik.dev/docs/">
-                Docs
-              </a>
-              <a class="hover:text-interactive-blue" href="https://qwik.dev/docs/qwikcity/">
-                Qwik City
-              </a>
-              <a class="hover:text-interactive-blue" href="https://qwik.dev/ecosystem/">
-                Ecosystem
-              </a>
-              <a class="hover:text-interactive-blue" href="https://qwik.dev/playground/">
-                Playground
-              </a>
-            </div>
-            <div class="flex flex-col gap-4 flex-1">
-              <a
-                class="hover:text-interactive-blue"
-                href="https://qwik.dev/ecosystem/#integrations"
-              >
-                Integrations
-              </a>
-              <a class="hover:text-interactive-blue" href="https://qwik.dev/ecosystem/#deployments">
-                Deployments
-              </a>
-              <a class="hover:text-interactive-blue" href="">
-                Media
-              </a>
-              <a class="hover:text-interactive-blue" href="">
-                Showcase
-              </a>
-            </div>
-            <div class="flex flex-col gap-4 flex-1">
-              <a class="hover:text-interactive-blue" href="">
-                Tutorial
-              </a>
-              <a class="hover:text-interactive-blue" href="">
-                Presentations
-              </a>
-              <a class="hover:text-interactive-blue" href="">
-                Community
-              </a>
-            </div>
+          <div class="flex flex-1 sm:flex-[3] max-w-screen-sm flex-wrap gap-y-8 gap-x-20 text-xs sm:px-4">
+            <FooterLinks />
           </div>
         </div>
-      </footer>
-      <BuilderContentComp apiKey={BUILDER_PUBLIC_API_KEY} model={BUILDER_FOOTER_MODEL} tag="div" />
+      </div>
+      <div class="flex justify-center">
+        <div class="max-w-screen-xl w-full">
+          <div class="italic text-xs">
+            <p>
+              Made with <span class="not-italic">❤️</span> by
+            </p>
+            <p class="text-xl sm:text-2xl">
+              The <strong>Qwik</strong> Team
+            </p>
+          </div>
+          <div class="border border-b-0 border-current mt-2"></div>
+          <div class="flex justify-between items-end">
+            <p class="text-xs">MIT License © 2024</p>
+            <FooterSocialLinks />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+});
+
+export const FooterLinks = component$(() => {
+  return (
+    <>
+      {linkColumns.map((column, colIndex) => (
+        <div key={colIndex} class="flex flex-col gap-4 flex-1">
+          {column.map((link, linkIndex) => (
+            <a key={linkIndex} class="hover:text-interactive-blue" href={link.href}>
+              {link.title}
+            </a>
+          ))}
+        </div>
+      ))}
     </>
+  );
+});
+
+export const FooterSocialLinks = component$(() => {
+  const socialLinks = [
+    { href: 'https://qwik.dev/chat', title: 'Discord', Logo: DiscordLogo },
+    { href: 'https://github.com/QwikDev/qwik', title: 'GitHub', Logo: GithubLogo },
+    { href: 'https://twitter.com/QwikDev', title: 'Twitter', Logo: TwitterLogo },
+  ];
+
+  return (
+    <div class="flex gap-4 mt-4">
+      {socialLinks.map(({ title, href, Logo }) => (
+        <li key={title} class="list-none">
+          <a href={href} target="_blank" title={title}>
+            <span>
+              <Logo width={22} height={22} />
+            </span>
+          </a>
+        </li>
+      ))}
+    </div>
   );
 });

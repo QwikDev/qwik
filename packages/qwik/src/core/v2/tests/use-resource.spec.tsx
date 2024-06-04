385bcb8818d8598598aae84e33159b6bca6fd621
Varixo <admin@varixo.pl>
2024-06-04 16:33:29 +0200

    add failing tests

--- packages/qwik/src/core/v2/tests/use-resource.spec.tsx
+++ packages/qwik/src/core/v2/tests/use-resource.spec.tsx
@@ -3,6 +3,7 @@ import {
   Fragment as Component,
   Fragment,
   Fragment as InlineComponent,
+  Fragment as Signal,
   Resource,
   component$,
   useResource$,
@@ -82,6 +83,109 @@ describe.each([
             <span>1</span>
           </InlineComponent>
         </button>
+      </Component>
+    );
+  });
+
+  it('should show loading state', async () => {
+    const ResourceCmp = component$(() => {
+      const count = useSignal(0);
+      const rsrc = useResource$(async ({ track }) => {
+        const value = track(() => count.value);
+        await delay(10);
+        return value;
+      });
+      return (
+        <button onClick$={() => count.value++}>
+          <Resource
+            value={rsrc}
+            onResolved={(v) => <span>{v}</span>}
+            onPending={() => <span>loading</span>}
+          />
+        </button>
+      );
+    });
+
+    const { vNode, container } = await render(<ResourceCmp />, { debug });
+    // TODO: we should send the loading state for ssr instead of waiting for the promise
+    expect(vNode).toMatchVDOM(
+      <Component>
+        <button>
+          <InlineComponent>
+            <span>loading</span>
+          </InlineComponent>
+        </button>
+      </Component>
+    );
+
+    await trigger(container.element, 'button', 'click');
+    await delay(30);
+    expect(vNode).toMatchVDOM(
+      <Component>
+        <button>
+          <InlineComponent>
+            <span>1</span>
+          </InlineComponent>
+        </button>
+      </Component>
+    );
+  });
+
+  it('should handle multiple the same resource tasks', async () => {
+    const ResourceRaceCondition = component$(() => {
+      const count = useSignal(0);
+      const resource = useResource$<number>(async ({ track }) => {
+        track(count);
+        const value = count.value;
+        // console.log('scheduled', value);
+        if (count.value === 1) {
+          await delay(30);
+        }
+        // console.log('return', value);
+        return value;
+      });
+
+      return (
+        <>
+          <button onClick$={() => count.value++}>{count.value}</button>
+          <Resource value={resource} onResolved={(data) => <div>{data}</div>} />
+        </>
+      );
+    });
+
+    const { vNode, container } = await render(<ResourceRaceCondition />, { debug });
+    expect(vNode).toMatchVDOM(
+      <Component>
+        <Fragment>
+          <button>
+            <Signal>0</Signal>
+          </button>
+          <InlineComponent>
+            <Fragment>
+              <Awaited>
+                <div>0</div>
+              </Awaited>
+            </Fragment>
+          </InlineComponent>
+        </Fragment>
+      </Component>
+    );
+    // double click
+    await trigger(container.element, 'button', 'click');
+    await delay(10);
+    await trigger(container.element, 'button', 'click');
+    // wait for the second resource to finish
+    await delay(50);
+    expect(vNode).toMatchVDOM(
+      <Component>
+        <Fragment>
+          <button>
+            <Signal>2</Signal>
+          </button>
+          <InlineComponent>
+            <div>2</div>
+          </InlineComponent>
+        </Fragment>
       </Component>
     );
   });

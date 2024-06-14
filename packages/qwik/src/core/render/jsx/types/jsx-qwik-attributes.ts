f3bab0727b3272b48b8a128b7bbb20a57869747f
Jerry_wu <409187100@qq.com>
2024-06-14 15:53:37 +0800

    fix: correct onSubmit$ / onSubmitComplete$ type signature (#6542)
    * fix: fix a ts issue
    
    * fix: fix a ts issue
    
    * fix: fix a ts issue
    
    * fix: fix a ts issue
    
    ---------
    
    Co-authored-by: wuls <linsheng.wu@beantechs.com>

--- packages/qwik/src/core/render/jsx/types/jsx-qwik-attributes.ts
+++ packages/qwik/src/core/render/jsx/types/jsx-qwik-attributes.ts
@@ -178,7 +178,8 @@ export type QRLEventHandlerMulti<EV extends Event, EL> =
   | QRL<EventHandler<EV, EL>>
   | undefined
   | null
-  | QRLEventHandlerMulti<EV, EL>[];
+  | QRLEventHandlerMulti<EV, EL>[]
+  | EventHandler<EV, EL>;
 
 type QwikCustomEvents<EL> = {
   /**

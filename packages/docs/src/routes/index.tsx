f9c956d60505bcbf3d30f33b5421f248f4b8251b
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2024-06-15 17:48:07 -0500

    docs: comment out extra sponsor component (#6555)
    comment out footer sponsors on homepage

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -3,7 +3,6 @@ import { type DocumentHead } from '@builder.io/qwik-city';
 import BuilderContentComp from '../components/builder-content';
 import { Footer } from '../components/footer/footer';
 import { Header } from '../components/header/header';
-import { Sponsors } from '~/components/sponsors/sponsors';
 import { QWIK_MODEL, QWIK_PUBLIC_API_KEY } from '../constants';
 
 export default component$(() => {
@@ -12,7 +11,6 @@ export default component$(() => {
       <Header />
       <main>
         <BuilderContentComp apiKey={QWIK_PUBLIC_API_KEY} model={QWIK_MODEL} tag="main" />
-        <Sponsors />
       </main>
       <div class="px-4">
         <Footer />

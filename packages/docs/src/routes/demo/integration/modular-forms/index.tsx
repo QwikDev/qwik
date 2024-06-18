467b956d7d0c80b57754b0cf42b5523ed46875e1
Ahmed Zougari <zougariahmed007@gmail.com>
2024-06-18 23:30:12 +0100

    chore(valibot): change minLength with nonEmpty

--- packages/docs/src/routes/demo/integration/modular-forms/index.tsx
+++ packages/docs/src/routes/demo/integration/modular-forms/index.tsx
@@ -9,12 +9,12 @@ import * as v from 'valibot';
 const LoginSchema = v.object({
   email: v.pipe(
     v.string(),
-    v.minLength(1, 'Please enter your email.'),
+    v.nonEmpty('Please enter your email.'),
     v.email('The email address is badly formatted.')
   ),
   password: v.pipe(
     v.string(),
-    v.minLength(1, 'Please enter your password.'),
+    v.nonEmpty('Please enter your password.'),
     v.minLength(8, 'Your password must have 8 characters or more.')
   ),
 });

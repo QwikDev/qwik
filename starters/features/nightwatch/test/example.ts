d216780e1ac392f8590dd4d9f43fa78180aadec0
Priyansh Garg <priyanshgarg30@gmail.com>
2024-06-25 01:53:37 +0530

    Add missing example test.

---
+++ starters/features/nightwatch/test/example.ts
@@ -0,0 +1,40 @@
+describe("Nightwatch demo test", () => {
+  before(async function (browser) {
+    // navigate to baseUrl
+    await browser.navigateTo("/");
+  });
+
+  it("should have correct title", async function (browser) {
+    // assert the title
+    const title = await browser.getTitle();
+    browser.assert.equal(title, "Welcome to Qwik");
+
+    // other way of asserting on title
+    browser.assert.titleContains("Qwik");
+  });
+
+  it("test the working of counter", async () => {
+    // find element by text using xpath
+    const countUpButton = browser.element.find(by.xpath('//*[text()="+"]'));
+
+    // another better way to find element by text
+    const countDownButton = browser.element.findByText("-");
+
+    // get the counter element (next sibling of the `-` element)
+    const counterElement = countDownButton.getNextElementSibling();
+
+    // assert the current value of the counter
+    counterElement.getText().assert.equals("70");
+
+    // click the "+" button 5 times
+    for (let i = 0; i < 5; i++) {
+      countUpButton.click();
+    }
+
+    // uncomment the below line to visualize the change
+    // browser.pause(2000);
+
+    // assert the new value of the counter
+    counterElement.getText().assert.equals("75");
+  });
+});

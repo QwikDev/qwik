ca5287aca42927ceb00cd9f66f31e0aebf0d7635
wuls <linsheng.wu@beantechs.com>
2024-07-02 14:06:35 +0800

    fix: there is a bug when we change DEBUG into true

--- packages/qwik/src/core/v2/client/vnode.ts
+++ packages/qwik/src/core/v2/client/vnode.ts
@@ -140,7 +140,7 @@ import {
   QStyle,
   QStylesAllSelector,
 } from '../../util/markers';
-import { isHtmlElement, isObject } from '../../util/types';
+import { isHtmlElement } from '../../util/types';
 import { DEBUG_TYPE, QContainerValue, VirtualType, VirtualTypeName } from '../shared/types';
 import { VNodeDataChar } from '../shared/vnode-data-types';
 import { getDomContainer, _getQContainerElement } from './dom-container';
@@ -1880,15 +1880,15 @@ const stringify = (value: any): any => {
       }
     } else if (Array.isArray(value)) {
       return '[' + value.map(stringify).join(', ') + ']';
-    } if (value instanceof SignalImpl) {
-      return stringify(value.value)
+    }
+    if (value instanceof SignalImpl) {
+      return stringify(value.value);
     } else {
-      if(value.toString){
+      if (value.toString) {
         return String(value);
-      }else {
-        return JSON.stringify(value)
+      } else {
+        return JSON.stringify(value);
       }
-      
     }
   } finally {
     stringifyPath.pop();

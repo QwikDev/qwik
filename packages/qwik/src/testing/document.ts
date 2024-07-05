35be2b21022c36046a7ca2cc6c7e214b66216eae
Arly Xiao <kingla_pei@163.com>
2024-07-06 02:33:59 +0800

    refactor: tiny adjustment for document testing (#6647)
    * refactor: tiny adjustment for document testing
    
    * Update condition check order
    
    ---------
    
    Co-authored-by: Arly Xiao <shaofu.xiao@bsci.com>

--- packages/qwik/src/testing/document.ts
+++ packages/qwik/src/testing/document.ts
@@ -20,53 +20,54 @@ export function createDocument(opts?: MockDocumentOptions) {
  * @public
  */
 export function createWindow(opts: MockDocumentOptions = {}): MockWindow {
-  const win = createDocument(opts).defaultView!;
-  return win;
+  return createDocument(opts).defaultView!;
 }
 
 export function ensureGlobals(doc: any, opts?: MockDocumentOptions) {
-  if (!doc[QWIK_DOC]) {
-    if (!doc || doc.nodeType !== 9) {
-      throw new Error(`Invalid document`);
-    }
-
-    doc[QWIK_DOC] = true;
-
-    const loc = normalizeUrl(opts?.url);
-
-    Object.defineProperty(doc, 'baseURI', {
-      get: () => loc.href,
-      set: (url: string) => (loc.href = normalizeUrl(url).href),
-    });
-
-    doc.defaultView = {
-      get document() {
-        return doc;
-      },
-      get location() {
-        return loc;
-      },
-      get origin() {
-        return loc.origin;
-      },
-      addEventListener: noop,
-      removeEventListener: noop,
-      history: {
-        pushState: noop,
-        replaceState: noop,
-        go: noop,
-        back: noop,
-        forward: noop,
-      },
-      CustomEvent: class CustomEvent {
-        type: string;
-        constructor(type: string, details: any) {
-          Object.assign(this, details);
-          this.type = type;
-        }
-      },
-    };
+  if (doc && doc[QWIK_DOC]) {
+    return doc.defaultView;
   }
+
+  if (!doc || doc.nodeType !== 9) {
+    throw new Error(`Invalid document`);
+  }
+
+  doc[QWIK_DOC] = true;
+
+  const loc = normalizeUrl(opts?.url);
+
+  Object.defineProperty(doc, 'baseURI', {
+    get: () => loc.href,
+    set: (url: string) => (loc.href = normalizeUrl(url).href),
+  });
+
+  doc.defaultView = {
+    get document() {
+      return doc;
+    },
+    get location() {
+      return loc;
+    },
+    get origin() {
+      return loc.origin;
+    },
+    addEventListener: noop,
+    removeEventListener: noop,
+    history: {
+      pushState: noop,
+      replaceState: noop,
+      go: noop,
+      back: noop,
+      forward: noop,
+    },
+    CustomEvent: class CustomEvent {
+      type: string;
+      constructor(type: string, details: any) {
+        Object.assign(this, details);
+        this.type = type;
+      }
+    },
+  };
 
   return doc.defaultView;
 }

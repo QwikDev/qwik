c9954edf0168e55254b5ca23b8b05ed9545a2ad6
Varixo <admin@varixo.pl>
2024-07-15 14:26:53 +0200

    fix(v2): mutating props

--- packages/qwik/src/core/util/flyweight.ts
+++ packages/qwik/src/core/util/flyweight.ts
@@ -1,11 +1,5 @@
-// import { qDev } from './qdev';
-
-import { qDev } from './qdev';
-
 export const EMPTY_ARRAY = [] as any[];
 export const EMPTY_OBJ = {} as Record<string, any>;
 
-if (qDev) {
-  Object.freeze(EMPTY_ARRAY);
-  Object.freeze(EMPTY_OBJ);
-}
+Object.freeze(EMPTY_ARRAY);
+Object.freeze(EMPTY_OBJ);
--- packages/qwik/src/core/v2/ssr/ssr-render-jsx.ts
+++ packages/qwik/src/core/v2/ssr/ssr-render-jsx.ts
@@ -141,7 +141,7 @@ function processJSXNode(
         enqueue(value[i]);
       }
     } else if (isSignal(value)) {
-      ssr.openFragment(isDev ? [DEBUG_TYPE, VirtualType.DerivedSignal] : EMPTY_ARRAY);
+      ssr.openFragment(isDev ? [DEBUG_TYPE, VirtualType.DerivedSignal] : []);
       const signalNode = ssr.getLastNode() as fixMeAny;
       // TODO(mhevery): It is unclear to me why we need to serialize host for SignalDerived.
       // const host = ssr.getComponentFrame(0)!.componentNode as fixMeAny;
@@ -277,7 +277,7 @@ function processJSXNode(
           isPromise(jsxOutput) && enqueue(Promise);
           enqueue(new SetScopedStyle(compStyleComponentId));
         } else {
-          ssr.openFragment(isDev ? [DEBUG_TYPE, VirtualType.InlineComponent] : EMPTY_ARRAY);
+          ssr.openFragment(isDev ? [DEBUG_TYPE, VirtualType.InlineComponent] : []);
           enqueue(ssr.closeFragment);
           const component = ssr.getComponentFrame(0)!;
           const jsxOutput = applyInlineComponent(

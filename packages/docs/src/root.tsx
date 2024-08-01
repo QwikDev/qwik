7150b266ec07ee6d0011fcd1e8b403df0ee478ea
Wout Mertens <Wout.Mertens@gmail.com>
2024-08-01 14:38:32 +0200

    chore(docs): use prefetch service worker

--- packages/docs/src/root.tsx
+++ packages/docs/src/root.tsx
@@ -1,5 +1,11 @@
-import { component$, useContextProvider, useStore } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import {
+  PrefetchGraph,
+  PrefetchServiceWorker,
+  component$,
+  useContextProvider,
+  useStore,
+} from '@builder.io/qwik';
+import { QwikCityProvider, RouterOutlet } from '@builder.io/qwik-city';
 import RealMetricsOptimization from './components/real-metrics-optimization/real-metrics-optimization';
 import { RouterHead } from './components/router-head/router-head';
 import { GlobalStore, type SiteStore } from './context';
@@ -54,7 +60,8 @@ export default component$(() => {
         <meta charset="utf-8" />
         <script dangerouslySetInnerHTML={uwu} />
         <RouterHead />
-        <ServiceWorkerRegister />
+        <PrefetchServiceWorker />
+        <PrefetchGraph />
         {/* <script dangerouslySetInnerHTML={`(${collectSymbols})()`} /> */}
         <Insights publicApiKey={import.meta.env.PUBLIC_QWIK_INSIGHTS_KEY} />
       </head>

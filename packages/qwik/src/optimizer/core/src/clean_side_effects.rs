dd22c6e03d879de60688c7e42a77aa5720438ce9
Wout Mertens <Wout.Mertens@gmail.com>
2024-08-01 19:44:05 +0200

    fix(optimizer): correct side effect cleaner
    The existing side effects should be retained so that e.g. serviceworker entries don't get cleared out (they are only side effects)

--- packages/qwik/src/optimizer/core/src/clean_side_effects.rs
+++ packages/qwik/src/optimizer/core/src/clean_side_effects.rs
@@ -1,5 +1,11 @@
-use swc_common::Mark;
-use swc_ecmascript::ast;
+use std::cell::RefCell;
+use std::collections::HashSet;
+use std::rc::Rc;
+
+use swc_common::Span;
+use swc_common::Spanned;
+use swc_ecmascript::ast::Module;
+use swc_ecmascript::ast::{Expr, ModuleItem, Stmt};
 use swc_ecmascript::visit::VisitMut;
 
 /// This is a simple treeshaker that removes anything with side effects from the module.
@@ -7,48 +13,51 @@ use swc_ecmascript::visit::VisitMut;
 /// - `new` expressions
 /// - `call` expressions
 ///
-/// but only when they are not assigned and used elsewhere.
+/// but only when they are not assigned and used elsewhere, or when they were already present before simplifying.
 ///
 /// - First it marks top-level expressions
 /// - Then the code needs to be simplified by you
 /// - Then it removes all top-level expressions that are not marked. Those will be expressions that were
 ///   assigned to unused variables etc.
+
 pub struct Treeshaker {
 	pub marker: CleanMarker,
 	pub cleaner: CleanSideEffects,
 }
 
-pub struct CleanSideEffects {
-	pub did_drop: bool,
-	pub mark: Mark,
+pub struct CleanMarker {
+	set: Rc<RefCell<HashSet<Span>>>,
 }
 
-pub struct CleanMarker {
-	pub mark: Mark,
+pub struct CleanSideEffects {
+	pub did_drop: bool,
+	set: Rc<RefCell<HashSet<Span>>>,
 }
 
 impl Treeshaker {
 	pub fn new() -> Self {
-		let mark = Mark::new();
+		let set = Rc::new(RefCell::new(HashSet::new()));
 		Self {
-			marker: CleanMarker { mark },
+			marker: CleanMarker {
+				set: Rc::clone(&set),
+			},
 			cleaner: CleanSideEffects {
 				did_drop: false,
-				mark,
+				set: Rc::clone(&set),
 			},
 		}
 	}
 }
 
 impl VisitMut for CleanMarker {
-	fn visit_mut_module_item(&mut self, node: &mut ast::ModuleItem) {
-		if let ast::ModuleItem::Stmt(ast::Stmt::Expr(expr)) = node {
+	fn visit_mut_module_item(&mut self, node: &mut ModuleItem) {
+		if let ModuleItem::Stmt(Stmt::Expr(expr)) = node {
 			match &*expr.expr {
-				ast::Expr::New(e) => {
-					e.ctxt.apply_mark(self.mark);
+				Expr::New(e) => {
+					self.set.borrow_mut().insert(e.span());
 				}
-				ast::Expr::Call(e) => {
-					e.ctxt.apply_mark(self.mark);
+				Expr::Call(e) => {
+					self.set.borrow_mut().insert(e.span());
 				}
 				_ => {}
 			}
@@ -57,18 +66,18 @@ impl VisitMut for CleanMarker {
 }
 
 impl VisitMut for CleanSideEffects {
-	fn visit_mut_module(&mut self, node: &mut ast::Module) {
+	fn visit_mut_module(&mut self, node: &mut Module) {
 		node.body.retain(|item| match item {
-			ast::ModuleItem::Stmt(ast::Stmt::Expr(expr)) => match &*expr.expr {
-				ast::Expr::New(e) => {
-					if e.ctxt.has_mark(self.mark) {
+			ModuleItem::Stmt(Stmt::Expr(expr)) => match &*expr.expr {
+				Expr::New(e) => {
+					if self.set.borrow().contains(&e.span()) {
 						return true;
 					}
 					self.did_drop = true;
 					false
 				}
-				ast::Expr::Call(e) => {
-					if e.ctxt.has_mark(self.mark) {
+				Expr::Call(e) => {
+					if self.set.borrow().contains(&e.span()) {
 						return true;
 					}
 					self.did_drop = true;

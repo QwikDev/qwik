a9b200527aaf4a83407ab83fc9b2709aac4a801e
wuls <linsheng.wu@beantechs.com>
2024-08-26 09:15:46 +0800

    fix bug

--- packages/qwik/src/optimizer/src/plugins/plugin.ts
+++ packages/qwik/src/optimizer/src/plugins/plugin.ts
@@ -428,7 +428,7 @@ export function createPlugin(optimizerOptions: OptimizerOptions = {}) {
         };
       }
 
-      const firstInput = Object.values(opts.input || [])[0];
+      const firstInput = opts.input && Object.values(opts.input)[0];
       return {
         id: normalizePath(getPath().resolve(firstInput, QWIK_CLIENT_MANIFEST_ID)),
         moduleSideEffects: false,
@@ -574,7 +574,7 @@ export function createPlugin(optimizerOptions: OptimizerOptions = {}) {
       debug(`load()`, 'Found', id);
       let { code } = transformedModule[0];
       const { map, segment } = transformedModule[0];
-      const firstInput = Object.values(opts.input || [])[0];
+      const firstInput = opts.input && Object.values(opts.input)[0];
 
       if (devServer && firstInput) {
         // doing this because vite will not use resolveId() when "noExternal" is false

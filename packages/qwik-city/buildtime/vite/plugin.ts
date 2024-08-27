18cffefc17903050c46b9001365ade7ff06fcb36
maiieul <maieul.chevalier@gmail.com>
2024-08-27 14:55:36 +0200

    Revert "fix(qwikcity dev): do not serve dist/ during dev"
    This reverts commit 5595af1a4eb69ca3453579a6d6035d9d4c860e5e.

--- packages/qwik-city/buildtime/vite/plugin.ts
+++ packages/qwik-city/buildtime/vite/plugin.ts
@@ -84,7 +84,7 @@ function qwikCityPlugin(userOpts?: QwikCityVitePluginOptions): any {
 
       ctx = createBuildContext(rootDir!, config.base, userOpts, target);
 
-      ctx.isDevServer = config.command === 'serve' && config.mode !== 'production';
+      ctx.isDevServer = config.command === 'serve';
       ctx.isDevServerClientOnly = ctx.isDevServer && config.mode !== 'ssr';
 
       await validatePlugin(ctx.opts);
@@ -105,17 +105,14 @@ function qwikCityPlugin(userOpts?: QwikCityVitePluginOptions): any {
 
     configureServer(server) {
       return () => {
-        if (!ctx) {
-          throw new Error('configureServer: Missing ctx from configResolved');
+        // handles static files physically found in the dist directory
+        server.middlewares.use(staticDistMiddleware(server));
+        if (ctx) {
+          // qwik city middleware injected BEFORE vite internal middlewares
+          // and BEFORE @builder.io/qwik/optimizer/vite middlewares
+          // handles only known user defined routes
+          server.middlewares.use(ssrDevMiddleware(ctx, server));
         }
-        if (!ctx.isDevServer) {
-          // preview server: serve static files from the dist directory
-          server.middlewares.use(staticDistMiddleware(server));
-        }
-        // qwik city middleware injected BEFORE vite internal middlewares
-        // and BEFORE @builder.io/qwik/optimizer/vite middlewares
-        // handles only known user defined routes
-        server.middlewares.use(ssrDevMiddleware(ctx, server));
       };
     },
 

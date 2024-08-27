2e09636e7198c9331511a7bf04ce944bbc4002c1
maiieul <maieul.chevalier@gmail.com>
2024-08-27 13:35:02 +0200

    Revert "chore: update vite and vitest"
    This reverts commit 42880cf5f1ae61ec47fbf17d4cbd426569426a51.

--- starters/playwright.config.ts
+++ starters/playwright.config.ts
@@ -1,8 +1,6 @@
 import { expect } from "@playwright/test";
 import type { Locator, PlaywrightTestConfig } from "@playwright/test";
 
-const inGithubCI = !!process.env.GITHUB_ACTIONS;
-
 expect.extend({
   async hasAttribute(recieved: Locator, attribute: string) {
     const pass = await recieved.evaluate((node, attribute) => {
@@ -25,8 +23,8 @@ const config: PlaywrightTestConfig = {
     },
   },
   testIgnore: /.*example.spec.tsx?$/,
-  retries: inGithubCI ? 0 : 1,
-  expect: { timeout: inGithubCI ? 120000 : 10000 },
+  retries: 3,
+  expect: { timeout: 10000 },
   webServer: {
     command: "pnpm tsm ./starters/dev-server.ts 3301",
     port: 3301,

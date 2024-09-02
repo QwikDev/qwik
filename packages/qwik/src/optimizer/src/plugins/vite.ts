47ccf348bbfaebcc97a042c0e3122c5e67749ca8
thejackshelton <me@jackshelton.com>
2024-09-02 14:30:26 -0500

    fix: qwikVite transforming id's with ?rawe

--- packages/qwik/src/optimizer/src/plugins/vite.ts
+++ packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -490,6 +490,11 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
       if (id.startsWith('\0') || !fileFilter(id, 'transform')) {
         return null;
       }
+
+      if (id.endsWith('?raw')) {
+        return null;
+      }
+
       if (isClientDevOnly) {
         const parsedId = parseId(id);
         if (parsedId.params.has(VITE_DEV_CLIENT_QS)) {

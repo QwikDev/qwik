b4bfb0a7b020f0a9330dfd60ef2cf13001144464
Shai Reznik <shairez@users.noreply.github.com>
2024-09-05 19:26:28 +0300

    covered prefetch queue logic with tests

--- packages/qwik-city/src/runtime/src/service-worker/prefetch.unit.ts
+++ packages/qwik-city/src/runtime/src/service-worker/prefetch.unit.ts
@@ -1,5 +1,7 @@
-import { test } from 'vitest';
-import { prefetchBundleNames } from './prefetch';
+import { describe } from 'node:test';
+import { afterEach, expect, test } from 'vitest';
+import { prefetchQueue } from './constants';
+import { addBundlesToPrefetchQueue, prefetchBundleNames } from './prefetch';
 import type { AppBundle, Fetch } from './types';
 
 function getStubWorkerCache() {
@@ -31,7 +33,118 @@ function createFakeFetch(): Fetch {
   1. imports 
 */
 
-test('getCacheToDelete, delete bundles no longer possible', () => {
+afterEach(() => {
+  prefetchQueue.length = 0;
+});
+
+const urlPrefix = 'http://localhost';
+
+describe('addBundlesToPrefetchQueue', () => {
+  test(`GIVEN 3 modules with 2 imports each
+        WHEN attempting to prefetch the first 3 modules
+        THEN 9 modules should be added to the prefetching queue`, () => {
+    const fakeBaseUrl = new URL(urlPrefix);
+    const isHighPriority = false;
+
+    const fakeAppBundles: AppBundle[] = [
+      ['a.js', [1, 2]],
+      ['b.js', []],
+      ['c.js', []],
+      ['d.js', [4, 5]],
+      ['e.js', []],
+      ['f.js', []],
+      ['g.js', [7, 8]],
+      ['h.js', []],
+      ['i.js', []],
+    ];
+
+    const bundlesToPrefetch = ['a.js', 'd.js', 'g.js'];
+
+    addBundlesToPrefetchQueue(bundlesToPrefetch, fakeAppBundles, fakeBaseUrl, isHighPriority);
+
+    const expectedResult = [
+      'a.js',
+      'b.js',
+      'c.js',
+      'd.js',
+      'e.js',
+      'f.js',
+      'g.js',
+      'h.js',
+      'i.js',
+    ].map((bundle) => `${urlPrefix}/${bundle}`);
+
+    expect(prefetchQueue).toEqual(expectedResult);
+  });
+  test(`GIVEN 5 modules each importing the following one
+        WHEN attempting to prefetch the first module
+        THEN all 5 modules should be added to the prefetching queue`, () => {
+    const fakeBaseUrl = new URL(urlPrefix);
+    const isHighPriority = false;
+
+    const fakeAppBundles: AppBundle[] = [
+      ['a.js', [1]],
+      ['b.js', [2]],
+      ['c.js', [3]],
+      ['d.js', [4]],
+      ['e.js', []],
+    ];
+
+    const bundlesToPrefetch = ['a.js'];
+
+    addBundlesToPrefetchQueue(bundlesToPrefetch, fakeAppBundles, fakeBaseUrl, isHighPriority);
+
+    const expectedResult = ['a.js', 'b.js', 'c.js', 'd.js', 'e.js'].map(
+      (bundle) => `${urlPrefix}/${bundle}`
+    );
+
+    expect(prefetchQueue).toEqual(expectedResult);
+  });
+  test(`GIVEN prefetch queue already has 2 modules
+        WHEN attempting to prefetch again the second module from the queue but with a high priority
+        THEN the second module should be added in front of the first`, () => {
+    const fakeBaseUrl = new URL(urlPrefix);
+    const isHighPriority = true;
+
+    const fakeAppBundles: AppBundle[] = [
+      ['a.js', []],
+      ['b.js', []],
+    ];
+
+    prefetchQueue.push(`${urlPrefix}/a.js`, `${urlPrefix}/b.js`);
+
+    const bundlesToPrefetch = ['b.js'];
+
+    addBundlesToPrefetchQueue(bundlesToPrefetch, fakeAppBundles, fakeBaseUrl, isHighPriority);
+
+    const expectedResult = ['b.js', 'a.js'].map((bundle) => `${urlPrefix}/${bundle}`);
+
+    expect(prefetchQueue).toEqual(expectedResult);
+  });
+  test(`GIVEN prefetch queue already has module "a"
+        WHEN attempting to prefetch module "b" with a high priority
+        THEN module "b" should be added in front of "a"`, () => {
+    const fakeBaseUrl = new URL(urlPrefix);
+    const isHighPriority = true;
+
+    const fakeAppBundles: AppBundle[] = [
+      ['a.js', []],
+      ['b.js', []],
+    ];
+
+    prefetchQueue.push(`${urlPrefix}/a.js`);
+
+    const bundlesToPrefetch = ['b.js'];
+
+    addBundlesToPrefetchQueue(bundlesToPrefetch, fakeAppBundles, fakeBaseUrl, isHighPriority);
+
+    const expectedResult = ['b.js', 'a.js'].map((bundle) => `${urlPrefix}/${bundle}`);
+
+    expect(prefetchQueue).toEqual(expectedResult);
+  });
+});
+
+test.skip('getCacheToDelete, delete bundles no longer possible', () => {
   const appBundles: AppBundle[] = createLongAppBundels();
   const bundlesToPrefetch = createBundlesToPrefetch();
   const fakeBaseUrl = new URL('');

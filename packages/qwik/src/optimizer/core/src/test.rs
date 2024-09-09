ea3ad25f1d7dadd5537eb2cc50e5922e169769eb
Wout Mertens <Wout.Mertens@gmail.com>
2024-09-09 15:32:40 +0200

    feat(optimizer): don't qwik-transform lib builds (#6850)
    * feat(optimizer): don't qwik-transform lib builds
    
    this way libraries won't use internal APIs that may break between minor versions
    
    * formatted changeset
    
    ---------
    
    Co-authored-by: Shai Reznik <shairez@users.noreply.github.com>

--- packages/qwik/src/optimizer/core/src/test.rs
+++ packages/qwik/src/optimizer/core/src/test.rs
@@ -3396,7 +3396,7 @@ export const Local = component$(() => {
 		source_maps: true,
 		minify: MinifyMode::Simplify,
 		explicit_extensions: true,
-		mode: EmitMode::Lib,
+		mode: EmitMode::Test,
 		manual_chunks: None,
 		entry_strategy: EntryStrategy::Segment,
 		transpile_ts: true,
@@ -3434,10 +3434,10 @@ export const Greeter = component$(() => {
 
 "#;
 	let options = vec![
-		(EmitMode::Lib, EntryStrategy::Segment, true),
-		(EmitMode::Lib, EntryStrategy::Single, true),
-		(EmitMode::Lib, EntryStrategy::Component, true),
-		// (EmitMode::Lib, EntryStrategy::Inline, true),
+		(EmitMode::Test, EntryStrategy::Segment, true),
+		(EmitMode::Test, EntryStrategy::Single, true),
+		(EmitMode::Test, EntryStrategy::Component, true),
+		// (EmitMode::Test, EntryStrategy::Inline, true),
 		(EmitMode::Prod, EntryStrategy::Segment, true),
 		(EmitMode::Prod, EntryStrategy::Single, true),
 		(EmitMode::Prod, EntryStrategy::Component, true),
@@ -3446,10 +3446,10 @@ export const Greeter = component$(() => {
 		(EmitMode::Dev, EntryStrategy::Single, true),
 		(EmitMode::Dev, EntryStrategy::Component, true),
 		// (EmitMode::Dev, EntryStrategy::Inline, true),
-		(EmitMode::Lib, EntryStrategy::Segment, false),
-		(EmitMode::Lib, EntryStrategy::Single, false),
-		(EmitMode::Lib, EntryStrategy::Component, false),
-		// (EmitMode::Lib, EntryStrategy::Inline, false),
+		(EmitMode::Test, EntryStrategy::Segment, false),
+		(EmitMode::Test, EntryStrategy::Single, false),
+		(EmitMode::Test, EntryStrategy::Component, false),
+		// (EmitMode::Test, EntryStrategy::Inline, false),
 		(EmitMode::Prod, EntryStrategy::Segment, false),
 		(EmitMode::Prod, EntryStrategy::Single, false),
 		(EmitMode::Prod, EntryStrategy::Component, false),
@@ -3476,7 +3476,7 @@ export const Greeter = component$(() => {
 		minify: MinifyMode::Simplify,
 		root_dir: None,
 		explicit_extensions: true,
-		mode: EmitMode::Lib,
+		mode: EmitMode::Test,
 		manual_chunks: None,
 		entry_strategy: EntryStrategy::Segment,
 		transpile_ts: true,
@@ -3731,9 +3731,6 @@ fn lib_mode_fn_signal() {
 "#
 		.to_string(),
 		transpile_jsx: true,
-		mode: EmitMode::Lib,
-		// make sure it overrides it
-		is_server: Some(false),
 		..TestInput::default()
 	});
 }
@@ -3873,7 +3870,7 @@ impl TestInput {
 			preserve_filenames: false,
 			explicit_extensions: false,
 			snapshot: true,
-			mode: EmitMode::Lib,
+			mode: EmitMode::Test,
 			scope: None,
 			core_module: None,
 			reg_ctx_name: None,

f31bb3ecf70ed6272381f91eb6786067d9804d1d
Ian Letourneau <letourneau.ian@gmail.com>
2024-10-27 12:32:20 -0400

    update to latest changes

--- starters/features/testing-library/vitest.setup.ts
+++ starters/features/testing-library/vitest.setup.ts
@@ -1,4 +1,4 @@
-import { afterEach } from "vitest";
+// Configure DOM matchers to work in Vitest
 import "@testing-library/jest-dom/vitest";
 
 // This has to run before qdev.ts loads. `beforeAll` is too late
@@ -6,8 +6,3 @@ globalThis.qTest = false; // Forces Qwik to run as if it was in a Browser
 globalThis.qRuntimeQrl = true;
 globalThis.qDev = true;
 globalThis.qInspector = false;
-
-afterEach(async () => {
-  const { cleanup } = await import("@noma.to/qwik-testing-library");
-  cleanup();
-});

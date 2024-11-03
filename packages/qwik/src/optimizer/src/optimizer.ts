de3052f48a8ad65331fd3dbaa68cc571a662ad47
Wout Mertens <Wout.Mertens@gmail.com>
2024-11-04 00:06:55 +0100

    chore(optimizer): remove fs and parallel features
    they are not being used

--- packages/qwik/src/optimizer/src/optimizer.ts
+++ packages/qwik/src/optimizer/src/optimizer.ts
@@ -1,16 +1,5 @@
-import {
-  getPlatformInputFiles,
-  getSystem,
-  loadPlatformBinding,
-  type PlatformBinding,
-} from './platform';
-import type {
-  TransformModulesOptions,
-  TransformFsOptions,
-  Optimizer,
-  OptimizerSystem,
-  OptimizerOptions,
-} from './types';
+import { getSystem, loadPlatformBinding, type PlatformBinding } from './platform';
+import type { TransformModulesOptions, Optimizer, OptimizerOptions } from './types';
 
 /** @public */
 export const createOptimizer = async (optimizerOptions: OptimizerOptions = {}) => {
@@ -21,12 +10,6 @@ export const createOptimizer = async (optimizerOptions: OptimizerOptions = {}) =
     async transformModules(opts: TransformModulesOptions) {
       return transformModules(binding, opts);
     },
-    async transformFs(opts: TransformFsOptions) {
-      return transformFsAsync(sys, binding, opts);
-    },
-    transformFsSync(opts: TransformFsOptions) {
-      return transformFsSync(binding, opts);
-    },
     sys,
   };
 
@@ -36,58 +19,6 @@ export const createOptimizer = async (optimizerOptions: OptimizerOptions = {}) =
 /** Transforms the input code string, does not access the file system. */
 const transformModules = (binding: PlatformBinding, opts: TransformModulesOptions) => {
   return binding.transform_modules(convertOptions(opts));
-};
-
-const transformFsSync = (binding: PlatformBinding, opts: TransformFsOptions) => {
-  if (binding.transform_fs) {
-    return binding.transform_fs(convertOptions(opts));
-  }
-  throw new Error('Not implemented');
-};
-
-const transformFsAsync = async (
-  sys: OptimizerSystem,
-  binding: PlatformBinding,
-  fsOpts: TransformFsOptions
-) => {
-  if (binding.transform_fs && !sys.getInputFiles) {
-    return binding.transform_fs(convertOptions(fsOpts));
-  }
-
-  const getInputFiles = await getPlatformInputFiles(sys);
-
-  if (getInputFiles) {
-    const input = await getInputFiles(fsOpts.srcDir);
-    for (const root of fsOpts.vendorRoots) {
-      const rootFiles = await getInputFiles(root);
-      input.push(...rootFiles);
-    }
-    input.forEach((file) => {
-      file.path = sys.path.relative(fsOpts.srcDir, file.path);
-    });
-    const modulesOpts: Required<TransformModulesOptions> = {
-      srcDir: fsOpts.srcDir,
-      rootDir: fsOpts.rootDir!,
-      entryStrategy: fsOpts.entryStrategy!,
-      minify: fsOpts.minify!,
-      sourceMaps: !!fsOpts.sourceMaps,
-      transpileTs: fsOpts.transpileTs!,
-      transpileJsx: fsOpts.transpileJsx!,
-      explicitExtensions: fsOpts.explicitExtensions!,
-      preserveFilenames: fsOpts.preserveFilenames!,
-      mode: fsOpts.mode!,
-      scope: fsOpts.scope!,
-      input,
-      regCtxName: fsOpts.regCtxName!,
-      stripEventHandlers: fsOpts.stripEventHandlers!,
-      stripCtxName: fsOpts.stripCtxName!,
-      stripExports: fsOpts.stripExports!,
-      isServer: fsOpts.isServer!,
-    };
-    return binding.transform_modules(convertOptions(modulesOpts));
-  }
-
-  throw new Error('Not implemented');
 };
 
 const convertOptions = (opts: any) => {

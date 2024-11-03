8b80fe91b3b4000447d4f8ef166b826c0e13f257
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2024-11-03 19:32:03 +0200

    feat: updated package names, added additional migrations

--- packages/qwik/src/cli/migrate-v2/run-migration.ts
+++ packages/qwik/src/cli/migrate-v2/run-migration.ts
@@ -3,15 +3,19 @@ import type { AppCommand } from '../utils/app-command';
 import { bgMagenta, green } from 'kleur/colors';
 import { bye } from '../utils/utils';
 import { replacePackage } from './replace-package';
-import { updateDependencies } from './update-dependencies';
+import {
+  installTsMorph,
+  removeTsMorphFromPackageJson,
+  updateDependencies,
+} from './update-dependencies';
 import { versions } from './versions';
+import { replaceImportInFiles } from './rename-import';
 
 export async function runV2Migration(app: AppCommand) {
   intro(
     `✨  ${bgMagenta(' This command will migrate your Qwik application from v1 to v2 \n')}` +
       `This includes the following: \n` +
-      //   TODO(migrate-v2): package names
-      `  - "@builder.io/qwik", "@builder.io/qwik-city" packages will be rescoped to "@qwik.dev/core" and "@qwik.dev/city" \n` +
+      `  - "@builder.io/qwik", "@builder.io/qwik-city" and "@builder.io/qwik-react" packages will be rescoped to "@qwik.dev/core", "@qwik.dev/router" and "@qwik.dev/react" respectively \n` +
       `  - related dependencies will be updated \n`
   );
   const proceed = await confirm({
@@ -24,8 +28,29 @@ export async function runV2Migration(app: AppCommand) {
   }
 
   try {
-    replacePackage('@builder.io/qwik-city', '@qwik.dev/city', versions['@qwik.dev/city']);
-    replacePackage('@builder.io/qwik', '@qwik.dev/qwik', versions['@qwik.dev/qwik']);
+    const installedTsMorph = await installTsMorph();
+
+    replaceImportInFiles(
+      [
+        ['QwikCityProvider', 'QwikRouterProvider'],
+        ['qwikCity', 'qwikRouter'],
+        ['QwikCityVitePluginOptions', 'QwikRouterVitePluginOptions'],
+        ['QwikCityPlugin', 'QwikRouterPlugin'],
+        ['createQwikCity', 'createQwikRouter'],
+        ['QwikCityNodeRequestOptions', 'QwikRouterNodeRequestOptions'],
+      ],
+      '@builder.io/qwik-city'
+    );
+
+    replacePackage('@builder.io/qwik-city', '@qwik.dev/router', versions['@qwik.dev/router']);
+    replacePackage('@builder.io/qwik-react', '@qwik.dev/react', versions['@qwik.dev/react']);
+    // "@builder.io/qwik" should be the last one because it's name is a substring of the package names above
+    replacePackage('@builder.io/qwik', '@qwik.dev/core', versions['@qwik.dev/core']);
+
+    if (installedTsMorph) {
+      await removeTsMorphFromPackageJson();
+    }
+
     await updateDependencies();
     log.success(`${green(`Your application has been successfully migrated to v2!`)}`);
   } catch (error) {
--- packages/qwik/src/cli/migrate-v2/update-dependencies.ts
+++ packages/qwik/src/cli/migrate-v2/update-dependencies.ts
@@ -1,26 +1,48 @@
-import { readPackageJson, writePackageJson } from './../utils/utils';
-// import { getPackageManager } from './../utils/utils';
-// import { installDeps } from '../utils/install-deps';
+import { installDeps } from '../utils/install-deps';
+import { getPackageManager, readPackageJson, writePackageJson } from './../utils/utils';
 import { versions } from './versions';
 
 export async function updateDependencies() {
   // TODO(migrate-v2): rely on workspaceRoot instead?
   const packageJson = await readPackageJson(process.cwd());
   const devDependencies = (packageJson.devDependencies ??= {});
+  const dependencies = (packageJson.dependencies ??= {});
 
-  // TODO: this logic should be enhanced to check in both dependencies and devDependencies
   for (const key of Object.keys(devDependencies)) {
     if (Object.prototype.hasOwnProperty.call(versions, key)) {
-      // for now only updating existing dependencies if they exist in root package.json
       devDependencies[key] = versions[key as unknown as keyof typeof versions];
+    }
+  }
+  for (const key of Object.keys(dependencies)) {
+    if (Object.prototype.hasOwnProperty.call(versions, key)) {
+      dependencies[key] = versions[key as unknown as keyof typeof versions];
     }
   }
 
   await writePackageJson(process.cwd(), packageJson);
-  // TODO(migrate-v2): not installing dependencies because we don't have correct versions set
-  // const { install } = installDeps(getPackageManager(), process.cwd());
-  // const passed = await install;
-  // if (!passed) {
-  //   throw new Error('Failed to install dependencies');
-  // }
+  runInstall();
+}
+
+export async function installTsMorph() {
+  const packageJson = await readPackageJson(process.cwd());
+  if (packageJson.dependencies?.['ts-morph'] || packageJson.devDependencies?.['ts-morph']) {
+    return false;
+  }
+  (packageJson.devDependencies ??= {})['ts-morph'] = 'latest';
+  await runInstall();
+  return true;
+}
+
+async function runInstall() {
+  const { install } = installDeps(getPackageManager(), process.cwd());
+  const passed = await install;
+  if (!passed) {
+    throw new Error('Failed to install dependencies');
+  }
+}
+
+export async function removeTsMorphFromPackageJson() {
+  const packageJson = await readPackageJson(process.cwd());
+  delete packageJson.dependencies?.['ts-morph'];
+  delete packageJson.devDependencies?.['ts-morph'];
 }

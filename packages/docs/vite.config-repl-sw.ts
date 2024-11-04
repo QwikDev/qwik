add0279767c2a415ea0f0a3db37536736c008a71
Wout Mertens <Wout.Mertens@gmail.com>
2024-11-04 13:07:47 +0100

    chore(docs, insights): make ESM projects
    this avoids having to transpile the vite.config.mts file, which sometimes causes eslint errors during builds

--- packages/docs/vite.config-repl-sw.ts
+++ packages/docs/vite.config-repl-sw.ts
@@ -21,9 +21,9 @@ export default defineConfig({
         // The service worker is generated in the build directory
         // but we need it in the public directory
         // so that the REPL can access it
-        copyFileSync(join('dist', 'repl', 'repl-sw.js'), join('public', 'repl', 'repl-sw.js'));
+        copyFileSync(join('dist', 'repl', 'repl-sw.cjs'), join('public', 'repl', 'repl-sw.js'));
         copyFileSync(
-          join('dist', 'repl', 'repl-sw.js.map'),
+          join('dist', 'repl', 'repl-sw.cjs.map'),
           join('public', 'repl', 'repl-sw.js.map')
         );
       },

0968737a747deb2c93ddef7387897ad0ed4d3cef
wuls <linsheng.wu@beantechs.com>
2024-11-07 14:23:44 +0800

    fix test

--- packages/qwik/src/core/render/dom/render.unit.tsx
+++ packages/qwik/src/core/render/dom/render.unit.tsx
@@ -224,7 +224,7 @@ test('should render a component with scoped styles', async () => {
     </style>
     <!--qv -->
     <div class="⭐️ml52vk-0">
-      <div class="⭐️ml52vk-0 stuff" aria-hidden="true">
+      <div q:key="b" class="⭐️ml52vk-0 stuff" aria-hidden="true">
         Hello
         <button class="⭐️ml52vk-0">
           Toggle

c399e337cbc34937a2a268bea39ea3a757be7a5e
Shai Reznik <shairez@users.noreply.github.com>
2024-11-19 16:25:58 +0200

    updated migration version

--- packages/qwik/src/cli/migrate-v2/versions.ts
+++ packages/qwik/src/cli/migrate-v2/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  '@qwik.dev/core': '2.0.0-alpha.0',
-  '@qwik.dev/router': '2.0.0-alpha.0',
-  '@qwik.dev/react': '2.0.0-alpha.0',
-  'eslint-plugin-qwik': '2.0.0-alpha.0',
+  '@qwik.dev/core': '2.0.0-alpha.1',
+  '@qwik.dev/router': '2.0.0-alpha.1',
+  '@qwik.dev/react': '2.0.0-alpha.1',
+  'eslint-plugin-qwik': '2.0.0-alpha.1',
 };

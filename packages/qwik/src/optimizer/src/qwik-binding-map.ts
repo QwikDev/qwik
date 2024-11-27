863139987d734aa474b066d59229572d1ae20363
samsonssali <ssali@jitpomi.com>
2024-11-26 21:41:11 -0700

    add props documentation to docs

--- packages/qwik/src/optimizer/src/qwik-binding-map.ts
+++ packages/qwik/src/optimizer/src/qwik-binding-map.ts
@@ -1,44 +1,44 @@
 // AUTO-GENERATED IN OPTIMIZER BUILD SCRIPT
 // created from data provided by @napi-rs/triples
+
 // prettier-ignore
 export const QWIK_BINDING_MAP = {
-  'darwin': {
-    'arm64': [
+  "darwin": {
+    "arm64": [
       {
-        'platform': 'darwin',
-        'arch': 'arm64',
-        'abi': null,
-        'platformArchABI': 'qwik.darwin-arm64.node',
-      },
-
+        "platform": "darwin",
+        "arch": "arm64",
+        "abi": null,
+        "platformArchABI": "qwik.darwin-arm64.node"
+      }
     ],
-    'x64': [
+    "x64": [
       {
-        'platform': 'darwin',
-        'arch': 'x64',
-        'abi': null,
-        'platformArchABI': 'qwik.darwin-x64.node',
-      },
-    ],
+        "platform": "darwin",
+        "arch": "x64",
+        "abi": null,
+        "platformArchABI": "qwik.darwin-x64.node"
+      }
+    ]
   },
-  'win32': {
-    'x64': [
+  "win32": {
+    "x64": [
       {
-        'platform': 'win32',
-        'arch': 'x64',
-        'abi': 'msvc',
-        'platformArchABI': 'qwik.win32-x64-msvc.node',
-      },
-    ],
+        "platform": "win32",
+        "arch": "x64",
+        "abi": "msvc",
+        "platformArchABI": "qwik.win32-x64-msvc.node"
+      }
+    ]
   },
-  'linux': {
-    'x64': [
+  "linux": {
+    "x64": [
       {
-        'platform': 'linux',
-        'arch': 'x64',
-        'abi': 'gnu',
-        'platformArchABI': 'qwik.linux-x64-gnu.node',
-      },
-    ],
-  },
+        "platform": "linux",
+        "arch": "x64",
+        "abi": "gnu",
+        "platformArchABI": "qwik.linux-x64-gnu.node"
+      }
+    ]
+  }
 };

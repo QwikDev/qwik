324d38d5a31748968f262d1faf3cb930438749c2
wuls <linsheng.wu@beantechs.com>
2024-12-27 10:31:10 +0800

    fix

--- packages/docs/src/routes/demo/qwikcity/middleware/layout.tsx
+++ packages/docs/src/routes/demo/qwikcity/middleware/layout.tsx
@@ -3,8 +3,10 @@ import { type RequestHandler } from '@builder.io/qwik-city';
 export const onRequest: RequestHandler = async ({ next }) => {
   try {
     await next();
-  } catch (error) {
-    if (
+  } catch (error: any) {
+    if (error?.message === 'ERROR: Demonstration of an error response.') {
+      return await next();
+    } else if (
       error &&
       typeof error === 'object' &&
       'message' in error &&

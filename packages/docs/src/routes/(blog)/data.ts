b401e00712eb376ee0318ab648901f0416357f76
gioboa <giorgiob.boa@gmail.com>
2024-12-31 03:04:30 +0100

    fix: fix up typo

--- packages/docs/src/routes/(blog)/data.ts
+++ packages/docs/src/routes/(blog)/data.ts
@@ -31,7 +31,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Towards Qwik 2.0: Lighter, Faster, Better',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F9dd98bcf8fee4b42a718449c5151d53d',
-    path: '/blog/qwik-2-coming-soon',
+    path: '/blog/qwik-2-coming-soon/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -39,7 +39,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Astro + Qwik: Houston, we have Resumability!',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F2418eac0b25046b197bf7b8bf5dfd637',
-    path: '/blog/astro-qwik',
+    path: '/blog/astro-qwik/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -47,7 +47,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Qwik City Routing: A Visual Guide',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F4f52dab5f87142a38002269540e69cef',
-    path: '/blog/qwik-city-routing',
+    path: '/blog/qwik-city-routing/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -55,7 +55,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Qwik 1.2: Performance in Autopilot',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F447d6d0349d4442496c3d7e246ce3d24',
-    path: '/blog/qwik-1-2-performance-autopilot',
+    path: '/blog/qwik-1-2-performance-autopilot/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -63,7 +63,7 @@ export const blogArticles: BlogArticle[] = [
     title: "Boost Your Site Perf with Qwik's useVisibleTask$ Hook",
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F5d90a118ceac4678a9e0576e61a955a9',
-    path: '/blog/qwik-tasks',
+    path: '/blog/qwik-tasks/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -71,7 +71,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Qwik Reaches v1.0',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fca46e4da83b7429ea159e2a42a197288',
-    path: '/blog/qwik-v1',
+    path: '/blog/qwik-v1/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -79,7 +79,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Type Safe Forms in Qwik with Modular Forms',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F7abec2f848764aa99fead32861505344',
-    path: '/blog/type-safe-forms-in-qwik',
+    path: '/blog/type-safe-forms-in-qwik/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -87,7 +87,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Qwik Reaches RC Milestone',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F043bec968cb7465fac152147b9e5bd57',
-    path: '/blog/qwik-rc-milestone',
+    path: '/blog/qwik-rc-milestone/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -95,7 +95,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Building Framer Motion Animations with Qwik',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fbf1420524b2241619cc68efbba7b7c13',
-    path: '/blog/framer-motion-qwik',
+    path: '/blog/framer-motion-qwik/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -103,7 +103,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Code Extraction: The Silent Web Revolution',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F0a8170ff2659474883ee032a0129cddd',
-    path: '/blog/module-extraction-the-silent-web-revolution',
+    path: '/blog/module-extraction-the-silent-web-revolution/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -111,7 +111,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Introducing Qwik City Server Functions',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F48d8fd6d22ae4c3b9c1ec4817ce4046d',
-    path: '/blog/qwik-city-server-functions',
+    path: '/blog/qwik-city-server-functions/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -119,7 +119,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Resumable React: How to Use React Inside Qwik',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F9071a7def0b240e5b5ca6aab756e81b8',
-    path: '/blog/resumable-react-how-to-use-react-inside-qwik',
+    path: '/blog/resumable-react-how-to-use-react-inside-qwik/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -127,7 +127,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'The Qase for Qwik: Love At First TTI',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F69e3a278f34d4bfc9deb783dc705cd9a',
-    path: '/blog/the-qase-for-qwik-love-at-first-tti',
+    path: '/blog/the-qase-for-qwik-love-at-first-tti/',
     tags: ['Qwik'],
     minRead: 5,
   },
@@ -135,7 +135,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Qwik and Qwik City have reached beta! 🎉',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F5f8db18f68c74f6f9919f3877b6246b4',
-    path: '/blog/qwik-and-qwik-city-have-reached-beta',
+    path: '/blog/qwik-and-qwik-city-have-reached-beta/',
     tags: ['Web Development'],
     minRead: 5,
   },
@@ -143,7 +143,7 @@ export const blogArticles: BlogArticle[] = [
     title: 'Introducing Qwik starters - get up and running with Qwik now',
     image:
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F85209017e99f4753a56614f6712817c5',
-    path: '/blog/introducing-qwik-starters',
+    path: '/blog/introducing-qwik-starters/',
     tags: ['Qwik'],
     minRead: 5,
   },

e7991f641c871ff7a4b63b3208eff64490147d73
Wout Mertens <Wout.Mertens@gmail.com>
2025-01-03 07:10:15 +0100

    chore(core signals): cleanup

--- packages/qwik/src/core/signal/signal-api.ts
+++ packages/qwik/src/core/signal/signal-api.ts
@@ -2,11 +2,13 @@ import type { QRLInternal } from '../shared/qrl/qrl-class';
 import type { QRL } from '../shared/qrl/qrl.public';
 import { ComputedSignal, Signal, throwIfQRLNotResolved } from './signal';
 
+/** @internal */
 export const createSignal = <T>(value?: T) => {
   return new Signal(null, value);
 };
 
+/** @internal */
 export const createComputedSignal = <T>(qrl: QRL<() => T>) => {
   throwIfQRLNotResolved(qrl);
-  return new ComputedSignal(null, qrl as QRLInternal<() => T>);
+  return new ComputedSignal<T>(null, qrl as QRLInternal<() => T>);
 };

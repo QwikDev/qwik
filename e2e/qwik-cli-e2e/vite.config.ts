be6f9acbb9b7393a2e03c30e4fb59464dc3b6b8e
Wout Mertens <Wout.Mertens@gmail.com>
2025-02-03 17:13:08 +0100

    fix(ci): fix e2e cli tests + cache result

--- e2e/qwik-cli-e2e/vite.config.ts
+++ e2e/qwik-cli-e2e/vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
+import { mkdirSync } from 'fs';
+
+// if we're running in github CI
+if (process.env.CI) {
+  // Workaround for npm/pnpm crashing in scaffoldQwikProject because "name is too long"
+  const testPath = resolve(process.cwd(), 'e2e-test-tmp');
+  mkdirSync(testPath);
+  process.env.TEMP_E2E_PATH = testPath;
+}
 
 export default defineConfig({
   plugins: [tsconfigPaths({ ignoreConfigErrors: true, root: '../../' })],

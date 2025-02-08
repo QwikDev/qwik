fa82495b39c32c6795880de1308c68cb267a8a28
JerryWu <409187100@qq.com>
2025-02-08 14:16:13 +0800

    upgrade

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,43 +3,50 @@ import tseslint from 'typescript-eslint';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import typescript from '@typescript-eslint/eslint-plugin';
+import { includeIgnoreFile } from '@eslint/compat';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 import globals from 'globals';
-
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  languageOptions: {
-    parserOptions: {
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
+const gitignorePath = fileURLToPath(import.meta.resolve('./.eslintignore'));
+const gitignorePathConfig = includeIgnoreFile(gitignorePath);
+export default tseslint.config(
+  gitignorePathConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        globals: {
+          ...globals.node,
+          ...globals.es2021,
+          ...globals.browser,
+        },
+        ecmaVersion: 2021,
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
       },
-      globals: {
-        ...globals.node,
-        ...globals.es2021,
-        ...globals.browser,
-      },
-      ecmaVersion: 2021,
-      project: ['./tsconfig.json'],
-      tsconfigRootDir: __dirname,
+    },
+    plugins: { '@typescript-eslint': typescript },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'prefer-spread': 'off',
+      'no-case-declarations': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-unnecessary-condition': 'warn',
     },
   },
-  plugins: { '@typescript-eslint': typescript },
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-this-alias': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'prefer-spread': 'off',
-    'no-case-declarations': 'off',
-    'no-console': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    '@typescript-eslint/no-unnecessary-condition': 'warn',
-  },
-});
+  eslint.configs.recommended,
+  tseslint.configs.recommended
+);

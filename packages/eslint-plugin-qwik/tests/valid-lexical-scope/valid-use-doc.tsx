d7fe35884fb2bd61732954c963c0cb232e99745f
JerryWu <409187100@qq.com>
2025-02-23 19:07:47 +0800

    When it is a Qwik hook, there is no need for recursive lookup.

--- packages/eslint-plugin-qwik/tests/valid-lexical-scope/valid-use-doc.tsx
+++ packages/eslint-plugin-qwik/tests/valid-lexical-scope/valid-use-doc.tsx
@@ -1,10 +1,18 @@
 import { component$, useComputed$ } from '@builder.io/qwik';
 import { useDocumentHead } from '@builder.io/qwik-city';
+import { useLocation as exampleTest } from '@builder.io/qwik-city';
+const loc = exampleTest();
 
 export default component$(() => {
   const head = useDocumentHead();
   const authorId = useComputed$(() => {
     return head.meta; // <--- ESLint not happy
   });
-  return <>{authorId.value}</>;
+  return (
+    <>
+      {authorId.value}
+      <p>pathname: {loc.url.pathname}</p>
+      <p>skuId: {loc.params.skuId}</p>
+    </>
+  );
 });

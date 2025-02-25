cdad46d50758be40e34ff5614ad40b42bbb9ce68
Wout Mertens <Wout.Mertens@gmail.com>
2025-02-25 22:31:08 +0100

    chore: vite 6 (just upgrade, no env)

--- packages/qwik/src/core/shared/shared-serialization.unit.ts
+++ packages/qwik/src/core/shared/shared-serialization.unit.ts
@@ -153,16 +153,18 @@ describe('shared-serialization', () => {
       err.stack = err
         .stack!.replaceAll(/([A-Z]:){0,1}(\/|\\).*\./g, '/...path/file.')
         .replaceAll(/:\d+:\d+/g, ':123:456');
-      expect(await dump(err)).toMatchInlineSnapshot(`
+      const dumpNoSize = async (obj: any) =>
+        (await dump(obj)).replaceAll(/\(\d+ chars\)/g, '(x chars)');
+      expect(await dumpNoSize(err)).toMatchInlineSnapshot(`
         "
         0 Error [
           String "hi"
           String "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\"...
         ]
-        (513 chars)"
+        (x chars)"
       `);
       (err as any).extra = 'yey';
-      expect(await dump(err)).toMatchInlineSnapshot(`
+      expect(await dumpNoSize(err)).toMatchInlineSnapshot(`
         "
         0 Error [
           String "hi"
@@ -172,7 +174,7 @@ describe('shared-serialization', () => {
           ]
           String "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\"...
         ]
-        (535 chars)"
+        (x chars)"
       `);
     });
     it(title(TypeIds.Object), async () => {

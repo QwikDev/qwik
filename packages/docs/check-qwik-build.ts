dc26aef229153135faef848c40ab9ef42836a42d
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-02 16:51:09 +0000

    refactor: windows dirname variable check

--- packages/docs/check-qwik-build.ts
+++ packages/docs/check-qwik-build.ts
@@ -8,42 +8,39 @@ import { spawnSync } from 'node:child_process';
 
 let __dirname = path.dirname(new URL(import.meta.url).pathname);
 const isWindows = process.platform === 'win32';
-if (isWindows) {
+if (isWindows && __dirname.startsWith('/')) {
   // in Windows __dirname starts with a / causing errors
   // before
   //  /C:/Users/{location stuff}/qwik/packages/docs
+  __dirname = __dirname.substring(1);
+  // after
+  // C:/Users/{location stuff}/qwik/packages/docs
+}
+const qwikPkgDir = path.join(__dirname, '..', 'qwik', 'dist');
 
-  if (__dirname.startsWith('/')) {
-    __dirname = __dirname.substring(1);
-    // after
-    // C:/Users/{location stuff}/qwik/packages/docs
+if (!fs.existsSync(path.join(qwikPkgDir, 'core.d.ts'))) {
+  console.warn(
+    `\n\n=== Running 'pnpm run build.local' to generate missing imports for the docs ===\n`
+  );
+  const out = spawnSync('pnpm', ['run', 'build.local'], {
+    cwd: path.join(__dirname, '..', '..'),
+    stdio: 'inherit',
+  });
+  if (out.status !== 0) {
+    console.error('Failed to build local packages');
+    process.exit(1);
   }
-  const qwikPkgDir = path.join(__dirname, '..', 'qwik', 'dist');
+}
 
-  if (!fs.existsSync(path.join(qwikPkgDir, 'core.d.ts'))) {
-    console.warn(
-      `\n\n=== Running 'pnpm run build.local' to generate missing imports for the docs ===\n`
-    );
-    const out = spawnSync('pnpm', ['run', 'build.local'], {
-      cwd: path.join(__dirname, '..', '..'),
-      stdio: 'inherit',
-    });
-    if (out.status !== 0) {
-      console.error('Failed to build local packages');
-      process.exit(1);
-    }
-  }
-
-  if (!fs.existsSync(path.join(__dirname, 'public', 'repl', 'repl-sw.js'))) {
-    console.warn(
-      `\n\n=== Running 'pnpm run build.repl-sw' to generate missing REPL service worker public/repl/repl-sw.js ===\n`
-    );
-    const out = spawnSync('pnpm', ['run', 'build.repl-sw'], {
-      stdio: 'inherit',
-    });
-    if (out.status !== 0) {
-      console.error('Failed to build REPL service worker');
-      process.exit(1);
-    }
+if (!fs.existsSync(path.join(__dirname, 'public', 'repl', 'repl-sw.js'))) {
+  console.warn(
+    `\n\n=== Running 'pnpm run build.repl-sw' to generate missing REPL service worker public/repl/repl-sw.js ===\n`
+  );
+  const out = spawnSync('pnpm', ['run', 'build.repl-sw'], {
+    stdio: 'inherit',
+  });
+  if (out.status !== 0) {
+    console.error('Failed to build REPL service worker');
+    process.exit(1);
   }
 }

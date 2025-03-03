0937e79b5ff5f30bae63394b08cf20229ed4841d
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-03-03 10:17:04 +0100

    feat: add missing types for ViewTransition

---
+++ packages/qwik-router/src/runtime/src/view-transition.tsx
@@ -0,0 +1,49 @@
+// This types are missing in current typescript version: 5.4.5
+
+interface StartViewTransitionOptions {
+  types?: string[] | null;
+  update?: ViewTransitionUpdateCallback | null;
+}
+interface ViewTransitionUpdateCallback {
+  (): any;
+}
+
+interface DocumentViewTransition extends Document {
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Document/startViewTransition) */
+  startViewTransition(
+    callbackOptions?: ViewTransitionUpdateCallback | StartViewTransitionOptions
+  ): ViewTransition;
+}
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition) */
+interface ViewTransition {
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/finished) */
+  readonly finished: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/ready) */
+  readonly ready: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/updateCallbackDone) */
+  readonly updateCallbackDone: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/skipTransition) */
+  skipTransition(): void;
+  types?: Set<string>;
+}
+
+export const startViewTransition = (params: StartViewTransitionOptions) => {
+  if (!params.update) {
+    return;
+  }
+  if ('startViewTransition' in document) {
+    let transition: ViewTransition;
+    try {
+      // Typed transition starts with Chrome 125 & Safari 18
+      transition = (document as DocumentViewTransition).startViewTransition(params);
+    } catch {
+      // Fallback for Chrome 111 until Chrome 125
+      transition = (document as DocumentViewTransition).startViewTransition(params.update);
+    }
+    const event = new CustomEvent('qviewTransition', { detail: transition });
+    document.dispatchEvent(event);
+  } else {
+    params.update?.();
+  }
+};

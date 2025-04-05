20b58f2aa26efff69c70948800c124a3de12de36
Michał Popek <admin@varixo.pl>
2025-04-05 15:09:52 +0200

    fix: integration tests on CI

--- e2e/adapters-e2e/playwright.config.ts
+++ e2e/adapters-e2e/playwright.config.ts
@@ -33,10 +33,10 @@ export default defineConfig({
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 
   webServer: {

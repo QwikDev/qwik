2fca63ddb167848f886681af4e2c919462502a23
wuls <linsheng.wu@beantechs.com>
2025-04-07 10:57:28 +0800

    fix information

--- .changeset/sweet-hairs-remember.md
+++ .changeset/sweet-hairs-remember.md
@@ -1,5 +1,5 @@
 ---
-'@qwik.dev/core': major
+'@qwik.dev/core': patch
 ---
 
-FIX: Add a new e2e test to verify QRL behavior without uncaught promises. Introduce retry logic for QRL resolution to handle potential promise retries, ensuring robustness in asynchronous operations.
+FIX: Introduce retry logic for QRL resolution to handle potential promise retries, ensuring robustness in asynchronous operations.
--- packages/qwik/src/core/shared/qrl/qrl-class.ts
+++ packages/qwik/src/core/shared/qrl/qrl-class.ts
@@ -93,13 +93,11 @@ export const createQRL = <TYPE>(
     // Note that we bind the current `this`
     const bound = (...args: QrlArgs<TYPE>): ValueOrPromise<QrlReturn<TYPE> | undefined> => {
       if (!qrl.resolved) {
-        //@ts-ignore
         return retryOnPromise(() => qrl.resolve()).then((fn) => {
           if (!isFunction(fn)) {
             throw qError(QError.qrlIsNotFunction);
           }
           return bound(...args);
-          // @ts-ignore
         });
       }
       if (beforeFn && beforeFn() === false) {

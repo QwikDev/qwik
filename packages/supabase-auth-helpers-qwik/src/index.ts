42570927a0e7ddd2096598dcffdc5f28acea81d1
Sreekanth Nagareddy <sreeisalso@gmail.com>
2025-04-21 11:47:18 +0530

    draft

--- packages/supabase-auth-helpers-qwik/src/index.ts
+++ packages/supabase-auth-helpers-qwik/src/index.ts
@@ -1,5 +1,7 @@
 // Methods
-export { createBrowserClient, createServerClient } from './utils/createSupabaseClient';
+// export { createBrowserClient, createServerClient } from './utils/createSupabaseClient';
+export { createBrowserClient } from './utils/createBrowserClient';
+export { createServerClient } from './utils/createServerClient';
 
 // Types
 export type { Session, User, SupabaseClient } from '@supabase/supabase-js';

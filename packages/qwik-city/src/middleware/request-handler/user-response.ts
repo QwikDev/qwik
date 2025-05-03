2e7f41e8e01c7167d86aad4ae1fa299357917131
Omer Prizner <omer.prizner@gmail.com>
2025-05-03 17:31:05 +0300

    WIP - rewrite somewhat works

--- packages/qwik-city/src/middleware/request-handler/user-response.ts
+++ packages/qwik-city/src/middleware/request-handler/user-response.ts
@@ -37,6 +37,9 @@ export function runQwikCity<T>(
   serverRequestEv: ServerRequestEvent<T>,
   loadedRoute: LoadedRoute | null,
   requestHandlers: RequestHandler<any>[],
+  applyRewrite: (
+    url: URL
+  ) => Promise<{ loadedRoute: LoadedRoute; requestHandlers: RequestHandler<any>[] }>,
   trailingSlash = true,
   basePathname = '/',
   qwikSerializer: QwikSerializer
@@ -53,67 +56,97 @@ export function runQwikCity<T>(
     qwikSerializer,
     resolve!
   );
+
   return {
     response: responsePromise,
     requestEv,
     completion: asyncStore
-      ? asyncStore.run(requestEv, runNext, requestEv, resolve!)
-      : runNext(requestEv, resolve!),
+      ? asyncStore.run(requestEv, runNext, requestEv, applyRewrite, resolve!)
+      : runNext(requestEv, applyRewrite, resolve!),
   };
 }
 
-async function runNext(requestEv: RequestEventInternal, resolve: (value: any) => void) {
-  try {
-    // Run all middlewares
-    await requestEv.next();
-  } catch (e) {
-    if (e instanceof RedirectMessage) {
-      console.log('OMER___runNext - RedirectMessage');
-      const stream = requestEv.getWritableStream();
-      await stream.close();
-    } else if (e instanceof RewriteMessage) {
-      console.log('OMER___runNext - RewriteMessage');
-      const stream = requestEv.getWritableStream();
-      await stream.close();
-    } else if (e instanceof ServerError) {
-      if (!requestEv.headersSent) {
-        const status = e.status as StatusCodes;
-        const accept = requestEv.request.headers.get('Accept');
-        if (accept && !accept.includes('text/html')) {
-          const qwikSerializer = requestEv[RequestEvQwikSerializer];
-          requestEv.headers.set('Content-Type', 'application/qwik-json');
-          requestEv.send(status, await qwikSerializer._serializeData(e.data, true));
-        } else {
-          const html = getErrorHtml(e.status, e.data);
-          requestEv.html(status, html);
-        }
-      }
-    } else if (!(e instanceof AbortMessage)) {
-      if (getRequestMode(requestEv) !== 'dev') {
-        try {
-          if (!requestEv.headersSent) {
-            requestEv.headers.set('content-type', 'text/html; charset=utf-8');
-            requestEv.cacheControl({ noCache: true });
-            requestEv.status(500);
+async function runNext(
+  requestEv: RequestEventInternal,
+  applyRewrite: (
+    url: URL
+  ) => Promise<{ loadedRoute: LoadedRoute; requestHandlers: RequestHandler<any>[] }>,
+  resolve: (value: any) => void
+) {
+  async function _runNext(
+    resolve: (value: any) => void,
+    loadedRoute?: LoadedRoute | null,
+    requestHandlers?: RequestHandler<any>[],
+    rewriteAttempt = 1
+  ) {
+    try {
+      try {
+        // Run all middlewares
+        await requestEv.next(loadedRoute, requestHandlers);
+      } catch (e) {
+        if (e instanceof RewriteMessage) {
+          console.log('OMER___runNext - RewriteMessage');
+
+          if (rewriteAttempt > 5) {
+            throw new Error(`Rewrite failed - Max rewrite attempts reached: ${rewriteAttempt - 1}`);
           }
-          const stream = requestEv.getWritableStream();
-          if (!stream.locked) {
-            const writer = stream.getWriter();
-            await writer.write(encoder.encode(minimalHtmlResponse(500, 'Internal Server Error')));
-            await writer.close();
-          }
-        } catch {
-          console.error('Unable to render error page');
+
+          const url = new URL(requestEv.url);
+          url.pathname = requestEv.headers.get('Rewrite-Location')!;
+          console.log('OMER___runNext - RewriteMessage - url', url);
+          const { loadedRoute, requestHandlers } = await applyRewrite(url);
+          return await _runNext(resolve, loadedRoute, requestHandlers, rewriteAttempt + 1);
         }
+
+        throw e;
       }
-      return e;
+    } catch (e) {
+      if (e instanceof RedirectMessage) {
+        console.log('OMER___runNext - RedirectMessage');
+        const stream = requestEv.getWritableStream();
+        await stream.close();
+      } else if (e instanceof ServerError) {
+        if (!requestEv.headersSent) {
+          const status = e.status as StatusCodes;
+          const accept = requestEv.request.headers.get('Accept');
+          if (accept && !accept.includes('text/html')) {
+            const qwikSerializer = requestEv[RequestEvQwikSerializer];
+            requestEv.headers.set('Content-Type', 'application/qwik-json');
+            requestEv.send(status, await qwikSerializer._serializeData(e.data, true));
+          } else {
+            const html = getErrorHtml(e.status, e.data);
+            requestEv.html(status, html);
+          }
+        }
+      } else if (!(e instanceof AbortMessage)) {
+        if (getRequestMode(requestEv) !== 'dev') {
+          try {
+            if (!requestEv.headersSent) {
+              requestEv.headers.set('content-type', 'text/html; charset=utf-8');
+              requestEv.cacheControl({ noCache: true });
+              requestEv.status(500);
+            }
+            const stream = requestEv.getWritableStream();
+            if (!stream.locked) {
+              const writer = stream.getWriter();
+              await writer.write(encoder.encode(minimalHtmlResponse(500, 'Internal Server Error')));
+              await writer.close();
+            }
+          } catch {
+            console.error('Unable to render error page');
+          }
+        }
+        return e;
+      }
+    } finally {
+      if (!requestEv.isDirty()) {
+        resolve(null);
+      }
     }
-  } finally {
-    if (!requestEv.isDirty()) {
-      resolve(null);
-    }
+    return undefined;
   }
-  return undefined;
+
+  return _runNext(resolve);
 }
 
 /**

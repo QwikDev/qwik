3645f09deb43cec22318f8fad6cc64b268588784
Varixo <admin@varixo.pl>
2025-07-27 20:05:44 +0200

    fix: immediately run RUN_QRL chores

--- packages/qwik/src/core/client/queue-qrl.ts
+++ packages/qwik/src/core/client/queue-qrl.ts
@@ -1,6 +1,8 @@
 import { QError, qError } from '../shared/error/error';
 import type { QRLInternal } from '../shared/qrl/qrl-class';
+import { getChorePromise } from '../shared/scheduler';
 import { ChoreType } from '../shared/util-chore-type';
+import type { ValueOrPromise } from '../shared/utils/types';
 import { getInvokeContext } from '../use/use-core';
 import { useLexicalScope } from '../use/use-lexical-scope.public';
 import { getDomContainer } from './dom-container';
@@ -10,7 +12,7 @@ import { getDomContainer } from './dom-container';
  *
  * @internal
  */
-export const _run = (...args: unknown[]): void => {
+export const _run = (...args: unknown[]): ValueOrPromise<unknown> => {
   // This will already check container
   const [runQrl] = useLexicalScope<[QRLInternal<(...args: unknown[]) => unknown>]>();
   const context = getInvokeContext();
@@ -29,5 +31,6 @@ export const _run = (...args: unknown[]): void => {
   }
 
   // We don't return anything, the scheduler is in charge now
-  scheduler.schedule(ChoreType.RUN_QRL, hostElement, runQrl, args);
+  const chore = scheduler.schedule(ChoreType.RUN_QRL, hostElement, runQrl, args);
+  return getChorePromise(chore);
 };

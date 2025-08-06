419926817904bb286a3860572166548c92a03a44
Maïeul <45822175+maiieul@users.noreply.github.com>
2025-08-06 09:48:19 +0200

    fix: service-worker.js and qwik-prefetch-service-worker.js unregistering (#7781)
    * fix: service-worker.js and qwik-prefetch-service-worker.js unregistering
    
    * chore: changeset

---
+++ .changeset/early-eels-listen.md
@@ -0,0 +1,6 @@
+---
+'@builder.io/qwik': patch
+'@builder.io/qwik-city': patch
+---
+
+PATCH: Keeping the service worker components now properly unregisters them.
--- packages/qwik-city/src/runtime/src/sw-register.ts
+++ packages/qwik-city/src/runtime/src/sw-register.ts
@@ -2,7 +2,14 @@
 
 (() => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('__url').catch((e) => console.error(e));
+    navigator.serviceWorker.getRegistrations().then((regs) => {
+      for (const reg of regs) {
+        const url = '__url'.split('/').pop();
+        if (reg.active?.scriptURL.endsWith(url || 'service-worker.js')) {
+          reg.unregister().catch(console.error);
+        }
+      }
+    });
   } else {
     // eslint-disable-next-line no-console
     console.log('Service worker not supported in this browser.');

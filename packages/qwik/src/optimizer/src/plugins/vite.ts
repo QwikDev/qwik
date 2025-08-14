f94e2aa340567ccf8875408bdae329b1e07fe18c
Wout Mertens <Wout.Mertens@gmail.com>
2025-08-15 00:21:04 +0200

    fix(router): more external deps, remove core from vite plugin
    still gets core via dev server

--- packages/qwik/src/optimizer/src/plugins/vite.ts
+++ packages/qwik/src/optimizer/src/plugins/vite.ts
@@ -356,13 +356,7 @@ export function qwikVite(qwikViteOpts: QwikVitePluginOptions = {}): any {
     async configResolved(config) {
       basePathname = config.base;
       if (!(basePathname.startsWith('/') && basePathname.endsWith('/'))) {
-        // TODO v2: make this an error
-        console.error(
-          `warning: vite's config.base must begin and end with /. This will be an error in v2. If you have a valid use case, please open an issue.`
-        );
-        if (!basePathname.endsWith('/')) {
-          basePathname += '/';
-        }
+        throw new Error(`vite's config.base must begin and end with /`);
       }
       const useSourcemap = !!config.build.sourcemap;
       if (useSourcemap && qwikViteOpts.optimizerOptions?.sourcemap === undefined) {

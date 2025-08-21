470596b5701312a0094642de577259cf7fbdff20
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-08-21 09:22:12 +0100

    e2e add sandbox runtimes-less tests

--- e2e/docs-e2e/tests/Sandbox/sandbox.spec.ts
+++ e2e/docs-e2e/tests/Sandbox/sandbox.spec.ts
@@ -164,3 +164,150 @@ test.describe('Sandbox Hello World Example', () => {
     await expect(DebugCheckBox).not.toBeChecked();
   });
 });
+
+test.describe('Sandbox Runtime-less Example', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/examples/introduction/runtime-less/');
+  });
+  test('Sandbox Runtime-less page loads', async ({ page }) => {
+    await expect(page).toHaveTitle('Runtime-less 📚 Qwik Documentation');
+    const tabButtonsTop = page.getByText('app.tsxentry.server.tsxroot.');
+
+    await expect(tabButtonsTop.getByRole('button')).toHaveCount(3);
+    await expect(page.getByRole('button', { name: 'app.tsx' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'entry.server.tsx' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'root.tsx' })).toBeVisible();
+
+    const tabButtonsBottom = page.getByText('AppHTMLSymbolsClient');
+    await expect(tabButtonsBottom.getByRole('button')).toHaveCount(6);
+
+    await expect(tabButtonsBottom.getByRole('button', { name: 'App' })).toBeVisible();
+    await expect(tabButtonsBottom.getByRole('button', { name: 'HTML' })).toBeVisible();
+    await expect(tabButtonsBottom.getByRole('button', { name: 'Symbols' })).toBeVisible();
+    await expect(tabButtonsBottom.getByRole('button', { name: 'Client Bundles' })).toBeVisible();
+    await expect(tabButtonsBottom.getByRole('button', { name: 'SSR Module' })).toBeVisible();
+    await expect(tabButtonsBottom.getByRole('button', { name: 'Diagnostics' })).toBeVisible();
+
+    const consoleTabBottom = page.getByText('ConsoleOptions');
+    await expect(consoleTabBottom.getByRole('button')).toHaveCount(2);
+    await expect(consoleTabBottom.getByRole('button', { name: 'Console' })).toBeVisible();
+    await expect(consoleTabBottom.getByRole('button', { name: 'Options' })).toBeVisible();
+
+    const consoleLogSSR = page.getByText('ssr render <App>');
+    await expect(consoleLogSSR).toHaveCount(1);
+    await expect(consoleLogSSR).toBeVisible();
+  });
+
+  test('Runtime-less APP.tsx loads', async ({ page }) => {
+    const replInputAppTsx = page
+      .getByRole('code')
+      .locator('div')
+      .filter({ hasText: "console.log('render <App>');" })
+      .nth(4);
+    await expect(replInputAppTsx).toBeVisible();
+    const serverEntryButton = page.getByRole('button', { name: 'entry.server.tsx' });
+    serverEntryButton.click();
+    await expect(replInputAppTsx).not.toBeVisible();
+    const appTsxButton = page.getByRole('button', { name: 'app.tsx' });
+    appTsxButton.click();
+    await expect(replInputAppTsx).toBeVisible();
+  });
+
+  test('Runtime-less entry.server.tsx loads', async ({ page }) => {
+    const root = page
+      .getByRole('code')
+      .locator('div')
+      .filter({ hasText: "import { Root } from './root';" })
+      .nth(4);
+
+    await expect(root).not.toBeVisible();
+    const button = page.getByRole('button', { name: 'entry.server.tsx' });
+    button.click();
+    await expect(root).toBeVisible();
+  });
+
+  test('Runtime-less root.tsx loads', async ({ page }) => {
+    const importStatement = page
+      .getByRole('code')
+      .locator('div')
+      .filter({ hasText: "import App from './app';" })
+      .nth(4);
+    await expect(importStatement).not.toBeVisible();
+    const button = page.getByRole('button', { name: 'root.tsx' });
+    await button.click();
+    await expect(importStatement).toBeVisible();
+  });
+
+  test('Runtime-less Action Button', async ({ page }) => {
+    const spinner = page.locator('.repl-spinner');
+    await expect(spinner).toBeVisible();
+    await expect(spinner).not.toBeVisible();
+    const button = page
+      .locator('iframe')
+      .contentFrame()
+      .locator('iframe')
+      .contentFrame()
+      .getByRole('button', { name: 'Action' });
+    await button.click();
+    const clicked = page.getByText('client click');
+    await expect(clicked).toHaveCount(1);
+    await button.click();
+    await expect(clicked).toHaveCount(2);
+  });
+  test('Runtime-less HTML Button', async ({ page }) => {
+    const spinner = page.locator('.repl-spinner');
+    await expect(spinner).toBeVisible();
+    await expect(spinner).not.toBeVisible();
+    const button = page.getByRole('button', { name: 'HTML' });
+    await button.click();
+    const htmlCode = page.locator('code.language-html');
+    await expect(htmlCode).toBeVisible();
+    await expect(htmlCode).toContainText('<!DOCTYPE html>');
+  });
+
+  test('Runtime-less Symbols Button', async ({ page }) => {
+    const button = page.getByRole('button', { name: 'Symbols' });
+    await expect(button).toBeVisible();
+    await button.click();
+
+    const symbolsText = page.getByText('import { _jsxQ } from');
+    await expect(symbolsText).toBeVisible();
+  });
+
+  test('Runtime-less Client Bundles Button', async ({ page }) => {
+    const button = page.getByRole('button', { name: 'Client Bundles' });
+    await expect(button).toBeVisible();
+    await button.click();
+    const bundles = page.locator('#file-modules-client-modules').getByText('build/app.js');
+    await expect(bundles).toBeVisible();
+  });
+
+  test('Runtime-less SSR Module Button', async ({ page }) => {
+    const button = page.getByRole('button', { name: 'SSR Module' });
+    await expect(button).toBeVisible();
+    await button.click();
+    const module = page.locator('#file-modules-client-modules').getByText('entry.server.js');
+    await expect(module).toBeVisible();
+  });
+
+  test('Runtime-less Diagnostics Button', async ({ page }) => {
+    const button = page.getByRole('button', { name: 'Diagnostics' });
+    await expect(button).toBeVisible();
+    await button.click();
+    const diagnostics = page.locator('.output-result.output-diagnostics');
+    await expect(diagnostics).toBeVisible();
+    await expect(diagnostics).toHaveText('- No Reported Diagnostics -');
+  });
+
+  test('Runtime-less Options Button', async ({ page }) => {
+    const button = page.getByRole('button', { name: 'Options' });
+    const serverConsoleText = page.getByText('🔴 Paused in server');
+    await expect(serverConsoleText).toBeVisible();
+    await expect(button).toBeVisible();
+    await button.click();
+    await expect(serverConsoleText).not.toBeVisible();
+    const DebugCheckBox = page.locator('label').filter({ hasText: 'Debug' });
+    await expect(DebugCheckBox).toBeVisible();
+    await expect(DebugCheckBox).not.toBeChecked();
+  });
+});

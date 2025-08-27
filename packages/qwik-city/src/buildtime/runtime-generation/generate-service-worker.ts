1333911bca50d60c21929adb9c667220d26fca2b
maiieul <maieul.chevalier@rosa.be>
2025-08-27 14:20:32 +0200

    fix: also unregister when service-worker.ts is removed and delete cache in any case

--- packages/qwik-city/src/buildtime/runtime-generation/generate-service-worker.ts
+++ packages/qwik-city/src/buildtime/runtime-generation/generate-service-worker.ts
@@ -8,18 +8,16 @@ export function generateServiceWorkerRegister(
   let swReg: string;
   let swUrl = '/service-worker.js';
 
-  // Also unregister if the developer did not add custom code to the service worker since Qwik 1.14.0 and above now use modulepreload by default
-  if (ctx.isDevServer || didNotAddCustomCode) {
+  // Also unregister if the developer removed the service-worker.ts file or did not add custom code to it; since Qwik 1.14.0 and above now use modulepreload by default
+  if (ctx.isDevServer || didNotAddCustomCode || ctx.serviceWorkers.length === 0) {
     swReg = SW_UNREGISTER;
   } else {
     swReg = swRegister;
 
-    if (ctx.serviceWorkers.length > 0) {
-      const sw = ctx.serviceWorkers.sort((a, b) =>
-        a.chunkFileName.length < b.chunkFileName.length ? -1 : 1
-      )[0];
-      swUrl = ctx.opts.basePathname + sw.chunkFileName;
-    }
+    const sw = ctx.serviceWorkers.sort((a, b) =>
+      a.chunkFileName.length < b.chunkFileName.length ? -1 : 1
+    )[0];
+    swUrl = ctx.opts.basePathname + sw.chunkFileName;
   }
   swReg = swReg.replace('__url', swUrl);
 
@@ -27,27 +25,29 @@ export function generateServiceWorkerRegister(
 }
 
 const SW_UNREGISTER = `
-(() => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then((regs) => {
-      for (const reg of regs) {
-        const url = '__url'.split('/').pop();
-        if (reg.active?.scriptURL.endsWith(url || 'service-worker.js')) {
-          reg.unregister().catch(console.error);
-        }
-      }
-    });
-  }
-  if ('caches' in window) {
-    caches
-      .keys()
-      .then((names) => {
-        const cacheName = names.find((name) => name.startsWith('QwikBuild'));
-        if (cacheName) {
-          caches.delete(cacheName).catch(console.error);
-        }
-      })
-      .catch(console.error);
-  }
-})();
+"serviceWorker"in navigator&&navigator.serviceWorker.getRegistrations().then(r=>{for(const e of r){const c='__url'.split("/").pop();e.active?.scriptURL.endsWith(c||"service-worker.js")&&e.unregister().catch(console.error)}}),"caches"in window&&caches.keys().then(r=>{const e=r.find(c=>c.startsWith("QwikBuild"));e&&caches.delete(e).catch(console.error)}).catch(console.error)
 `;
+// Code in SW_UNREGISTER unregisters the service worker and deletes the cache; it is the minified version of the following:
+// (() => {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.getRegistrations().then((regs) => {
+//       for (const reg of regs) {
+//         const url = '__url'.split('/').pop();
+//         if (reg.active?.scriptURL.endsWith(url || 'service-worker.js')) {
+//           reg.unregister().catch(console.error);
+//         }
+//       }
+//     });
+//   }
+//   if ('caches' in window) {
+//     caches
+//       .keys()
+//       .then((names) => {
+//         const cacheName = names.find((name) => name.startsWith('QwikBuild'));
+//         if (cacheName) {
+//           caches.delete(cacheName).catch(console.error);
+//         }
+//       })
+//       .catch(console.error);
+//   }
+// })();

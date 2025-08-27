23f69f116c06e6d32d3f9247bde9bda974024637
Varixo <admin@varixo.pl>
2025-08-27 17:32:55 +0200

    feat: expose loading and error fields of async computed signal

--- packages/qwik/src/core/reactive-primitives/signal.public.ts
+++ packages/qwik/src/core/reactive-primitives/signal.public.ts
@@ -17,9 +17,10 @@ export interface ReadonlySignal<T = unknown> {
 
 /** @public */
 export interface AsyncComputedReadonlySignal<T = unknown> extends ComputedSignal<T> {
-  // TODO: enable later this, after the scheduler changes for "streaming" signals values
-  // loading: boolean;
-  // error: Error | null;
+  /** Whether the signal is currently loading. */
+  loading: boolean;
+  /** The error that occurred while computing the signal. */
+  error: Error | null;
 }
 
 /**

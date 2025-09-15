2037102685f0f37adab3ecfe751f8c51d63145d5
Wout Mertens <Wout.Mertens@gmail.com>
2025-09-15 10:27:53 +0200

    fix: compiled-string maxParallelFileOps=1 error
    See https://github.com/vitejs/vite/issues/20775

--- scripts/compiled-string-plugin.ts
+++ scripts/compiled-string-plugin.ts
@@ -3,9 +3,14 @@ import { minify } from 'terser';
 
 const isCompiledStringId = (id: string) => /[?&]compiled-string/.test(id);
 
-/** This returns the source code of a module after transforming */
+/**
+ * This returns the source code of a module after transforming. Note that imports aren't
+ * transformed.
+ */
 export function compiledStringPlugin(): Plugin {
   let devServer: any;
+  const originals = new Map<string, string>();
+
   return {
     name: 'compiled-string-plugin',
     enforce: 'pre',
@@ -21,6 +26,25 @@ export function compiledStringPlugin(): Plugin {
           const cleanId = id.replace(/([?&])compiled-string/, '$1').replace(/[?&]$/, '');
           const resolved = await this.resolve(cleanId, importer, { skipSelf: true });
           if (resolved) {
+            /**
+             * Note: we load the code here instead of in the load hook to prevent a bug in Vite when
+             * `rollupOptions.maxParallelFileOps=1`. See
+             * https://github.com/vitejs/vite/issues/20775
+             */
+            let code: string | null;
+            if (devServer) {
+              // in dev mode, you need to use the dev server to transform the request
+              const transformResult = await devServer.transformRequest(resolved.id);
+              code = transformResult?.code;
+              this.addWatchFile(resolved.id);
+            } else {
+              const loaded = await this.load({ id: resolved.id });
+              code = loaded.code;
+            }
+            if (!code) {
+              throw new Error(`compiled-string: Unable to load code for ${resolved.id}`);
+            }
+            originals.set(resolved.id, code);
             return `virtual:compiled-string:${resolved.id}`;
           }
         } else if (id.startsWith('virtual:compiled-string:')) {
@@ -35,30 +59,13 @@ export function compiledStringPlugin(): Plugin {
       async handler(id) {
         if (id.startsWith('virtual:compiled-string:')) {
           const originalId = id.slice('virtual:compiled-string:'.length);
-
-          const result = await this.load({
-            id: originalId,
-            moduleSideEffects: true,
-          });
-
-          let code: string;
-          if (result && 'code' in result && result.code) {
-            // If this.load provides code, use it
-            code = result.code;
-          } else if (devServer) {
-            // in dev mode, you need to use the dev server to transform the request
-            const transformResult = await devServer.transformRequest(originalId);
-            if (transformResult && transformResult.code) {
-              code = transformResult.code;
-            }
-            this.addWatchFile(originalId);
-          }
-          if (!code!) {
-            throw new Error(`Unable to load code for ${originalId}`);
+          const code = originals.get(originalId);
+          if (!code) {
+            throw new Error(`compiled-string: Unable to retrieve loaded code for ${originalId}`);
           }
           const minified = await minify(code);
           if (!minified.code) {
-            throw new Error(`Unable to minify code for ${originalId}`);
+            throw new Error(`compiled-string: Unable to minify code for ${originalId}`);
           }
           const withoutExports = minified.code.replace('export{}', '').replace(/;+$/g, '');
           return {

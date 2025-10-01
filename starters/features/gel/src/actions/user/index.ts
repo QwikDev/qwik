9f3045fdf68869cd7954250caa20af001c7dc9ff
Natalie Abrams <natalieabrams@mac.lan>
2025-10-01 18:16:47 -0400

    fix linting errors

--- starters/features/gel/src/actions/user/index.ts
+++ starters/features/gel/src/actions/user/index.ts
@@ -2,8 +2,6 @@ import { server$ } from "@qwik.dev/router";
 import { executeQuery } from "../client";
 import * as queries from "../../../gel/queries";
 
-
 export const getAllUsers = server$(async () => {
   return await executeQuery(queries.getAllUsers);
 });
-
--- starters/features/gel/src/hooks/user.hooks.ts
+++ starters/features/gel/src/hooks/user.hooks.ts
@@ -1,12 +1,10 @@
-import { useSignal, useTask$ } from "@qwik.dev/core";
-import {
-  getUsers,
-} from "~/actions/user";
+import { useSignal, useTask$ } from "@builder.io/qwik";
+import { getAllUsers } from "../actions/user";
 
 export const useGetUsers = () => {
-  const signal = useSignal<Awaited<ReturnType<typeof getUsers>>>([]);
+  const signal = useSignal<Awaited<ReturnType<typeof getAllUsers>>>([]);
   useTask$(async () => {
-    const result = await getUsers();
+    const result = await getAllUsers();
     signal.value = result;
   });
   return signal;

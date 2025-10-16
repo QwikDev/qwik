183646caaf75158c01cdd661444b2e929f9a583d
Jerry_Wu <409187100@qq.com>
2025-10-16 17:32:33 +0800

    refactor: extract theme loading logic into a separate module
    - Created a new load-theme.js file to encapsulate the theme loading logic.
    - Updated ThemeScript component to import and utilize the new loadThemeScript for improved code organization and maintainability.

---
+++ packages/docs/src/components/theme-toggle/load-theme.js
@@ -0,0 +1,18 @@
+(() => {
+  try {
+    const getItem = localStorage.getItem('theme-preference');
+    const el = document.firstElementChild;
+    if (!el) {
+      throw new Error('documentElement not found');
+    }
+
+    if (getItem === 'light' || getItem === 'dark') {
+      el.setAttribute('data-theme', getItem);
+    } else {
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      el.setAttribute('data-theme', isDark ? 'dark' : 'light');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})();
--- packages/docs/src/components/theme-toggle/theme-script.tsx
+++ packages/docs/src/components/theme-toggle/theme-script.tsx
@@ -1,3 +1,4 @@
+import loadThemeScript from './load-theme?compiled-string';
 export const themeStorageKey = 'theme-preference';
 
 export const colorSchemeChangeListener = (onColorSchemeChange: (isDark: boolean) => void) => {
@@ -13,18 +14,5 @@ export const colorSchemeChangeListener = (onColorSchemeChange: (isDark: boolean)
 };
 
 export const ThemeScript = () => {
-  const themeScript = `
-        try {
-          const getItem = localStorage.getItem('${themeStorageKey}')
-          const el = document.firstElementChild;
-          if(!el) { throw new Error('documentElement not found'); }
-
-          if(getItem === 'light' || getItem === 'dark'){
-              el.setAttribute('data-theme', getItem);
-          } else {
-              const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-              el.setAttribute('data-theme', isDark ? 'dark' : 'light');
-          }
-        } catch (err) { }`;
-  return <script dangerouslySetInnerHTML={themeScript} />;
+  return <script dangerouslySetInnerHTML={loadThemeScript} />;
 };

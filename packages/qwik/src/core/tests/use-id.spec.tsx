17efd7162f524149c0f6ce581d753dcb2a0f1e5a
Varixo <admin@varixo.pl>
2025-10-27 19:14:24 +0100

    fix: useId should genereate different id for ssr

--- packages/qwik/src/core/tests/use-id.spec.tsx
+++ packages/qwik/src/core/tests/use-id.spec.tsx
@@ -1,6 +1,6 @@
-import { component$, componentQrl, inlinedQrl, useId } from '@qwik.dev/core';
+import { component$, componentQrl, inlinedQrl, useId, useSignal } from '@qwik.dev/core';
 import { describe, expect, it } from 'vitest';
-import { domRender, ssrRenderToDom } from '@qwik.dev/core/testing';
+import { domRender, ssrRenderToDom, trigger } from '@qwik.dev/core/testing';
 
 const debug = false; //true;
 Error.stackTraceLimit = 100;
@@ -34,11 +34,47 @@ describe.each([
 
     const { document } = await render(<Parent />, { debug });
     if (render === ssrRenderToDom) {
-      expect(document.querySelector('#cmp1')?.textContent).toMatch(/^\w{3}cmp0$/);
-      expect(document.querySelector('#cmp2')?.textContent).toMatch(/^\w{3}2cm1$/);
+      expect(document.querySelector('#cmp1')?.textContent).toMatch(/^\w{3}cmp255s$/);
+      expect(document.querySelector('#cmp2')?.textContent).toMatch(/^\w{3}2cm255t$/);
     } else {
       expect(document.querySelector('#cmp1')?.textContent).toMatch(/^cmp0$/);
       expect(document.querySelector('#cmp2')?.textContent).toMatch(/^Ccm1$/);
     }
+  });
+
+  it('should generate different ids for csr and ssr', async () => {
+    const Checkbox = component$((props: { label: string }) => {
+      const id = useId();
+      return (
+        <div>
+          <input type="checkbox" id={id} />
+          <label for={id}>{props.label}</label>
+        </div>
+      );
+    });
+
+    const Cmp = component$(() => {
+      const enabled = useSignal(false);
+
+      return (
+        <div>
+          <h1>useId Example</h1>
+          <Checkbox label="Item 1" />
+          <button
+            onClick$={() => {
+              enabled.value = !enabled.value;
+            }}
+          ></button>
+
+          {enabled.value && <Checkbox label="Subitem 1" />}
+        </div>
+      );
+    });
+
+    const { document } = await render(<Cmp />, { debug });
+    await trigger(document.body, 'button', 'click');
+    const inputs = document.querySelectorAll('input');
+    expect(inputs.length).toBe(2);
+    expect(inputs[0].id).not.toBe(inputs[1].id);
   });
 });

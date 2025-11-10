3cf3be36eb9cfdda5c218e415abd555a227d9ca7
Jerry_Wu <409187100@qq.com>
2025-11-10 16:14:24 +0800

    refactor: replace cross-spawn with execa

--- scripts/submodule-cli.ts
+++ scripts/submodule-cli.ts
@@ -35,7 +35,7 @@ export async function submoduleCli(config: BuildConfig) {
         },
       },
     ],
-    external: ['prettier', 'typescript', 'ts-morph', 'semver', 'ignore'],
+    external: ['prettier', 'typescript', 'ts-morph', 'semver', 'ignore', 'execa'],
     define: {
       'globalThis.CODE_MOD': 'true',
       'globalThis.QWIK_VERSION': JSON.stringify(config.distVersion),

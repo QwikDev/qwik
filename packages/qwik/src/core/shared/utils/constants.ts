a52364dbc125b9e8d1637d09be8b3d7143db2f46
Varixo <admin@varixo.pl>
2025-11-11 16:02:05 +0100

    feat: reactive var props

--- packages/qwik/src/core/shared/utils/constants.ts
+++ packages/qwik/src/core/shared/utils/constants.ts
@@ -4,7 +4,8 @@ export const _CONST_PROPS = Symbol('CONST');
 export const _VAR_PROPS = Symbol('VAR');
 /** @internal */
 export const _OWNER = Symbol('OWNER');
-
+/** @internal */
+export const _PROPS_HANDLER = Symbol('PROPS_HANDLER');
 /** @internal @deprecated v1 compat */
 export const _IMMUTABLE = Symbol('IMMUTABLE');
 

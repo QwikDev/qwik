60d31b6cdd0fc49172eba2aecc373203fbacfbda
Wout Mertens <Wout.Mertens@gmail.com>
2025-12-09 20:10:08 +0100

    fix(cursors): show virtual node dirty on vnode_toString

--- packages/qwik/src/core/client/vnode.ts
+++ packages/qwik/src/core/client/vnode.ts
@@ -1688,6 +1688,9 @@ export function vnode_toString(
     } else if (vnode_isVirtualVNode(vnode)) {
       const idx = vnode.flags >>> VNodeFlagsIndex.shift;
       const attrs: string[] = ['[' + String(idx) + ']'];
+      if (vnode.dirty) {
+        attrs.push(` dirty:${vnode.dirty}`);
+      }
       vnode_getAttrKeys(vnode).forEach((key) => {
         if (key !== DEBUG_TYPE) {
           const value = vnode_getProp(vnode!, key, null);

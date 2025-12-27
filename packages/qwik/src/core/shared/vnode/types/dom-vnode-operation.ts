a8baea968dcfa5a72526ae7eed07565c0e5d2fda
Varixo <admin@varixo.pl>
2025-12-27 20:34:12 +0100

    feat(perf): factory functions for operations

--- packages/qwik/src/core/shared/vnode/types/dom-vnode-operation.ts
+++ packages/qwik/src/core/shared/vnode/types/dom-vnode-operation.ts
@@ -1,4 +1,4 @@
-import type { VNodeOperationType } from '../enums/vnode-operation-type.enum';
+import { VNodeOperationType } from '../enums/vnode-operation-type.enum';
 
 export type VNodeOperation =
   | DeleteOperation
@@ -36,3 +36,42 @@ export type SetAttributeOperation = {
   attrName: string;
   attrValue: string | null | boolean;
 };
+
+/** Factory functions to create operations with consistent hidden classes. */
+export const createDeleteOperation = (target: Element | Text): DeleteOperation => ({
+  operationType: VNodeOperationType.Delete,
+  target,
+});
+
+export const createRemoveAllChildrenOperation = (target: Element): RemoveAllChildrenOperation => ({
+  operationType: VNodeOperationType.RemoveAllChildren,
+  target,
+});
+
+export const createSetTextOperation = (target: Text, text: string): SetTextOperation => ({
+  operationType: VNodeOperationType.SetText,
+  target,
+  text,
+});
+
+export const createInsertOrMoveOperation = (
+  target: Element | Text,
+  parent: Element,
+  beforeTarget: Element | Text | null
+): InsertOrMoveOperation => ({
+  operationType: VNodeOperationType.InsertOrMove,
+  target,
+  parent,
+  beforeTarget,
+});
+
+export const createSetAttributeOperation = (
+  target: Element,
+  attrName: string,
+  attrValue: string | null | boolean
+): SetAttributeOperation => ({
+  operationType: VNodeOperationType.SetAttribute,
+  target,
+  attrName,
+  attrValue,
+});

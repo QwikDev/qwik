e5beab6db8a90d02137871a66eb284a9d04f6217
Wout Mertens <Wout.Mertens@gmail.com>
2025-12-30 19:27:32 +0100

    feat(core): binding args for useConstant
    this allows moving functions to enclosing scope

--- packages/qwik/src/core/use/use-signal.ts
+++ packages/qwik/src/core/use/use-signal.ts
@@ -1,7 +1,7 @@
 import { isQwikComponent } from '../shared/component.public';
 import { isFunction } from '../shared/utils/types';
 import { createSignal, type Signal } from '../reactive-primitives/signal.public';
-import { invoke } from './use-core';
+import { invoke, untrack } from './use-core';
 import { useSequentialScope } from './use-sequential-scope';
 
 /** @public */
@@ -25,16 +25,28 @@ export const useSignal: UseSignal = <STATE>(initialState?: STATE): Signal<STATE>
  * Stores a value which is retained for the lifetime of the component. Subsequent calls to
  * `useConstant` will always return the first value given.
  *
- * If the value is a function, the function is invoked once to calculate the actual value.
+ * If the value is a function, the function is invoked once to calculate the actual value. You can
+ * then also pass arguments to call the function with, so that you don't need to create a new
+ * function on every render.
+ *
+ * @example
+ *
+ * ```tsx
+ * const fixedRandomValue = useConstant(() => Math.random);
+ * const otherFixedRandomValue = useConstant(Math.random);
+ *
+ * const getConfig = (env: string) => { ... }
+ * const config = useConstant(getConfig, environment);
+ * ```
  *
  * @public
  */
-export const useConstant = <T>(value: (() => T) | T): T => {
+export const useConstant = <T, A extends any[]>(value: ((...args: A) => T) | T, ...args: A): T => {
   const { val, set } = useSequentialScope<T>();
   if (val != null) {
     return val;
   }
   // Note: We are not using `invoke` here because we don't want to clear the context
-  value = isFunction(value) && !isQwikComponent(value) ? value() : value;
+  value = isFunction(value) && !isQwikComponent(value) ? untrack(value, ...args) : value;
   return set(value as T);
 };

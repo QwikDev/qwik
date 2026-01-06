7798930422881e4a5b016986a7acfc26f427a962
Varixo <admin@varixo.pl>
2026-01-06 13:53:50 +0100

    fix: serialize iteration item

--- packages/qwik/src/core/client/run-qrl.ts
+++ packages/qwik/src/core/client/run-qrl.ts
@@ -2,11 +2,11 @@ import type { QRLInternal } from '../shared/qrl/qrl-class';
 import { ITERATION_ITEM } from '../shared/utils/markers';
 import { retryOnPromise } from '../shared/utils/promises';
 import type { ValueOrPromise } from '../shared/utils/types';
-import type { ElementVNode } from '../shared/vnode/element-vnode';
+import type { VNode } from '../shared/vnode/vnode';
 import { getInvokeContext } from '../use/use-core';
 import { useLexicalScope } from '../use/use-lexical-scope.public';
 import { VNodeFlags } from './types';
-import { vnode_getProp } from './vnode-utils';
+import { vnode_ensureElementInflated, vnode_getProp } from './vnode-utils';
 
 /**
  * This is called by qwik-loader to run a QRL. It has to be synchronous.
@@ -17,12 +17,13 @@ export const _run = (...args: unknown[]): ValueOrPromise<unknown> => {
   // This will already check container
   const [runQrl] = useLexicalScope<[QRLInternal<(...args: unknown[]) => unknown>]>();
   const context = getInvokeContext();
-  const hostElement = context.$hostElement$;
+  const hostElement = context.$hostElement$ as VNode;
   if (hostElement) {
+    vnode_ensureElementInflated(hostElement);
     return retryOnPromise(() => {
       if (!(hostElement.flags & VNodeFlags.Deleted)) {
         const iterationItems = vnode_getProp<unknown[]>(
-          hostElement as ElementVNode,
+          hostElement,
           ITERATION_ITEM,
           context.$container$?.$getObjectById$ || null
         );

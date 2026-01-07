3214add1c1fa585200f8ea844fd8febab5635f97
Wout Mertens <Wout.Mertens@gmail.com>
2026-01-07 10:32:35 +0100

    chore: dep updates

--- packages/qwik/src/core/shared/serdes/serdes.unit.ts
+++ packages/qwik/src/core/shared/serdes/serdes.unit.ts
@@ -189,7 +189,7 @@ describe('shared-serialization', () => {
         0 Error [
           {string} "hi"
           {string} "stack"
-          {string} "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at new Promise (<anonymous>)\\n    at runWithTimeout (file:/...path/file.js:123:456)\\n    at runTest (file:/...path/file.js:123:456)\\n    at processTicksAndRejections (node:internal/process/task_queues:123:456)\\n    at runSuite (file:/...path/file.js:123:456)\\n    at runSuite (file:/...path/file.js:123:456)"
+          {string} "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at new Promise (<anonymous>)\\n    at runWithTimeout (file:/...path/file.js:123:456)\\n    at file:/...path/file.js:123:456\\n    at Traces.$ (file:/...path/file.js:123:456)\\n    at trace (file:/...path/file.js:123:456)\\n    at runTest (file:/...path/file.js:123:456)"
         ]
         (x chars)"
       `);
@@ -201,7 +201,7 @@ describe('shared-serialization', () => {
           {string} "extra"
           {string} "yey"
           {string} "stack"
-          {string} "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at new Promise (<anonymous>)\\n    at runWithTimeout (file:/...path/file.js:123:456)\\n    at runTest (file:/...path/file.js:123:456)\\n    at processTicksAndRejections (node:internal/process/task_queues:123:456)\\n    at runSuite (file:/...path/file.js:123:456)\\n    at runSuite (file:/...path/file.js:123:456)"
+          {string} "Error: hi\\n    at /...path/file.ts:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at file:/...path/file.js:123:456\\n    at new Promise (<anonymous>)\\n    at runWithTimeout (file:/...path/file.js:123:456)\\n    at file:/...path/file.js:123:456\\n    at Traces.$ (file:/...path/file.js:123:456)\\n    at trace (file:/...path/file.js:123:456)\\n    at runTest (file:/...path/file.js:123:456)"
         ]
         (x chars)"
       `);

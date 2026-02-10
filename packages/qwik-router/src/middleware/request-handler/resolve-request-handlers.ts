ce81e8b21523da7a21b084653c25f81267df478e
Wout Mertens <Wout.Mertens@gmail.com>
2026-02-10 12:37:05 +0100

    fix(serdes): fix _(de)serialize

--- packages/qwik-router/src/middleware/request-handler/resolve-request-handlers.ts
+++ packages/qwik-router/src/middleware/request-handler/resolve-request-handlers.ts
@@ -347,51 +347,53 @@ async function runServerFunction(ev: RequestEvent) {
   ) {
     ev.exit();
     const isDev = getRequestMode(ev) === 'dev';
-    const data = (await ev.parseBody()) as [args?: unknown[] | undefined, captured?: unknown[]];
-    if (Array.isArray(data)) {
-      // Semi-hack: Look up the server function by its hash
-      // it will have been registered in the global map when the server function was created
-      const qrl = inlinedQrl(null, serverFnHash, data[1]);
-      let result: unknown;
-      try {
-        if (isDev) {
-          result = await measure(ev, `server_${serverFnHash}`, () =>
-            (qrl as Function).apply(ev, data[0])
-          );
-        } else {
-          result = await (qrl as Function).apply(ev, data[0]);
-        }
-      } catch (err) {
-        if (err instanceof ServerError) {
-          throw ev.error(err.status as ErrorCodes, err.data);
-        }
-        console.error(`Server function ${serverFnHash} failed:`, err);
-        throw ev.error(500, 'Invalid request');
-      }
-      if (isAsyncIterator(result)) {
-        ev.headers.set('Content-Type', 'text/qwik-json-stream');
-        const writable = ev.getWritableStream();
-        const stream = writable.getWriter();
-        for await (const item of result) {
-          if (isDev) {
-            verifySerializable(item, qrl);
-          }
-          const message = await _serialize([item]);
-          if (ev.signal.aborted) {
-            break;
-          }
-          await stream.write(encoder.encode(`${message}\n`));
-        }
-        stream.close();
-      } else {
-        verifySerializable(result, qrl);
-        ev.headers.set('Content-Type', 'application/qwik-json');
-        const message = await _serialize([result]);
-        ev.send(200, message);
-      }
-      return;
+    const data = (await ev.parseBody()) as
+      | [args?: unknown[] | undefined, ...captured: unknown[]]
+      | undefined;
+    if (!Array.isArray(data) || !(Array.isArray(data[0]) || data[0] === undefined)) {
+      throw ev.error(500, 'Invalid request');
     }
-    throw ev.error(500, 'Invalid request');
+    // Semi-hack: Look up the server function by its hash
+    // it will have been registered in the global map when the server function was created
+    const qrl = inlinedQrl(null, serverFnHash, data.slice(1));
+    let result: unknown;
+    try {
+      if (isDev) {
+        result = await measure(ev, `server_${serverFnHash}`, () =>
+          (qrl as Function).apply(ev, data[0])
+        );
+      } else {
+        result = await (qrl as Function).apply(ev, data[0]);
+      }
+    } catch (err) {
+      if (err instanceof ServerError) {
+        throw ev.error(err.status as ErrorCodes, err.data);
+      }
+      console.error(`Server function ${serverFnHash} failed:`, err);
+      throw ev.error(500, 'Invalid request');
+    }
+    if (isAsyncIterator(result)) {
+      ev.headers.set('Content-Type', 'text/qwik-json-stream');
+      const writable = ev.getWritableStream();
+      const stream = writable.getWriter();
+      for await (const item of result) {
+        if (isDev) {
+          verifySerializable(item, qrl);
+        }
+        const message = await _serialize(item);
+        if (ev.signal.aborted) {
+          break;
+        }
+        await stream.write(encoder.encode(`${message}\n`));
+      }
+      stream.close();
+    } else {
+      verifySerializable(result, qrl);
+      ev.headers.set('Content-Type', 'application/qwik-json');
+      const message = await _serialize(result);
+      ev.send(200, message);
+    }
+    return;
   }
 }
 
@@ -643,7 +645,7 @@ async function renderQData(requestEv: RequestEvent) {
       };
   const writer = requestEv.getWritableStream().getWriter();
   // write just the page json data to the response body
-  const data = await _serialize([qData]);
+  const data = await _serialize(qData);
   writer.write(encoder.encode(data));
   requestEv.sharedMap.set(RequestEvShareQData, qData);
 

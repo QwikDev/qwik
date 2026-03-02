fb086691ad47cfc7e2f06d2abedfc3218bf40355
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-02 15:46:39 +0100

    lint++

--- packages/docs/src/routes/(blog)/blog/components/mdx/article-block.tsx
+++ packages/docs/src/routes/(blog)/blog/components/mdx/article-block.tsx
@@ -9,7 +9,7 @@ export const ArticleBlock = component$<Props>(({ authorLink }) => {
   const location = useLocation();
   const { frontmatter } = useDocumentHead();
   const article = blogArticles.find(({ path }) => path === location.url.pathname);
-  const authorLinks = frontmatter.authors.map((author: string) => authors[author].socialLink);
+  const authorLinks = frontmatter.authors?.map((author: string) => authors[author].socialLink);
 
   return (
     <div class="docs">

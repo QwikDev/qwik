82a13fdafeb452c24362c84530bdd0f6786b0d2e
thejackshelton <me@jackshelton.com>
2026-03-04 01:37:36 -0600

    feat: button component and tailwind intellisense with variants

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@qwik.dev/core';
 import { type DocumentHead } from '@qwik.dev/router';
+import { Button } from '~/components/button/button';
 // import { Header } from '../components/header/header';
 
 export default component$(() => {
@@ -7,15 +8,19 @@ export default component$(() => {
     <>
       {/* <Header /> */}
       <main>
-        <div class="space-y-10 *:mx-auto">
+        <section class="space-y-10 *:mx-auto">
           <h1 class="uppercase font-display text-h2 max-w-[15ch] text-center">
             Automatically <span class="text-violet-75">Instant</span> Web Apps
           </h1>
-          <p class="font-body text-body-md max-w-[50ch] text-center">
+          <p class="text-body-md max-w-[50ch] text-center">
             A new kind of framework for you to ship quicker and provide better user experiences
             every step of the way.
           </p>
-        </div>
+          <div class="flex gap-6 justify-center">
+            <Button variant="primary">Qwik Start</Button>
+            <Button variant="secondary">npm create qwik@latest</Button>
+          </div>
+        </section>
       </main>
     </>
   );

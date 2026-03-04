de4b91a4efb83cb26866632a9acdaca3946e152b
thejackshelton <me@jackshelton.com>
2026-03-04 00:45:37 -0600

    feat: strip semantics for now, setup hero spacing

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -7,13 +7,15 @@ export default component$(() => {
     <>
       {/* <Header /> */}
       <main>
-        <h1 class="uppercase font-display text-h2 max-w-[15ch] text-center">
-          Automatically Instant Web Apps
-        </h1>
-        <p class="font-body text-body-md max-w-[50ch] text-center">
-          A new kind of framework for you to ship quicker and provide better user experiences every
-          step of the way.
-        </p>
+        <div class="space-y-10 *:mx-auto">
+          <h1 class="uppercase font-display text-h2 max-w-[15ch] text-center">
+            Automatically <span class="text-violet-75">Instant</span> Web Apps
+          </h1>
+          <p class="font-body text-body-md max-w-[50ch] text-center">
+            A new kind of framework for you to ship quicker and provide better user experiences
+            every step of the way.
+          </p>
+        </div>
       </main>
     </>
   );

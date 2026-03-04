fa3f8c19663397a879372c2ac72ab5ea56d774fa
thejackshelton <me@jackshelton.com>
2026-03-04 00:21:00 -0600

    feat: hero text position

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -7,8 +7,10 @@ export default component$(() => {
     <>
       {/* <Header /> */}
       <main>
-        <h1 class="uppercase font-display">Automatically Instant Web Apps</h1>
-        <p class="font-body">
+        <h1 class="uppercase font-display text-h2 max-w-[15ch] text-center">
+          Automatically Instant Web Apps
+        </h1>
+        <p class="font-body text-body-md max-w-[50ch] text-center">
           A new kind of framework for you to ship quicker and provide better user experiences every
           step of the way.
         </p>

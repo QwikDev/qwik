a3a77f16c8a71159092367f5bc658d57cf6bff8f
thejackshelton <me@jackshelton.com>
2026-03-04 02:08:41 -0600

    feat: hero decorations

--- packages/docs/src/routes/index.tsx
+++ packages/docs/src/routes/index.tsx
@@ -9,6 +9,7 @@ export default component$(() => {
     <>
       {/* <Header /> */}
       <main>
+        <Decor />
         <section class="space-y-10 *:mx-auto">
           <h1 class="uppercase font-display text-h2 max-w-[15ch] text-center">
             Automatically <span class="text-violet-75">Instant</span> Web Apps
@@ -30,6 +31,21 @@ export default component$(() => {
           </div>
         </section>
       </main>
+    </>
+  );
+});
+
+export const Decor = component$(() => {
+  return (
+    <>
+      {/* blue gradient */}
+      <div class="fixed top-[20%] h-screen w-screen bg-hero-gradient-blue mix-blend-color -z-1 lg:-left-1/2 lg:-top-[20%]" />
+
+      {/* purple gradient */}
+      <div class="fixed -top-[10%] -right-[40%] h-screen w-screen bg-hero-gradient-purple mix-blend-color -z-1 lg:-right-1/2" />
+
+      {/* star background */}
+      <div aria-hidden="true" class="absolute inset-0 -z-2 bg-grid-stars" />
     </>
   );
 });

fbba54b0ba7db541c3454f87f04a0b44d8a27036
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-16 18:59:46 +0100

    fix(core): deduplicate css qrl imports

---
+++ starters/apps/e2e/src/components/styles/use-styles-parent.tsx
@@ -0,0 +1,14 @@
+import { component$, useStyles$ } from "@qwik.dev/core";
+import sharedStyles from "./use-styles-shared.css?inline";
+import { UseStylesChild } from "./use-styles-child";
+
+export const UseStylesParent = component$(() => {
+  useStyles$(sharedStyles);
+
+  return (
+    <section id="use-styles-dedupe">
+      <div class="use-styles-dedupe-text">Inline styles fixture parent</div>
+      <UseStylesChild />
+    </section>
+  );
+});

e39f671399000d90140e93695b029c1e71fb6061
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-17 07:53:25 +0100

    perf(core): rename Each internal exports

--- packages/qwik/handlers.mjs
+++ packages/qwik/handlers.mjs
@@ -6,8 +6,4 @@
  *
  * Make sure that these handlers are listed in manifest.ts
  */
-export { _chk, _rsc, _res, _run, _task, _val } from '@qwik.dev/core';
-export {
-  _Each_component_useTask_1IvuA9ZneGc as Each_component_useTask_1IvuA9ZneGc,
-  _Each_component_zi6m0DQBsr8 as Each_component_zi6m0DQBsr8,
-} from '@qwik.dev/core';
+export { _chk, _rsc, _res, _run, _task, _val, _eaC, _eaT } from '@qwik.dev/core/internal';

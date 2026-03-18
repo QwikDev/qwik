3b06eb2abc2e9d9511fa5c6f9764d59891c80294
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-18 20:33:20 +0100

    wip: libraries get  two builds

--- starters/apps/library/vite.config.ts
+++ starters/apps/library/vite.config.ts
@@ -7,30 +7,75 @@ import tsconfigPaths from "vite-tsconfig-paths";
 const { dependencies = {}, peerDependencies = {} } = pkg as any;
 const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
 const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
+const external = [
+  /^node:.*/,
+  ...excludeAll(dependencies),
+  ...excludeAll(peerDependencies),
+];
 
 export default defineConfig(() => {
   return {
+    builder: {},
     build: {
       outDir: "lib",
       target: "es2020",
       lib: {
         entry: "./src/index",
-        formats: ["es", "cjs"] as const,
+        formats: ["es"],
         // This adds .qwik so all files are processed by the optimizer
-        fileName: (format, entryName) =>
-          `${entryName}.qwik.${format === "es" ? "mjs" : "cjs"}`,
+        fileName: (_format, entryName) => `${entryName}.qwik.mjs`,
       },
       rollupOptions: {
         output: {
           preserveModules: true,
           preserveModulesRoot: "src",
         },
-        // externalize deps that shouldn't be bundled into the library
-        external: [
-          /^node:.*/,
-          ...excludeAll(dependencies),
-          ...excludeAll(peerDependencies),
-        ],
+        external,
+      },
+    },
+    environments: {
+      "lib-server-dev": {
+        consumer: "server",
+        define: {
+          "globalThis.qDev": true,
+          "globalThis.qInspector": true,
+          "globalThis.qSerialize": true,
+          "globalThis.qTest": false,
+        },
+        build: {
+          outDir: "lib",
+          ssr: "./src/index",
+          rollupOptions: {
+            output: {
+              preserveModules: true,
+              preserveModulesRoot: "src",
+              entryFileNames: "[name].server-dev.js",
+            },
+            external,
+          },
+        },
+      },
+      "lib-server-prod": {
+        consumer: "server",
+        define: {
+          "globalThis.qDev": false,
+          "globalThis.qInspector": false,
+          "globalThis.qSerialize": false,
+          "globalThis.qTest": false,
+        },
+        build: {
+          outDir: "lib",
+          ssr: "./src/index",
+          minify: true,
+          rollupOptions: {
+            output: {
+              preserveModules: true,
+              preserveModulesRoot: "src",
+              entryFileNames: "[name].server-prod.js",
+            },
+            external,
+          },
+        },
       },
     },
     plugins: [qwikVite(), qwikRouter(), tsconfigPaths({ root: "." })],

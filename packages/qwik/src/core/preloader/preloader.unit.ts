9a11cbc20dbb41c3c46d7b9b9304099ab2907902
maiieul <45822175+maiieul@users.noreply.github.com>
2026-03-22 15:27:52 +0100

    test: preloader.unit.ts

--- packages/qwik/src/core/preloader/preloader.unit.ts
+++ packages/qwik/src/core/preloader/preloader.unit.ts
@@ -33,8 +33,8 @@ test('preloader script', async () => {
   const compressed = compress(Buffer.from(code), { mode: 1, quality: 11 });
   expect({ brotli: compressed.length, minified: code.length }).toMatchInlineSnapshot(`
     {
-      "brotli": 1287,
-      "minified": 2518,
+      "brotli": 1257,
+      "minified": 2449,
     }
   `);
 });

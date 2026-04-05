80c1a0cecbca9b09ec256880a953aa30652ce436
Varixo <admin@varixo.pl>
2026-04-05 21:35:33 +0200

    docs: refactor REPL option values into separate file

---
+++ packages/docs/src/repl/ui/repl-option-values.ts
@@ -0,0 +1,4 @@
+export const BUILD_MODE_OPTIONS = ['development', 'production'];
+
+// We don't support `inline` and `hoist` for client bundles
+export const ENTRY_STRATEGY_OPTIONS = ['component', 'segment', 'single', 'smart'];

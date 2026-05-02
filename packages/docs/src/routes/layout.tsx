b27cdb6e89c34f385c41a6db8c863bd13b50125f
Jack Shelton <me@jackshelton.com>
2026-05-02 14:29:47 -0500

    fix(docs): enable iframe CORS headers

--- packages/docs/src/routes/layout.tsx
+++ packages/docs/src/routes/layout.tsx
@@ -1,14 +1,16 @@
 import type { RequestHandler } from '@qwik.dev/router';
 import { component$, Slot } from '@qwik.dev/core';
+import { setReplCorsHeaders } from '~/utils/utils';
 
 export default component$(() => {
   return <Slot />;
 });
 
-export const onGet: RequestHandler = ({ cacheControl }) => {
+export const onGet: RequestHandler = ({ cacheControl, headers }) => {
   // cache for pages using this layout
   cacheControl({
     public: true,
     maxAge: 3600,
   });
+  setReplCorsHeaders(headers);
 };

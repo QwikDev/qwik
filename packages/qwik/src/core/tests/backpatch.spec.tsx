53021d355edc46f580a74e1a1fd21569dabda586
Varixo <admin@varixo.pl>
2026-05-16 21:18:26 +0200

    fix(ssr): serialize OOOS slot parent vnode refs

--- packages/qwik/src/core/tests/backpatch.spec.tsx
+++ packages/qwik/src/core/tests/backpatch.spec.tsx
@@ -31,7 +31,21 @@ describe('SSR Backpatching', () => {
       `,
     });
     const container = document.querySelector('[q\\:container]')!;
-
+    (globalThis as any).NodeFilter = {
+      SHOW_ELEMENT: 1,
+      SHOW_ALL: -1,
+      SHOW_ATTRIBUTE: 2,
+      SHOW_TEXT: 4,
+      SHOW_CDATA_SECTION: 8,
+      SHOW_ENTITY_REFERENCE: 16,
+      SHOW_ENTITY: 32,
+      SHOW_PROCESSING_INSTRUCTION: 64,
+      SHOW_COMMENT: 128,
+      SHOW_DOCUMENT: 256,
+      SHOW_DOCUMENT_TYPE: 512,
+      SHOW_DOCUMENT_FRAGMENT: 1024,
+      SHOW_NOTATION: 2048,
+    };
     executeBackpatch(document, container);
 
     const input = document.querySelector('input')!;

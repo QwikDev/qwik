6dcdb9fee0981c3310df2fb5d04a9f65170c2303
PatrickJS <github@patrickjs.com>
2026-05-22 18:55:02 -0700

    feat(examples): serve Qwik favicon

--- examples/qwik-qcomponent-partials-app/src/root.tsx
+++ examples/qwik-qcomponent-partials-app/src/root.tsx
@@ -9,6 +9,7 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
         <script src="https://cdn.tailwindcss.com"></script>
         <DocumentHeadTags />
       </head>

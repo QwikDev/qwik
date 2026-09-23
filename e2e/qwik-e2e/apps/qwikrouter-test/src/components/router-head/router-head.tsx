c9fc447c185c72164049e6a83bef6d26ffa7a78b
Varixo <admin@varixo.pl>
2026-09-23 18:33:48 +0200

    test(e2e): pass the router head title as a live prop expression

--- e2e/qwik-e2e/apps/qwikrouter-test/src/components/router-head/router-head.tsx
+++ e2e/qwik-e2e/apps/qwikrouter-test/src/components/router-head/router-head.tsx
@@ -8,14 +8,13 @@ export const RouterHead = component$(() => {
 
   const head = useDocumentHead();
 
-  const title = head.title ? `${head.title} - Qwik` : `Qwik`;
   return (
     <>
       <link rel="canonical" href={loc.url.href} />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <meta name="viewport" content="width=device-width" />
 
-      <DocumentHeadTags title={title} />
+      <DocumentHeadTags title={head.title ? `${head.title} - Qwik` : 'Qwik'} />
 
       <Social loc={loc} head={head} />
       <Vendor loc={loc} />
